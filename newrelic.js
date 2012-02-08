b8a37f0e1a4e8b63bf98a8be4ea829dde123e37b
Saxon <saxon@newrelic.com>
2012-02-08 14:11:15 -0800

    Hooking up mysql instrumentation

---
+++ newrelic.js
@@ -0,0 +1,12 @@
+
+exports.config =
+{
+  app_name: ['My App'],
+  license_key: 'd67afc830dab717fd163bfcb0b8b88423e9a1a3b',
+  host: 'staging-collector.newrelic.com',
+  
+  transaction_tracer: {
+    enabled: true
+  }
+  
+}
--- test.js
+++ test.js
@@ -1,11 +1,28 @@
 var agent = require('newrelic_agent');
-agent.connect('d67afc830dab717fd163bfcb0b8b88423e9a1a3b', 'staging-collector.newrelic.com', 80);
+agent.connect('d67afc830dab717fd163bfcb0b8b88423e9a1a3b', 'staging-collector.newrelic.com');
 //agent.connect('bootstrap_newrelic_admin_license_key_000', 'localhost', '8081');
 
 var fs = require('fs');
 var http = require('http');
 
 //agent.logToConsole();
+
+var Client = require('mysql').Client;
+var client = new Client();
+ 
+client.user = 'root';
+client.password = '';
+client.database = 'seldon_development';
+
+	
+/*
+client.connect(function(error, results) {
+  if(error) {
+    console.log('Connection Error: ' + error.message);
+    return;
+  }
+  console.log('Connected to MySQL');
+});*/
  
 http.createServer(function (request, response) {
 	if (request.url == '/favicon.ico') {
@@ -15,6 +32,7 @@ http.createServer(function (request, response) {
 
 	// add this timeout to force another event hop.  make the timeout random so
 	// we get some requests out of order
+	/*
 	setTimeout(function() {
 		fs.readdir(request.url, function(err, files) {
 			if (!files) {
@@ -25,5 +43,17 @@ http.createServer(function (request, response) {
 			console.log("Request end " + request.url);
 		});
 	}, Math.floor(Math.random()*300));
+	*/
+	client.query('Select * from ' + request.url.substring(1), function(error, results) {
+	        if(error) {
+	            console.log('ClientConnectionReady Error: ' + error.message);
+//	            client.end();
+				response.statusCode = 404;
+				response.end('Error: ' + error + '\n');
+	        } else {
+				response.end('Results: ' + JSON.stringify(results) + '\n');
+			}
+	    });
+	
 	
 }).listen(8000);
