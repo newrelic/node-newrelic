2e96e945960d4bee79226acc1d1181939f63d6b8
Saxon <saxon@newrelic.com>
2012-02-08 22:12:31 -0800

    Add dependencies to package

--- newrelic.js
+++ newrelic.js
@@ -1,9 +1,8 @@
 
 exports.config =
 {
-  app_name: ['My App'],
-  license_key: 'd67afc830dab717fd163bfcb0b8b88423e9a1a3b',
-  host: 'staging-collector.newrelic.com',
+  app_name: ['My Application'],
+  license_key: 'license key here',
   
   transaction_tracer: {
     enabled: true
