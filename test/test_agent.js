e97d55c88e8f02232aa90cec8782c9d9015220be
Saxon <saxon@newrelic.com>
2012-02-10 07:38:50 -0800

    More stuff

--- test/test_agent.js
+++ test/test_agent.js
@@ -1,11 +1,19 @@
 var winston = require('winston');
 var logger = require('../lib/logger.js').getLogger();
 var stats = require('../lib/stats.js');
+var trace = require('../lib/trace.js');
 
 exports.createAgent = function() {
     return new function() {
         logger.logToConsole();
+        logger.setLevel('debug');
+        this.transactions = [];
+        this.transactionFinished = function(transaction) {
+            this.transactions.push(transaction);
+        }
+
         var statsEngine = stats.createStatsEngine(logger);
+        trace.setTransactions(this);
         this.getLogger = function() {
             return logger;
         }
--- test/trace.test.js
+++ test/trace.test.js
@@ -1,4 +1,6 @@
 var trace = require('../lib/trace.js');
+var stats = require('../lib/stats.js');
+var util = require('util');
 var _agent = require('./test_agent.js');
 
 
@@ -7,36 +9,37 @@ function FixedTime() {
     return now;
 }
 
-exports['test metricName'] = function(beforeExit, assert) {
+    
+exports.testMetricName = function(beforeExit, assert) {
     var agent = _agent.createAgent();
     var tx = trace.createTransaction(agent);
-
     var tracer = new trace.Tracer(tx, 'Custom/Test');
     tracer.getStartTime = tracer.getEndTime = FixedTime;
     tracer.finish();
+    assert.equal(1, agent.transactions.length);
 
-    var md = agent.getStatsEngine().getMetricData();
-    var data = JSON.stringify(md);
-    assert.equal('[[{"name":"Custom/Test","scope":"FIXME"},[1,0,0,0,0,0]]]', data);
-}
-
-exports['test after finished'] = function(beforeExit, assert) {
-    var agent = _agent.createAgent();
-    var tx = trace.createTransaction(agent);
-
-    var tracer = new trace.Tracer(tx, 'Custom/Test');
-    tracer.getStartTime = tracer.getEndTime = FixedTime;
-    tracer.finish();
-
-    var tracer = new trace.Tracer(tx, 'Custom/Test');
-    tracer.getStartTime = tracer.getEndTime = FixedTime;
-    tracer.finish();
-
-    var md = agent.getStatsEngine().getMetricData();
-    var data = JSON.stringify(md);
-    assert.equal('[[{"name":"Custom/Test","scope":"FIXME"},[1,0,0,0,0,0]]]', data);
-
+    var stats = agent.transactions[0].getScopedStats().getStats('Custom/Test');
+    var data = JSON.stringify(stats);
+    assert.equal('[1,0,0,0,0,0]', data);
 }
 
 
+exports.testAfterFinished = function(beforeExit, assert) {
+    var agent = _agent.createAgent();
+    var tx = trace.createTransaction(agent);
 
+    var tracer = new trace.Tracer(tx, 'Custom/Test2');
+    tracer.getStartTime = tracer.getEndTime = FixedTime;
+    tracer.finish();
+    assert.equal(1, agent.transactions.length);
+
+    var tracer = new trace.Tracer(tx, 'Custom/Test3');
+    tracer.getStartTime = tracer.getEndTime = FixedTime;
+    tracer.finish();
+
+    assert.equal(1, agent.transactions.length);
+
+    var stats = agent.transactions[0].getScopedStats();
+    var data = JSON.stringify(stats.getMetricData());
+    assert.equal('[[{"name":"Custom/Test2"},[1,0,0,0,0,0]]]', data);
+}
