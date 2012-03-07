25fca68f6ecbb324e267dbb4bc86b49754eee818
Saxon <saxon@newrelic.com>
2012-03-06 17:36:04 -0800

    Add proxy support

--- test/service.test.js
+++ test/service.test.js
@@ -1,5 +1,6 @@
 var winston = require('winston');
 var service = require('../lib/service');
+var config = require('../lib/config');
 var logger = require('../lib/logger').getLogger();
 
 var agent = require('./test_agent').createAgent();
@@ -7,7 +8,8 @@ var agent = require('./test_agent').createAgent();
 var testLicense = 'd67afc830dab717fd163bfcb0b8b88423e9a1a3b';
 
 exports['test conn'] = function(beforeExit, assert) {
-    var nr = service.createNewRelicService(agent, testLicense, 'staging-collector.newrelic.com', 80);
+	var c = config.initialize(logger, { 'config': {'license_key': testLicense, 'host':'staging-collector.newrelic.com', 'port':80 }});
+    var nr = service.createNewRelicService(agent, c);
     var connected = false;
     nr.on('connect', function() {
         connected = true;
