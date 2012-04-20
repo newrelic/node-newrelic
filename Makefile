28c3c8e82e7cdc7c959b983a68e318f3bf69e856
Forrest L Norvell <forrest@newrelic.com>
2012-04-20 16:33:55 -0700

    Set up mocha; port simple test case.

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 SHELL := /bin/bash
 NODE   = node
 EXPRESSO = node_modules/expresso/bin/expresso
+MOCHA = node_modules/mocha/bin/mocha
 
-test:
+test: newtest oldtest
+
+oldtest:
 	@$(EXPRESSO) test/*test.js
+
+newtest:
+	@$(MOCHA) test/*.mocha.js
 
 .PHONY: test
---
+++ test/config.mocha.js
@@ -0,0 +1,22 @@
+var mocha   = require('mocha')
+  , should  = require('should')
+  , winston = require('winston')
+  , config  = require('../lib/config')
+  ;
+
+describe('disabled test agent', function () {
+  var logger;
+
+  before(function (done) {
+    logger = new (winston.Logger)({transports : [new (winston.transports.Console)()]});
+
+    return done();
+  });
+
+  it('should handle a minimal configuration', function (done) {
+    var c = config.initialize(logger, {config : {'agent_enabled' : false}});
+    c.agent_enabled.should.equal(false);
+
+    return done();
+  });
+});
