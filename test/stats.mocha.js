8dc404a6f13a747a91e3b5f1b40693288bc569e3
Forrest L Norvell <forrest@newrelic.com>
2012-04-23 13:40:22 -0700

    Moved stats calculator tests over to mocha.

---
+++ test/stats.mocha.js
@@ -0,0 +1,46 @@
+var should  = require('should')
+  , winston = require('winston')
+  , stats   = require('../lib/stats')
+  ;
+
+function verifyStats(stats, callCount, totalTime, totalExclusive, min, max) {
+    var data = stats.toJSON();
+    data.should.exist;
+
+    data[0].should.equal(callCount, "the call counts should match");
+    data[1].should.equal(totalTime, "the total time tracked should match");
+    data[2].should.equal(totalExclusive, "the total exclusive should match");
+    data[3].should.equal(min, "the minimum should match");
+    data[4].should.equal(max, "the maximum should match");
+}
+
+describe('statistics calculation', function () {
+  var statistics
+    , logger
+    ;
+
+  beforeEach(function (done) {
+    statistics = stats.createStats();
+
+    return done();
+  });
+
+  it("should properly process a sample set of statistics", function (done) {
+    statistics.recordValueInMillis(51);
+    statistics.recordValueInMillis(120, 34);
+    statistics.recordValueInMillis(120, 34);
+
+    verifyStats(statistics, 3, 0.291, 0.119, 0.051, 0.120);
+
+    return done();
+  });
+
+  it("should properly process a simple set of statistics", function (done) {
+    statistics.recordValueInMillis(120, 0);
+    statistics.recordValueInMillis(120, 0);
+
+    verifyStats(statistics, 2, 0.240, 0.0, 0.120, 0.120);
+
+    return done();
+  });
+});
