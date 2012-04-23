1f650d7106656e5b795e85a11f0d8436280d5772
Forrest L Norvell <forrest@newrelic.com>
2012-04-23 13:25:53 -0700

    Ported service test to mocha.

---
+++ test/service.mocha.js
@@ -0,0 +1,36 @@
+var should  = require('should')
+  , logger  = require('../lib/logger').getLogger()
+  , config  = require('../lib/config')
+  , service = require('../lib/service')
+  ;
+
+describe('connecting to New Relic', function () {
+  var agent
+    , configuration
+    , newRelic
+    , testLicense   = 'd67afc830dab717fd163bfcb0b8b88423e9a1a3b'
+    , collectorHost = 'staging-collector.newrelic.com'
+    ;
+
+  before(function (done) {
+    agent = require('./test_agent').createAgent();
+    configuration = config.initialize(logger, {
+      'config' : {
+        'license_key' : testLicense,
+        'host'        : collectorHost,
+        'port'        : 80
+      }
+    });
+    newRelic = service.createNewRelicService(agent, configuration);
+
+    return done();
+  });
+
+  it('should establish a connection', function (done) {
+    newRelic.on('connect', function () {
+      // TODO: this should test more, and handle failure better.
+      return done();
+    });
+    newRelic.connect();
+  });
+});
