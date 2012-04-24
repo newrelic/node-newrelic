d4608c3531ae76891f964fd72786a8e82dd53139
Forrest L Norvell <forrest@newrelic.com>
2012-04-24 13:38:22 -0700

    Standardized formatting of sample config.

--- newrelic.js
+++ newrelic.js
@@ -1,11 +1,7 @@
-
-exports.config =
-{
-  app_name: ['My Application'],
-  license_key: 'license key here',
-  
-  transaction_tracer: {
-    enabled: true
+exports.config = {
+  app_name           : ['My Application'],
+  license_key        : 'license key here',
+  transaction_tracer : {
+    enabled : true
   }
-  
 };
