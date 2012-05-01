0704cd3129bfdeac112201532106c0f6693ba83b
Forrest L Norvell <forrest@newrelic.com>
2012-04-30 17:12:30 -0700

    Added first event-handling tests.
    Also exposed the New Relic service connection and removed hateful underscore.

--- test/agent.test.js
+++ test/agent.test.js
@@ -7,7 +7,7 @@ describe('the New Relic agent', function () {
   var agent;
 
   before(function (done) {
-    agent = nr._agent;
+    agent = nr.agent;
 
     return done();
   });
@@ -34,5 +34,60 @@ describe('the New Relic agent', function () {
     should.exist(agent.metricNormalizer);
 
     return done();
+  });
+
+  describe("when triggering events defined on the agent", function () {
+    var connection;
+
+    before(function (done) {
+      agent.on('connect', function () {
+        connection = agent.connection;
+        should.exist(connection);
+
+        return done();
+      });
+    });
+
+    after(function (done) {
+      agent.stop();
+
+      return done();
+    });
+
+    it("should fire the statsEngine.onConnect handler when the config is changed", function (done) {
+      agent.config.listeners('change').length.should.equal(1);
+
+      return done();
+    });
+
+    it("should have two handlers defined on the 'connect' event on the agent", function (done) {
+      connection.listeners('connect').length.should.equal(2);
+
+      return done();
+    });
+
+    it("should have one handler defined on the 'metricDataError' event on the agent", function (done) {
+      connection.listeners('metricDataError').length.should.equal(1);
+
+      return done();
+    });
+
+    it("should have one handler defined on the 'metricDataResponse' event on the agent", function (done) {
+      connection.listeners('metricDataResponse').length.should.equal(1);
+
+      return done();
+    });
+
+    it("should have one handler defined on the 'errorDataError' event on the agent", function (done) {
+      connection.listeners('errorDataError').length.should.equal(1);
+
+      return done();
+    });
+
+    it("should have one handler defined on the 'connectError' event on the agent", function (done) {
+      connection.listeners('connectError').length.should.equal(1);
+
+      return done();
+    });
   });
 });
