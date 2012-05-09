ea2e5acbac820168f71b7107776bc9aca5b57c71
Forrest L Norvell <forrest@newrelic.com>
2012-05-09 15:46:06 -0700

    Broke up the tests, added a couple more expected cases.

--- test/instrumentation-http.test.js
+++ test/instrumentation-http.test.js
@@ -7,38 +7,61 @@ var should  = require('should')
   ;
 
 describe('agent instrumentation of the http module', function () {
-  var agent;
+  var agent
+    , fetchedResponse
+    , fetchedBody;
+
+  var PAGE = '<html><head><title>test response</title></head><body><p>I heard you like HTML.</p></body></html>';
 
   before(function (done) {
     var connection = new mocker.Connection();
     agent = new NR({connection : connection});
 
-    return done();
-  });
-
-  it("should record stats after a normal request", function (done) {
-    var page = '<html><head><title>test response</title></head><body><p>I heard you like HTML.</p></body></html>';
-
     var server = http.createServer(function (request, response) {
-      response.writeHead(200, {'Content-Length' : page.length, 'Content-Type' : 'text/html'});
-      response.end(page);
+      response.writeHead(200, {'Content-Length' : PAGE.length, 'Content-Type' : 'text/html'});
+      response.end(PAGE);
     });
 
     server.listen(8123, 'localhost', function () {
       request.get('http://localhost:8123/path', function (error, response, body) {
         if (error) return done(error);
 
-        response.statusCode.should.equal(200);
-
-        var stats = JSON.stringify(agent.statsEngine.unscopedStats.byName('WebTransaction/Uri/path'));
-        should.exist(stats);
-        stats.should.match(/^\[[0-9.,]+\]$/);
-
-        should.exist(body);
-        body.should.equal(page);
+        fetchedResponse = response;
+        fetchedBody = body;
 
         return done();
       });
     });
+  });
+
+  it("should successfully fetch the page", function (done) {
+    fetchedResponse.statusCode.should.equal(200);
+
+    should.exist(fetchedBody);
+    fetchedBody.should.equal(PAGE);
+
+    return done();
+  });
+
+  it("should record unscoped path stats after a normal request", function (done) {
+    var pathStats = JSON.stringify(agent.statsEngine.unscopedStats.byName('WebTransaction/Uri/path'));
+    should.exist(pathStats);
+    pathStats.should.match(/^\[[0-9.,]+\]$/);
+
+    return done();
+  });
+
+  it("should indicate that the http dispatcher is in play", function (done) {
+    agent.environment.toJSON().should.includeEql(['Dispatcher', 'http']);
+
+    return done();
+  });
+
+  it("should record unscoped HTTP dispatcher stats after a normal request", function (done) {
+    var dispatchStats = JSON.stringify(agent.statsEngine.unscopedStats.byName('HttpDispatcher'));
+    should.exist(dispatchStats);
+    dispatchStats.should.match(/^\[[0-9.,]+\]$/);
+
+    return done();
   });
 });
