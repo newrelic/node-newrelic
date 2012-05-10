77f41af603fc36a351e303ea6d0cc5f8a1f18272
Forrest L Norvell <forrest@newrelic.com>
2012-05-10 11:23:57 -0700

    Fixed a bug reported by Hernan Silberman.
    The code wasn't properly ensuring the path was separated
    properly when loading a custom configuration via the
    NR_HOME variable. Not only fixed that, but ensured that
    NR_HOME sets precedence over a local configuration file.

--- test/config.test.js
+++ test/config.test.js
@@ -1,5 +1,8 @@
-var logger  = require('../lib/logger')
-  , config  = require('../lib/config')
+var path   = require('path')
+  , util   = require('util')
+  , fs     = require('fs')
+  , logger = require(path.join(__dirname, '..', 'lib', 'logger'))
+  , config = require(path.join(__dirname, '..', 'lib', 'config'))
   ;
 
 describe('disabled test agent', function () {
@@ -8,5 +11,66 @@ describe('disabled test agent', function () {
     c.agent_enabled.should.equal(false);
 
     return done();
+  });
+
+  describe('when overriding the config file location via NR_HOME', function () {
+    var origHome
+      , startDir
+      , DESTDIR = path.join(__dirname, 'xXxNRHOMETESTxXx')
+      , NOPLACEDIR = path.join(__dirname, 'NOHEREHERECHAMP')
+      , CONFIGPATH = path.join(DESTDIR, 'newrelic.js')
+      ;
+
+    beforeEach(function (done) {
+      if (process.env.NEWRELIC_HOME) {
+        origHome = process.env.NEWRELIC_HOME;
+      }
+
+      startDir = process.cwd();
+
+      fs.mkdir(DESTDIR, function (error) {
+        if (error) return done(error);
+
+        fs.mkdir(NOPLACEDIR, function (error) {
+          if (error) return done(error);
+
+          process.chdir(NOPLACEDIR);
+          process.env.NEWRELIC_HOME = DESTDIR;
+
+          var sampleConfig = fs.createReadStream(path.join(__dirname, '..', 'lib', 'config.default.js'));
+          var sandboxedConfig = fs.createWriteStream(CONFIGPATH);
+
+          util.pump(sampleConfig, sandboxedConfig, done);
+        });
+      });
+    });
+
+    afterEach(function (done) {
+      if (origHome) {
+        process.env.NEWRELIC_HOME = origHome;
+      }
+      else {
+        delete process.env.NEWRELIC_HOME;
+      }
+      origHome = null;
+
+      fs.unlink(CONFIGPATH, function (error) {
+        if (error) return done(error);
+
+        fs.rmdir(DESTDIR, function (error) {
+          if (error) return done(error);
+
+          process.chdir(startDir);
+
+          fs.rmdir(NOPLACEDIR, done);
+        });
+      });
+    });
+
+    it('should load the configuration', function (done) {
+      (function () { config.initialize(logger); }).should.not.throw();
+
+      return done();
+    });
   });
 });
