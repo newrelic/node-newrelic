792b3a71007a2ae3d92c87aba4780fe0ca3fe741
Forrest L Norvell <forrest@newrelic.com>
2012-05-11 15:15:21 -0700

    Don't require options to create an object.
    As hsilberman@ngmoco.com pointed out, the old logic was
    exactly backwards -- it wouldn't create a new agent unless
    an options object was passed in, otherwise preventing the
    agent from being instantiated.

--- test/agent.test.js
+++ test/agent.test.js
@@ -1,141 +1,158 @@
 var should = require('should')
   , path   = require('path')
-  , mocker = require(path.join(__dirname, 'lib', 'mock_connection'))
-  , NR     = require(path.join(__dirname, '..', 'lib', 'newrelic_agent.js'))
+  , helper = require(path.join(__dirname, 'lib', 'agent_helper'))
   ;
 
 describe('the New Relic agent', function () {
-  var agent
-    , connection
-    ;
+  describe("when working with a live connection to the service, created in the traditional manner", function () {
+    it("should start up properly", function (done) {
+      var agent = helper.loadAgent();
+      should.exist(agent);
 
-  before(function (done) {
-    agent = new NR({connection : new mocker.Connection()});
+      agent.on('connect', function () {
+        agent.connection.isConnected.should.be.ok;
 
-    agent.on('connect', function () {
-      connection = agent.connection;
-      should.exist(connection);
+        var deaded = helper.unloadAgent(agent);
+        should.exist(deaded);
+
+        return done();
+      });
+    });
+  });
+
+  describe("when working offline with a mocked service connection", function () {
+    var agent
+      , connection
+      ;
+
+    before(function (done) {
+      agent = helper.loadMockedAgent();
+
+      agent.on('connect', function () {
+        connection = agent.connection;
+        should.exist(connection);
+
+        return done();
+      });
+    });
+
+    after(function (done) {
+      var deaded = helper.unloadAgent(agent);
 
       return done();
     });
-  });
 
-  after(function (done) {
-    agent.stop();
+    it('should expose its configured statsEngine directly', function (done) {
+      should.exist(agent.statsEngine);
 
-    return done();
-  });
-
-  it('should expose its configured statsEngine directly', function (done) {
-    should.exist(agent.statsEngine);
-
-    return done();
-  });
-
-  it('should expose its configuration directly', function (done) {
-    should.exist(agent.config);
-
-    return done();
-  });
-
-  it('should expose its configured errorService directly', function (done) {
-    should.exist(agent.errorService);
-
-    return done();
-  });
-
-  it('should expose its configured metricNormalizer directly', function (done) {
-    should.exist(agent.metricNormalizer);
-
-    return done();
-  });
-
-  describe("when dealing with its event handlers", function () {
-    describe("when setting up event subscriptions", function () {
-      it("should have one handler defined on the 'change' event on the agent's configuration", function (done) {
-        agent.config.listeners('change').length.should.equal(1);
-
-        return done();
-      });
-
-      it("should have two handlers defined on the 'connect' event on the agent", function (done) {
-        connection.listeners('connect').length.should.equal(2);
-
-        return done();
-      });
-
-      it("should have one handler defined on the 'metricDataError' event on the agent", function (done) {
-        connection.listeners('metricDataError').length.should.equal(1);
-
-        return done();
-      });
-
-      it("should have one handler defined on the 'metricDataResponse' event on the agent", function (done) {
-        connection.listeners('metricDataResponse').length.should.equal(1);
-
-        return done();
-      });
-
-      it("should have one handler defined on the 'errorDataError' event on the agent", function (done) {
-        connection.listeners('errorDataError').length.should.equal(1);
-
-        return done();
-      });
-
-      it("should have one handler defined on the 'connectError' event on the agent", function (done) {
-        connection.listeners('connectError').length.should.equal(1);
-
-        return done();
-      });
+      return done();
     });
 
-    describe("when signaling events out of band", function () {
-      it("should reset the stats engine's ApdexT value when the configuration is changed", function (done) {
-        should.not.exist(agent.statsEngine.apdexT);
-        process.nextTick(function () {
-          should.exist(agent.statsEngine.apdexT);
-          agent.statsEngine.apdexT.should.equal(0.666);
+    it('should expose its configuration directly', function (done) {
+      should.exist(agent.config);
+
+      return done();
+    });
+
+    it('should expose its configured errorService directly', function (done) {
+      should.exist(agent.errorService);
+
+      return done();
+    });
+
+    it('should expose its configured metricNormalizer directly', function (done) {
+      should.exist(agent.metricNormalizer);
+
+      return done();
+    });
+
+    describe("when dealing with its event handlers", function () {
+      describe("when setting up event subscriptions", function () {
+        it("should have one handler defined on the 'change' event on the agent's configuration", function (done) {
+          agent.config.listeners('change').length.should.equal(1);
 
           return done();
         });
 
-        agent.config.emit('change', {'apdex_t' : 0.666});
+        it("should have two handlers defined on the 'connect' event on the agent", function (done) {
+          connection.listeners('connect').length.should.equal(2);
+
+          return done();
+        });
+
+        it("should have one handler defined on the 'metricDataError' event on the agent", function (done) {
+          connection.listeners('metricDataError').length.should.equal(1);
+
+          return done();
+        });
+
+        it("should have one handler defined on the 'metricDataResponse' event on the agent", function (done) {
+          connection.listeners('metricDataResponse').length.should.equal(1);
+
+          return done();
+        });
+
+        it("should have one handler defined on the 'errorDataError' event on the agent", function (done) {
+          connection.listeners('errorDataError').length.should.equal(1);
+
+          return done();
+        });
+
+        it("should have one handler defined on the 'connectError' event on the agent", function (done) {
+          connection.listeners('connectError').length.should.equal(1);
+
+          return done();
+        });
       });
 
-      it("should reset the configuration and metrics normalizer when the agent connects", function (done) {
-        should.not.exist(agent.config.apdex_t);
-        process.nextTick(function () {
-          should.exist(agent.config.apdex_t);
-          agent.config.apdex_t.should.equal(0.742);
+      describe("when signaling events out of band", function () {
+        it("should reset the stats engine's ApdexT value when the configuration is changed", function (done) {
+          should.not.exist(agent.statsEngine.apdexT);
+          process.nextTick(function () {
+            should.exist(agent.statsEngine.apdexT);
+            agent.statsEngine.apdexT.should.equal(0.666);
 
-          should.exist(agent.statsEngine.apdexT);
-          agent.statsEngine.apdexT.should.equal(0.742);
+            return done();
+          });
 
-          should.exist(agent.metricNormalizer.rules);
-          agent.metricNormalizer.rules.should.eql([0]);
-
-          return done();
+          agent.config.emit('change', {'apdex_t' : 0.666});
         });
 
-        connection.emit('connect', {'apdex_t' : 0.742, url_rules : [0]});
-      });
+        it("should reset the configuration and metrics normalizer when the agent connects", function (done) {
+          should.not.exist(agent.config.apdex_t);
+          process.nextTick(function () {
+            should.exist(agent.config.apdex_t);
+            agent.config.apdex_t.should.equal(0.742);
 
-      it("should parse metrics responses when metric data is received", function (done) {
-        var STATNAME = "Custom/Test/events";
-        var SCOPE    = "TEST";
-        var METRICID = "test000000001";
+            should.exist(agent.statsEngine.apdexT);
+            agent.statsEngine.apdexT.should.equal(0.742);
 
-        var testIDs = {};
-        testIDs[STATNAME + ',' + SCOPE] = METRICID;
+            should.exist(agent.metricNormalizer.rules);
+            agent.metricNormalizer.rules.should.eql([0]);
 
-        agent.statsEngine.metricIds.should.eql([]);
-        process.nextTick(function () {
-          agent.statsEngine.metricIds.should.eql(testIDs);
+            return done();
+          });
 
-          return done();
+          connection.emit('connect', {'apdex_t' : 0.742, url_rules : [0]});
         });
 
-        connection.emit('metricDataResponse', [[{"name" : STATNAME, "scope" : SCOPE}, METRICID]]);
+        it("should parse metrics responses when metric data is received", function (done) {
+          var STATNAME = "Custom/Test/events";
+          var SCOPE    = "TEST";
+          var METRICID = "test000000001";
+
+          var testIDs = {};
+          testIDs[STATNAME + ',' + SCOPE] = METRICID;
+
+          agent.statsEngine.metricIds.should.eql([]);
+          process.nextTick(function () {
+            agent.statsEngine.metricIds.should.eql(testIDs);
+
+            return done();
+          });
+
+          connection.emit('metricDataResponse', [[{"name" : STATNAME, "scope" : SCOPE}, METRICID]]);
+        });
       });
     });
   });
