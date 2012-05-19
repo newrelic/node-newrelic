d74eee031236507f58cfa1750c42d3abb0860604
Forrest L Norvell <forrest@newrelic.com>
2012-05-18 18:39:38 -0700

    Logger now will log at verbose (which it defaults to).

--- newrelic.js
+++ newrelic.js
@@ -1,6 +1,7 @@
 exports.config = {
   app_name           : ['My Application'],
   license_key        : 'license key here',
+  log_level          : 'verbose',
   transaction_tracer : {
     enabled : true
   }
