9b11734f54babb33f7e17ad097420dda96b60536
Forrest L Norvell <forrest@newrelic.com>
2012-05-18 18:21:16 -0700

    Validate that Express instrumentation is working.
    Writing the tests for Express was easy; getting them all to pass was
    incredibly hard. In the end, there was a nasty and subtle bug related to
    an unintentional closure being created around module._load that would
    cause it to retain a reference to an outdated agent.
    shimmer.preserveMethod tries its best to be idempotent, and it fixed
    this problem, but first I needed to figure out where it needed to go.
    
    There's still a lot of overhead around module loading, some of which is
    maybe unnecessary. That's a big TODO.

---
+++ test/instrumentation-express.test.js
@@ -0,0 +1,84 @@
+var should  = require('should')
+  , path    = require('path')
+  , request = require('request')
+  , helper  = require(path.join(__dirname, 'lib', 'agent_helper'))
+  , logger  = require(path.join(__dirname, '..', 'lib', 'logger'))
+  ;
+
+describe("an instrumented Express application", function () {
+  var app
+    , agent
+    , fetchedResponse
+    , fetchedBody
+    , PORT = 8062
+    ;
+
+  before(function (done) {
+    agent = helper.loadMockedAgent();
+    // set apdexT so apdex stats will be recorded
+    agent.statsEngine.apdexT = 1;
+
+    var express = require('express');
+
+    app = express.createServer();
+    app.get('/test-get', function (req, res) {
+      res.send({yep : true});
+    });
+
+    app.listen(8062, function () {
+      request.get('http://localhost:8062/test-get', function (error, response, body) {
+        if (error) return done(error);
+
+        fetchedResponse = response;
+        fetchedBody = body;
+
+        return done();
+      });
+    });
+  });
+
+  after(function (done) {
+    app.close();
+    helper.unloadAgent(agent);
+
+    return done();
+  });
+
+  it("should serve content", function (done) {
+    fetchedResponse.headers['content-type'].should.equal('application/json; charset=utf-8');
+    fetchedBody.should.equal('{"yep":true}');
+
+    return done();
+  });
+
+  it("should record unscoped path statistics", function (done) {
+    var summary = agent.statsEngine.unscopedStats.byName('WebTransaction/Uri/test-get').toJSON();
+    summary[0].should.equal(1);
+
+    return done();
+  });
+
+  /**
+   * This test case took three days to get running.
+   */
+  it("should record apdex without some low-level method-wrapping problem", function (done) {
+    var summary = agent.statsEngine.unscopedStats.getApdexStats('Apdex/Uri/test-get').toJSON();
+    summary[0].should.equal(1);
+
+    return done();
+  });
+
+  it("should roll up web transaction statistics", function (done) {
+    var summary = agent.statsEngine.unscopedStats.byName('WebTransaction').toJSON();
+    summary[0].should.equal(1);
+
+    return done();
+  });
+
+  it("should roll up HTTP dispatcher statistics", function (done) {
+    var summary = agent.statsEngine.unscopedStats.byName('HttpDispatcher').toJSON();
+    summary[0].should.equal(1);
+
+    return done();
+  });
+});
