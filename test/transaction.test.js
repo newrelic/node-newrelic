bc0c8a3f99b18536ac9b9b7aca301bac83c09f69
Forrest L Norvell <forrest@newrelic.com>
2012-06-08 17:55:52 -0700

    More WIP reworking of transaction tracing.
    * Add support for scoped and named traces.
    * Rebuild some of the stats code to have fewer dependencies on
      other objects, as well as providing a friendlier format for
      inspection and debugging.

--- test/transaction.test.js
+++ test/transaction.test.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var path        = require('path')
   , chai        = require('chai')
   , should      = chai.should()
@@ -5,7 +7,7 @@ var path        = require('path')
   , transaction = require(path.join(__dirname, '..', 'lib', 'transaction'))
   ;
 
-describe("transaction management", function () {
+describe("new-school transaction tracing", function () {
   // don't add Sinon into the mix until I know what to spy on
   var agent = {name : "test application"};
 
@@ -65,20 +67,143 @@ describe("transaction management", function () {
     transaction.byApplication(firstApp).length.should.equal(3);
   });
 
-  it("should allow the addition of traces by name", function () {
-    var tt = transaction.create(agent);
+  describe("when tracing scoped metrics", function () {
+    var tt;
 
-    tt.measure('Custom/Test01');
-    should.exist(tt.metric('Custom/Test01'));
+    beforeEach(function () {
+      tt = transaction.create(agent);
+    });
+
+    it("should add traces by scope", function () {
+      tt.measure('Custom/Test08', 'TEST');
+      expect(tt.metrics('Custom/Test08', 'TEST')).to.not.equal(null);
+    });
+
+    it("should track the same metric name separately in separate scopes", function () {
+      var one = tt.measure('Custom/Test09', 'ONCE');
+      one.end();
+
+      var other = tt.measure('Custom/Test09', 'TWICE');
+      other.end();
+
+      var third = tt.measure('Custom/Test09', 'TWICE');
+      third.end();
+
+      expect(tt.statistics('Custom/Test09', 'ONCE').toObject().calls).to.equal(1);
+      expect(tt.statistics('Custom/Test09', 'TWICE').toObject().calls).to.equal(2);
+    });
   });
 
-  it("shouldn't trace calls added after the transaction has finished", function () {
-    var tt = transaction.create(agent);
+  describe("when dealing with individual traces", function () {
+    it("should add traces by name", function () {
+      var tt = transaction.create(agent);
 
-    tt.measure('Custom/Test02');
-    tt.end();
+      tt.measure('Custom/Test01');
+      should.exist(tt.metrics('Custom/Test01'));
+    });
 
-    tt.measure('Custom/Test03');
-    should.not.exist(tt.metric('Custom/Test03'));
+    it("should allow multiple traces for same name", function () {
+      var TRACE_NAME = 'Custom/Test02'
+        , tt = transaction.create(agent)
+        , traces = []
+        ;
+
+      for (var i = 0; i < 5; i++) {
+        traces[i] = tt.measure(TRACE_NAME);
+        traces[i].end();
+      }
+
+      tt.end();
+
+      expect(tt.statistics(TRACE_NAME).toObject().calls).to.equal(traces.length);
+    });
+
+    it("should allow multiple overlapping traces for same name", function (done) {
+      var TRACE_NAME = 'Custom/Test06'
+        , SLEEP_DURATION = 43
+        , tt = transaction.create(agent)
+        ;
+
+      var first = tt.measure(TRACE_NAME);
+      var second = tt.measure(TRACE_NAME);
+      second.end();
+
+      setTimeout(function () {
+        // this will automatically close out any open transactions,
+        // so in this case will close the first transaction
+        tt.end();
+
+        var statistics = tt.statistics(TRACE_NAME).toObject();
+        expect(statistics.calls).to.equal(2);
+        expect(statistics.max).to.be.above(SLEEP_DURATION - 1);
+
+        return done();
+      }, SLEEP_DURATION);
+    });
+
+    it("shouldn't trace calls added after the transaction has finished", function () {
+      var tt = transaction.create(agent);
+
+      tt.measure('Custom/Test03');
+      tt.end();
+
+      tt.measure('Custom/Test04');
+      should.not.exist(tt.metrics('Custom/Test04'));
+    });
+
+    describe("when fetching statistics", function () {
+      it("should return statistics properly", function () {
+        var tt = transaction.create(agent);
+
+        tt.measure('Custom/Test05');
+        tt.end();
+
+        expect(tt.statistics('Custom/Test05').toJSON()).to.deep.equal([1, 0, 0, 0, 0, 0]);
+      });
+    });
+  });
+
+  describe("when producing a summary of the whole transaction", function () {
+    var tt;
+
+    beforeEach(function () {
+      tt = transaction.create(agent);
+
+      // scoped metrics
+      tt.measure('Custom/Test11', 'TEST').end();
+      tt.measure('Custom/Test12', 'TEST').end();
+      tt.measure('Custom/Test11', 'ANOTHER').end();
+
+      // unscoped metrics
+      tt.measure('Custom/Lucky13').end();
+      tt.measure('Custom/Lucky13').end();
+      tt.measure('Custom/Lucky13').end();
+      tt.measure('Custom/Test14').end();
+      tt.measure('Custom/Test15').end();
+
+      tt.end();
+    });
+
+    it("should be returned when statistics is called with no parameters", function () {
+      var summary = tt.statistics();
+
+      expect(summary.scoped.TEST['Custom/Test11'].toJSON()[0]).to.equal(1);
+      expect(summary.scoped.TEST['Custom/Test12'].toJSON()[0]).to.equal(1);
+      expect(summary.scoped.ANOTHER['Custom/Test11'].toJSON()[0]).to.equal(1);
+      expect(summary.unscoped['Custom/Lucky13'].toJSON()[0]).to.equal(3);
+      expect(summary.unscoped['Custom/Test14'].toJSON()[0]).to.equal(1);
+      expect(summary.unscoped['Custom/Test15'].toJSON()[0]).to.equal(1);
+    });
+
+    it("should be returned when summary is called", function () {
+      var summary = tt.summary();
+
+      expect(summary.scoped.TEST['Custom/Test11'].toJSON()[0]).to.equal(1);
+      expect(summary.scoped.TEST['Custom/Test12'].toJSON()[0]).to.equal(1);
+      expect(summary.scoped.ANOTHER['Custom/Test11'].toJSON()[0]).to.equal(1);
+      expect(summary.unscoped['Custom/Lucky13'].toJSON()[0]).to.equal(3);
+      expect(summary.unscoped['Custom/Test14'].toJSON()[0]).to.equal(1);
+      expect(summary.unscoped['Custom/Test15'].toJSON()[0]).to.equal(1);
+    });
   });
 });
