91e12ed381fc348c129df11c659e1dc277f7ffb1
Forrest L Norvell <forrest@newrelic.com>
2012-06-29 18:09:11 -0700

    WIP -- replacing internal API for DB calls.

--- test/transaction.test.js
+++ test/transaction.test.js
@@ -151,17 +151,6 @@ describe("new-school transaction tracing", function () {
       should.not.exist(tt.getMetrics('Custom/Test04'));
     });
 
-    describe("when fetching statistics", function () {
-      it("should return statistics properly", function () {
-        var tt = transaction.create(agent);
-
-        tt.measure('Custom/Test05');
-        tt.end();
-
-        expect(tt.getStatistics('Custom/Test05').toJSON()).to.deep.equal([1, 0, 0, 0, 0, 0]);
-      });
-    });
-
     it("should allow manual setting of trace durations", function () {
       var tt = transaction.create(agent);
 
@@ -175,96 +164,114 @@ describe("new-school transaction tracing", function () {
       metrics[0].getDurationInMillis().should.equal(65);
     });
 
-    it("should allow adding child traces to an existing trace", function () {
-      var tt = transaction.create(agent)
-        , trace = tt.measure('Custom/Test17');
+    describe("when adding child traces", function () {
+      var tt;
 
-      expect(function () { trace.addChild('Custom/Test17/Child1'); }).not.throws();
+      beforeEach(function () {
+        tt = transaction.create(agent);
+      });
+
+      it("should allow child traces on an existing trace", function () {
+        var trace = tt.measure('Custom/Test17');
+
+        expect(function () { trace.addChild('Custom/Test17/Child1'); }).not.throws();
+      });
+
+      it("should measure exclusive time vs total time", function () {
+        var trace = tt.measure('Custom/Test18')
+          , child = trace.addChild('Custom/Test18/Child1');
+
+        trace.setDurationInMillis(42);
+        child.setDurationInMillis(22);
+
+        var metrics = tt.getMetrics('Custom/Test18');
+        expect(metrics.length).to.equal(1);
+        metrics[0].getDurationInMillis().should.equal(42);
+        metrics[0].getExclusiveDurationInMillis().should.equal(20);
+      });
+
+      it("should accurately sum overlapping child traces", function () {
+        var trace = tt.measure('Custom/Test19');
+
+        trace.setDurationInMillis(42);
+
+        var now = Date.now();
+
+        var child1 = trace.addChild('Custom/Test19/Child1');
+        child1.setDurationInMillis(22, now);
+
+        // add another child trace completely encompassed by the first
+        var child2 = trace.addChild('Custom/Test19/Child2');
+        child2.setDurationInMillis(5, now + 5);
+
+        // add another that starts within the first range but that extends beyond
+        var child3 = trace.addChild('Custom/Test19/Child3');
+        child3.setDurationInMillis(22, now + 11);
+
+        // add a final child that's entirely disjoint
+        var child4 = trace.addChild('Custom/Test19/Child4');
+        child4.setDurationInMillis(4, now + 35);
+
+        var metrics = tt.getMetrics('Custom/Test19');
+        expect(metrics.length).to.equal(1);
+        metrics[0].getDurationInMillis().should.equal(42);
+        metrics[0].getExclusiveDurationInMillis().should.equal(5);
+      });
+
+      it("should accurately sum partially overlapping child traces", function () {
+        var trace = tt.measure('Custom/Test20');
+
+        trace.setDurationInMillis(42);
+
+        var now = Date.now();
+
+        var child1 = trace.addChild('Custom/Test20/Child1');
+        child1.setDurationInMillis(22, now);
+
+        // add another child trace completely encompassed by the first
+        var child2 = trace.addChild('Custom/Test20/Child2');
+        child2.setDurationInMillis(5, now + 5);
+
+        // add another that starts simultaneously with the first range but that extends beyond
+        var child3 = trace.addChild('Custom/Test20/Child3');
+        child3.setDurationInMillis(33, now);
+
+        var metrics = tt.getMetrics('Custom/Test20');
+        expect(metrics.length).to.equal(1);
+        metrics[0].getDurationInMillis().should.equal(42);
+        metrics[0].getExclusiveDurationInMillis().should.equal(9);
+      });
+
+      it("should accurately sum partially overlapping, open-ranged child traces", function () {
+        var trace = tt.measure('Custom/Test21');
+
+        trace.setDurationInMillis(42);
+
+        var now = Date.now();
+
+        var child1 = trace.addChild('Custom/Test21/Child1');
+        child1.setDurationInMillis(22, now);
+
+        // add a range that starts at the exact end of the first
+        var child2 = trace.addChild('Custom/Test21/Child2');
+        child2.setDurationInMillis(11, now + 22);
+
+        var metrics = tt.getMetrics('Custom/Test21');
+        expect(metrics.length).to.equal(1);
+        metrics[0].getDurationInMillis().should.equal(42);
+        metrics[0].getExclusiveDurationInMillis().should.equal(9);
+      });
     });
 
-    it("should measure exclusive time vs total time", function () {
-      var tt    = transaction.create(agent)
-        , trace = tt.measure('Custom/Test18')
-        , child = trace.addChild('Custom/Test18/Child1');
+    describe("when fetching statistics", function () {
+      it("should return statistics properly", function () {
+        var tt = transaction.create(agent);
 
-      trace.setDurationInMillis(42);
-      child.setDurationInMillis(22);
+        tt.measure('Custom/Test05');
+        tt.end();
 
-      var metrics = tt.getMetrics('Custom/Test18');
-      expect(metrics.length).to.equal(1);
-      metrics[0].getExclusiveDurationInMillis().should.equal(20);
-    });
-
-    it("should accurately sum overlapping child traces", function () {
-      var tt    = transaction.create(agent)
-        , trace = tt.measure('Custom/Test19');
-
-      trace.setDurationInMillis(42);
-
-      var now = Date.now();
-
-      var child1 = trace.addChild('Custom/Test19/Child1');
-      child1.setDurationInMillis(22, now);
-
-      // add another child trace completely encompassed by the first
-      var child2 = trace.addChild('Custom/Test19/Child2');
-      child2.setDurationInMillis(5, now + 5);
-
-      // add another that starts within the first range but that extends beyond
-      var child3 = trace.addChild('Custom/Test19/Child3');
-      child3.setDurationInMillis(22, now + 11);
-
-      // add a final child that's entirely disjoint
-      var child4 = trace.addChild('Custom/Test19/Child4');
-      child4.setDurationInMillis(4, now + 35);
-
-      var metrics = tt.getMetrics('Custom/Test19');
-      expect(metrics.length).to.equal(1);
-      metrics[0].getExclusiveDurationInMillis().should.equal(5);
-    });
-
-    it("should accurately sum partially overlapping child traces", function () {
-      var tt    = transaction.create(agent)
-        , trace = tt.measure('Custom/Test20');
-
-      trace.setDurationInMillis(42);
-
-      var now = Date.now();
-
-      var child1 = trace.addChild('Custom/Test20/Child1');
-      child1.setDurationInMillis(22, now);
-
-      // add another child trace completely encompassed by the first
-      var child2 = trace.addChild('Custom/Test20/Child2');
-      child2.setDurationInMillis(5, now + 5);
-
-      // add another that starts simultaneously the first range but that extends beyond
-      var child3 = trace.addChild('Custom/Test20/Child3');
-      child3.setDurationInMillis(33, now);
-
-      var metrics = tt.getMetrics('Custom/Test20');
-      expect(metrics.length).to.equal(1);
-      metrics[0].getExclusiveDurationInMillis().should.equal(9);
-    });
-
-    it("should accurately sum partially overlapping, open-ranged child traces", function () {
-      var tt    = transaction.create(agent)
-        , trace = tt.measure('Custom/Test21');
-
-      trace.setDurationInMillis(42);
-
-      var now = Date.now();
-
-      var child1 = trace.addChild('Custom/Test21/Child1');
-      child1.setDurationInMillis(22, now);
-
-      // add another that starts simultaneously the first range but that extends beyond
-      var child2 = trace.addChild('Custom/Test21/Child2');
-      child2.setDurationInMillis(11, now + 22);
-
-      var metrics = tt.getMetrics('Custom/Test21');
-      expect(metrics.length).to.equal(1);
-      metrics[0].getExclusiveDurationInMillis().should.equal(9);
+        expect(tt.getStatistics('Custom/Test05').toJSON()).to.deep.equal([1, 0, 0, 0, 0, 0]);
+      });
     });
   });
 
