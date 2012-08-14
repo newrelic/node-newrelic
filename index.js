05970562318937a43dc98b4dcc6caf5d4f4c5247
Forrest L Norvell <ogd@aoaioxxysz.net>
2012-08-13 23:22:41 -0700

    lib/newrelic_agent.js -> index.js (after Agent refactor).

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-var path = require('path');
+'use strict';
 
-module.exports = require(path.join(__dirname, 'lib', 'newrelic_agent'));
+var path    = require('path')
+  , shimmer = require(path.join(__dirname, 'lib', 'shimmer'))
+  , Agent   = require(path.join(__dirname, 'lib', 'agent'))
+  ;
+
+var agent;
+
+module.exports = function (options) {
+  if (!agent) {
+    agent = new Agent(options);
+
+    // set up all of the instrumentation
+    shimmer.wrapAgent(agent);
+    shimmer.patchModule(agent);
+    shimmer.bootstrapInstrumentation(agent);
+
+    agent.start();
+  }
+
+  return agent;
+};
