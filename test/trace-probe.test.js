6ab9422b2927addc6a8e40c35d2f023bc74f3844
Forrest L Norvell <forrest@newrelic.com>
2012-08-16 19:37:58 -0700

    Very beginning of less confused transaction tracing.

---
+++ test/trace-probe.test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe("Probe", function () {
+  it("should be bound to a Trace");
+  it("should have 0 children at creation");
+  it("should have a timer");
+  describe("when finished", function () {
+    it("should know its exclusive runtime");
+  });
+});
