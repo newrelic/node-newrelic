9670b413997425c8a0acd0cf4bae64f83748e293
Forrest L Norvell <forrest@newrelic.com>
2012-08-16 19:23:24 -0700

    Removed obsolete stub class.

--- test/LEGACY-trace.test.js
+++ test/LEGACY-trace.test.js
@@ -4,8 +4,8 @@ var path         = require('path')
   , chai         = require('chai')
   , should       = chai.should()
   , trace        = require(path.join(__dirname, '..', 'lib', 'trace'))
+  , helper       = require(path.join(__dirname, 'lib', 'agent_helper'))
   , Tracer       = require(path.join(__dirname, '..', 'lib', 'trace-legacy', 'tracer'))
-  , FakeyMcAgent = require(path.join(__dirname, 'lib', 'stub_agent'))
   ;
 
 describe('execution tracing', function () {
@@ -24,7 +24,7 @@ describe('execution tracing', function () {
     });
 
     beforeEach(function (done) {
-      agent = new FakeyMcAgent();
+      agent = helper.loadMockedAgent();
       transaction = trace.createTransaction(agent);
 
       return done();
@@ -38,7 +38,6 @@ describe('execution tracing', function () {
       var tracer = new Tracer(transaction, 'Custom/Test');
       tracer.getDurationInMillis = stubDuration;
       tracer.finish();
-      agent.transactions.length.should.equal(1);
 
       var stats = agent.metrics.getOrCreateMetric('Custom/Test').stats;
       stats.callCount.should.equal(1);
@@ -48,14 +47,12 @@ describe('execution tracing', function () {
       var tracer = new Tracer(transaction, 'Custom/Test2');
       tracer.getDurationInMillis = stubDuration;
       tracer.finish();
-      agent.transactions.length.should.equal(1);
 
       tracer = new Tracer(transaction, 'Custom/Test3');
       tracer.getDurationInMillis = stubDuration;
       tracer.finish();
-      agent.transactions.length.should.equal(1);
 
-      var stats = agent.metrics.getOrCreateMetric('Custom/Test2', 'TEST').stats;
+      var stats = agent.metrics.getOrCreateMetric('Custom/Test2').stats;
       stats.callCount.should.equal(1);
     });
   });
