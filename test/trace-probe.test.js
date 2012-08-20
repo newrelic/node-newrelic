f761dd8a263a3ffaa516573d7a414fd39689d0e0
Forrest L Norvell <forrest@newrelic.com>
2012-08-20 16:10:19 -0700

    Filled in first set of Probe features.

--- test/trace-probe.test.js
+++ test/trace-probe.test.js
@@ -1,11 +1,50 @@
 'use strict';
 
+var path   = require('path')
+  , chai   = require('chai')
+  , expect = chai.expect
+  , Probe  = require(path.join(__dirname, '..', 'lib', 'trace', 'probe'))
+  , Trace  = require(path.join(__dirname, '..', 'lib', 'trace-nu'))
+  ;
+
 describe("Probe", function () {
-  it("should be bound to a Trace");
-  it("should have 0 children at creation");
-  it("should have a timer");
-  describe("when finished", function () {
-    it("should know its exclusive runtime");
+  it("should be bound to a Trace", function () {
+    expect(function noTrace() {
+      var probe = new Probe(null, 'UnitTest');
+    }).throws();
+    var success = new Probe(new Trace('Test/TraceExample07'), 'UnitTest');
+    expect(success.trace).an('object');
   });
+
+  it("should be named", function () {
+    expect(function noName() {
+      var probe = new Probe(new Trace('Test/TraceExample06'));
+    }).throws();
+    var success = new Probe(new Trace('Test/TraceExample07'), 'UnitTest');
+    expect(success.name).equal('UnitTest');
+  });
+
+  it("should have 0 children at creation", function () {
+    var probe = new Probe(new Trace('Test/TraceExample02'), 'UnitTest');
+    expect(probe.children.length).equal(0);
+  });
+
+  it("should have a timer", function () {
+    var probe = new Probe(new Trace('Test/TraceExample03'), 'UnitTest');
+    expect(probe.timer.isRunning()).equal(true);
+  });
+
   it("should retain any associated SQL statements");
+
+  describe("when finished", function () {
+    it("should have a finished timer", function () {
+      var probe = new Probe(new Trace('Test/TraceExample04'), 'UnitTest');
+      probe.finish();
+      expect(probe.timer.isRunning()).equal(false);
+    });
+
+    it("should know its exclusive duration");
+    it("should produce human-readable JSON");
+    it("should record its own metrics onto the trace");
+  });
 });
