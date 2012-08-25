7c52f1bafe7189a104e37d3b0241fb0a15c52231
Forrest L Norvell <forrest@newrelic.com>
2012-08-24 17:33:56 -0700

    Added a bunch more (increasingly pathological) test cases.
    And lo and behold, one of them runs just fine when the file is run on
    its own, but fails when it's run as part of the whole suite. This is the
    price I pay for my wanton abuse of the call stack.

--- test/shimmer.test.js
+++ test/shimmer.test.js
@@ -117,13 +117,19 @@ describe("the instrumentation injector", function () {
       helper.unloadAgent(agent);
     });
 
-    it("should scope transactions to the appropriate context", function (done) {
-      var synchronizer = new EventEmitter();
-      var transactions = [];
+    it("should push transactions through process.nextTick", function (done) {
+      expect(agent.getTransaction()).equal(undefined);
+
+      var synchronizer = new EventEmitter()
+        , transactions = []
+        , ids          = []
+        ;
 
       var spamTransaction = function (i) {
         var current = transaction.create(agent);
         transactions[i] = current;
+        ids[i] = current.id;
+
         process.nextTick(function () {
           var lookup = agent.getTransaction();
           expect(lookup).equal(current);
@@ -136,12 +142,161 @@ describe("the instrumentation injector", function () {
       synchronizer.on('inner', function (trans, j) {
         doneCount += 1;
         expect(trans).equal(transactions[j]);
+        expect(trans.id).equal(ids[j]);
+
+        trans.end();
 
         if (doneCount === 10) return done();
       });
 
       for (var i = 0; i < 10; i += 1) {
         process.nextTick(spamTransaction.bind(this, i));
+      }
+    });
+
+    it("should push transactions through setTimeout", function (done) {
+      expect(agent.getTransaction()).equal(undefined);
+
+      var synchronizer = new EventEmitter()
+        , transactions = []
+        , ids          = []
+        ;
+
+      var spamTransaction = function (i) {
+        var current = transaction.create(agent);
+        transactions[i] = current;
+        ids[i] = current.id;
+
+        setTimeout(function () {
+          var lookup = agent.getTransaction();
+          expect(lookup).equal(current);
+
+          synchronizer.emit('inner', lookup, i);
+        }, 1);
+      };
+
+      var doneCount = 0;
+      synchronizer.on('inner', function (trans, j) {
+        doneCount += 1;
+        expect(trans).equal(transactions[j]);
+        expect(trans.id).equal(ids[j]);
+
+        trans.end();
+
+        if (doneCount === 10) return done();
+      });
+
+      for (var i = 0; i < 10; i += 1) {
+        // You know what this test needs? Some non-determinism!
+        var timeout = Math.floor(Math.random() * 20);
+        setTimeout(spamTransaction.bind(this, i), timeout);
+      }
+    });
+
+    it("should push transactions through EventEmitters", function (done) {
+      expect(agent.getTransaction()).equal(undefined);
+
+      var eventer      = new EventEmitter()
+        , transactions = []
+        , ids          = []
+        ;
+
+      var eventTransaction = function (j) {
+        var current = transaction.create(agent)
+          , id      = current.id
+          , name    = ('ttest' + (j + 1))
+          ;
+
+        transactions[j] = current;
+        ids[j]          = id;
+
+        eventer.on(name, function () {
+          var lookup = agent.getTransaction();
+          expect(lookup).equal(current);
+          expect(lookup.id).equal(id);
+
+          eventer.emit('inner', lookup, j);
+        });
+
+        eventer.emit(name);
+      };
+
+      var doneCount = 0;
+      eventer.on('inner', function (trans, j) {
+        doneCount += 1;
+        expect(trans).equal(transactions[j]);
+        expect(trans.id).equal(ids[j]);
+
+        trans.end();
+
+        if (doneCount === 10) return done();
+      });
+
+      for (var i = 0; i < 10; i += 1) {
+        eventTransaction(i);
+      }
+    });
+
+    it("should handle whatever ridiculous nonsense you throw at it", function (done) {
+      expect(agent.getTransaction()).equal(undefined);
+
+      var synchronizer = new EventEmitter()
+        , eventer      = new EventEmitter()
+        , transactions = []
+        , ids = []
+        , doneCount = 0
+        ;
+
+      var verifier = function (i, phase, passed) {
+        var lookup = agent.getTransaction();
+        console.log(i + ' ' + phase + ' ' +
+                    (lookup ? lookup.id : 'missing') + ' ' +
+                    (passed ? passed.id : 'missing'));
+        expect(lookup).equal(passed);
+        expect(lookup).equal(transactions[i]);
+        expect(lookup.id).equal(ids[i]);
+      };
+
+      eventer.on('rntest', function(trans, j) {
+        verifier(j, 'eventer', trans);
+        synchronizer.emit('inner', trans, j);
+      });
+
+      var createTimer = function (trans, j) {
+        return function () {
+          verifier(j, 'createTimer', trans);
+          eventer.emit('rntest', trans, j);
+        };
+      };
+
+      var createTicker = function (j) {
+        return function () {
+          var current = transaction.create(agent);
+          transactions[j] = current;
+          ids[j] = current.id;
+
+          verifier(j, 'createTicker', current);
+
+          process.nextTick(function () {
+            verifier(j, 'nextTick', current);
+            setTimeout(createTimer(current, j), 0);
+          });
+        };
+      };
+
+      synchronizer.on('inner', function (trans, j) {
+        verifier(j, 'synchronizer', trans);
+        doneCount += 1;
+        expect(trans).equal(transactions[j]);
+        expect(trans.id).equal(ids[j]);
+
+        trans.end();
+
+        if (doneCount === 10) return done();
+      });
+
+      for (var i = 0; i < 10; i++) {
+        process.nextTick(createTicker(i));
       }
     });
   });
