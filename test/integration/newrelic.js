56ffdc91106186c24843be15a27dbf51c1e4a0cd
Forrest L Norvell <ogd@aoaioxxysz.net>
2012-09-21 00:32:05 -0700

    Integration tests need their own copy of the stub config.

---
+++ test/integration/newrelic.js
@@ -0,0 +1,8 @@
+exports.config = {
+  app_name           : ['My Application'],
+  license_key        : 'license key here',
+  log_level          : 'verbose',
+  transaction_tracer : {
+    enabled : true
+  }
+};
