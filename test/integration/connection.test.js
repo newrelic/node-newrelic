6a8ad4d636dd8ab70e3a9df2f13e56b399b238cb
Forrest L Norvell <forrest@newrelic.com>
2012-09-20 21:31:25 -0700

    WIP: rewriting the integration tests to use tap instead of mocha.
    This is done for two reasons: most importantly, node-tap runs each test
    file as its own child process, ensuring that any leftover monkeypatching
    is cleared up between tests using different sets of instrumentation (the
    motivating reason for the change -- Express and Restify both modify
    HTTPResponse objects, in incompatible ways). Also, tap has a much
    simpler DSL, which makes more explicit what's under test for more
    complicated scenarios. Plus it opens the possibility of running the test
    files concurrently in the future, speeding up integration testing
    considerably.

--- test/integration/connection.test.js
+++ test/integration/connection.test.js
@@ -1,57 +1,51 @@
 'use strict';
 
-var path                = require('path')
-  , chai                = require('chai')
-  , expect              = chai.expect
-  , logger              = require(path.join(__dirname, '..', '..', 'lib', 'logger'))
-  , config              = require(path.join(__dirname, '..', '..', 'lib', 'config'))
-  , Agent               = require(path.join(__dirname, '..', '..', 'lib', 'agent'))
+var path         = require('path')
+  , tap          = require('tap')
+  , test         = tap.test
+  , logger       = require(path.join(__dirname, '..', '..', 'lib', 'logger'))
+  , configurator = require(path.join(__dirname, '..', '..', 'lib', 'config'))
+  , Agent        = require(path.join(__dirname, '..', '..', 'lib', 'agent'))
   ;
 
-function generateSubmissionURL(protocolVersion, key, method, runId) {
-  return '/agent_listener/invoke_raw_method' +
-    '?marshal_format=json' +
-    '&protocol_version=' + protocolVersion +
-    '&license_key=' + key +
-    '&method=' + method +
-    '&agent_run_id=' + runId;
-}
+test("CollectorConnection should connect to staging-collector.newrelic.com",
+     {timeout : 5 * 1000},
+     function (t) {
+  t.plan(3);
 
-describe("CollectorConnection", function () {
-  var agent
-    , testLicense   = 'd67afc830dab717fd163bfcb0b8b88423e9a1a3b'
+  var testLicense   = 'd67afc830dab717fd163bfcb0b8b88423e9a1a3b'
     , collectorHost = 'staging-collector.newrelic.com'
     ;
 
-  describe("connecting to staging-collector.newrelic.com", function () {
-    before(function () {
-      this.timeout(10000);
+  var config = configurator.initialize(logger, {
+    'config' : {
+      'app_name'    : 'node.js Tests',
+      'license_key' : testLicense,
+      'host'        : collectorHost,
+      'port'        : 80
+    }
+  });
 
-      agent = new Agent();
-      agent.config = config.initialize(logger, {
-        'config' : {
-          'app_name'    : 'node.js Tests',
-          'license_key' : testLicense,
-          'host'        : collectorHost,
-          'port'        : 80
-        }
-      });
-      agent.applicationPort = 6666;
-    });
+  var agent = new Agent({config : config});
 
-    it("should establish a connection", function (done) {
-      agent.on('connect', function () {
-        expect(agent.connection.applicationName).deep.equal(['node.js Tests']);
+  /*
+   * Immediately set the applicationPort (don't do this in production code)
+   * to get the connect handler to fire immediately, instead of waiting for
+   * 15 seconds.
+   */
+  agent.applicationPort = 6666;
 
-        agent.connection.on('connect', function () {
-          expect(agent.connection.agentRunId).not.equal(undefined);
-          agent.stop();
+  agent.on('connect', function () {
+    t.ok(agent.connection, "agent connection initialized");
+    t.deepEquals(agent.connection.applicationName, ['node.js Tests'], "application name is set");
 
-          return done();
-        });
-      });
+    agent.connection.on('connect', function () {
+      t.ok(agent.connection.agentRunId, "agent run ID is set after connect");
+      agent.stop();
 
-      agent.start();
+      t.end();
     });
   });
+
+  agent.start();
 });
