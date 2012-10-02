37a482e8ba0a512dcca3f09f49c8b1e9ce77e41c
Forrest L Norvell <forrest@newrelic.com>
2012-10-01 18:29:58 -0700

    Yuck. Don't return a partially initialized agent.
    Definitely don't allow instrumentation to be bootstrapped on error --
    modified the initialization code to log the error along with some
    boilerplate text to error to ensure the user knows that something weird
    happened on startup, but allows their application to start up
    regardless.

--- index.js
+++ index.js
@@ -5,22 +5,28 @@ var path    = require('path')
   , Agent   = require(path.join(__dirname, 'lib', 'agent'))
   ;
 
-var agent = new Agent();
+try {
+  if (process.version && process.version.split('.')[1] < 6) {
+    return console.error('New Relic requires a version of Node equal to or greater than 0.6');
+  }
 
-if (process.version && process.version.split('.')[1] < 6) {
-  return console.error('New Relic requires a version of Node equal to or greater than 0.6');
+  var agent = new Agent();
+
+  /**
+   * Don't set up the rest of the agent if it didn't successfully load its
+   * configuration.
+   */
+  if (agent.config) {
+    // set up all of the instrumentation
+    shimmer.patchModule(agent);
+    shimmer.bootstrapInstrumentation(agent);
+
+    agent.start();
+  }
+
+  module.exports = agent;
 }
-
-/**
- * Don't set up the rest of the agent if it didn't successfully load its
- * configuration.
- */
-if (agent.config) {
-  // set up all of the instrumentation
-  shimmer.patchModule(agent);
-  shimmer.bootstrapInstrumentation(agent);
-
-  agent.start();
+catch (error) {
+  console.error("The New Relic Node.js agent was unable to start due to an error:");
+  console.error(error.stack);
 }
-
-module.exports = agent;
