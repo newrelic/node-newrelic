327b8e9ea387be11cc80caebb63b94133ec50030
Forrest L Norvell <forrest@newrelic.com>
2012-10-15 18:07:36 -0700

    Started working on Cursor-based tests.

--- test/integration/instrumentation-mongodb.tap.js
+++ test/integration/instrumentation-mongodb.tap.js
@@ -7,10 +7,10 @@ var path    = require('path')
   , helper  = require(path.join(__dirname, '..', 'lib', 'agent_helper'))
   ;
 
-test("MongoDB instrumentation should find the MongoDB call in the transaction trace",
+test("MongoDB instrumentation should put DB calls in the transaction trace",
      {timeout : 5000},
      function (t) {
-  t.plan(14);
+  t.plan(2);
 
   var self = this;
   helper.bootstrapMongoDB(function (error, app) {
@@ -33,46 +33,76 @@ test("MongoDB instrumentation should find the MongoDB call in the transaction tr
       });
     });
 
-    db.open(function (error, db) {
-      if (error) return t.fail(error);
+    t.test("with a callback", function (t) {
+      t.plan(14);
 
-      db.createCollection('test', {safe : true}, function (error, collection) {
+      db.open(function (error, db) {
         if (error) return t.fail(error);
-        t.notOk(agent.getTransaction(), "no transaction should be in play yet.");
 
-        helper.runInTransaction(agent, function transactionInScope() {
-          var transaction = agent.getTransaction();
-          t.ok(transaction, "transaction should be visible.");
+        db.createCollection('test', {safe : true}, function (error, collection) {
+          if (error) return t.fail(error);
+          t.notOk(agent.getTransaction(), "no transaction should be in play yet");
 
-          var hunx = {id : 1, hamchunx : "verbloks"};
-          collection.insert(hunx, function insertCallback(error, result) {
-            if (error) return t.fail(error);
+          helper.runInTransaction(agent, function transactionInScope() {
+            var transaction = agent.getTransaction();
+            t.ok(transaction, "transaction should be visible");
 
-            t.ok(agent.getTransaction(), "transaction should still be visible.");
-
-            collection.findOne({id : 1}, function findOneCallback(error, item) {
+            var hunx = {id : 1, hamchunx : "verbloks"};
+            collection.insert(hunx, function insertCallback(error, result) {
               if (error) return t.fail(error);
 
-              t.ok(agent.getTransaction(), "transaction should still still be visible.");
+              t.ok(agent.getTransaction(), "transaction should still be visible");
 
-              t.deepEquals(item, hunx, "MongoDB should still work.");
+              collection.findOne({id : 1}, function findOneCallback(error, item) {
+                if (error) return t.fail(error);
 
-              transaction.end();
+                t.ok(agent.getTransaction(), "transaction should still still be visible");
 
-              var trace = transaction.getTrace();
-              t.ok(trace, "trace should exist.");
-              t.ok(trace.root, "root element should exist.");
-              t.equals(trace.root.children.length, 1, "There should be only one child.");
+                t.deepEquals(item, hunx, "MongoDB should still work.");
 
-              var insertSegment = trace.root.children[0];
-              t.ok(insertSegment, "trace segment for insert should exist");
-              t.equals(insertSegment.name, "MongoDB/test/insert", "should register the insert");
-              t.equals(insertSegment.children.length, 1, "insert should have a child");
+                transaction.end();
 
-              var findSegment = insertSegment.children[0];
-              t.ok(findSegment, "trace segment for find should exist");
-              t.equals(findSegment.name, "MongoDB/test/find", "should register the find");
-              t.equals(findSegment.children.length, 0, "find should leave us here at the end");
+                var trace = transaction.getTrace();
+                t.ok(trace, "trace should exist.");
+                t.ok(trace.root, "root element should exist.");
+                t.equals(trace.root.children.length, 1, "There should be only one child.");
+
+                var insertSegment = trace.root.children[0];
+                t.ok(insertSegment, "trace segment for insert should exist");
+                t.equals(insertSegment.name, "MongoDB/test/insert", "should register the insert");
+                t.equals(insertSegment.children.length, 1, "insert should have a child");
+
+                var findSegment = insertSegment.children[0];
+                t.ok(findSegment, "trace segment for find should exist");
+                t.equals(findSegment.name, "MongoDB/test/find", "should register the find");
+                t.equals(findSegment.children.length, 0, "find should leave us here at the end");
+
+                db.close(function (error, result) {
+                  if (error) t.fail(error);
+
+                  t.end();
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+
+    t.test("with a Cursor", function (t) {
+      t.plan(1);
+
+      db.open(function (error, db) {
+        if (error) return t.fail(error);
+
+        db.createCollection('test2', function (error, collection) {
+          if (error) return t.fail(error);
+
+          helper.runInTransaction(agent, function transactionInScope() {
+            var hunx = {id : 1, hamchunx : "verbloks"};
+            var insertCursor = collection.insert(hunx, function (error, result) {
+              var cursor = collection.find({id : 1});
+              t.ok(cursor, "cursor should be returned by callback-less find");
 
               t.end();
             });
