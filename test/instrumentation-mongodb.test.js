355a35afd47c87b637c69469441a6f87cf982811
Forrest L Norvell <forrest@newrelic.com>
2012-11-17 23:02:34 -0800

    Figured out how to mock MongoDB driver.

--- test/instrumentation-mongodb.test.js
+++ test/instrumentation-mongodb.test.js
@@ -12,14 +12,20 @@ var path    = require('path')
 describe("agent instrumentation of MongoDB", function () {
   var agent
     , mongodb
+    , db
+    , pkfactory
     ;
 
   beforeEach(function () {
     agent = helper.loadMockedAgent();
     shimmer.bootstrapInstrumentation(agent);
 
-    // load mongodb-native
+    // load the driver after loading is patched
     mongodb = require('mongodb');
+
+    var serverConfig = new mongodb.Server('localhost', 27017);
+    db = new mongodb.Db('notreal', serverConfig, {safe : true});
+    pkfactory = mongodb.ObjectID;
   });
 
   afterEach(function () {
@@ -27,37 +33,53 @@ describe("agent instrumentation of MongoDB", function () {
   });
 
   describe("for each operation", function () {
-    it("should update the global database aggregate statistics", function (done) {
-      // mock setup
-      var pkfactory    = require('bson').BSONPure.ObjectID
-        , serverConfig = new mongodb.Server('localhost', 27017)
-        , db           = new mongodb.Db('notreal', serverConfig, {safe : true})
-        , collection   = new mongodb.Collection(db, 'fake', pkfactory)
-        ;
-
-      serverConfig._serverState = 'connected';
-      sinon.stub(serverConfig, 'checkoutWriter', function () {
-        return {write : function () {}};
+    beforeEach(function (done) {
+      sinon.stub(db, '_executeInsertCommand', function (inserter, options, callback) {
+        callback(null, {documents : [{}]});
       });
 
       helper.runInTransaction(agent, function () {
-        collection.insert({id : 1, hamchunx : 'verbloks'}, function (error, result) {
+        var collection = new mongodb.Collection(db, 'fake', pkfactory);
+        collection.insert({id : 1, hamchunx : 'verbloks'},
+                          {safe : true},
+                          function (error, result) {
           if (error) return done(error);
 
           var transaction = agent.getTransaction();
           should.exist(transaction);
 
-          mongodb.Connection.prototype.write.restore();
-          return done();
+          agent.once('transactionFinished', function () {
+            return done();
+          });
+          transaction.end();
         });
-        db._callBackStore.emit('1', null, 'ham');
       });
     });
 
-    it("should also update the global web aggregate statistics");
-    it("should update the aggregate statistics for the operation type");
-    it("should update the aggregate statistics for the specific query");
-    it("should update the scoped aggregate statistics for the operation type");
+    it("should update the global database aggregate statistics", function () {
+      var stats = agent.metrics.getMetric('Database/all').stats;
+      expect(stats.callCount).equal(1);
+    });
+
+    it("should update the aggregate statistics for the operation type", function () {
+      var stats = agent.metrics.getMetric('Database/insert').stats;
+      expect(stats.callCount).equal(1);
+    });
+
+    it("should update the aggregate statistics for the specific query", function () {
+      var stats = agent.metrics.getMetric('Database/fake/insert').stats;
+      expect(stats.callCount).equal(1);
+    });
+
+    it("should update the scoped aggregate statistics for the operation type", function () {
+      var stats = agent.metrics.getMetric('Database/insert').stats;
+      expect(stats.callCount).equal(1);
+    });
+
+    it("should update the scoped aggregate statistics for the operation type", function () {
+      var stats = agent.metrics.getMetric('Database/fake/insert', 'MongoDB/fake/insert').stats;
+      expect(stats.callCount).equal(1);
+    });
   });
 
   it("should instrument inserting documents");
