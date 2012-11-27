454c8bba3981fd2a187bb2897dfce226168e375b
Forrest L Norvell <forrest@newrelic.com>
2012-11-27 15:57:18 -0800

    Documented configuration variables.

--- newrelic.js
+++ newrelic.js
@@ -1,8 +1,22 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
 exports.config = {
-  app_name           : ['My Application'],
-  license_key        : 'license key here',
-  log_level          : 'trace',
-  transaction_tracer : {
-    enabled : true
-  }
+  /**
+   * Array of application names.
+   */
+  app_name : ['My Application'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'license key here',
+  /**
+   * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+   * issues with the agent, 'info' and higher will impose the least overhead on
+   * production applications.
+   */
+  log_level : 'debug'
 };
