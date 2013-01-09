795e88e1baefa3e4e49e4e561c0b9bdab1fb09ab
Forrest L Norvell <forrest@newrelic.com>
2013-01-09 01:23:45 -0800

    More or less rewrote CollectorConnection to be simpler.
    The old approach used a tough-to-understand mixture of normal,
    callback-based Node programming and semi-dynamic overrides via
    passed-in callbacks and errorbacks. The new approach is almost
    entirely evented, which has the bonus side effect of making the
    connection far more observable -- see the additional tests to
    the connection integration test case to see how observable it is
    now. Every step in the connection's life cycle now has an
    associated event.
    
    A lot of work was put into improving the logging, as well.

--- test/integration/connection.tap.js
+++ test/integration/connection.tap.js
@@ -11,22 +11,94 @@ var path         = require('path')
 test("CollectorConnection should connect to staging-collector.newrelic.com",
      {timeout : 5 * 1000},
      function (t) {
-  t.plan(3);
-
-  var testLicense   = 'd67afc830dab717fd163bfcb0b8b88423e9a1a3b'
-    , collectorHost = 'staging-collector.newrelic.com'
+  var config = configurator.initialize(logger, {
+        'config' : {
+          'app_name'    : 'node.js Tests',
+          'license_key' : 'd67afc830dab717fd163bfcb0b8b88423e9a1a3b',
+          'host'        : 'staging-collector.newrelic.com',
+          'port'        : 80
+        }
+      })
+    , agent = new Agent({config : config})
     ;
 
-  var config = configurator.initialize(logger, {
-    'config' : {
-      'app_name'    : 'node.js Tests',
-      'license_key' : testLicense,
-      'host'        : collectorHost,
-      'port'        : 80
-    }
-  });
+  agent.on('connect', function () {
+    var connection = agent.connection
+      , config     = agent.config
+      , connectResponseCalled
+      , handshakeResponseCalled
+      , configUpdated
+      , connected
+      , ended
+      , shutDown
+      , closed
+      ;
 
-  var agent = new Agent({config : config});
+    t.ok(connection, "agent connection initialized");
+    t.ok(agent.id, "agent's ID is known");
+
+    connection.on('connectResponse', function (host) {
+      t.ok(host, "got a redirect host");
+      t.notOk(connectResponseCalled, "connectResponse fired, but only once");
+      connectResponseCalled = true;
+    });
+
+    connection.on('handshakeResponse', function (response) {
+      t.ok(connectResponseCalled, "already got redirect host at handshake time");
+      t.ok(response, "got a response hash from the server");
+      t.notOk(handshakeResponseCalled, "handshakeResponse fired, but only once");
+      handshakeResponseCalled = true;
+    });
+
+    config.on('change', function (changed) {
+      t.ok(connectResponseCalled, "already got redirect host at config time");
+      t.ok(handshakeResponseCalled, "already performed handshake at config time");
+      t.ok(changed, "configuration is present");
+      t.notOk(configUpdated, "configuration updated, but only once");
+      configUpdated = true;
+    });
+
+    connection.on('end', function () {
+      t.ok(connectResponseCalled, "already got redirect host at connection end time");
+      t.ok(handshakeResponseCalled, "already performed handshake at connection end time");
+      t.ok(connected, "already connected at connection end time");
+      t.notOk(ended, "connection ended, but only once");
+      ended = true;
+    });
+
+    connection.on('shutdown', function () {
+      t.ok(connectResponseCalled, "already got redirect host at shutdown time");
+      t.ok(handshakeResponseCalled, "already performed handshake at shutdown time");
+      t.ok(connected, "already connected at shutdown time");
+      t.ok(ended, "already ended at shutdown time");
+      t.notOk(shutDown, "shutdown happened, but only once");
+      shutDown = true;
+    });
+
+    connection.on('close', function () {
+      t.ok(connectResponseCalled, "already got redirect host at close time");
+      t.ok(handshakeResponseCalled, "already performed handshake at close time");
+      t.ok(connected, "already connected at close time");
+      t.ok(ended, "already ended at close time");
+      t.ok(shutDown, "shutdown happened at close time");
+      t.notOk(connection.agentRunId, "agent run ID is cleared after close");
+      t.notOk(closed, "connection closed only once");
+      closed = true;
+
+      t.end();
+    });
+
+    connection.on('connect', function (response) {
+      t.ok(connectResponseCalled, "already got redirect host when connected");
+      t.ok(handshakeResponseCalled, "already fetched agent configuration when connected");
+      t.ok(configUpdated, "configuration updated at connect time");
+      t.ok(response, "agent configuration made it through to the connect event");
+      t.ok(connection.agentRunId, "agent run ID is set after connect");
+
+      connected = true;
+      agent.stop();
+    });
+  });
 
   /*
    * Immediately set the applicationPort (don't do this in production code)
@@ -34,18 +106,5 @@ test("CollectorConnection should connect to staging-collector.newrelic.com",
    * 15 seconds.
    */
   agent.applicationPort = 6666;
-
-  agent.on('connect', function () {
-    t.ok(agent.connection, "agent connection initialized");
-    t.deepEquals(agent.connection.applicationName, ['node.js Tests'], "application name is set");
-
-    agent.connection.on('connect', function () {
-      t.ok(agent.connection.agentRunId, "agent run ID is set after connect");
-      agent.stop();
-
-      t.end();
-    });
-  });
-
   agent.start();
 });
