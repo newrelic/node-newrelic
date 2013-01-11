964bb71aa5e5e962c9ca9c894a555ea3bb099d93
Forrest L Norvell <forrest@newrelic.com>
2013-01-10 17:01:51 -0800

    Ensure fs instrumentation bootstrapping is safe.

--- test/instrumentation-fs.test.js
+++ test/instrumentation-fs.test.js
@@ -1,5 +1,31 @@
 'use strict';
 
+var path         = require('path')
+  , chai         = require('chai')
+  , expect       = chai.expect
+  , helper       = require(path.join(__dirname, 'lib', 'agent_helper'))
+  ;
+
 describe("built-in fs module instrumentation", function () {
+  describe("shouldn't cause bootstrapping to fail", function () {
+    var agent
+      , initialize
+      ;
+
+    before(function () {
+      agent = helper.loadMockedAgent();
+      initialize = require(path.join(__dirname, '..', 'lib',
+                                     'instrumentation', 'core', 'fs'));
+    });
+
+    it("when passed no module", function () {
+      expect(function () { initialize(agent); }).not.throws();
+    });
+
+    it("when passed a module with no RedisClient present.", function () {
+      expect(function () { initialize(agent, {}); }).not.throws();
+    });
+  });
+
   it("should pick up scope when called in a scoped transaction");
 });
