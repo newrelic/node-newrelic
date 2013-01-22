624a0879edcf0c1a5f5f3d2b2cf02bcdca0e75d1
Forrest L Norvell <forrest@newrelic.com>
2013-01-22 14:35:35 -0800

    WIP: Versioned Express test refactor, added error-catcher tests.
    This is the very rare occasion on which I commit failing tests, because
    the scope of the changes is getting pretty huge, and I wanted to get
    this refactor in before I started pulling Transaction and other tracer
    classes in to get the tests passing.
    
    A byproduct of pushing the Express and Connect tests into the versioned
    tests is that the core modulee no longer has dev dependencies on Express
    or Connect -- the examples will need updates to their package.json
    files, but that can come later.
    
    Also includes some documentation cleanup in shimmer and a new, dangerous
    helper method.

--- test/integration/versioned/express3-ejs/express3-render-ejs.tap.js
+++ test/integration/versioned/express3-ejs/express3-render-ejs.tap.js
@@ -1,117 +1,209 @@
 'use strict';
 
+// shut up, Express
+process.env.NODE_ENV = 'test';
+
 var path    = require('path')
   , test    = require('tap').test
   , helper  = require(path.join(__dirname, '..', '..', '..', 'lib', 'agent_helper'))
   , request = require('request')
   ;
 
-var BODY = "<!DOCTYPE html>\n" +
-           "<html>\n" +
-           "<head>\n" +
-           "  <title>yo dawg</title>\n" +
-           "</head>\n" +
-           "<body>\n" +
-           "  <p>I heard u like HTML.</p>\n" +
-           "</body>\n" +
-           "</html>\n";
+var TEST_PATH = '/test'
+  , TEST_PORT = 9876
+  , TEST_HOST = 'localhost'
+  , TEST_URL  = 'http://' + TEST_HOST + ':' + TEST_PORT + TEST_PATH
+  , DELAY     = 600
+  , BODY      = "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "  <title>yo dawg</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "  <p>I heard u like HTML.</p>\n" +
+                "</body>\n" +
+                "</html>\n"
+  ;
 
-test("Express 3 with EJS templates",
-     {timeout : 2000},
-     function (t) {
-  t.plan(3);
+test("agent instrumentation of Express 3", function (t) {
+  t.plan(4);
 
-  var agent = helper.instrumentMockedAgent();
+  t.test("for a normal request", {timeout : 1000}, function (t) {
+    var agent = helper.instrumentMockedAgent()
+      , app = require('express')()
+      , server = require('http').createServer(app)
+      ;
 
-  var express = require('express')
-    , app = express()
-    ;
+    this.tearDown(function () {
+      server.close();
+      helper.unloadAgent(agent);
+    });
 
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
+    // set apdexT so apdex stats will be recorded
+    agent.apdexT = 1;
 
-  app.get('/', function (req, res) {
-    res.render('index', { title: 'yo dawg' });
+    app.get(TEST_PATH, function (req, res) {
+      res.send({yep : true});
+    });
+
+    server.listen(TEST_PORT, TEST_HOST, function () {
+      request.get(TEST_URL, function (error, response, body) {
+        if (error) t.fail(error);
+
+        var contentType = 'application/json; charset=utf-8';
+        t.equal(response.headers['content-type'], contentType,
+                "got correct content type");
+        t.deepEqual(JSON.parse(body), {"yep":true}, "Express correctly serves.");
+
+        var stats;
+
+        stats = agent.metrics.getMetric('WebTransaction/Uri/test').stats;
+        t.ok(stats, "found unscoped stats for request path");
+        t.equal(stats.callCount, 1, "/test was only requested once");
+
+        stats = agent.metrics.getOrCreateApdexMetric('Apdex/Uri/test').stats;
+        t.ok(stats, "found apdex stats for request path");
+        t.equal(stats.satisfying, 1, "got satisfactory response time");
+        t.equal(stats.tolerating, 0, "got no tolerable requests");
+        t.equal(stats.frustrating, 0, "got no frustrating requests");
+
+        stats = agent.metrics.getMetric('WebTransaction').stats;
+        t.ok(stats, "found roll-up statistics for web requests");
+        t.equal(stats.callCount, 1, "only one web request was made");
+
+        stats = agent.metrics.getMetric('HttpDispatcher').stats;
+        t.ok(stats, "found HTTP dispatcher statistics");
+        t.equal(stats.callCount, 1, "only one HTTP-dispatched request was made");
+
+        var serialized = JSON.stringify(agent.metrics);
+        t.ok(serialized.match(/WebTransaction\/Uri\/test/),
+             "serialized metrics as expected");
+
+        t.end();
+      });
+    });
   });
 
-  var server = require('http').createServer(app);
-  server.listen(8765);
+  t.test("using EJS templates",
+       {timeout : 1000},
+       function (t) {
+    var agent  = helper.instrumentMockedAgent()
+      , app    = require('express')()
+      , server = require('http').createServer(app)
+      ;
 
-  this.tearDown(function () {
-    server.close();
-    helper.unloadAgent(agent);
-  });
+    this.tearDown(function () {
+      server.close();
+      helper.unloadAgent(agent);
+    });
 
-  agent.once('transactionFinished', function () {
-    var stats = agent.metrics.getMetric('View/index/Rendering').stats;
-    t.equal(stats.callCount, 1, "should note the view rendering");
-  });
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
 
-  request('http://localhost:8765/', function (error, response, body) {
-    if (error) t.fail(error);
+    app.get(TEST_PATH, function (req, res) {
+      res.render('index', { title: 'yo dawg' });
+    });
 
-    t.equal(response.statusCode, 200, "response code should be 200");
-    t.equal(body, BODY, "template should still render fine");
+    server.listen(TEST_PORT, TEST_HOST);
 
-    t.end();
-  });
-});
+    agent.once('transactionFinished', function () {
+      var stats = agent.metrics.getMetric('View/index/Rendering').stats;
+      t.equal(stats.callCount, 1, "should note the view rendering");
+    });
 
-test("agent instrumentation of Express should measure request duration properly (NA-46)",
-     {timeout : 2 * 1000},
-     function (t) {
-  var TEST_PATH = '/test'
-    , TEST_PORT = 9876
-    , TEST_HOST = 'localhost'
-    , TEST_URL  = 'http://' + TEST_HOST + ':' + TEST_PORT + TEST_PATH
-    , DELAY     = 600
-    , PAGE      = '<html>' +
-                  '<head><title>test response</title></head>' +
-                  '<body><p>I heard you like HTML.</p></body>' +
-                  '</html>'
-    ;
-
-  var agent  = helper.instrumentMockedAgent()
-    , app    = require('express')()
-    , server = require('http').createServer(app)
-    ;
-
-  this.tearDown(function () {
-    server.close();
-    helper.unloadAgent(agent);
-  });
-
-  app.get(TEST_PATH, function (request, response) {
-    t.ok(agent.getTransaction(),
-         "the transaction should be visible inside the Express handler");
-    setTimeout(function () { response.send(PAGE); }, DELAY);
-  });
-
-  server.listen(TEST_PORT, TEST_HOST, function ready() {
-    request.get(TEST_URL, function (error, response, body) {
+    request(TEST_URL, function (error, response, body) {
       if (error) t.fail(error);
 
-      t.ok(agent.environment.toJSON().some(function (pair) {
-             return pair[0] === 'Dispatcher' && pair[1] === 'express';
-           }),
-           "should indicate that the Express dispatcher is in play");
-
-      t.ok(agent.environment.toJSON().some(function (pair) {
-             return pair[0] === 'Framework' && pair[1] === 'express';
-           }),
-           "should indicate that Express itself is in play");
-
-      t.notOk(agent.getTransaction(), "transaction shouldn't be visible from request");
-      t.equals(body, PAGE, "response and original page text match");
-
-      var stats = agent.metrics.getMetric('WebTransaction/Uri/test').stats;
-      t.ok(stats, "Statistics should have been found for request.");
-
-      var timing = stats.total * 1000;
-      t.ok(timing > DELAY - 50,
-           "given some setTimeout slop, the request was long enough");
+      t.equal(response.statusCode, 200, "response code should be 200");
+      t.equal(body, BODY, "template should still render fine");
 
       t.end();
+    });
+  });
+
+  t.test("should trap errors correctly", function (t) {
+    var agent  = helper.instrumentMockedAgent()
+      , app    = require('express')()
+      , server = require('http').createServer(app)
+      ;
+
+    this.tearDown(function () {
+      server.close();
+      helper.unloadAgent(agent);
+    });
+
+    app.get(TEST_PATH, function () {
+      var hmm;
+      hmm.ohno.failure.is.terrible();
+    });
+
+    server.listen(TEST_PORT, TEST_HOST, function () {
+      request.get(TEST_URL, function (error, response, body) {
+        if (error) t.fail(error);
+
+        t.ok(response, "got a response from Express");
+        t.ok(body, "got back a body");
+
+        var errors = agent.errors.errors;
+        t.ok(errors, "errors were found");
+        t.equal(errors.length, 1, "Only one error thrown.");
+
+        var first = errors[0];
+        t.ok(first, "have the first error");
+
+        var matcher = /TypeError: Cannot read property 'ohno' of undefined/;
+        t.ok(first[2].match(matcher), "got the expected error");
+
+        t.end();
+      });
+    });
+  });
+
+  t.test("should measure request duration properly (NA-46)",
+       {timeout : 2 * 1000},
+       function (t) {
+    var agent  = helper.instrumentMockedAgent()
+      , app    = require('express')()
+      , server = require('http').createServer(app)
+      ;
+
+    this.tearDown(function () {
+      server.close();
+      helper.unloadAgent(agent);
+    });
+
+    app.get(TEST_PATH, function (request, response) {
+      t.ok(agent.getTransaction(),
+           "the transaction should be visible inside the Express handler");
+           setTimeout(function () { response.send(BODY); }, DELAY);
+    });
+
+    server.listen(TEST_PORT, TEST_HOST, function ready() {
+      request.get(TEST_URL, function (error, response, body) {
+        if (error) t.fail(error);
+
+        t.ok(agent.environment.toJSON().some(function (pair) {
+          return pair[0] === 'Dispatcher' && pair[1] === 'express';
+        }),
+        "should indicate that the Express dispatcher is in play");
+
+        t.ok(agent.environment.toJSON().some(function (pair) {
+          return pair[0] === 'Framework' && pair[1] === 'express';
+        }),
+        "should indicate that Express itself is in play");
+
+        t.notOk(agent.getTransaction(), "transaction shouldn't be visible from request");
+        t.equals(body, BODY, "response and original page text match");
+
+        var stats = agent.metrics.getMetric('WebTransaction/Uri/test').stats;
+        t.ok(stats, "Statistics should have been found for request.");
+
+        var timing = stats.total * 1000;
+        t.ok(timing > DELAY - 50,
+             "given some setTimeout slop, the request was long enough");
+
+        t.end();
+      });
     });
   });
 });
