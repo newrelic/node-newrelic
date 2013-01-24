fc7d157aab1a0c4bf39f24def1b198db3c3b8632
Forrest L Norvell <forrest@newrelic.com>
2013-01-23 18:26:58 -0800

    Test the instrumentation contract for Express without Express.

--- test/instrumentation-express.test.js
+++ test/instrumentation-express.test.js
@@ -3,6 +3,7 @@
 var path   = require('path')
   , chai   = require('chai')
   , expect = chai.expect
+  , should = chai.should()
   , helper = require(path.join(__dirname, 'lib', 'agent_helper'))
   ;
 
@@ -25,6 +26,170 @@ describe("an instrumented Express application", function () {
     it("when passed an empty module", function () {
       initialize(agent, {});
       expect(function () { initialize(agent, {}); }).not.throws();
+    });
+  });
+
+  describe("for Express 2 (mocked)", function () {
+    var agent
+      , stub
+      , http
+      ;
+
+    before(function () {
+      agent = helper.instrumentMockedAgent();
+
+      stub = {
+        version : '2.5.3',
+        createServer : function () { return 'server'; }
+      };
+
+      http = require('http');
+      should.not.exist(http.ServerResponse.prototype.render);
+      http.ServerResponse.prototype.render = function (view, options, cb) {
+        process.nextTick(cb);
+        return 'rendered';
+      };
+
+      require(path.join(__dirname, '..', 'lib',
+                        'instrumentation', 'express'))(agent, stub);
+    });
+
+    after(function () {
+      helper.unloadAgent(agent);
+      delete http.ServerResponse.prototype.render;
+    });
+
+    it("should set dispatcher to Express when a new server is created", function () {
+      expect(stub.createServer()).equal('server');
+
+      var dispatchers = agent.environment.get('Dispatcher');
+      expect(dispatchers.length).equal(1);
+      expect(dispatchers[0]).equal('express');
+    });
+
+    it("should set framework to Express when a new server is created", function () {
+      expect(stub.createServer()).equal('server');
+
+      var frameworks = agent.environment.get('Framework');
+      expect(frameworks.length).equal(1);
+      expect(frameworks[0]).equal('express');
+    });
+
+    it("should wrap http.ServerResponse.prototype.render", function (done) {
+      should.exist(http.ServerResponse.prototype.render);
+      helper.runInTransaction(agent, function () {
+        var transaction = agent.getTransaction();
+
+        expect(http.ServerResponse.prototype.render.call(null,
+                                                         'TEST',
+                                                         {},
+                                                         function () {
+          process.nextTick(function () {
+            var json = transaction.getTrace().root.toJSON();
+            expect(json[4][0][2]).equal('View/TEST/Rendering');
+
+            return done();
+          });
+        })).equal('rendered');
+      });
+    });
+
+    it("should wrap http.ServerResponse.prototype.render with missing options",
+       function (done) {
+      should.exist(http.ServerResponse.prototype.render);
+      helper.runInTransaction(agent, function () {
+        var transaction = agent.getTransaction();
+
+        expect(http.ServerResponse.prototype.render.call(null,
+                                                         'TEST',
+                                                         function () {
+          process.nextTick(function () {
+            var json = transaction.getTrace().root.toJSON();
+            expect(json[4][0][2]).equal('View/TEST/Rendering');
+
+            return done();
+          });
+        })).equal('rendered');
+      });
+    });
+  });
+
+  describe("for Express 3 (mocked)", function () {
+    var agent
+      , stub
+      ;
+
+    before(function () {
+      agent = helper.instrumentMockedAgent();
+
+      stub = {
+        version : '3.1.4',
+        application : {
+          init : function () { return 'server'; }
+        },
+        response : {
+          render : function (view, options, cb) {
+            process.nextTick(cb);
+            return 'rendered';
+          }
+        }
+      };
+
+      require(path.join(__dirname, '..', 'lib',
+                        'instrumentation', 'express'))(agent, stub);
+    });
+
+    after(function () {
+      helper.unloadAgent(agent);
+    });
+
+    it("should set dispatcher to Express when a new app is created", function () {
+      expect(stub.application.init()).equal('server');
+
+      var dispatchers = agent.environment.get('Dispatcher');
+      expect(dispatchers.length).equal(1);
+      expect(dispatchers[0]).equal('express');
+    });
+
+    it("should set framework to Express when a new app is created", function () {
+      expect(stub.application.init()).equal('server');
+
+      var frameworks = agent.environment.get('Framework');
+      expect(frameworks.length).equal(1);
+      expect(frameworks[0]).equal('express');
+    });
+
+    it("should wrap express.response.render", function (done) {
+      should.exist(stub.response.render);
+      helper.runInTransaction(agent, function () {
+        var transaction = agent.getTransaction();
+
+        expect(stub.response.render.call(null, 'TEST', {}, function () {
+          process.nextTick(function () {
+            var json = transaction.getTrace().root.toJSON();
+            expect(json[4][0][2]).equal('View/TEST/Rendering');
+
+            return done();
+          });
+        })).equal('rendered');
+      });
+    });
+
+    it("should wrap express.response.render with a missing options",
+       function (done) {
+      should.exist(stub.response.render);
+      helper.runInTransaction(agent, function () {
+        var transaction = agent.getTransaction();
+
+        expect(stub.response.render.call(null, 'TEST', function () {
+          process.nextTick(function () {
+            var json = transaction.getTrace().root.toJSON();
+            expect(json[4][0][2]).equal('View/TEST/Rendering');
+
+            return done();
+          });
+        })).equal('rendered');
+      });
     });
   });
 });
