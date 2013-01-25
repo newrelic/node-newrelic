f820272839983cb5973bcc470bb75e39d8699bb9
Forrest L Norvell <forrest@newrelic.com>
2013-01-24 18:22:10 -0800

    Correctly end segment in Redis calls without callbacks.
    Fixes newrelic/node-newrelic#4.

--- test/instrumentation-redis.test.js
+++ test/instrumentation-redis.test.js
@@ -109,6 +109,89 @@ describe("agent instrumentation of Redis", function () {
       });
     });
 
+    it("PING without callback", function (done) {
+      mockConnection.expects('write').withExactArgs('*1\r\n$4\r\nping\r\n').once();
+
+      agent.once('transactionFinished', function (transaction) {
+        var stats = transaction.metrics.getMetric('Redis/ping').stats;
+        expect(stats.callCount).equal(1);
+
+        return done();
+      });
+
+      helper.runInTransaction(agent, function () {
+        var transaction = agent.getTransaction();
+        should.exist(transaction);
+
+        client.PING();
+
+        should.exist(connection.on_data);
+        connection.on_data(new Buffer('$21\r\nredis_version:2.6.0\r\n'));
+        connection.on_data(new Buffer('+PONG\r\n'));
+
+        transaction.end();
+      });
+    });
+
+    it("PING with callback in array", function (done) {
+      mockConnection
+        .expects('write')
+        .withExactArgs('*3\r\n$4\r\nping\r\n$1\r\n1\r\n$1\r\n2\r\n')
+        .once();
+
+      agent.once('transactionFinished', function (transaction) {
+        var stats = transaction.metrics.getMetric('Redis/ping').stats;
+        expect(stats.callCount).equal(1);
+
+        return done();
+      });
+
+      helper.runInTransaction(agent, function () {
+        var transaction = agent.getTransaction();
+        should.exist(transaction);
+
+        client.PING(1, 2, function (error, results) {
+          if (error) return done(error);
+
+          should.exist(agent.getTransaction());
+          expect(results, "PING should still work").equal('PONG');
+        });
+
+        should.exist(connection.on_data);
+        connection.on_data(new Buffer('$21\r\nredis_version:2.6.0\r\n'));
+        connection.on_data(new Buffer('+PONG\r\n'));
+
+        transaction.end();
+      });
+    });
+
+    it("PING with no callback in array", function (done) {
+      mockConnection
+        .expects('write')
+        .withExactArgs('*3\r\n$4\r\nping\r\n$1\r\n1\r\n$1\r\n2\r\n')
+        .once();
+
+      agent.once('transactionFinished', function (transaction) {
+        var stats = transaction.metrics.getMetric('Redis/ping').stats;
+        expect(stats.callCount).equal(1);
+
+        return done();
+      });
+
+      helper.runInTransaction(agent, function () {
+        var transaction = agent.getTransaction();
+        should.exist(transaction);
+
+        client.PING(1, 2);
+
+        should.exist(connection.on_data);
+        connection.on_data(new Buffer('$21\r\nredis_version:2.6.0\r\n'));
+        connection.on_data(new Buffer('+PONG\r\n'));
+
+        transaction.end();
+      });
+    });
+
     it("SET");
     it("HSET");
     it("MSET");
