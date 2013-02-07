fea04f8e29c0921457c81f5707a557789a5980e7
Forrest L Norvell <forrest@newrelic.com>
2013-02-07 01:43:05 -0800

    Run tests for each multiverse directory.
    There's probably a way to do this without using yet another control flow
    module, but I was too dumb tonight to find the correct way to do this
    with promises, seeing as how I want to sequence both the installation of
    the various versions of the modules *and* run the tests. The important
    part is that I have a highline summary of how many tests are failing per
    module version.

--- test/lib/multi_installer.js
+++ test/lib/multi_installer.js
@@ -1,21 +1,17 @@
 'use strict';
 
-var path         = require('path')
-  , util         = require('util')
-  , exec         = require('child_process').exec
-  , install      = require(path.join(__dirname, 'install'))
-  , EventEmitter = require('events').EventEmitter
+var path    = require('path')
+  , exec    = require('child_process').exec
+  , async   = require('async')
+  , install = require(path.join(__dirname, 'install'))
   ;
 
 function MultiInstaller(target, prefix) {
   if (!target || !prefix) throw new Error("Missing required parameters!");
 
-  EventEmitter.call(this);
-
   this.target = target;
   this.prefix = prefix;
 }
-util.inherits(MultiInstaller, EventEmitter);
 
 MultiInstaller.prototype.versions = function (callback) {
   exec(
@@ -28,25 +24,19 @@ MultiInstaller.prototype.versions = function (callback) {
   );
 };
 
-MultiInstaller.prototype._each = function(versions, callback) {
-  var version = versions.pop();
-  install(this.target, version, this.prefix, function (error, results) {
-    callback(error, results);
-
-    if (versions.length > 0) {
-      this._each(versions, callback);
-    }
-    else if (this._done) {
-      this.emit('finished');
-    }
-  }.bind(this));
-};
-
-MultiInstaller.prototype.each = function (callback) {
+MultiInstaller.prototype.forEach = function (visitor, callback) {
   this.versions(function (error, versions) {
-    if (error) return console.error(error.stack);
+    if (error) return callback(error);
 
-    this._each(versions, callback);
+    async.forEachSeries(
+      versions.reverse(),
+      function (version, nested) {
+        install(this.target, version, this.prefix, function (error, results) {
+          visitor(error, results, nested);
+        });
+      }.bind(this),
+      callback
+    );
   }.bind(this));
 };
 
