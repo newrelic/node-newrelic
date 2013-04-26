840db317c460dffd35503fcd67649b77132f5245
Adam Rudd <adam.rudd@uqconnect.edu.au>
2013-04-26 14:31:00 +0200

    Wrap init code in profile method

--- index.js
+++ index.js
@@ -1,52 +1,59 @@
 'use strict';
 
-var path    = require('path')
-  , logger  = require(path.join(__dirname, 'lib', 'logger'))
-  , shimmer = require(path.join(__dirname, 'lib', 'shimmer'))
-  , Agent   = require(path.join(__dirname, 'lib', 'agent'))
-  ;
+var path = require('path');
 
-try {
-  logger.debug("Process was running %s seconds before agent was loaded.",
-               process.uptime());
+module.exports.profile = function(opts) {
+  // Configure
+  require(path.join(__dirname, 'lib', 'config')).initialize(null, {config: opts});
 
-  if (process.version && process.version.split('.')[1] < 6) {
-    var message = "The New Relic agent requires a version of Node equal to or " +
-                  "greater than 0.6.0. Not starting!";
-    logger.error(message);
-    console.error(message);
-    return;
+  var logger  = require(path.join(__dirname, 'lib', 'logger'))
+    , shimmer = require(path.join(__dirname, 'lib', 'shimmer'))
+    , Agent   = require(path.join(__dirname, 'lib', 'agent'))
+    ;
+
+  try {
+    logger.debug("Process was running %s seconds before agent was loaded.",
+                process.uptime());
+
+    if (process.version && process.version.split('.')[1] < 6) {
+      var message = "The New Relic agent requires a version of Node equal to or " +
+                    "greater than 0.6.0. Not starting!";
+      logger.error(message);
+      console.error(message);
+      return;
+    }
+
+    logger.debug("Current working directory at agent load is %s.", process.cwd());
+    logger.debug("Process title is %s.", process.title);
+    logger.debug("Application was invoked as %s.", process.argv.join(' '));
+
+    var agent = new Agent();
+    /*
+    * Don't set up the rest of the agent if it didn't successfully load its
+    * configuration.
+    */
+    if (agent.config) {
+      /* In order to ensure all user code is using instrumented versions of
+      * modules, instrumentation must be loaded at startup regardless of whether
+      * or not the agent is enabled in the config. It should be possible for
+      * users to switch the agent on and off at runtime.
+      *
+      * This also requires the agent to be a singleton, or else module loading
+      * will be patched multiple times, with undefined results.
+      */
+      shimmer.patchModule(agent);
+      shimmer.bootstrapInstrumentation(agent);
+
+      agent.start();
+    }
+
+    module.exports = agent;
+  }
+  catch (error) {
+    logger.error(error,
+                "The New Relic Node.js agent was unable to start due to an error:");
+    console.error("The New Relic Node.js agent was unable to start due to an error:");
+    console.error(error.stack);
   }
 
-  logger.debug("Current working directory at agent load is %s.", process.cwd());
-  logger.debug("Process title is %s.", process.title);
-  logger.debug("Application was invoked as %s.", process.argv.join(' '));
-
-  var agent = new Agent();
-  /*
-   * Don't set up the rest of the agent if it didn't successfully load its
-   * configuration.
-   */
-  if (agent.config) {
-    /* In order to ensure all user code is using instrumented versions of
-     * modules, instrumentation must be loaded at startup regardless of whether
-     * or not the agent is enabled in the config. It should be possible for
-     * users to switch the agent on and off at runtime.
-     *
-     * This also requires the agent to be a singleton, or else module loading
-     * will be patched multiple times, with undefined results.
-     */
-    shimmer.patchModule(agent);
-    shimmer.bootstrapInstrumentation(agent);
-
-    agent.start();
-  }
-
-  module.exports = agent;
-}
-catch (error) {
-  logger.error(error,
-               "The New Relic Node.js agent was unable to start due to an error:");
-  console.error("The New Relic Node.js agent was unable to start due to an error:");
-  console.error(error.stack);
-}
+};
