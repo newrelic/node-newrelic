7e3f63bb7f427e2e373c4d6fdb1a0a91d1923c7b
Forrest L Norvell <forrest@newrelic.com>
2013-08-01 15:14:20 -0700

    Updated Express example for Express 3.
    Also added simple JMeter config.

--- examples/express/random-delays/express_random_delays.js
+++ examples/express/random-delays/express_random_delays.js
@@ -2,22 +2,25 @@
 
 require('newrelic');
 
-var express = require('express')
-  , app     = express.createServer()
+var http    = require('http')
+  , express = require('express')
+  , app     = express()
   , logger  = require('../../../lib/logger').child({component : 'express_random_delays'})
   ;
 
 app.use(express.logger('dev'));
 
 app.get('*', function (request, response) {
-  var body = '<html><head><title>yo dawg</title></head><body><p>I heard you like HTML.</p></body></html>';
+  var body = '<html><head><title>yo dawg</title></head>' +
+             '<body><p>I heard you like HTML.</p></body></html>';
   response.writeHead(200, {'Content-Length' : body.length, 'Content-Type' : 'text/html'});
 
   // let's generate some slow transaction traces
   var wait = Math.random() * 4000;
-  if (wait > 2000) logger.trace("waiting " + wait + " milliseconds to return for " + request.url);
+  if (wait > 2000) logger.trace("waiting " + wait +
+                                " milliseconds to return for " + request.url);
 
   setTimeout(function () { response.end(body); }, wait);
 });
 
-app.listen(8088, 'localhost');
+http.createServer(app).listen(8088, 'localhost');
