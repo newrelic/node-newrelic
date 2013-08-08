c9abf14238948829f3667fc189be93d837b97ad4
Forrest L Norvell <forrest@newrelic.com>
2013-08-07 23:19:31 -0700

    instrumentation: set memcached key as a parameter.
    Also, modify the trace segments so that each has a parameters object by
    default.

--- test/integration/instrumentation-memcached.tap.js
+++ test/integration/instrumentation-memcached.tap.js
@@ -9,7 +9,7 @@ var path   = require('path')
 test("memcached instrumentation should find memcached calls in the transaction trace",
      {timeout : 5000},
      function (t) {
-  t.plan(15);
+  t.plan(29);
 
   var self = this;
   helper.bootstrapMemcached(function (error, app) {
@@ -55,16 +55,59 @@ test("memcached instrumentation should find memcached calls in the transaction t
           var setSegment = trace.root.children[0];
           t.ok(setSegment, "trace segment for set should exist");
           t.equals(setSegment.name, "MemCache/set", "should register the set");
+          t.equals(setSegment.parameters.key, "[\"testkey\"]",
+                   "should have the set key as a parameter");
           t.equals(setSegment.children.length, 1,
                    "set should have an only child");
 
           var getSegment = setSegment.children[0];
           t.ok(getSegment, "trace segment for get should exist");
           t.equals(getSegment.name, "MemCache/get", "should register the get");
+          t.equals(getSegment.parameters.key, "[\"testkey\"]",
+                   "should have the get key as a parameter");
           t.equals(getSegment.children.length, 0,
                    "get should leave us here at the end");
+        });
+      });
+    });
 
-          t.end();
+    t.notOk(agent.getTransaction(), "no transaction should be in play");
+
+    helper.runInTransaction(agent, function transactionInScope() {
+      var transaction = agent.getTransaction();
+
+      memcached.set('otherkey', 'blerg', 1000, function (error, ok) {
+        if (error) return t.fail(error);
+
+        t.ok(ok, "everything should still be peachy after setting again");
+
+        memcached.getMulti(['testkey', 'otherkey'], function (error, values) {
+          if (error) return t.fail(error);
+
+          t.deepEquals(values, {testkey : 'arglbargle', otherkey : 'blerg'},
+                       "memcached client should still work");
+
+          transaction.end();
+
+          var trace = transaction.getTrace();
+          t.ok(trace, "trace should exist");
+          t.ok(trace.root, "root element should exist");
+          t.equals(trace.root.children.length, 1,
+                   "there should be only one child of the root");
+
+          var setSegment = trace.root.children[0];
+          t.equals(setSegment.name, "MemCache/set", "should register the set");
+          t.equals(setSegment.parameters.key, "[\"otherkey\"]",
+                   "should have the set key as a parameter");
+          t.equals(setSegment.children.length, 1,
+                   "set should have an only child");
+
+          var getSegment = setSegment.children[0];
+          t.equals(getSegment.name, "MemCache/get", "should register the get");
+          t.equals(getSegment.parameters.key, "[\"testkey\",\"otherkey\"]",
+                   "should have the multiple keys fetched as a parameter");
+          t.equals(getSegment.children.length, 0,
+                   "get should leave us here at the end");
         });
       });
     });
