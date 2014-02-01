301efa7001183453b60eb64c974fa41b62ecfac6
Forrest L Norvell <forrest@newrelic.com>
2014-02-01 14:49:44 -0800

    sampler: state is now observable
    Added for testing, but maybe useful for some kind of health check
    function down the road.

--- test/sampler.test.js
+++ test/sampler.test.js
@@ -21,13 +21,20 @@ describe("environmental sampler", function () {
 
   it("should depend on Agent to provide the current metrics summary", function () {
     expect(function () { sampler.start(agent); }).not.throws();
-    expect(function () { sampler.sampleMemory(agent); }).not.throws();
-    expect(function () { sampler.checkEvents(agent); }).not.throws();
     expect(function () { sampler.stop(agent); }).not.throws();
   });
 
+  it("should default to a state of stopped", function () {
+    expect(sampler.state).equal('stopped');
+  });
+
+  it("should say it's running after start", function () {
+    sampler.start(agent);
+    expect(sampler.state).equal('running');
+  });
+
   it("should have a rough idea of how much memory Node is using", function () {
-    sampler.sampleMemory(agent);
+    sampler.sampleMemory(agent)();
 
     var stats = agent.metrics.getOrCreateMetric('Memory/Physical');
     expect(stats.callCount).equal(1);
@@ -35,29 +42,30 @@ describe("environmental sampler", function () {
   });
 
   it("should have some rough idea of how deep the event queue is", function (done) {
-    sampler.checkEvents(agent);
+    sampler.checkEvents(agent)();
 
-    /*
-     * sampler.checkEvents works by creating a timer and using setTimeout to
-     * schedule an "immediate" callback execution, which gives a rough idea of
-     * how much stuff is sitting pending on the libuv event queue (and whether
-     * there's a  lot of stuff being handled through process.nextTick, which
-     * maintains its own queue of immediate callbacks). It remains to be seen
-     * how high this metric will ever get, but at least the underlying timer
-     * has nanosecond precision (and probably significantly
+    /* sampler.checkEvents works by creating a timer and using
+     * setTimeout to schedule an "immediate" callback execution,
+     * which gives a rough idea of how much stuff is sitting pending
+     * on the libuv event queue (and whether there's a lot of stuff
+     * being handled through process.nextTick, which maintains its
+     * own queue of immediate callbacks). It remains to be seen how
+     * high this metric will ever get, but at least the underlying
+     * timer has nanosecond precision (and probably significantly
      * greater-than-millisecond accuracy).
      */
     setTimeout(function () {
       var stats = agent.metrics.getOrCreateMetric('Events/wait');
       expect(stats.callCount).equal(1);
-      // process.hrtime will notice the passage of time, but this happens
-      // too fast to measure any meaningful latency in versions of Node
-      // that don't have process.hrtime available, so just make sure
-      // we're not getting back undefined or null here.
+      /* process.hrtime will notice the passage of time, but this
+       * happens too fast to measure any meaningful latency in versions
+       * of Node that don't have process.hrtime available, so just make
+       * sure we're not getting back undefined or null here.
+       */
       expect(stats.total).a('number');
       if (process.hrtime) expect(stats.total).above(0);
 
-      return done();
+      done();
     }, 0);
   });
 });
