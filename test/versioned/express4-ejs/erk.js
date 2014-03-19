3d6818c5f55add3f2f959257638b00d2d1a8b5b9
Forrest L Norvell <forrest@newrelic.com>
2014-03-19 11:30:35 -0700

    instrumentation: first pass at Express 4 tests

---
+++ test/versioned/express4-ejs/erk.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var path    = require('path')
+  , request = require('request')
+  , helper  = require(path.join(__dirname, '..', '..', 'lib', 'agent_helper.js'))
+  ;
+
+// bootstrap instrumentation
+helper.instrumentMockedAgent();
+
+// once instrumentation is bootstrapped
+var express = require('express')
+  , app     = express()
+  , server  = require('http').createServer(app)
+  ;
+
+app.get('/test/:id', function (req, res, next) {
+  process.nextTick(function () { throw new Error('threw in a timer', next); });
+});
+
+server.listen(8080, function () {
+  process.on('message', function (code) {
+    request.get('http://localhost:8080/test/31337', function () {
+      process.exit(code);
+    });
+  });
+  process.send('ready');
+});
---
+++ test/versioned/express4-ejs/express4-ignoring.tap.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var path    = require('path')
+  , test    = require('tap').test
+  , request = require('request')
+  , helper  = require(path.join(__dirname, '..', '..', 'lib', 'agent_helper.js'))
+  , API     = require(path.join(__dirname, '..', '..', '..', 'api.js'))
+  ;
+
+test("ignoring an Express 3 route", function (t) {
+  t.plan(7);
+
+  var agent   = helper.instrumentMockedAgent()
+    , api     = new API(agent)
+    , express = require('express')
+    , app     = express()
+    , server  = require('http').createServer(app)
+    ;
+
+  this.tearDown(function () {
+    server.close(function () {
+      helper.unloadAgent(agent);
+    });
+  });
+
+  agent.on('transactionFinished', function (transaction) {
+    t.equal(transaction.name, 'WebTransaction/Expressjs/GET//polling/:id',
+            "transaction has expected name even on error");
+    t.ok(transaction.ignore, "transaction is ignored");
+
+    t.notOk(agent.traces.trace, "should have no transaction trace");
+
+    var metrics = agent.metrics.unscoped;
+    t.equal(Object.keys(metrics).length, 0, "no metrics added to agent collection");
+
+    var errors = agent.errors.errors;
+    t.equal(errors.length, 0, "no errors noticed");
+  });
+
+  app.get('/polling/:id', function (req, res) {
+    api.setIgnoreTransaction(true);
+
+    res.send(400, {status : 'pollpollpoll'});
+    res.end();
+  });
+
+  server.listen(8080, function () {
+    request.get('http://localhost:8080/polling/31337',
+                {json : true},
+                function (error, res, body) {
+
+      t.equal(res.statusCode, 400, "got expected error");
+      t.deepEqual(body, {status : 'pollpollpoll'}, "got expected response");
+    });
+  });
+});
