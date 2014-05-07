5e146fbe6b8cb49850e40b33a70067c4ee454883
Chase Douglas <chase@newrelic.com>
2014-05-07 16:53:19 -0700

    Add flatten util to flatten nested maps
    Ex: {a: 5, b: {c: true, d: 7}} -> {a: 5, 'b.c': true, 'b.d': 7}
    
    This will be used for sending agent settings to the collector, which
    expects a flattened settings map for browser monitoring settings.
    
    The test is in integration so I can use tap...

---
+++ test/integration/flatten.tap.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var tap  = require('tap')
+  , test = tap.test
+  , flatten = require('../../lib/util/flatten')
+  ;
+
+test('flatten flattens things', function (t) {
+  t.deepEqual(flatten({}, '', {a: 5, b: true}), {a: 5, b: true}, '1 level');
+  t.deepEqual(flatten({}, '', {a: 5, b: {c: true, d: 7}}), {a: 5, 'b.c': true, 'b.d': 7}, '2 levels');
+  t.deepEqual(flatten({}, '', {a: 5, b: {c: true, d: 7, e: {foo: 'efoo', bar: 'ebar'}}}), {a: 5, 'b.c': true, 'b.d': 7, 'b.e.foo': 'efoo', 'b.e.bar': 'ebar'}, '3 levels');
+
+  t.end();
+});
