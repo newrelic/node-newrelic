d534746d306cc0f1d85ccda79b36f25348feefa5
Chase Douglas <chase@newrelic.com>
2014-09-18 11:07:48 -0700

    test: Add test to ensure all dependencies are bundled

---
+++ test/bundles.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var assert = require('chai').assert,
+    packageJSON = require('../package.json'),
+    dependencies = Object.keys(packageJSON.dependencies),
+    bundled = packageJSON.bundledDependencies
+  ;
+
+describe('bundledDependencies in package.json', function() {
+  it('should include all dependencies', function() {
+    assert.deepEqual(bundled, dependencies, 'all dependencies accounted for');
+  });
+});
