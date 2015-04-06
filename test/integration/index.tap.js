adc2e62dbd12d17f52bf986b6f670dc66b345e6b
Bryan Clement <bclement@newrelic.com>
2015-04-06 16:10:18 -0700

    style: add test to check for crash when process.version is junk

--- test/integration/index.tap.js
+++ test/integration/index.tap.js
@@ -1,11 +1,10 @@
 'use strict'
 
-var path = require('path')
-  , test = require('tap').test
+var test = require('tap').test
   
 
 test("loading the application via index.js", function (t) {
-  t.plan(6)
+  t.plan(7)
 
   var agent
 
@@ -21,6 +20,31 @@ test("loading the application via index.js", function (t) {
     t.equal(agent._state, 'starting', "agent is booting")
   }, "just loading the agent doesn't throw")
 
+  t.doesNotThrow(function cb_doesNotThrow() {
+
+    // in order to sub out the readonly version we have to make a full
+    // copy of process, then restore the original when we are done
+    var _process = process
+    process = {}
+
+    var keys = Object.keys(_process)
+    for(var i = 0; i < keys.length; i++){
+      var key = keys[i]
+      if(key === 'version'){
+        process[key] = 'garbage'
+      } else {
+        process[key] = _process[key]
+      }
+    }
+
+    var api = require('../../index.js')
+    agent = api.agent
+    t.ok(agent)
+
+    process = _process
+
+  }, "malformed process.version doesn't blow up the process")
+
   function shutdown() {
     t.equal(agent._state, 'started', "agent didn't error connecting to staging")
     t.deepEquals(agent.config.applications(), ['My Application'], "app name is valid")
@@ -32,4 +56,4 @@ test("loading the application via index.js", function (t) {
 
   agent.once('errored', shutdown)
   agent.once('started', shutdown)
-});
+})
