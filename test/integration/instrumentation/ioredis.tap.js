0661dcbaf58de7fa70448301e8be79570bbda683
Martin Kuba <mkuba@newrelic.com>
2016-03-29 14:55:55 -0700

    instrumentation: added tests for the ioredis instrumentation

--- test/integration/instrumentation/ioredis.tap.js
+++ test/integration/instrumentation/ioredis.tap.js
@@ -3,75 +3,106 @@
 var tap = require('tap')
 var test = tap.test
 var helper = require('../../lib/agent_helper')
+var assertMetrics = require('../../lib/metrics_helper').assertMetrics
 var params = require('../../lib/params')
+var semver = require('semver')
 
 
 // CONSTANTS
 var DB_INDEX = 2
 
-test("Ioredis instrumentation should find ioredis calls in the transaction trace",
-     {timeout : 5000},
-     function (t) {
-  t.plan(18)
 
-  var self = this
-  helper.bootstrapRedis(DB_INDEX, function cb_bootstrapRedis(error, app) {
-    if (error) return t.fail(error)
-    var agent = helper.instrumentMockedAgent()
-    var Redis = require('ioredis')
-    var client = new Redis(params.redis_port, params.redis_host)
+test('ioredis instrumentation', {skip: semver.satisfies(process.version, "<0.10")},
+  function(t) {
 
+  t.test('creates expected metrics',
+       {timeout : 5000}, function (t) {
 
-    self.tearDown(function cb_tearDown() {
-      helper.unloadAgent(agent)
-    })
+    var self = this
     var onError = function(error){return t.fail(error)}
-    // need to capture parameters
-    agent.config.capture_params = true
 
-    t.notOk(agent.getTransaction(), "no transaction should be in play")
+    var name = require.resolve('ioredis')
+    name = require.resolve('bluebird')
 
-    helper.runInTransaction(agent, function transactionInScope() {
-      var transaction = agent.getTransaction()
-      t.ok(transaction, "transaction should be visible")
+    setup(t, function runTest(agent, redisClient) {
+      agent.on('transactionFinished', function(tx) {
+        var expected = [
+          [{ name: 'Datastore/all' }],
+          [{ name: 'Datastore/Redis/all' }],
+          [{ name: 'Datastore/operation/Redis/set' }]
+        ]
+        assertMetrics(tx.metrics, expected, false, false)
+        t.end()
+      })
 
-      client.set('testkey', 'arglbargle').then(function (ok) {
-        t.ok(agent.getTransaction(), "transaction should still be visible")
-        t.ok(ok, "everything should be peachy after setting")
-
-        client.get('testkey').then(function (value) {
-          t.ok(agent.getTransaction(), "transaction should still still be visible")
-          t.equals(value, 'arglbargle', "redis client should still work")
-
-          var trace = transaction.trace
-          t.ok(trace, "trace should exist")
-          t.ok(trace.root, "root element should exist")
-          t.equals(trace.root.children.length, 1,
-                   "there should be only one child of the root")
-          var setSegment = trace.root.children[0]
-          t.ok(setSegment, "trace segment for set should exist")
-          t.equals(setSegment.name, "Datastore/operation/Redis/set",
-                   "should register the set")
-          t.equals(setSegment.parameters.key, "\"testkey\"",
-                   "should have the set key as a parameter")
-          t.equals(setSegment.children.length, 1,
-                   "set should have an only child")
-
-          var getSegment = setSegment.children[0].children[0]
-          t.ok(getSegment, "trace segment for get should exist")
-          t.equals(getSegment.name, "Datastore/operation/Redis/get",
-                   "should register the get")
-          t.equals(getSegment.parameters.key, "\"testkey\"",
-                   "should have the get key as a parameter")
-          t.ok(getSegment.children.length >= 1,
-                   "get should have a callback segment")
-          t.ok(getSegment.timer.hrDuration, "trace segment should have ended")
-
-          transaction.end(function() {
-            client.disconnect()
-          })
+      helper.runInTransaction(agent, function transactionInScope(transaction) {
+        redisClient.set('testkey', 'testvalue').then(function (ok) {
+          transaction.end()
         }, onError).catch(onError)
-      }, onError).catch(onError)
+      })
+    })
+  })
+
+  t.test('creates expected segments',
+       {timeout : 5000}, function (t) {
+
+    var self = this
+    var onError = function(error){return t.fail(error)}
+
+    var name = require.resolve('ioredis')
+    name = require.resolve('bluebird')
+
+    setup(t, function runTest(agent, redisClient) {
+      agent.on('transactionFinished', function(tx) {
+        var root = tx.trace.root
+        t.equals(root.children.length, 2, 'root has two children')
+
+        var setSegment = root.children[0]
+        t.equals(setSegment.name, 'Datastore/operation/Redis/set')
+
+        // ioredis operations return promise, any 'then' callbacks will be sibling segments
+        // of the original redis call
+        var getSegment = root.children[1]
+        t.equals(getSegment.name, 'Datastore/operation/Redis/get')
+        t.equals(getSegment.children.length, 0, 'should not contain any segments')
+
+        t.end()
+      })
+
+      helper.runInTransaction(agent, function transactionInScope(transaction) {
+        var p1 = redisClient.set('testkey', 'testvalue')
+
+        p1.then(function() {
+          return redisClient.get('testkey')
+        })
+        .then(function() {
+          transaction.end()
+        })
+        .catch(onError)
+      })
     })
   })
 })
+
+
+function setup(t, callback) {
+  helper.bootstrapRedis(DB_INDEX, function cb_bootstrapRedis(error, app) {
+    if (error) return t.fail(error)
+    var agent = helper.instrumentMockedAgent()
+
+    // remove from cache, so that the bluebird library that ioredis uses gets
+    // re-instrumented
+    var name = require.resolve('ioredis')
+    delete require.cache[name]
+
+    var Redis = require('ioredis')
+    var client = new Redis(params.redis_port, params.redis_host)
+
+    t.tearDown(function cb_tearDown() {
+      helper.unloadAgent(agent)
+      client.disconnect()
+    })
+
+    callback(agent, client)
+  })
+}
