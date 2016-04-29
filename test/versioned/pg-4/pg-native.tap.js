5f0be04d9f92f37b44b57fab277866c0f2077d67
Martin Kuba <mkuba@newrelic.com>
2016-04-29 11:36:07 -0700

    tests: enabled PG 4 native tests on Node 4, 5, and 6

--- test/versioned/pg-4/pg-native.tap.js
+++ test/versioned/pg-4/pg-native.tap.js
@@ -1,10 +1,8 @@
 'use strict'
-// We cant test v0.8 here because pg-native 4 uses stream.Duplex, which doesn't exist
-// in v0.8
-// The native component currently can not build on >= v3, so we skip
-// this test for now
+// We cant test v0.8 here because pg-native 4 uses stream.Duplex, which doesn't
+// exist in v0.8.
 var semver = require('semver')
-if (semver.satisfies(process.versions.node, '0.8.x||>=3')) return
+if (semver.satisfies(process.versions.node, '0.8.x')) return
 
 var runTests = require('./pg.common.js')
 var helper = require('../../lib/agent_helper')
