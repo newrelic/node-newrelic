2fa00403f5d1eed66eb00b4dc65c902ea4cb7cc4
Martin Kuba <mkuba@newrelic.com>
2016-05-24 14:06:04 -0700

    collector: added custom certificates to https calls to collector without a proxy

--- test/integration/collector-remote-method.tap.js
+++ test/integration/collector-remote-method.tap.js
@@ -2,11 +2,15 @@
 
 var path = require('path')
 var test = require('tap').test
+var read = require('fs').readFileSync
+var join = require('path').join
+var http = require('http')
+var https = require('https')
+var url = require('url')
+var RemoteMethod = require('../../lib/collector/remote-method.js')
+var collector = require('../lib/fake-collector.js')
 
 test("DataSender (callback style) talking to fake collector", function (t) {
-  var RemoteMethod = require('../../lib/collector/remote-method.js')
-  var collector = require('../lib/fake-collector.js')
-
   var config = {
     host        : 'collector.lvh.me',
     port        : 8765,
@@ -41,4 +45,103 @@ test("DataSender (callback style) talking to fake collector", function (t) {
       t.end()
     })
   })
+})
+
+test("remote method to get redirect host", function (t) {
+
+  t.test("https with custom certificate", function(t) {
+    var test = t
+    var method = createRemoteMethod(true, true)
+
+    // create mock collector
+    startMockCollector(true, function(err, server) {
+      method.invoke([], function(error, returnValue, json) {
+        validateResponse(test, error, returnValue)
+        server.close()
+        t.end()
+      })
+    })
+  })
+
+  t.test("http without custom certificate", function(t) {
+    var test = t
+    var method = createRemoteMethod(false, false)
+
+    // create mock collector
+    startMockCollector(false, function(err, server) {
+      method.invoke([], function(error, returnValue, json) {
+        validateResponse(test, error, returnValue)
+        server.close()
+        t.end()
+      })
+    })
+  })
+
+  t.test("http with custom certificate", function(t) {
+    var test = t
+    var method = createRemoteMethod(false, true)
+
+    // create mock collector
+    startMockCollector(false, function(err, server) {
+      method.invoke([], function(error, returnValue, json) {
+        validateResponse(test, error, returnValue)
+        server.close()
+        t.end()
+      })
+    })
+  })
+
+  function validateResponse(t, error, returnValue) {
+    if (error) {
+      t.fail(error)
+    }
+    t.equal(returnValue, 'some-collector-url', 'got back expected response')
+  }
+
+  function createRemoteMethod(ssl, useCertificate) {
+    var config = {
+      host: 'localhost',
+      port: 8765
+    }
+
+    if (ssl) {
+      config['host'] = 'ssl.lvh.me'
+      config['ssl'] = true
+    }
+
+    if (useCertificate) {
+      config['certificates'] = read(join(__dirname, '../lib/ca-certificate.crt'), 'utf8')
+    }
+
+    var method = new RemoteMethod('get_redirect_host', config)
+    return method
+  }
+
+  function startMockCollector(ssl, startedCallback) {
+    var opts = {
+      port: 8765
+    }
+
+    var server
+    if (ssl) {
+      opts['key'] = read(join(__dirname, '../lib/test-key.key'))
+      opts['cert'] = read(join(__dirname, '../lib/self-signed-test-certificate.crt'))
+      server = https.createServer(opts, responder)
+    } else {
+      server = http.createServer(responder)
+    }
+
+    server.listen(8765, function(err) {
+      startedCallback(err, this)
+    })
+
+    function responder(req, res) {
+      var parsed = url.parse(req.url, true)
+      t.equal(parsed.query['method'], 'get_redirect_host', 'got redirect host request')
+      res.write(JSON.stringify({
+        "return_value": "some-collector-url"
+      }))
+      res.end()
+    }
+  }
 })
