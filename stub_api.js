98aae7784ecccfa0de7892e7510f46cf1ecd51c0
Vlad Fedosov <vlad.fedosov@gmail.com>
2016-08-30 07:21:21 +0300

    Check that callback was passed through options or didn't passed at all

--- stub_api.js
+++ stub_api.js
@@ -57,8 +57,18 @@ function createBackgroundTransaction(name, group, callback) {
 }
 
 // Normally the following call executes callback asynchronously
-function shutdown(options, callback) {
+function shutdown(options, cb) {
   logger.debug('Not calling shutdown because New Relic is disabled.')
+  
+  var callback = cb
+  if (!callback) {
+    if (typeof options === 'function') {
+      callback = options
+    } else {
+      callback = new Function()
+    }
+  }
+  
   process.nextTick(callback);
 }
 
