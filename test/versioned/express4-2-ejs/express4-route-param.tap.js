baee45d0ac62e5c97a51cb117a4b21633603ad6f
Natalie Wolfe <nwolfe@newrelic.com>
2016-10-18 15:49:40 -0700

    instrumentation: fixed issue with paramware with inactive transaction

--- test/versioned/express4-2-ejs/express4-route-param.tap.js
+++ test/versioned/express4-2-ejs/express4-route-param.tap.js
@@ -52,6 +52,23 @@ test('Express 4 route param', {skip: skip()}, function(t) {
         t.equal(body, 'denied', 'should have responded from within paramware')
       })
     })
+
+    t.test('in-active transaction in param handler', function(t) {
+      t.plan(4)
+
+      agent.once('transactionFinished', function(tx) {
+        t.equal(
+          tx.name, 'WebTransaction/Expressjs/GET//a/b/preempt/c',
+          'should have correct transaction name'
+        )
+      })
+
+      testRequest('preempt', function(err, body) {
+        t.notOk(err, 'should not have errored')
+        t.equal(body.action, 'preempt', 'should pass through correct parameter value')
+        t.equal(body.name, 'action', 'should pass through correct parameter name')
+      })
+    })
   })
 })
 
@@ -70,7 +87,9 @@ function createServer(express) {
   var cRouter = new express.Router()
 
   cRouter.get('', function(req, res) {
-    res.json({action: req.action, name: req.name})
+    if (req.action !== 'preempt') {
+      res.json({action: req.action, name: req.name})
+    }
   })
 
   bRouter.use('/c', cRouter)
@@ -86,6 +105,10 @@ function createServer(express) {
   })
 
   aRouter.use('/b/:action', bRouter)
+  app.use('/a/b/preempt/c', function(req, res, next) {
+    res.send({action: 'preempt', name: 'action'})
+    process.nextTick(next)
+  })
   app.use('/a', aRouter)
 
   return require('http').createServer(app)
