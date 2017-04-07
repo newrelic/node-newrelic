f6a00bec89afa2413c078204ede3d47383798fe6
Natalie Wolfe <nwolfe@newrelic.com>
2017-04-07 15:27:48 -0700

    tests: added checks for new restify metrics

--- test/integration/instrumentation/restify.tap.js
+++ test/integration/instrumentation/restify.tap.js
@@ -1,8 +1,9 @@
 'use strict'
 
-var tap     = require('tap')
+var tap = require('tap')
 var request = require('request')
-var helper  = require('../../lib/agent_helper')
+var helper = require('../../lib/agent_helper')
+var assertMetrics = require('../../lib/metrics_helper').assertMetrics
 
 
 /*
@@ -26,9 +27,10 @@ tap.test('should not crash when Restify handles a connection', function(t) {
     server.close()
   })
 
-  server.get('/hello/:name', function sayHello(req, res) {
+  server.get('/hello/:name', function sayHello(req, res, next) {
     t.ok(agent.getTransaction(), "transaction should be available in handler")
     res.send('hello ' + req.params.name)
+    next()
   })
 
   server.listen(8765, function() {
@@ -77,9 +79,10 @@ tap.test('Restify should still be instrumented when run with SSL', function(t) {
       server.close()
     })
 
-    server.get('/hello/:name', function sayHello(req, res) {
+    server.get('/hello/:name', function sayHello(req, res, next) {
       t.ok(agent.getTransaction(), "transaction should be available in handler")
       res.send('hello ' + req.params.name)
+      next()
     })
 
     server.listen(8443, function() {
@@ -116,3 +119,73 @@ tap.test('Restify should still be instrumented when run with SSL', function(t) {
     })
   })
 })
+
+tap.test('Restify should generate middleware metrics', function(t) {
+  t.plan(5)
+
+  var agent = helper.instrumentMockedAgent()
+  var restify = require('restify')
+  var server = restify.createServer()
+
+  t.tearDown(function() {
+    helper.unloadAgent(agent)
+    server.close()
+  })
+
+  server.use(function middleware(req, res, next) {
+    t.ok(agent.getTransaction(), 'should be in transaction context')
+    next()
+  })
+
+  server.use(function middleware2(req, res, next) {
+    t.ok(agent.getTransaction(), 'should be in transaction context')
+    next()
+  })
+
+  server.get('/foo/:bar', function handler(req, res, next) {
+    t.ok(agent.getTransaction(), 'should be in transaction context')
+    res.send({'message': 'done'})
+    next()
+  })
+
+  var port = null
+  server.listen(function() {
+    port = server.address().port
+
+    agent.on('transactionFinished', function(tx) {
+      checkMetrics(t, tx.metrics, [
+        // Metrics for this transaction with the right name.
+        [{"name": "WebTransaction/Restify/GET//foo/:bar"}],
+        [{"name": "WebTransactionTotalTime/Restify/GET//foo/:bar"}],
+        [{"name": "Apdex/Restify/GET//foo/:bar"}],
+
+        // Unscoped middleware metrics.
+        [{"name": "Nodejs/Middleware/Restify/middleware//"}],
+        [{"name": "Nodejs/Middleware/Restify/middleware2//"}],
+        [{"name": "Restify/Route Path: handler/foo/:bar"}],
+
+        // Scoped middleware metrics.
+        [{"name": "Nodejs/Middleware/Restify/middleware//",
+          "scope": "WebTransaction/Restify/GET//foo/:bar"}],
+        [{"name": "Nodejs/Middleware/Restify/middleware2//",
+          "scope": "WebTransaction/Restify/GET//foo/:bar"}],
+        [{"name": "Restify/Route Path: handler/foo/:bar",
+          "scope": "WebTransaction/Restify/GET//foo/:bar"}]
+      ])
+    })
+
+    var url = 'http://localhost:' + port + '/foo/bar'
+    request.get(url, function(err) {
+      t.error(err, 'should not fail to make request')
+    })
+  })
+})
+
+function checkMetrics(t, metrics, expected, exclusive) {
+  try {
+    assertMetrics(metrics, expected, exclusive || false, false)
+    t.pass('should have expected segments')
+  } catch (e) {
+    t.error(e, 'should have expected segments')
+  }
+}
