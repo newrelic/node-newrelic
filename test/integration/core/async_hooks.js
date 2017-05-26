13bc8846754c83f8ea4519061cf204bf24cf3347
Bryan Clement <bclement@newrelic.com>
2017-05-26 15:37:34 -0700

    added async/await support via async_hook

---
+++ test/integration/core/async_hooks.js
@@ -0,0 +1,36 @@
+'use strict'
+var semver = require('semver')
+
+if (semver.satisfies(process.version, "<8")) {
+  console.log('async hooks are not supported in node version: ' + process.version)
+  return
+}
+
+var test = require('tap').test
+var helper = require('../../lib/agent_helper')
+var asyncHooks = require('async_hooks')
+
+test('await', function(t) {
+  var agent = setupAgent(t)
+  helper.runInTransaction(agent, async function(txn) {
+    var transaction = agent.getTransaction()
+    t.equal(transaction && transaction.id, txn.id)
+    await Promise.resolve("i'll be back")
+    transaction = agent.getTransaction()
+    t.equal(transaction && transaction.id, txn.id)
+    txn.end(t.end.bind(t))
+  })
+})
+
+function setupAgent(t) {
+  var agent = helper.instrumentMockedAgent({
+    feature_flag: {
+      await_support: true
+    }
+  })
+  t.tearDown(function() {
+    helper.unloadAgent(agent)
+  })
+
+  return agent
+}
