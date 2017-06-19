2eb9516b88b6b86f0fc57e7d8faa6adc92c080c8
Bryan Clement <bclement@newrelic.com>
2017-06-19 16:55:20 -0700

    instrumentation: check for null module in inspector instrumentation

---
+++ test/unit/instrumentation/core/inspector.test.js
@@ -0,0 +1,22 @@
+'use strict'
+
+var chai = require('chai')
+var helper = require('../../../lib/agent_helper')
+var inspectorInstrumentation = require('../../../../lib/instrumentation/core/inspector')
+
+var expect = chai.expect
+
+describe('Inspector instrumentation', function() {
+  var agent = null
+  before(function() {
+    agent = helper.loadMockedAgent()
+  })
+
+  after(function () {
+    helper.unloadAgent(agent)
+  })
+
+  it('should not throw when passed null for the module', function() {
+    expect(inspectorInstrumentation.bind(null, agent, null)).to.not.throw()
+  })
+})
