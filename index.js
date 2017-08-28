9b047666abb859fe5cd395dff0cd7f27a499da38
Natalie Wolfe <natalie@lifewanted.com>
2017-08-28 11:20:38 -0700

    Added feature_flag supportabilities

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 'use strict'
 
-var logger = require('./lib/logger.js')
+var featureFlags = require('./lib/feature_flags').prerelease
+var logger = require('./lib/logger')
 var semver = require('semver')
 
-var message
-var agent
-
-var agentVersion = require('./package.json').version
+var agentVersion = require('./package').version
 logger.info(
   "Using New Relic for Node.js. Agent version: %s; Node version: %s.",
   agentVersion, process.version
@@ -26,10 +24,9 @@ if (require.cache.__NR_cache) {
 }
 
 function initialize() {
-  logger.debug(
-    'Loading agent from %s',
-    __dirname
-  )
+  logger.debug('Loading agent from %s', __dirname)
+  var agent = null
+  var message = null
 
   try {
     logger.debug(
@@ -50,64 +47,17 @@ function initialize() {
     logger.debug("Process title is %s.", process.title)
     logger.debug("Application was invoked as %s.", process.argv.join(' '))
 
-    var config = require('./lib/config.js').getOrCreateInstance()
+    var config = require('./lib/config').getOrCreateInstance()
 
     // Get the initialized logger as we likely have a bootstrap logger which
     // just pipes to stdout.
-    logger = require('./lib/logger.js')
+    logger = require('./lib/logger')
 
     if (!config || !config.agent_enabled) {
       logger.info("Module not enabled in configuration; not starting.")
     } else {
-      /* Only load the rest of the module if configuration is available and the
-       * configurator didn't throw.
-       *
-       * The agent must be a singleton, or else module loading will be patched
-       * multiple times, with undefined results. New Relic's instrumentation
-       * can't be enabled or disabled without an application restart.
-       */
-      var Agent = require('./lib/agent.js')
-      agent = new Agent(config)
-      var appNames = agent.config.applications()
-
-      if (config.logging.diagnostics) {
-        logger.warn(
-          'Diagnostics logging is enabled, this may cause significant overhead.'
-        )
-      }
-
-      if (appNames.length < 1) {
-        message = "New Relic requires that you name this application!\n" +
-                  "Set app_name in your newrelic.js file or set environment variable\n" +
-                  "NEW_RELIC_APP_NAME. Not starting!"
-        logger.error(message)
-        throw new Error(message)
-      }
-
-      var shimmer = require('./lib/shimmer.js')
-      shimmer.patchModule(agent)
-      shimmer.bootstrapInstrumentation(agent)
-
-      agent.start(function cb_start(error) {
-        if (!error) {
-          // TODO: After deprecating Node 0.10 and 0.12, simplify this regex.
-          // TODO: As new versions come out, make sure to update Angler metrics.
-          var nodeMajor = /^v?((?:0\.)?\d+)/.exec(process.version)
-          agent.recordSupportability(
-            'Version/' + ((nodeMajor && nodeMajor[1]) || 'unknown')
-          )
-
-          return logger.debug("New Relic for Node.js is connected to New Relic.")
-        }
-
-        var errorMessage = "New Relic for Node.js halted startup due to an error:"
-        logger.error(error, errorMessage)
-
-        /* eslint-disable no-console */
-        console.error(errorMessage)
-        console.error(error.stack)
-        /* eslint-enable no-console */
-      })
+      agent = createAgent(config)
+      addStartupSupportabilities(agent)
     }
   } catch (error) {
     message = "New Relic for Node.js was unable to bootstrap itself due to an error:"
@@ -119,12 +69,80 @@ function initialize() {
     /* eslint-enable no-console */
   }
 
-  var API
+  var API = null
   if (agent) {
-    API = require('./api.js')
+    API = require('./api')
   } else {
-    API = require('./stub_api.js')
+    API = require('./stub_api')
   }
 
   require.cache.__NR_cache = module.exports = new API(agent)
+}
+
+function createAgent(config) {
+  /* Only load the rest of the module if configuration is available and the
+   * configurator didn't throw.
+   *
+   * The agent must be a singleton, or else module loading will be patched
+   * multiple times, with undefined results. New Relic's instrumentation
+   * can't be enabled or disabled without an application restart.
+   */
+  var Agent = require('./lib/agent')
+  var agent = new Agent(config)
+  var appNames = agent.config.applications()
+
+  if (config.logging.diagnostics) {
+    logger.warn(
+      'Diagnostics logging is enabled, this may cause significant overhead.'
+    )
+  }
+
+  if (appNames.length < 1) {
+    var message =
+      'New Relic requires that you name this application!\n' +
+      'Set app_name in your newrelic.js file or set environment variable\n' +
+      'NEW_RELIC_APP_NAME. Not starting!'
+    logger.error(message)
+    throw new Error(message)
+  }
+
+  var shimmer = require('./lib/shimmer')
+  shimmer.patchModule(agent)
+  shimmer.bootstrapInstrumentation(agent)
+
+  agent.start(function cb_start(error) {
+    if (!error) {
+      return logger.debug("New Relic for Node.js is connected to New Relic.")
+    }
+
+    var errorMessage = "New Relic for Node.js halted startup due to an error:"
+    logger.error(error, errorMessage)
+
+    /* eslint-disable no-console */
+    console.error(errorMessage)
+    console.error(error.stack)
+    /* eslint-enable no-console */
+  })
+
+  return agent
+}
+
+function addStartupSupportabilities(agent) {
+  // TODO: After deprecating Node 0.10 and 0.12, simplify this regex.
+  // TODO: As new versions come out, make sure to update Angler metrics.
+  var nodeMajor = /^v?((?:0\.)?\d+)/.exec(process.version)
+  agent.recordSupportability(
+    'Version/' + ((nodeMajor && nodeMajor[1]) || 'unknown')
+  )
+
+  var configFlags = Object.keys(agent.config.feature_flag)
+  for (var i = 0; i < configFlags.length; ++i) {
+    var flag = configFlags[i]
+    var enabled = agent.config.feature_flag[flag]
+    if (enabled !== featureFlags[flag]) {
+      agent.recordSupportability(
+        'FeatureFlag/' + flag + '/' + (enabled ? 'enabled' : 'disabled')
+      )
+    }
+  }
 }
