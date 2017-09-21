9d86d7f50f1338dae07284724a28ed16e8f6cde2
Bryan Clement <bclement@newrelic.com>
2017-09-20 17:49:36 -0700

    hapi instrumentation now passes along defaults placed on handler functions

--- test/versioned/hapi-latest/hapi-router.tap.js
+++ test/versioned/hapi-latest/hapi-router.tap.js
@@ -14,19 +14,20 @@ var request = require('request')
 var helper  = require(path.join(__dirname, '..', '..', 'lib', 'agent_helper.js'))
 
 
-function verifier(t) {
+function verifier(t, verb) {
+  verb = verb || "GET"
   return function(transaction) {
-    t.equal(transaction.name, 'WebTransaction/Hapi/GET//test/{id}',
+    t.equal(transaction.name, 'WebTransaction/Hapi/' + verb + '//test/{id}',
             "transaction has expected name")
     t.equal(transaction.url, '/test/31337', "URL is left alone")
     t.equal(transaction.statusCode, 200, "status code is OK")
-    t.equal(transaction.verb, 'GET', "HTTP method is GET")
+    t.equal(transaction.verb, verb, "HTTP method is " + verb)
     t.ok(transaction.trace, "transaction has trace")
 
     var web = transaction.trace.root.children[0]
     t.ok(web, "trace has web segment")
     t.equal(web.name, transaction.name, "segment name and transaction name match")
-    t.equal(web.partialName, 'Hapi/GET//test/{id}',
+    t.equal(web.partialName, 'Hapi/' + verb + '//test/{id}',
             "should have partial name for apdex")
     t.equal(web.parameters.id, '31337', "namer gets parameters out of route")
   }
@@ -133,6 +134,62 @@ test("Hapi router introspection", function(t) {
 
     server.start(function() {
       request.get('http://localhost:8089/test/31337',
+                  {json : true},
+                  function(error, res, body) {
+
+        t.equal(res.statusCode, 200, "nothing exploded")
+        t.deepEqual(body, {status : 'ok'}, "got expected response")
+        t.end()
+      })
+    })
+  })
+
+  /*
+   * This test covers the use case of placing defaults on the handler
+   * function.
+   * for example: https://github.com/hapijs/h2o2/blob/v6.0.1/lib/index.js#L189-L198
+   */
+  t.test("using custom handler defaults", function(t) {
+    agent.on('transactionFinished', verifier(t, 'POST'))
+    function handler(route, options) {
+      t.equal(
+        route.settings.payload.parse,
+        false,
+        'should set the payload parse setting'
+      )
+
+      t.equal(
+        route.settings.payload.output,
+        'stream',
+        'should set the payload output setting'
+      )
+
+      return function customHandler(request, reply) {
+        t.ok(agent.getTransaction(), "transaction is available")
+        reply({status : 'ok'})
+      }
+    }
+
+    handler.defaults = {
+      payload: {
+        output: 'stream',
+        parse: false
+      }
+    }
+
+    server.handler('hello', handler)
+
+    var route = {
+      method : 'POST',
+      path   : '/test/{id}',
+      handler: {
+        hello: {}
+      }
+    }
+    server.route(route)
+
+    server.start(function() {
+      request.post('http://localhost:8089/test/31337',
                   {json : true},
                   function(error, res, body) {
 
