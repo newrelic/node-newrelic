b3ba58a5831e7da45cbb8801570c7bfa5f6ddf49
Natalie Wolfe <natalie@lifewanted.com>
2017-09-22 14:00:18 -0700

    Disable bad version test on 0.10/0.12

--- test/integration/index/index-bad-version.tap.js
+++ test/integration/index/index-bad-version.tap.js
@@ -1,7 +1,16 @@
 'use strict'
 
+var semver = require('semver')
 var test = require('tap').test
-var EventEmitter = require('events').EventEmitter
+
+
+// Some bug in early versions of node causes a bad process version to screw up
+// readable streams. Since this test is specifically about testing bad process
+// versions this renders the test incompatible with those versions of Node.
+// TODO: When deprecating Node 0.10 and 0.12, remove this check.
+if (semver.satisfies(process.version, '<4')) {
+  return
+}
 
 
 test('loading the agent with a bad version', {timeout: 5000}, function(t) {
@@ -12,26 +21,15 @@ test('loading the agent with a bad version', {timeout: 5000}, function(t) {
   process.env.NEW_RELIC_LICENSE_KEY = 'd67afc830dab717fd163bfcb0b8b88423e9a1a3b'
 
   t.doesNotThrow(function() {
-    // in order to sub out the readonly version we have to make a full
-    // copy of process, then restore the original when we are done
-    var _process = process
-    global.process = new EventEmitter()
-
-    var keys = Object.keys(_process)
-    for (var i = 0; i < keys.length; ++i) {
-      var key = keys[i]
-      if (key === 'version') {
-        process[key] = 'garbage'
-      } else {
-        process[key] = _process[key]
-      }
-    }
+    var _version = process.version
+    Object.defineProperty(process, 'version', {value: 'garbage', writable: true})
+    t.equal(process.version, 'garbage', 'should have set bad version')
 
     var api = require('../../../index.js')
     agent = api.agent
     t.ok(agent)
 
-    global.process = _process
+    process.version = _version
   }, "malformed process.version doesn't blow up the process")
   if (!t.passing()) {
     t.comment('Bailing out early.')
