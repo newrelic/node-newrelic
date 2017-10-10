21c93899f63b156c9bd25b38d4107c91c7173e37
Natalie Wolfe <natalie@lifewanted.com>
2017-10-10 16:40:13 -0700

    Added benchmark for wrap methods

---
+++ test/benchmark/func-wrap.js
@@ -0,0 +1,55 @@
+'use strict'
+
+var benchmark = require('../lib/benchmark')
+var helper = require('../lib/agent_helper')
+var Shim = require('../../lib/shim/shim')
+var shimmer = require('../../lib/shimmer')
+
+var agent = helper.loadMockedAgent()
+var shim = new Shim(agent, 'test-module')
+var suite = benchmark.createBenchmark({
+  name: 'function wrapping'
+})
+
+function getTest() {
+  return {
+    func: function testFunc(a, b, c) {
+      return a + b + c
+    }
+  }
+}
+
+suite.add({
+  name: 'shim.wrap',
+  fn: function() {
+    var test = getTest()
+    shim.wrap(test, 'func', function(shim, fn) {
+      return function() { return fn.apply(this, arguments) }
+    })
+    return test
+  }
+})
+
+suite.add({
+  name: 'shimmer.wrapMethod',
+  fn: function() {
+    var test = getTest()
+    shimmer.wrapMethod(test, 'test', 'func', function(fn) {
+      return function() { return fn.apply(this, arguments) }
+    })
+    return test
+  }
+})
+
+suite.add({
+  name: 'IIFE',
+  fn: function() {
+    var test = getTest()
+    test.func = (function(fn) {
+      return function() { return fn.apply(this, arguments) }
+    }(test.func))
+    return test
+  }
+})
+
+suite.run()
