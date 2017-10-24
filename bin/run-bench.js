05543b20fc3394c0e3415b712f0dcce966374545
Natalie Wolfe <natalie@lifewanted.com>
2017-10-23 17:24:57 -0700

    Add file glob args to run bench

--- bin/run-bench.js
+++ bin/run-bench.js
@@ -9,26 +9,68 @@ var path = require('path')
 var cwd = path.resolve(__dirname, '..')
 var benchpath = path.resolve(cwd, 'test/benchmark')
 
-glob(path.resolve(benchpath, '**/*.bench.js'), function globCb(err, files) {
-  if (err) {
-    return console.error('Failed to glob:', err)
+var tests = []
+var globs = []
+
+process.argv.slice(2).forEach(function forEachFileArg(file) {
+  if (/[*]/.test(file)) {
+    globs.push(path.join(benchpath, file))
+  } else if (/\.bench\.js$/.test(file)) {
+    tests.push(path.join(benchpath, file))
+  } else {
+    globs.push(
+      path.join(benchpath, file, '*.bench.js'),
+      path.join(benchpath, file, '**/*.bench.js')
+    )
   }
+})
 
-  a.eachSeries(files, function spawnEachFile(file, cb) {
-    var test = path.relative(benchpath, file)
+if (tests.length === 0 && globs.length === 0) {
+  globs.push(
+    path.join(benchpath, '*.bench.js'),
+    path.join(benchpath, '**/*.bench.js')
+  )
+}
 
-    console.log(test)
-    var child = cp.spawn('node', [file], {cwd: cwd, stdio: 'inherit'})
-    child.on('error', cb)
-    child.on('exit', function onChildExit(code) {
-      if (code) {
-        return cb(new Error('Benchmark exited with code ' + code))
+a.series([
+  function resolveGlobs(cb) {
+    if (!globs.length) {
+      return cb()
+    }
+
+    a.map(globs, glob, function afterGlobbing(err, resolved) {
+      if (err) {
+        console.error('Failed to glob:', err)
+        process.exitCode = -1
+        return cb(err)
+      }
+      resolved.forEach(function mergeResolved(files) {
+        tests.push.apply(tests, files)
+      })
+      cb()
+    })
+  },
+  function runBenchmarks(cb) {
+    tests.sort()
+    a.eachSeries(tests, function spawnEachFile(file, cb) {
+      var test = path.relative(benchpath, file)
+
+      console.log(test)
+      var child = cp.spawn('node', [file], {cwd: cwd, stdio: 'inherit'})
+      child.on('error', cb)
+      child.on('exit', function onChildExit(code) {
+        if (code) {
+          return cb(new Error('Benchmark exited with code ' + code))
+        }
+        cb()
+      })
+    }, function afterSpawnEachFile(err) {
+      if (err) {
+        console.error('Spawning failed:', err)
+        process.exitCode = -2
+        return cb(err)
       }
       cb()
     })
-  }, function afterSpawnEachFile(err) {
-    if (err) {
-      console.error('Spawning failed:', err)
-    }
-  })
-})
+  }
+])
