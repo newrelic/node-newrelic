9145e99581adf80aa5789daa87a7c11c9f7cbf03
Natalie Wolfe <natalie@lifewanted.com>
2017-11-17 18:02:18 -0800

    Optimized merge
    Integration:
    metrics/merge.bench.js
       1 metric  x 746,165 ops/sec ±0.65% (90 runs sampled)
      10 metrics x 110,175 ops/sec ±0.75% (90 runs sampled)
     100 metrics x 15,523 ops/sec ±1.42% (89 runs sampled)
    1000 metrics x 2,898 ops/sec ±0.82% (86 runs sampled)
    
    This commit:
    metrics/merge.bench.js
       1 metric  x 871,292 ops/sec ±1.19% (85 runs sampled)
      10 metrics x 118,964 ops/sec ±1.02% (89 runs sampled)
     100 metrics x 16,808 ops/sec ±0.91% (89 runs sampled)
    1000 metrics x 3,141 ops/sec ±0.93% (88 runs sampled)

---
+++ test/benchmark/metrics/merge.bench.js
@@ -0,0 +1,74 @@
+'use strict'
+
+var benchmark = require('../../lib/benchmark')
+var Metrics = require('../../../lib/metrics')
+var shared = require('./shared')
+
+
+var suite = benchmark.createBenchmark({
+  name: 'metrics.merge'
+})
+
+preOptMetrics()
+
+suite.add({
+  name: '   1 metric ',
+  fn: function() {
+    var m1 = makeMetrics(1)
+    var m2 = makeMetrics(1)
+
+    m1.merge(m2)
+  }
+})
+
+suite.add({
+  name: '  10 metrics',
+  fn: function() {
+    var m1 = makeMetrics(10)
+    var m2 = makeMetrics(10)
+
+    m1.merge(m2)
+  }
+})
+
+suite.add({
+  name: ' 100 metrics',
+  fn: function() {
+    var m1 = makeMetrics(100)
+    var m2 = makeMetrics(100)
+
+    m1.merge(m2)
+  }
+})
+
+suite.add({
+  name: '1000 metrics',
+  fn: function() {
+    var m1 = makeMetrics(1000)
+    var m2 = makeMetrics(1000)
+
+    m1.merge(m2)
+  }
+})
+
+
+suite.run()
+
+function makeMetrics(num) {
+  var metrics = new Metrics(1, {}, {})
+
+  for (var i = 0; i < num; ++i) {
+    metrics.getOrCreateMetric(shared.getMetric(), shared.getMaybeUnscoped())
+  }
+
+  return metrics
+}
+
+
+function preOptMetrics() {
+  for (var i = 0; i < 1000; ++i) {
+    var m1 = makeMetrics(i * 10)
+    var m2 = makeMetrics(i * 10)
+    m1.merge(m2)
+  }
+}
