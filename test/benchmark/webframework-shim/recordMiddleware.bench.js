a269acf1bbbda66053adc7abe27e26171c60a590
Natalie Wolfe <natalie@lifewanted.com>
2017-12-06 11:46:20 -0800

    Optimized WebFrameworkShim#recordMiddleware
    Before:
    implicit spec - function middleware x 656,349 ops/sec ±0.58% (90 runs sampled)
    implicit spec - property middleware x 487,476 ops/sec ±0.87% (83 runs sampled)
    implicit spec - mixed middleware    x 541,318 ops/sec ±1.11% (90 runs sampled)
    implicit spec - wrapper (no tx)     x 12,573,742 ops/sec ±1.23% (83 runs sampled)
    implicit spec - wrapper (tx)        x 142,756 ops/sec ±0.93% (82 runs sampled)
    explicit spec - function middleware x 632,613 ops/sec ±0.72% (87 runs sampled)
    explicit spec - property middleware x 467,427 ops/sec ±0.79% (87 runs sampled)
    explicit spec - mixed middleware    x 525,174 ops/sec ±0.66% (87 runs sampled)
    explicit spec - wrapper (no tx)     x 8,935,418 ops/sec ±1.22% (84 runs sampled)
    explicit spec - wrapper (tx)        x 135,224 ops/sec ±1.55% (82 runs sampled)
       mixed spec - function middleware x 539,172 ops/sec ±0.72% (87 runs sampled)
       mixed spec - property middleware x 415,323 ops/sec ±0.87% (84 runs sampled)
       mixed spec - mixed middleware    x 403,183 ops/sec ±0.84% (85 runs sampled)
       mixed spec - wrapper (no tx)     x 7,867,977 ops/sec ±1.15% (72 runs sampled)
       mixed spec - wrapper (tx)        x 79,219 ops/sec ±71.93% (70 runs sampled)
    
    After:
    implicit spec - function middleware x 840,644 ops/sec ±0.73% (87 runs sampled)
    implicit spec - property middleware x 572,187 ops/sec ±0.86% (84 runs sampled)
    implicit spec - mixed middleware    x 628,345 ops/sec ±0.82% (88 runs sampled)
    implicit spec - wrapper (no tx)     x 12,716,424 ops/sec ±1.38% (84 runs sampled)
    implicit spec - wrapper (tx)        x 139,980 ops/sec ±1.25% (80 runs sampled)
    explicit spec - function middleware x 673,992 ops/sec ±0.71% (83 runs sampled)
    explicit spec - property middleware x 468,613 ops/sec ±0.76% (85 runs sampled)
    explicit spec - mixed middleware    x 548,410 ops/sec ±0.82% (87 runs sampled)
    explicit spec - wrapper (no tx)     x 8,876,145 ops/sec ±1.17% (86 runs sampled)
    explicit spec - wrapper (tx)        x 126,698 ops/sec ±1.70% (80 runs sampled)
       mixed spec - function middleware x 585,164 ops/sec ±0.70% (84 runs sampled)
       mixed spec - property middleware x 451,206 ops/sec ±0.72% (84 runs sampled)
       mixed spec - mixed middleware    x 354,888 ops/sec ±1.00% (83 runs sampled)
       mixed spec - wrapper (no tx)     x 8,236,201 ops/sec ±1.13% (86 runs sampled)
       mixed spec - wrapper (tx)        x 118,544 ops/sec ±1.66% (82 runs sampled)

---
+++ test/benchmark/webframework-shim/recordMiddleware.bench.js
@@ -0,0 +1,145 @@
+'use strict'
+
+var benchmark = require('../../lib/benchmark')
+var helper = require('../../lib/agent_helper')
+var WebFrameworkShim = require('../../../lib/shim/webframework-shim')
+
+var agent = helper.loadMockedAgent()
+var shim = new WebFrameworkShim(agent, 'test-module', './')
+var suite = benchmark.createBenchmark({name: 'recordMiddleware'})
+
+var transaction = helper.runInTransaction(agent, function(tx) { return tx })
+
+shim.setFramework('benchmarks')
+
+preOptRecordMiddleware()
+
+addTests('implicit spec', implicitSpec)
+addTests('explicit spec', explicitSpec)
+addTests('   mixed spec', randomSpec)
+
+global.gc && global.gc()
+setTimeout(function() {
+  suite.run()
+}, 500)
+
+function addTests(name, speccer) {
+  var middleware = recordFunc(speccer())
+
+  suite.add({
+    name: name + ' - function middleware',
+    fn: function() {
+      return recordFunc(speccer())
+    }
+  })
+
+  suite.add({
+    name: name + ' - property middleware',
+    fn: function() {
+      return recordProperty(speccer())
+    }
+  })
+
+  suite.add({
+    name: name + ' - mixed middleware   ',
+    fn: function() {
+      return randomRecord(speccer())
+    }
+  })
+
+  suite.add({
+    name: name + ' - wrapper (no tx)    ',
+    fn: function() {
+      agent.tracer.segment = null
+      middleware(getReqd(), {}, noop)
+    }
+  })
+
+  suite.add({
+    name: name + ' - wrapper (tx)       ',
+    fn: function() {
+      agent.tracer.segment = transaction.trace.root
+      middleware(getReqd(), {}, noop)
+    }
+  })
+}
+
+function getTest() {
+  return {
+    func: function(req, res, next) {
+      next()
+    }
+  }
+}
+
+function getReqd() {
+  return {
+    params: {a: 1, b: 2, c: 3},
+    __NR_transactionInfo: {
+      transaction: transaction,
+      segmentStack: [],
+      errorHandled: false,
+      error: null
+    }
+  }
+}
+
+function implicitSpec() {
+  return {}
+}
+
+function partialSpec() {
+  return {
+    next: shim.LAST,
+    req: shim.FIRST
+  }
+}
+
+function explicitSpec() {
+  return {
+    req: shim.FIRST,
+    res: shim.SECOND,
+    next: shim.LAST,
+    name: 'funcy_name',
+    params: function(shim, fn, name, args) {
+      return args[0].params
+    }
+  }
+}
+
+function randomSpec() {
+  var n = Math.random()
+  if (n > 0.666) {
+    return implicitSpec()
+  } else if (n > 0.333) {
+    return partialSpec()
+  }
+  return explicitSpec()
+}
+
+function recordFunc(spec) {
+  return shim.recordMiddleware(getTest().func, spec)
+}
+
+function recordProperty(spec) {
+  return shim.recordMiddleware(getTest(), 'func', spec)
+}
+
+function randomRecord(spec) {
+  if (Math.random() > 0.5) {
+    return recordFunc(spec)
+  }
+  return recordProperty(spec)
+}
+
+function noop() {}
+
+function preOptRecordMiddleware() {
+  for (var i = 0; i < 100000; ++i) {
+    var m = randomRecord(randomSpec)
+    m = typeof m === 'function' ? m : m.func
+    for (var j = 0; j < 100; ++j) {
+      m(getReqd(), {}, noop)
+    }
+  }
+}
