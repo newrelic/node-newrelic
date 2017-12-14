fda40053943254cd74fe45548a4947be0feda11a
Peter Svetlichny <psvetlichny@newrelic.com>
2017-12-14 12:39:36 -0800

    chore(pg): skip all integration tests for <v4

--- test/versioned/pg-latest/pg.tap.js
+++ test/versioned/pg-latest/pg.tap.js
@@ -1,6 +1,11 @@
 'use strict'
 
 var runTests = require('./pg.common.js')
+var semver = require('semver')
+
+if (semver.satisfies(process.version, '<4.0.0')) {
+  return
+}
 
 runTests('pure JavaScript', function getClient() {
   return require('pg')
