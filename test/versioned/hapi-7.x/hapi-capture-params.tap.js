54dde3ab6b96e57037f33f0cf3f77e134b28e99c
Peter Svetlichny <psvetlichny@newrelic.com>
2017-12-28 18:02:42 -0800

    chore(versioned): remove hapi v7 test dir

--- test/versioned/hapi-7.x/hapi-capture-params.tap.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-// run capture params tests
-var runTests = require('../../integration/instrumentation/hapi/capture-params.js')
-runTests(function createServer(host, port) {
-  var hapi = require('hapi')
-  return hapi.createServer(host, port)
-})
