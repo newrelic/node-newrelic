b64ddd8e48496ddd057b3a2d29d05cfaee7669ff
Natalie Wolfe <natalie@lifewanted.com>
2018-01-11 15:19:45 -0800

    Fixed restify next error detection

--- test/versioned/restify/router.tap.js
+++ test/versioned/restify/router.tap.js
@@ -1,54 +1,138 @@
 'use strict'
 
-var tap    = require('tap')
+var tap = require('tap')
 var request = require('request').defaults({json: true})
 var helper  = require('../../lib/agent_helper')
 
 
-tap.test("Restify router introspection", function(t) {
-  t.plan(12)
+tap.test('Restify router', function(t) {
+  t.autoend()
 
-  var agent  = helper.instrumentMockedAgent()
-  var server = require('restify').createServer()
+  var agent = null
+  var server = null
 
+  t.beforeEach(function(done) {
+    agent = helper.instrumentMockedAgent()
+    server = require('restify').createServer()
+    done()
+  })
 
-  t.tearDown(function cb_tearDown() {
-    server.close(function cb_close() {
+  t.afterEach(function(done) {
+    server.close(function() {
       helper.unloadAgent(agent)
+      done()
     })
   })
 
-  // need to capture parameters
-  agent.config.capture_params = true
+  t.test('introspection', function(t) {
+    t.plan(12)
 
-  agent.on('transactionFinished', function(transaction) {
-    t.equal(transaction.name, 'WebTransaction/Restify/GET//test/:id',
-            "transaction has expected name")
-    t.equal(transaction.url, '/test/31337', "URL is left alone")
-    t.equal(transaction.statusCode, 200, "status code is OK")
-    t.equal(transaction.verb, 'GET', "HTTP method is GET")
-    t.ok(transaction.trace, "transaction has trace")
+    // need to capture parameters
+    agent.config.capture_params = true
 
-    var web = transaction.trace.root.children[0]
-    t.ok(web, "trace has web segment")
-    t.equal(web.name, transaction.name, "segment name and transaction name match")
-    t.equal(web.partialName, 'Restify/GET//test/:id',
-            "should have partial name for apdex")
-    t.equal(web.parameters.id, '31337', "namer gets parameters out of route")
-  })
+    agent.on('transactionFinished', function(transaction) {
+      t.equal(
+        transaction.name,
+        'WebTransaction/Restify/GET//test/:id',
+        'transaction has expected name'
+      )
+      t.equal(transaction.url, '/test/31337', 'URL is left alone')
+      t.equal(transaction.statusCode, 200, 'status code is OK')
+      t.equal(transaction.verb, 'GET', 'HTTP method is GET')
+      t.ok(transaction.trace, 'transaction has trace')
 
-  server.get('/test/:id', function(req, res, next) {
-    t.ok(agent.getTransaction(), "transaction is available")
-
-    res.send({status : 'ok'})
-    next()
-  })
-
-  server.listen(0, function() {
-    var port = server.address().port
-    request.get('http://localhost:' + port + '/test/31337', function(error, res, body) {
-      t.equal(res.statusCode, 200, "nothing exploded")
-      t.deepEqual(body, {status : 'ok'}, "got expected respose")
+      var web = transaction.trace.root.children[0]
+      t.ok(web, 'trace has web segment')
+      t.equal(web.name, transaction.name, 'segment name and transaction name match')
+      t.equal(
+        web.partialName,
+        'Restify/GET//test/:id',
+        'should have partial name for apdex'
+      )
+      t.equal(web.parameters.id, '31337', 'namer gets parameters out of route')
     })
+
+    server.get('/test/:id', function(req, res, next) {
+      t.ok(agent.getTransaction(), 'transaction should be available')
+
+      res.send({status : 'ok'})
+      next()
+    })
+
+    _listenAndRequest(t, '/test/31337')
   })
+
+  t.test('next(true): continue processing', function(t) {
+    t.plan(6)
+
+    server.get('/test/:id', function first(req, res, next) {
+      t.ok(agent.getTransaction(), 'transaction should be available')
+      next(true)
+    }, function second(req, res, next) {
+      t.ok(agent.getTransaction(), 'transaction should be available')
+      next(true)
+    }, function final(req, res, next) {
+      t.ok(agent.getTransaction(), 'transaction should be available')
+      res.send({status: 'ok'})
+      next()
+    })
+
+    agent.on('transactionFinished', function(tx) {
+      t.equal(tx.name, 'WebTransaction/Restify/GET//test/:id', 'should have correct name')
+    })
+
+    _listenAndRequest(t, '/test/foobar')
+  })
+
+  t.test('next(false): stop processing', function(t) {
+    t.plan(4)
+
+    server.get('/test/:id', function first(req, res, next) {
+      t.ok(agent.getTransaction(), 'transaction should be available')
+      res.send({status: 'ok'})
+      next(false)
+    }, function final(req, res, next) {
+      t.fail('should not enter this final middleware')
+      res.send({status: 'ok'})
+      next()
+    })
+
+    agent.on('transactionFinished', function(tx) {
+      t.equal(tx.name, 'WebTransaction/Restify/GET//test/:id', 'should have correct name')
+    })
+
+    _listenAndRequest(t, '/test/foobar')
+  })
+
+  t.test('next("other_route"): jump processing', function(t) {
+    t.plan(5)
+
+    server.get({name: 'first', path: '/test/:id'}, function final(req, res, next) {
+      t.ok(agent.getTransaction(), 'transaction should be available')
+      next('second')
+    })
+
+    server.get({name: 'second', path: '/other'}, function final(req, res, next) {
+      t.ok(agent.getTransaction(), 'transaction should be available')
+      res.send({status: 'ok'})
+      next()
+    })
+
+    agent.on('transactionFinished', function(tx) {
+      t.equal(tx.name, 'WebTransaction/Restify/GET//other', 'should have correct name')
+    })
+
+    _listenAndRequest(t, '/test/foobar')
+  })
+
+  function _listenAndRequest(t, route) {
+    server.listen(0, function() {
+      var port = server.address().port
+      var url = 'http://localhost:' + port + route
+      request.get(url, function(error, res, body) {
+        t.equal(res.statusCode, 200, 'nothing exploded')
+        t.deepEqual(body, {status : 'ok'}, 'got expected respose')
+      })
+    })
+  }
 })
