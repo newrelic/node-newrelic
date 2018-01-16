e38263b3d77970c6ba39c06b7a5dbcf2ee3004ec
Peter Svetlichny <psvetlichny@newrelic.com>
2018-01-16 15:01:37 -0800

    test(express): simplify attributes assertions

--- test/versioned/express/captures-params.tap.js
+++ test/versioned/express/captures-params.tap.js
@@ -3,8 +3,9 @@
 // shut up, Express
 process.env.NODE_ENV = 'test'
 
-var test = require('tap').test
+var tap = require('tap')
 var helper = require('../../lib/agent_helper')
+var HTTP_ATTS = require('../../lib/fixtures').httpAttributes
 
 
 // CONSTANTS
@@ -12,11 +13,10 @@ var TEST_HOST = 'localhost'
 var TEST_URL = 'http://' + TEST_HOST + ':'
 
 
-test("test attributes.enabled for express", function(t) {
+tap.test('test attributes.enabled for express', function(t) {
   t.autoend()
 
-  t.test("no variables", function(t) {
-    t.plan(5)
+  t.test('no variables', function(t) {
     var agent = helper.instrumentMockedAgent({
       express4: true,
       send_request_uri_attribute: true
@@ -35,10 +35,9 @@ test("test attributes.enabled for express", function(t) {
 
     // set attributes.enabled so we get the data we need.
     agent.config.attributes.enabled = true
-    agent.config.allow_all_headers = false
 
     app.get('/user/', function(req, res) {
-      t.ok(agent.getTransaction(), "transaction is available")
+      t.ok(agent.getTransaction(), 'transaction is available')
 
       res.send({yep : true})
       res.end()
@@ -47,28 +46,9 @@ test("test attributes.enabled for express", function(t) {
     agent.on('transactionFinished', function(transaction) {
       t.ok(transaction.trace, 'transaction has a trace.')
       var attributes = transaction.trace.attributes.get('transaction_tracer')
-      if (attributes.httpResponseMessage) {
-        t.deepEqual(attributes, {
-          "request.headers.host" : "localhost:" + port,
-          "request.method" : "GET",
-          "response.status" : '200',
-          "httpResponseCode": "200",
-          "httpResponseMessage": "OK",
-          "response.headers.contentLength" : 12,
-          "response.headers.contentType" : "application/json; charset=utf-8",
-          'request.uri' : "/user/"
-        }, 'attributes should only have request/response params')
-      } else {
-        t.deepEqual(attributes, {
-          "request.headers.host" : "localhost:" + port,
-          "request.method" : "GET",
-          "response.status" : '200',
-          "httpResponseCode": "200",
-          "response.headers.contentLength" : 12,
-          "response.headers.contentType" : "application/json; charset=utf-8",
-          'request.uri' : "/user/"
-        }, 'attributes should only have request/response params')
-      }
+      HTTP_ATTS.forEach(function(key) {
+        t.ok(attributes[key], 'Trace contains expected HTTP attribute: ' + key)
+      })
     })
 
     helper.randomPort(function(_port) {
@@ -79,15 +59,15 @@ test("test attributes.enabled for express", function(t) {
           if (error) t.fail(error)
 
           t.ok(/application\/json/.test(response.headers['content-type']),
-               "got correct content type")
-          t.deepEqual(JSON.parse(body), {"yep":true}, "Express correctly serves.")
+               'got correct content type')
+          t.deepEqual(JSON.parse(body), {'yep':true}, 'Express correctly serves.')
+          t.end()
         })
       })
     })
   })
 
-  t.test("route variables", function(t) {
-    t.plan(5)
+  t.test('route variables', function(t) {
     var agent = helper.instrumentMockedAgent({
       express4: true,
       send_request_uri_attribute: true
@@ -106,10 +86,9 @@ test("test attributes.enabled for express", function(t) {
 
     // set attributes.enabled so we get the data we need.
     agent.config.attributes.enabled = true
-    agent.config.allow_all_headers = false
 
     app.get('/user/:id', function(req, res) {
-      t.ok(agent.getTransaction(), "transaction is available")
+      t.ok(agent.getTransaction(), 'transaction is available')
 
       res.send({yep : true})
       res.end()
@@ -118,30 +97,7 @@ test("test attributes.enabled for express", function(t) {
     agent.on('transactionFinished', function(transaction) {
       t.ok(transaction.trace, 'transaction has a trace.')
       var attributes = transaction.trace.attributes.get('transaction_tracer')
-      if (attributes.httpResponseMessage) {
-        t.deepEqual(attributes, {
-          "request.headers.host" : "localhost:" + port,
-          "request.method" : "GET",
-          "response.status" : '200',
-          "httpResponseCode": "200",
-          "httpResponseMessage": "OK",
-          "response.headers.contentLength" : 12,
-          "response.headers.contentType" : "application/json; charset=utf-8",
-          "id" : "5",
-          'request.uri' : "/user/5"
-        }, 'attributes should include route params')
-      } else {
-        t.deepEqual(attributes, {
-          "request.headers.host" : "localhost:" + port,
-          "request.method" : "GET",
-          "response.status" : '200',
-          "httpResponseCode": "200",
-          "response.headers.contentLength" : 12,
-          "response.headers.contentType" : "application/json; charset=utf-8",
-          "id" : "5",
-          'request.uri' : "/user/5"
-        }, 'attributes should include route params')
-      }
+      t.equal(attributes.id, '5', 'Trace attributes include `id` route param')
     })
 
     helper.randomPort(function(_port) {
@@ -152,15 +108,15 @@ test("test attributes.enabled for express", function(t) {
           if (error) t.fail(error)
 
           t.ok(/application\/json/.test(response.headers['content-type']),
-               "got correct content type")
-          t.deepEqual(JSON.parse(body), {"yep":true}, "Express correctly serves.")
+               'got correct content type')
+          t.deepEqual(JSON.parse(body), {'yep':true}, 'Express correctly serves.')
+          t.end()
         })
       })
     })
   })
 
-  t.test("query variables", {timeout : 1000}, function(t) {
-    t.plan(5)
+  t.test('query variables', {timeout : 1000}, function(t) {
     var agent = helper.instrumentMockedAgent({
       express4: true,
       send_request_uri_attribute: true
@@ -179,10 +135,9 @@ test("test attributes.enabled for express", function(t) {
 
     // set attributes.enabled so we get the data we need.
     agent.config.attributes.enabled = true
-    agent.config.allow_all_headers = false
 
     app.get('/user/', function(req, res) {
-      t.ok(agent.getTransaction(), "transaction is available")
+      t.ok(agent.getTransaction(), 'transaction is available')
 
       res.send({yep : true})
       res.end()
@@ -191,30 +146,7 @@ test("test attributes.enabled for express", function(t) {
     agent.on('transactionFinished', function(transaction) {
       t.ok(transaction.trace, 'transaction has a trace.')
       var attributes = transaction.trace.attributes.get('transaction_tracer')
-      if (attributes.httpResponseMessage) {
-        t.deepEqual(attributes, {
-          "request.headers.host" : "localhost:" + port,
-          "request.method" : "GET",
-          "response.status" : '200',
-          "httpResponseCode": "200",
-          "httpResponseMessage": "OK",
-          "response.headers.contentLength" : 12,
-          "response.headers.contentType" : "application/json; charset=utf-8",
-          "name" : "bob",
-          'request.uri' : "/user/"
-        }, 'attributes should include query params')
-      } else {
-        t.deepEqual(attributes, {
-          "request.headers.host" : "localhost:" + port,
-          "request.method" : "GET",
-          "response.status" : '200',
-          "httpResponseCode": "200",
-          "response.headers.contentLength" : 12,
-          "response.headers.contentType" : "application/json; charset=utf-8",
-          "name" : "bob",
-          'request.uri' : "/user/"
-        }, 'attributes should include query params')
-      }
+      t.equal(attributes.name, 'bob', 'Trace attributes include `name` query param')
     })
 
     helper.randomPort(function(_port) {
@@ -225,15 +157,15 @@ test("test attributes.enabled for express", function(t) {
           if (error) t.fail(error)
 
           t.ok(/application\/json/.test(response.headers['content-type']),
-               "got correct content type")
-          t.deepEqual(JSON.parse(body), {"yep":true}, "Express correctly serves.")
+               'got correct content type')
+          t.deepEqual(JSON.parse(body), {'yep':true}, 'Express correctly serves.')
+          t.end()
         })
       })
     })
   })
 
-  t.test("route and query variables", function(t) {
-    t.plan(5)
+  t.test('route and query variables', function(t) {
     var agent = helper.instrumentMockedAgent({
       express4: true,
       send_request_uri_attribute: true
@@ -252,10 +184,9 @@ test("test attributes.enabled for express", function(t) {
 
     // set attributes.enabled so we get the data we need.
     agent.config.attributes.enabled = true
-    agent.config.allow_all_headers = false
 
     app.get('/user/:id', function(req, res) {
-      t.ok(agent.getTransaction(), "transaction is available")
+      t.ok(agent.getTransaction(), 'transaction is available')
 
       res.send({yep : true})
       res.end()
@@ -264,32 +195,8 @@ test("test attributes.enabled for express", function(t) {
     agent.on('transactionFinished', function(transaction) {
       t.ok(transaction.trace, 'transaction has a trace.')
       var attributes = transaction.trace.attributes.get('transaction_tracer')
-      if (attributes.httpResponseMessage) {
-        t.deepEqual(attributes, {
-          "request.headers.host" : "localhost:" + port,
-          "request.method" : "GET",
-          "response.status" : '200',
-          "httpResponseCode": "200",
-          "httpResponseMessage": "OK",
-          "response.headers.contentLength" : 12,
-          "response.headers.contentType" : "application/json; charset=utf-8",
-          "id" : "5",
-          "name" : "bob",
-          'request.uri' : "/user/5"
-        }, 'attributes should include query params')
-      } else {
-        t.deepEqual(attributes, {
-          "request.headers.host" : "localhost:" + port,
-          "request.method" : "GET",
-          "response.status" : '200',
-          "httpResponseCode": "200",
-          "response.headers.contentLength" : 12,
-          "response.headers.contentType" : "application/json; charset=utf-8",
-          "id" : "5",
-          "name" : "bob",
-          'request.uri' : "/user/5"
-        }, 'attributes should include query params')
-      }
+      t.equal(attributes.id, '5', 'Trace attributes include `id` route param')
+      t.equal(attributes.name, 'bob', 'Trace attributes include `name` query param')
     })
 
     helper.randomPort(function(_port) {
@@ -300,14 +207,15 @@ test("test attributes.enabled for express", function(t) {
           if (error) t.fail(error)
 
           t.ok(/application\/json/.test(response.headers['content-type']),
-               "got correct content type")
-          t.deepEqual(JSON.parse(body), {"yep":true}, "Express correctly serves.")
+               'got correct content type')
+          t.deepEqual(JSON.parse(body), {'yep':true}, 'Express correctly serves.')
+          t.end()
         })
       })
     })
   })
 
-  t.test("query params mask route attributes", function(t) {
+  t.test('query params mask route attributes', function(t) {
     var agent = helper.instrumentMockedAgent({
       express4: true,
       send_request_uri_attribute: true
@@ -326,36 +234,14 @@ test("test attributes.enabled for express", function(t) {
 
     // set attributes.enabled so we get the data we need.
     agent.config.attributes.enabled = true
-    agent.config.allow_all_headers = false
 
     app.get('/user/:id', function(req, res) {
       res.end()
     })
 
     agent.on('transactionFinished', function(transaction) {
-      var expectedValues = {
-            "request.headers.host" : "localhost:" + port,
-            "request.method" : "GET",
-            "response.status" : '200',
-            "httpResponseCode": "200",
-            "id" : 5,
-            'request.uri': "/user/5"
-      }
-      var possibleExpected = {
-        "httpResponseMessage": "OK",
-        "response.headers.contentLength": 0
-      }
-      var keys = ['response.headers.contentLength', 'httpResponseMessage']
       var attributes = transaction.trace.attributes.get('transaction_tracer')
-      for (var i = 0; i < keys.length; i++) {
-        var key = keys[i]
-        var value = attributes[key]
-        if (value !== undefined) {
-          expectedValues[key] = possibleExpected[key]
-        }
-      }
-      t.deepEqual(attributes,
-          expectedValues, 'attributes should include query params')
+      t.equal(attributes.id, '5', 'attributes should include query params')
       t.end()
     })
 
