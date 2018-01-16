871a1849bc6aa7b5585043baa71712d682196276
Peter Svetlichny <psvetlichny@newrelic.com>
2018-01-16 15:02:45 -0800

    test(restify): simplify attributes assertions

--- test/versioned/restify/capture-params.tap.js
+++ test/versioned/restify/capture-params.tap.js
@@ -3,21 +3,19 @@
 var test    = require('tap').test
 var request = require('request').defaults({json: true})
 var helper  = require('../../lib/agent_helper')
+var HTTP_ATTS = require('../../lib/fixtures').httpAttributes
 
 
 test("Restify capture params introspection", function(t) {
-  t.plan(4)
+  t.autoend()
 
   t.test('simple case with no params', function(t) {
-    t.plan(5)
-
     var agent  = helper.instrumentMockedAgent({ send_request_uri_attribute: true })
     var server = require('restify').createServer()
     var port = null
 
 
     agent.config.attributes.enabled = true
-    agent.config.allow_all_headers = false
 
     t.tearDown(function() {
       server.close()
@@ -28,30 +26,9 @@ test("Restify capture params introspection", function(t) {
       t.ok(transaction.trace, 'transaction has a trace.')
       // on older versions of node response messages aren't included
       var attributes = transaction.trace.attributes.get('transaction_tracer')
-      if (attributes.httpResponseMessage) {
-        t.deepEqual(attributes, {
-          "request.headers.accept" : "application/json",
-          "request.headers.host" : "localhost:" + port,
-          "request.method" : "GET",
-          "response.status" : '200',
-          "httpResponseCode": "200",
-          "httpResponseMessage": "OK",
-          "response.headers.contentLength" : 15,
-          "response.headers.contentType" : "application/json",
-          'request.uri' : "/test"
-        }, 'parameters should only have request/response params')
-      } else {
-        t.deepEqual(attributes, {
-          "request.headers.accept" : "application/json",
-          "request.headers.host" : "localhost:" + port,
-          "request.method" : "GET",
-          "response.status" : '200',
-          "httpResponseCode": "200",
-          "response.headers.contentLength" : 15,
-          "response.headers.contentType" : "application/json",
-          'request.uri' : "/test"
-        }, 'parameters should only have request/response params')
-      }
+      HTTP_ATTS.forEach(function(key) {
+        t.ok(attributes[key], 'Trace contains expected HTTP attribute: ' + key)
+      })
     })
 
     server.get('/test', function(req, res, next) {
@@ -66,20 +43,18 @@ test("Restify capture params introspection", function(t) {
       request.get('http://localhost:' + port + '/test', function(error, res, body) {
         t.equal(res.statusCode, 200, "nothing exploded")
         t.deepEqual(body, {status : 'ok'}, "got expected respose")
+        t.end()
       })
     })
   })
 
   t.test('case with route params', function(t) {
-    t.plan(5)
-
     var agent  = helper.instrumentMockedAgent({ send_request_uri_attribute: true })
     var server = require('restify').createServer()
     var port = null
 
 
     agent.config.attributes.enabled = true
-    agent.config.allow_all_headers = false
 
     t.tearDown(function() {
       server.close()
@@ -90,32 +65,7 @@ test("Restify capture params introspection", function(t) {
       t.ok(transaction.trace, 'transaction has a trace.')
       // on older versions of node response messages aren't included
       var attributes = transaction.trace.attributes.get('transaction_tracer')
-      if (attributes.httpResponseMessage) {
-        t.deepEqual(attributes, {
-          "request.headers.accept" : "application/json",
-          "request.headers.host" : "localhost:" + port,
-          "request.method" : "GET",
-          "response.status" : '200',
-          "httpResponseCode": "200",
-          "httpResponseMessage": "OK",
-          "response.headers.contentLength" : 15,
-          "response.headers.contentType" : "application/json",
-          "id" : "1337",
-          'request.uri' : "/test/1337"
-        }, 'parameters should have id')
-      } else {
-        t.deepEqual(attributes, {
-          "request.headers.accept" : "application/json",
-          "request.headers.host" : "localhost:" + port,
-          "request.method" : "GET",
-          "response.status" : '200',
-          "httpResponseCode": "200",
-          "response.headers.contentLength" : 15,
-          "response.headers.contentType" : "application/json",
-          "id" : "1337",
-          'request.uri' : "/test/1337"
-        }, 'parameters should have id')
-      }
+      t.equal(attributes.id, '1337', 'Trace attributes include `id` route param')
     })
 
     server.get('/test/:id', function(req, res, next) {
@@ -130,20 +80,18 @@ test("Restify capture params introspection", function(t) {
       request.get('http://localhost:' + port + '/test/1337', function(error, res, body) {
         t.equal(res.statusCode, 200, "nothing exploded")
         t.deepEqual(body, {status : 'ok'}, "got expected respose")
+        t.end()
       })
     })
   })
 
   t.test('case with query params', function(t) {
-    t.plan(5)
-
     var agent  = helper.instrumentMockedAgent({ send_request_uri_attribute: true })
     var server = require('restify').createServer()
     var port = null
 
 
     agent.config.attributes.enabled = true
-    agent.config.allow_all_headers = false
 
     t.tearDown(function() {
       server.close()
@@ -154,32 +102,7 @@ test("Restify capture params introspection", function(t) {
       t.ok(transaction.trace, 'transaction has a trace.')
       // on older versions of node response messages aren't included
       var attributes = transaction.trace.attributes.get('transaction_tracer')
-      if (attributes.httpResponseMessage) {
-        t.deepEqual(attributes, {
-          "request.headers.accept" : "application/json",
-          "request.headers.host" : "localhost:" + port,
-          "request.method" : "GET",
-          "response.status" : '200',
-          "httpResponseCode": "200",
-          "httpResponseMessage": "OK",
-          "response.headers.contentLength" : 15,
-          "response.headers.contentType" : "application/json",
-          "name" : "restify",
-          'request.uri' : "/test"
-        }, 'parameters should have name')
-      } else {
-        t.deepEqual(attributes, {
-          "request.headers.accept" : "application/json",
-          "request.headers.host" : "localhost:" + port,
-          "request.method" : "GET",
-          "response.status" : '200',
-          "httpResponseCode": "200",
-          "response.headers.contentLength" : 15,
-          "response.headers.contentType" : "application/json",
-          "name" : "restify",
-          'request.uri' : "/test"
-        }, 'parameters should have name')
-      }
+      t.equal(attributes.name, 'restify', 'Trace attributes include `name` query param')
     })
 
     server.get('/test', function(req, res, next) {
@@ -195,20 +118,18 @@ test("Restify capture params introspection", function(t) {
       request.get(url, function(error, res, body) {
         t.equal(res.statusCode, 200, "nothing exploded")
         t.deepEqual(body, {status : 'ok'}, "got expected respose")
+        t.end()
       })
     })
   })
 
   t.test('case with both route and query params', function(t) {
-    t.plan(5)
-
     var agent  = helper.instrumentMockedAgent({ send_request_uri_attribute: true })
     var server = require('restify').createServer()
     var port = null
 
 
     agent.config.attributes.enabled = true
-    agent.config.allow_all_headers = false
 
     t.tearDown(function() {
       server.close()
@@ -219,34 +140,8 @@ test("Restify capture params introspection", function(t) {
       t.ok(transaction.trace, 'transaction has a trace.')
       // on older versions of node response messages aren't included
       var attributes = transaction.trace.attributes.get('transaction_tracer')
-      if (attributes.httpResponseMessage) {
-        t.deepEqual(attributes, {
-          "request.headers.accept" : "application/json",
-          "request.headers.host" : "localhost:" + port,
-          "request.method" : "GET",
-          "response.status" : '200',
-          "httpResponseCode": "200",
-          "httpResponseMessage": "OK",
-          "response.headers.contentLength" : 15,
-          "response.headers.contentType" : "application/json",
-          "id" : "1337",
-          "name" : "restify",
-          'request.uri' : "/test/1337"
-        }, 'parameters should have id and name')
-      } else {
-        t.deepEqual(attributes, {
-          "request.headers.accept" : "application/json",
-          "request.headers.host" : "localhost:" + port,
-          "request.method" : "GET",
-          "response.status" : '200',
-          "httpResponseCode": "200",
-          "response.headers.contentLength" : 15,
-          "response.headers.contentType" : "application/json",
-          "id" : "1337",
-          "name" : "restify",
-          'request.uri' : "/test/1337"
-        }, 'parameters should have id and name')
-      }
+      t.equal(attributes.id, '1337', 'Trace attributes include `id` route param')
+      t.equal(attributes.name, 'restify', 'Trace attributes include `name` query param')
     })
 
     server.get('/test/:id', function(req, res, next) {
@@ -262,6 +157,7 @@ test("Restify capture params introspection", function(t) {
       request.get(url, function(error, res, body) {
         t.equal(res.statusCode, 200, "nothing exploded")
         t.deepEqual(body, {status : 'ok'}, "got expected respose")
+        t.end()
       })
     })
   })
