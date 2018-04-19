69733603d4a1ab5c694870c8191ef0360dfd4c13
Peter Svetlichny <psvetlichny@newrelic.com>
2018-04-19 10:25:31 -0700

    test(hapi): add route handler tests

--- test/versioned/hapi/hapi-pre-17/router-v16.tap.js
+++ test/versioned/hapi/hapi-pre-17/router-v16.tap.js
@@ -87,6 +87,34 @@ tap.test('Hapi router introspection', function(t) {
     })
   })
 
+  t.test('using route handler outside of config object', function(t) {
+    agent.on('transactionFinished', utils.verifier(t))
+
+    var route = {
+      method: 'GET',
+      path: '/test/{id}',
+      config: {},
+      handler: function(req, reply) {
+        t.ok(agent.getTransaction(), 'transaction is available')
+        reply({status: 'ok'})
+      }
+    }
+    server.route(route)
+
+    server.start(function() {
+      port = server.info.port
+      var params = {
+        uri: 'http://localhost:' + port + '/test/31337',
+        json: true
+      }
+      request.get(params, function(error, res, body) {
+        t.equal(res.statusCode, 200, 'nothing exploded')
+        t.deepEqual(body, {status: 'ok'}, 'got expected response')
+        t.end()
+      })
+    })
+  })
+
   t.test('using `pre` config option', function(t) {
     agent.on('transactionFinished', utils.verifier(t))
 
