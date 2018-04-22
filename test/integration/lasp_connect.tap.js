8edfc8003a1331ce8dd176a98b01fe89946a6f82
Bryan Clement <bclement@newrelic.com>
2018-04-22 12:07:14 -0700

    agent will no longer retry when missing required security policies

--- test/integration/lasp_connect.tap.js
+++ test/integration/lasp_connect.tap.js
@@ -40,3 +40,32 @@ tap.test('connecting with a LASP token should not error', function(t) {
     })
   })
 })
+
+tap.test('missing required policies should error', function(t) {
+  var config = configurator.initialize({
+    app_name: 'node.js Tests',
+    license_key: '5b3ca1e9fb083436ca49a444981e0e5a632907c9',
+    security_policies_token: 'ffff-ffff-ffff-ffff',
+    host: 'staging-collector.newrelic.com',
+    utilization: {
+      detect_aws: false,
+      detect_pcf: false,
+      detect_azure: false,
+      detect_gcp: false,
+      detect_docker: false
+    },
+    logging: {
+      level: 'trace'
+    }
+  })
+  var agent = new Agent(config)
+
+  agent.start(function(error, returned) {
+    t.ok(error, 'got an error while attempting to connect')
+    t.ok(returned, 'got boot configuration')
+    t.notOk(returned.agent_run_id, 'got run ID')
+    t.notOk(agent.config.run_id, 'run ID set in configuration')
+    t.equal(agent._state, 'errored')
+    t.end()
+  })
+})
