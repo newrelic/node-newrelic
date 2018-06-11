526d8157bac73c5371af15f1269248dceb53f97f
Natalie Wolfe <natalie@lifewanted.com>
2018-06-11 14:23:18 -0700

    Test cleanup from harvest refactor

--- test/integration/api/error-data.tap.js
+++ test/integration/api/error-data.tap.js
@@ -3,7 +3,6 @@
 var test = require('tap').test
 var configurator = require('../../../lib/config')
 var Agent = require('../../../lib/agent')
-var CollectorAPI = require('../../../lib/collector/api')
 
 
 test('Collector API should send errors to staging-collector.newrelic.com', function(t) {
@@ -24,11 +23,10 @@ test('Collector API should send errors to staging-collector.newrelic.com', funct
     }
   })
   var agent = new Agent(config)
-  var api = new CollectorAPI(agent)
-
+  var api = agent.collector
 
   api.connect(function(error) {
-    t.notOk(error, 'connected without error')
+    t.error(error, 'connected without error')
 
     var transaction
     var proxy = agent.tracer.transactionProxy(function() {
@@ -45,11 +43,14 @@ test('Collector API should send errors to staging-collector.newrelic.com', funct
     ]
 
     api.errorData(payload, function(error, response, json) {
-      t.notOk(error, 'sent errors without error')
+      t.error(error, 'sent errors without error')
       t.notOk(response, 'return value is null')
       t.deepEqual(json, {return_value: null}, 'got raw return value')
 
-      t.end()
+      agent.stop((err) => {
+        t.error(err, 'should not fail to stop')
+        t.end()
+      })
     })
   })
 })
