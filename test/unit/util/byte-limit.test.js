f420b01ed3d7d9c922cc6313de548c48cd4e35cf
Peter Svetlichny <psvetlichny@newrelic.com>
2018-07-11 14:51:48 -0700

    test(utils): add multibyte character truncate test

--- test/unit/util/byte-limit.test.js
+++ test/unit/util/byte-limit.test.js
@@ -5,33 +5,35 @@ const byteUtils = require('../../../lib/util/byte-limit')
 
 describe('byte-limit', () => {
   describe('#isValidLength', () => {
-    it('verifies string is within given byte limit', (done) => {
+    it('verifies string is within given byte limit', () => {
       const str = '123456789'
       const isValidLength = byteUtils.isValidLength(str, 255)
       expect(isValidLength).to.be.true
-      done()
     })
 
-    it('verifies string is larger than given byte limit', (done) => {
+    it('verifies string is larger than given byte limit', () => {
       const str = '123456789'
       const isValidLength = byteUtils.isValidLength(str, 2)
       expect(isValidLength).to.be.false
-      done()
     })
   })
 
   describe('#truncate', () => {
-    it('truncates string value to given limit', (done) => {
+    it('truncates string value to given limit', () => {
       let str = '123456789'
       str = byteUtils.truncate(str, 5)
       expect(str).to.equal('12345')
-      done()
     })
-    it('returns original string if within limit', (done) => {
+    it('returns original string if within limit', () => {
       let str = '123456789'
       str = byteUtils.truncate(str, 10)
       expect(str).to.equal('123456789')
-      done()
+    })
+    it('respects multibyte characters', () => {
+      let str = '\uD87E\uDC04\uD87E\uDC04'
+      expect(Buffer.byteLength(str, 'utf8')).to.equal(8)
+      str = byteUtils.truncate(str, 3)
+      expect(str).to.equal('\uD87E')
     })
   })
 })
