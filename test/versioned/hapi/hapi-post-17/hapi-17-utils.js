2d2d7ea6b439325e4ba2fcc4eb3630e46b21253e
Peter Svetlichny <psvetlichny@newrelic.com>
2018-07-12 15:42:34 -0700

    test(hapi): randomly choose server ref for hapi@17 tests

--- test/versioned/hapi/hapi-post-17/hapi-17-utils.js
+++ test/versioned/hapi/hapi-post-17/hapi-17-utils.js
@@ -7,6 +7,11 @@ exports.getServer = function getServer(cfg) {
   var opts = cfg.options || {}
   var hapi = cfg.hapi || require('hapi')
 
-  // v17
-  return new hapi.Server(Object.assign({}, opts, { host: host, port: port }))
+  // v17 exports two references to the server object,
+  // so we'll let fate decide which to use for a given test
+  const servers = ['Server', 'server']
+  const server = servers[Math.round(Math.random())]
+
+  console.log(`Testing with hapi.${server}`)
+  return hapi[server](Object.assign({}, opts, {host, port}))
 }
