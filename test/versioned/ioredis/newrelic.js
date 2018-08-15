ba2d8a5deb98985eb23478b9952c8666a7d76fca
Natalie Wolfe <natalie@lifewanted.com>
2018-08-15 11:29:58 -0700

    Moved ioredis tests to versioned

---
+++ test/versioned/ioredis/newrelic.js
@@ -0,0 +1,16 @@
+'use strict'
+
+exports.config = {
+  app_name: ['My Application'],
+  license_key: 'license key here',
+  utilization: {
+    detect_aws: false,
+    detect_pcf: false,
+    detect_azure: false,
+    detect_gcp: false,
+    detect_docker: false
+  },
+  logging: {
+    enabled: false,
+  }
+}
