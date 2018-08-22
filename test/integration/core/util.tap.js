06646783ed69cf5b5964f8dac6d38e772e9d83b9
Bryan Clement <bclement@newrelic.com>
2018-08-21 17:48:32 -0700

    the function being wrapped will now be the __proto__ of the wrapped version
    now property look ups can accurately fall through to the function
    instance being wrapped, returning the value off the original function

---
+++ test/integration/core/util.tap.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const test = require('tap').test
+const util = require('util')
+const helper = require('../../lib/agent_helper')
+
+test('promisify', function(t) {
+  t.plan(1)
+  t.test('should work on setTimeout', function(t) {
+    t.plan(1)
+    var agent = helper.instrumentMockedAgent()
+    t.tearDown(function() {
+      helper.unloadAgent(agent)
+    })
+    let asyncTimeout = util.promisify(setTimeout)
+    asyncTimeout(10)
+      .then(() => {
+        t.ok('should evaluate properly')
+        t.end()
+      })
+      .catch(ex => {
+        t.error(ex)
+      })
+  })
+})
