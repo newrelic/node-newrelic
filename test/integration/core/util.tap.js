6f337d77f3fa4990ecc9c7c1e6dd32592c24d926
Michael Goin <mgoin@newrelic.com>
2018-08-30 17:00:21 -0700

    Converts child_process exec/execFile to shim style intstrumentation. Updates promisify tests to catch when symbols aren't handle appropriately.

--- test/integration/core/util.tap.js
+++ test/integration/core/util.tap.js
@@ -40,32 +40,34 @@ test('promisify', {skip: !util.promisify}, function(t) {
       })
   })
   t.test('should work on child_process.exec', function(t) {
-    t.plan(1)
+    t.plan(3)
     var agent = helper.instrumentMockedAgent()
     t.tearDown(function() {
       helper.unloadAgent(agent)
     })
     let asyncExec = util.promisify(require('child_process').exec)
     asyncExec('ls')
-      .then(() => {
-        t.ok(true, 'should evaluate properly')
-        t.end()
+      .then((result) => {
+        t.type(result, 'object', 'first argument should be object')
+        t.type(result.stdout, 'string', 'should have string stdout')
+        t.type(result.stderr, 'string', 'should have string stderr')
       })
       .catch(ex => {
         t.error(ex)
       })
   })
   t.test('should work on child_process.execFile', function(t) {
-    t.plan(1)
+    t.plan(3)
     var agent = helper.instrumentMockedAgent()
     t.tearDown(function() {
       helper.unloadAgent(agent)
     })
     let asyncExec = util.promisify(require('child_process').execFile)
     asyncExec(path.join(__dirname, 'exec-me.js'))
-      .then(() => {
-        t.ok(true, 'should evaluate properly')
-        t.end()
+      .then((result) => {
+        t.type(result, 'object', 'first argument should be object')
+        t.type(result.stdout, 'string', 'should have string stdout')
+        t.type(result.stderr, 'string', 'should have string stderr')
       })
       .catch(ex => {
         t.error(ex)
