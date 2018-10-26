0e7d79c28a8902532321ec9d1906af78833dd922
Bryan Clement <bclement@newrelic.com>
2018-10-26 14:01:47 -0700

    updated test with feature flag

--- test/integration/agent/start-stop.tap.js
+++ test/integration/agent/start-stop.tap.js
@@ -44,6 +44,9 @@ tap.test('Agent should not connect to collector in serverless mode', (t) => {
     app_name: 'node.js Tests',
     license_key: 'd67afc830dab717fd163bfcb0b8b88423e9a1a3b',
     serverless_mode: true,
+    feature_flag: {
+      serverless_mode: true
+    },
     logging: {
       level: 'trace'
     }
