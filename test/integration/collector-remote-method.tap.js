022b77606f821e7767f9ba96912ad2eb9c93765c
Peter Svetlichny <psvetlichny@newrelic.com>
2018-11-05 16:01:38 -0800

    more linting

--- test/integration/collector-remote-method.tap.js
+++ test/integration/collector-remote-method.tap.js
@@ -1,70 +1,67 @@
 'use strict'
 
-var tap = require('tap')
-var read = require('fs').readFileSync
-var join = require('path').join
-var https = require('https')
-var url = require('url')
-var collector = require('../lib/fake-collector')
-var RemoteMethod = require('../../lib/collector/remote-method')
+const tap = require('tap')
+const read = require('fs').readFileSync
+const join = require('path').join
+const https = require('https')
+const url = require('url')
+const collector = require('../lib/fake-collector')
+const RemoteMethod = require('../../lib/collector/remote-method')
 
-// Specifying custom certs on 0.10 sends the process into a spin lock,
-// so we skip it.
-tap.test('DataSender (callback style) talking to fake collector', function(t) {
-    var config = {
-      host: 'ssl.lvh.me',
-      port: 8765,
-      run_id: 1337,
-      ssl: true,
-      license_key: 'whatever',
-      version: '0',
-      max_payload_size_in_bytes: 1000000
+tap.test('DataSender (callback style) talking to fake collector', (t) => {
+  const config = {
+    host: 'ssl.lvh.me',
+    port: 8765,
+    run_id: 1337,
+    ssl: true,
+    license_key: 'whatever',
+    version: '0',
+    max_payload_size_in_bytes: 1000000
+  }
+  config.certificates = [
+    read(join(__dirname, '../lib/ca-certificate.crt'), 'utf8')
+  ]
+  const method = new RemoteMethod('preconnect', config)
+
+  collector({port: 8765}, (error, server) => {
+    if (error) {
+      t.fail(error)
+      return t.end()
     }
-    config.certificates = [
-      read(join(__dirname, '../lib/ca-certificate.crt'), 'utf8')
-    ]
-    var method = new RemoteMethod('preconnect', config)
 
-    collector({port: 8765}, function(error, server) {
+    t.tearDown(() => {
+      server.close()
+    })
+
+    method._post('[]', (error, results, json) => {
       if (error) {
         t.fail(error)
         return t.end()
       }
 
-      t.tearDown(function() {
-        server.close()
-      })
+      t.equal(results, 'collector-1.lvh.me:8089', 'parsed result should come through')
+      t.notOk(json.validations, 'fake collector should find no irregularities')
+      t.equal(
+        json.return_value,
+        'collector-1.lvh.me:8089',
+        'collector returns expected collector redirect'
+      )
 
-      method._post('[]', function(error, results, json) {
-        if (error) {
-          t.fail(error)
-          return t.end()
-        }
-
-        t.equal(results, 'collector-1.lvh.me:8089', 'parsed result should come through')
-        t.notOk(json.validations, 'fake collector should find no irregularities')
-        t.equal(
-          json.return_value,
-          'collector-1.lvh.me:8089',
-          'collector returns expected collector redirect'
-        )
-
-        t.end()
-      })
+      t.end()
     })
-  }
-)
+  })
+})
 
-tap.test('remote method to preconnect', function(t) {
+tap.test('remote method to preconnect', (t) => {
   t.plan(1)
 
-  t.test('https with custom certificate', function(t) {
+  t.test('https with custom certificate', (t) => {
     t.plan(3)
-    var method = createRemoteMethod()
+    const method = createRemoteMethod()
 
     // create mock collector
-    startMockCollector(t, function() {
-      method.invoke([], function(error, returnValue) {
+    startMockCollector(t, () => {
+      method.invoke([], (error, returnValue) => {
         validateResponse(t, error, returnValue)
         t.end()
       })
@@ -77,7 +74,7 @@ tap.test('remote method to preconnect', function(t) {
   }
 
   function createRemoteMethod() {
-    var config = {
+    const config = {
       host: 'ssl.lvh.me',
       port: 8765,
       ssl: true,
@@ -88,29 +85,29 @@ tap.test('remote method to preconnect', function(t) {
       read(join(__dirname, '../lib/ca-certificate.crt'), 'utf8')
     ]
 
-    var method = new RemoteMethod('preconnect', config)
+    const method = new RemoteMethod('preconnect', config)
     return method
   }
 
   function startMockCollector(t, startedCallback) {
-    var opts = {
+    const opts = {
       port: 8765
     }
 
     opts.key = read(join(__dirname, '../lib/test-key.key'))
     opts.cert = read(join(__dirname, '../lib/self-signed-test-certificate.crt'))
-    var server = https.createServer(opts, responder)
+    const server = https.createServer(opts, responder)
 
-    server.listen(8765, function(err) {
+    server.listen(8765, (err) => {
       startedCallback(err, this)
     })
 
-    t.tearDown(function() {
+    t.tearDown(() => {
       server.close()
     })
 
     function responder(req, res) {
-      var parsed = url.parse(req.url, true)
+      const parsed = url.parse(req.url, true)
       t.equal(parsed.query.method, 'preconnect', 'should get redirect host request')
       res.write(JSON.stringify({return_value: 'some-collector-url'}))
       res.end()
