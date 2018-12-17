d4a36dc2cf815b7db6b6ff31a93782e1cbadcc36
Michael Goin <mgoin@newrelic.com>
2018-12-17 15:42:44 -0800

    Adds error_event_data to response handling tests.

--- test/integration/newrelic-response-handling.tap.js
+++ test/integration/newrelic-response-handling.tap.js
@@ -9,6 +9,7 @@ const helper = require('../lib/agent_helper')
 const TEST_DOMAIN = 'test-collector.newrelic.com'
 const TEST_COLLECTOR_URL = `https://${TEST_DOMAIN}`
 const RUN_ID = 'runId'
+const PROTOCOL_VERSION = 17
 
 tap.test('NewRelic server response code handling', (t) => {
   const crossAgentTestFile = path.resolve(
@@ -27,119 +28,52 @@ tap.test('NewRelic server response code handling', (t) => {
     t.autoend()
     t.plan(testCases.length)
 
-    let preconnect
-    let connect
-    let settings
-
-    let restartPreconnect
-    let restartConnect
-    let restartSettings
-
-    let shutdown
-
-    let agent
-
-    t.beforeEach((done) => {
-      nock.disableNetConnect()
-
-      preconnect = nockRequest('preconnect')
-        .reply(200, {return_value: TEST_DOMAIN})
-
-      connect = nockRequest('connect')
-        .reply(200, {return_value: {agent_run_id: RUN_ID}})
-
-      settings = nockRequest('agent_settings', RUN_ID)
-        .reply(200, {return_value: []})
-
-      agent = helper.loadMockedAgent({
-        license_key: 'license key here',
-        apdex_t: 0.005,
-        host: TEST_DOMAIN,
-        feature_flag: {
-          // turn off native metrics to avoid unwanted gc metrics
-          native_metrics: false
-        }
-      })
-
-      // We don't want any harvests before our manually triggered harvest
-      agent.config.no_immediate_harvest = true
-      agent._stopHarvester()
-
-      done()
-    })
-
-    t.afterEach((done) => {
-      if (agent.isRunning) { // this is bullshit, figure out realness
-        agent.stop()
-      }
-
-      helper.unloadAgent(agent)
-
-      if (!nock.isDone()) {
-        console.error('Cleaning pending mocks: %j', nock.pendingMocks())
-        nock.cleanAll()
-      }
-
-      agent = null
-
-      preconnect = null
-      connect = null
-      settings = null
-
-      restartPreconnect = null
-      restartConnect = null
-      restartSettings = null
-
-      shutdown = null
-
-      nock.enableNetConnect()
-
-      done()
-    })
-
     testCases.forEach((testCase) => {
       const statusCode = testCase.code
       const testName = `Status code: ${statusCode}`
 
-      t.test(testName, (subTest) => {
-        const sendMetrics = nockRequest('metric_data', RUN_ID).reply(statusCode)
+      t.test(testName, (statusCodeTest) => {
+        let preconnect
+        let connect
+        let settings
 
-        agent.start((error) => {
-          if (error) {
-            throw error
-          }
+        let restartPreconnect
+        let restartConnect
+        let restartSettings
 
-          let disconnected = false
-          agent.on('disconnected', () => {
-            disconnected = true
+        let shutdown
+
+        let agent
+
+        statusCodeTest.beforeEach((done) => {
+          nock.disableNetConnect()
+
+          preconnect = nockRequest('preconnect')
+            .reply(200, {return_value: TEST_DOMAIN})
+
+          connect = nockRequest('connect')
+            .reply(200, {return_value: {agent_run_id: RUN_ID}})
+
+          settings = nockRequest('agent_settings', RUN_ID)
+            .reply(200, {return_value: []})
+
+          agent = helper.loadMockedAgent({
+            license_key: 'license key here',
+            apdex_t: 0.005,
+            host: TEST_DOMAIN,
+            feature_flag: {
+              // turn off native metrics to avoid unwanted gc metrics
+              native_metrics: false
+            }
           })
 
-          let connecting = false
-          agent.on('connecting', () => {
-            connecting = true
-          })
-
-          subTest.ok(preconnect.isDone(), 'requested preconnect')
-          subTest.ok(connect.isDone(), 'requested connect')
-          subTest.ok(settings.isDone(), 'requested settings')
+          // We don't want any harvests before our manually triggered harvest
+          agent.config.no_immediate_harvest = true
+          agent._stopHarvester()
 
 
-          if (testCase.restart) {
-            restartPreconnect = nockRequest('preconnect')
-              .reply(200, {return_value: TEST_DOMAIN})
-
-            restartConnect = nockRequest('connect')
-              .reply(200, {return_value: {agent_run_id: RUN_ID}})
-
-            restartSettings = nockRequest('agent_settings', RUN_ID)
-              .reply(200, {return_value: []})
-          }
-
-
-          if (testCase.disconnect) {
-            shutdown = nockRequest('shutdown', RUN_ID).reply(200)
-          }
-
+          // create data for testing...
+          // TODO: can prob just create all types, allow some to fail per noted below
           const metric = agent.metrics.getOrCreateMetric(
             'myMetric'
           )
@@ -149,52 +83,148 @@ tap.test('NewRelic server response code handling', (t) => {
           // still successful when another harvest step fails
           agent.errors.addUserError(null, new Error('Why?!!!?!!'))
 
-          agent.harvest((error) => {
-            if (error) {
-              subTest.ok(error, 'should have nock error from error harvest step')
-
-              const isErrorEndpoint = error.message.includes('method=error')
-              subTest.ok(isErrorEndpoint, 'should be failure for error endpoint')
-            }
-
-            subTest.ok(sendMetrics.isDone(), 'sent metrics')
-
-            if (testCase.disconnect) {
-              subTest.ok(disconnected, 'should have disconnected')
-              subTest.notOk(connecting, 'should not have reconnected')
-
-              subTest.ok(shutdown, 'requested shutdown')
-            } else if (testCase.restart) {
-              subTest.ok(disconnected, 'should have disconnected')
-              subTest.ok(connecting, 'should have started reconnecting')
-
-              subTest.ok(restartPreconnect.isDone(), 'requested preconnect')
-              subTest.ok(restartConnect.isDone(), 'requested connect')
-              subTest.ok(restartSettings.isDone(), 'requested settings')
-            } else {
-              subTest.notOk(disconnected, 'should not have disconnected')
-              subTest.notOk(connecting, 'should not have reconnected')
-            }
-
-            const retrievedMetric = agent.metrics.getMetric('myMetric')
-            if (testCase.retain_data) {
-              subTest.ok(retrievedMetric, 'should still have custom metric')
-            } else {
-              subTest.notOk(retrievedMetric, 'should no longer have custom metric')
-            }
-
-            subTest.done()
-          })
+          done()
         })
+
+        statusCodeTest.afterEach((done) => {
+          if (agent.isRunning) { // this is bullshit, figure out realness
+            agent.stop()
+          }
+
+          helper.unloadAgent(agent)
+
+          if (!nock.isDone()) {
+            console.error('Cleaning pending mocks: %j', nock.pendingMocks())
+            nock.cleanAll()
+          }
+
+          agent = null
+
+          preconnect = null
+          connect = null
+          settings = null
+
+          restartPreconnect = null
+          restartConnect = null
+          restartSettings = null
+
+          shutdown = null
+
+          nock.enableNetConnect()
+
+          done()
+        })
+
+
+        statusCodeTest.plan(2)
+        statusCodeTest.test('metric_data', createReponseHandlingTest(
+          'metric_data',
+          function hasMetricData() {
+            return !!agent.metrics.getMetric('myMetric')
+          }
+        ))
+
+        statusCodeTest.test('error_event_data', createReponseHandlingTest(
+          'error_event_data',
+          function hasErrorData() {
+            return (agent.errors.length > 0)
+          }
+        ))
+
+        function createReponseHandlingTest(endpointName, hasTestData) {
+          return (subTest) => {
+            const mockEndpoint = nockRequest(endpointName, RUN_ID).reply(statusCode)
+
+            agent.start((error) => {
+              if (error) {
+                throw error
+              }
+
+              let disconnected = false
+              agent.on('disconnected', () => {
+                disconnected = true
+              })
+
+              let connecting = false
+              agent.on('connecting', () => {
+                connecting = true
+              })
+
+              subTest.ok(preconnect.isDone(), 'requested preconnect')
+              subTest.ok(connect.isDone(), 'requested connect')
+              subTest.ok(settings.isDone(), 'requested settings')
+
+
+              if (testCase.restart) {
+                restartPreconnect = nockRequest('preconnect')
+                  .reply(200, {return_value: TEST_DOMAIN})
+
+                restartConnect = nockRequest('connect')
+                  .reply(200, {return_value: {agent_run_id: RUN_ID}})
+
+                restartSettings = nockRequest('agent_settings', RUN_ID)
+                  .reply(200, {return_value: []})
+              }
+
+              if (testCase.disconnect) {
+                shutdown = nockRequest('shutdown', RUN_ID).reply(200)
+              }
+
+              agent.harvest((error) => {
+                if (testCase.restart || testCase.disconnect) {
+                  subTest.notOk(error, 'restart and disconnect overrule errors')
+                } else {
+                  subTest.ok(error, 'should have error from other harvest endpoints')
+
+                  const isNockError = error.message.includes('Nock: No match for request')
+                  if (!isNockError) {
+                    console.log(error)
+                  }
+                  subTest.ok(isNockError, 'should be nock specific error')
+
+                  const isEndpointUnderTest = error.message.includes(`method=${endpointName}`)
+                  subTest.notOk(isEndpointUnderTest, 'should not fail for endpoint under test')
+                }
+
+                subTest.ok(mockEndpoint.isDone(), `called ${endpointName} endpoint`)
+
+                if (testCase.disconnect) {
+                  subTest.ok(disconnected, 'should have disconnected')
+                  subTest.notOk(connecting, 'should not have reconnected')
+
+                  subTest.ok(shutdown, 'requested shutdown')
+                } else if (testCase.restart) {
+                  subTest.ok(disconnected, 'should have disconnected')
+                  subTest.ok(connecting, 'should have started reconnecting')
+
+                  subTest.ok(restartPreconnect.isDone(), 'requested preconnect')
+                  subTest.ok(restartConnect.isDone(), 'requested connect')
+                  subTest.ok(restartSettings.isDone(), 'requested settings')
+                } else {
+                  subTest.notOk(disconnected, 'should not have disconnected')
+                  subTest.notOk(connecting, 'should not have reconnected')
+                }
+
+                const hasDataPostHarvest = hasTestData()
+                if (testCase.retain_data) {
+                  subTest.ok(hasDataPostHarvest, `should have retained data after ${endpointName} call`)
+                } else {
+                  subTest.notOk(hasDataPostHarvest, `should not have retained data after ${endpointName} call`)
+                }
+
+                subTest.done()
+              })
+            })
+          }
+        }
       })
     })
   })
 })
 
-// TODO: update to p17
 function nockRequest(endpointMethod, runId) {
   let relativepath = '/agent_listener/invoke_raw_method?' +
-  `marshal_format=json&protocol_version=17&` +
+  `marshal_format=json&protocol_version=${PROTOCOL_VERSION}&` +
   `license_key=license%20key%20here&method=${endpointMethod}`
 
   if (runId) {
