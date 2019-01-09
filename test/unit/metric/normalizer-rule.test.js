5bfd6879ddd9f871d0e2abe4edaa599d8b4eb3b4
Michael Goin <mgoin@newrelic.com>
2019-01-09 11:53:22 -0800

    delints metric unit tests

--- test/unit/metric/normalizer-rule.test.js
+++ test/unit/metric/normalizer-rule.test.js
@@ -8,8 +8,8 @@ var Rule = require('../../../lib/metrics/normalizer/rule')
 describe("NormalizerRule", function() {
   var rule
 
-  describe("with a very simple specification", function () {
-    before(function () {
+  describe("with a very simple specification", function() {
+    before(function() {
       // sample rule sent by staging collector 1 on 2012-08-29
       var sample = {
         "each_segment"     : false,
@@ -24,51 +24,51 @@ describe("NormalizerRule", function() {
       rule = new Rule(sample)
     })
 
-    it("should know whether the rule terminates normalization", function () {
+    it("should know whether the rule terminates normalization", function() {
       expect(rule.isTerminal).equal(true)
     })
 
-    it("should know its own precedence", function () {
+    it("should know its own precedence", function() {
       expect(rule.precedence).equal(0)
     })
 
-    it("should correctly compile the included regexp", function () {
+    it("should correctly compile the included regexp", function() {
       expect(rule.matches('test_match_nothing')).equal(true)
       expect(rule.matches('a test_match_nothing')).equal(false)
       expect(rule.matches('test_match_nothin\'')).equal(false)
     })
 
-    it("shouldn't throw if the regexp doesn't compile", function () {
+    it("shouldn't throw if the regexp doesn't compile", function() {
       var whoops = {"match_expression" : '$[ad^'}
       var bad
-      expect(function () { bad = new Rule(whoops); }).not.throws()
+      expect(function() { bad = new Rule(whoops) }).not.throws()
       expect(bad.matches('')).equal(true)
     })
 
-    it("should know if the regexp is applied to each 'segment' in the URL", function () {
+    it("should know if the regexp is applied to each 'segment' in the URL", function() {
       expect(rule.eachSegment).equal(false)
     })
 
-    it("should know if the regexp replaces all instances in the URL", function () {
+    it("should know if the regexp replaces all instances in the URL", function() {
       expect(rule.replaceAll).equal(false)
     })
 
-    it("should parse the replacement pattern", function () {
+    it("should parse the replacement pattern", function() {
       expect(rule.replacement).equal('$1')
     })
 
-    it("should know whether to ignore the URL", function () {
+    it("should know whether to ignore the URL", function() {
       expect(rule.ignore).equal(false)
     })
 
-    it("should be able to take in a non-normalized URL and return it normalized", function () {
+    it("should be able to take in a non-normalized URL and return it normalized", () => {
       expect(rule.apply('test_match_nothing')).equal('test_match_nothing')
     })
   })
 
-  describe("with Saxon's patterns", function () {
-    describe("including '^(?!account|application).*'", function () {
-      beforeEach(function () {
+  describe("with Saxon's patterns", function() {
+    describe("including '^(?!account|application).*'", function() {
+      beforeEach(function() {
         rule = new Rule({
           "each_segment"     : true,
           "match_expression" : "^(?!account|application).*",
@@ -76,39 +76,48 @@ describe("NormalizerRule", function() {
         })
       })
 
-      it("implies '/account/myacc/application/test' -> '/account/*/application/*'",
-         function () {
-        expect(rule.apply('/account/myacc/application/test'))
-          .equal('/account/*/application/*')
-      })
+      it(
+        "implies '/account/myacc/application/test' -> '/account/*/application/*'",
+        function() {
+          expect(rule.apply('/account/myacc/application/test'))
+            .equal('/account/*/application/*')
+        }
+      )
 
-      it("implies '/oh/dude/account/myacc/application' -> '/*/*/account/*/application'",
-         function () {
-        expect(rule.apply('/oh/dude/account/myacc/application'))
-          .equal('/*/*/account/*/application')
-      })
+      it(
+        "implies '/oh/dude/account/myacc/application' -> '/*/*/account/*/application'",
+        function() {
+          expect(rule.apply('/oh/dude/account/myacc/application'))
+            .equal('/*/*/account/*/application')
+        }
+      )
     })
 
-    describe("including '^(?!channel|download|popups|search|tap|user|related|admin|api|genres|notification).*'",
-            function () {
-      beforeEach(function () {
-        rule = new Rule({
-          "each_segment"     : true,
-          "match_expression" : "^(?!channel|download|popups|search|tap|user|related|admin|api|genres|notification).*",
-          "replacement"      : "*"
+    const expression =
+      '^(?!channel|download|popups|search|tap|user' +
+      '|related|admin|api|genres|notification).*'
+
+    describe(
+      `including '${expression}'`,
+      function() {
+        beforeEach(function() {
+          rule = new Rule({
+            "each_segment"     : true,
+            "match_expression" : expression,
+            "replacement"      : "*"
+          })
         })
-      })
 
-      it("implies '/tap/stuff/user/gfy77t/view' -> '/tap/*/user/*/*'",
-         function () {
-        expect(rule.apply('/tap/stuff/user/gfy77t/view'))
-          .equal('/tap/*/user/*/*')
-      })
-    })
+        it("implies '/tap/stuff/user/gfy77t/view' -> '/tap/*/user/*/*'", function() {
+          expect(rule.apply('/tap/stuff/user/gfy77t/view'))
+            .equal('/tap/*/user/*/*')
+        })
+      }
+    )
   })
 
-  describe("with a more complex substitution rule", function () {
-    before(function () {
+  describe("with a more complex substitution rule", function() {
+    before(function() {
       // sample rule sent by staging collector 1 on 2012-08-29
       var sample = {
         "each_segment"     : true,
@@ -123,42 +132,42 @@ describe("NormalizerRule", function() {
       rule = new Rule(sample)
     })
 
-    it("should know whether the rule terminates normalization", function () {
+    it("should know whether the rule terminates normalization", function() {
       expect(rule.isTerminal).equal(false)
     })
 
-    it("should know its own precedence", function () {
+    it("should know its own precedence", function() {
       expect(rule.precedence).equal(1)
     })
 
-    it("should correctly compile the included regexp", function () {
+    it("should correctly compile the included regexp", function() {
       expect(rule.matches('/00dead_beef_00,b/hamburt')).equal(true)
       expect(rule.matches('a test_match_nothing')).equal(false)
       expect(rule.matches('/00 dead dad/nomatch')).equal(false)
     })
 
-    it("should know if the regexp is applied to each 'segment' in the URL", function () {
+    it("should know if the regexp is applied to each 'segment' in the URL", function() {
       expect(rule.eachSegment).equal(true)
     })
 
-    it("should know if the regexp replaces all instances in the URL", function () {
+    it("should know if the regexp replaces all instances in the URL", function() {
       expect(rule.replaceAll).equal(false)
     })
 
-    it("should parse the replacement pattern", function () {
+    it("should parse the replacement pattern", function() {
       expect(rule.replacement).equal('*')
     })
 
-    it("should know whether to ignore the URL", function () {
+    it("should know whether to ignore the URL", function() {
       expect(rule.ignore).equal(false)
     })
 
-    it("should be able to take in a non-normalized URL and return it normalized", function () {
+    it("should be able to take in a non-normalized URL and return it normalized", () => {
       expect(rule.apply('/00dead_beef_00,b/hamburt')).equal('/*/hamburt')
     })
   })
 
-  it("should replace all the instances of a pattern when so specified", function () {
+  it("should replace all the instances of a pattern when so specified", function() {
     var sample = {
       "each_segment"     : false,
       "eval_order"       : 0,
@@ -174,40 +183,40 @@ describe("NormalizerRule", function() {
     expect(rule.apply('/test/xXxxXx0xXxzxxxxXx')).equal('/test/yy0yzyy')
   })
 
-  describe("when given an incomplete specification", function () {
-    it("shouldn't throw (but it can log!)", function () {
-      expect(function () { rule = new Rule(); }).not.throws()
+  describe("when given an incomplete specification", function() {
+    it("shouldn't throw (but it can log!)", function() {
+      expect(function() { rule = new Rule() }).not.throws()
     })
 
-    it("should default to not applying the rule to each segment", function () {
+    it("should default to not applying the rule to each segment", function() {
       expect(new Rule().eachSegment).equal(false)
     })
 
-    it("should default the rule's precedence to 0", function () {
+    it("should default the rule's precedence to 0", function() {
       expect(new Rule().precedence).equal(0)
     })
 
-    it("should default to not terminating rule evaluation", function () {
+    it("should default to not terminating rule evaluation", function() {
       expect(new Rule().isTerminal).equal(false)
     })
 
-    it("should have a regexp that matches the empty string", function () {
+    it("should have a regexp that matches the empty string", function() {
       expect(new Rule().pattern).eql(/^$/i)
     })
 
-    it("should use the entire match as the replacement value", function () {
+    it("should use the entire match as the replacement value", function() {
       expect(new Rule().replacement).equal('$0')
     })
 
-    it("should default to not replacing all instances", function () {
+    it("should default to not replacing all instances", function() {
       expect(new Rule().replaceAll).equal(false)
     })
 
-    it("should default to not ignoring matching URLs", function () {
+    it("should default to not ignoring matching URLs", function() {
       expect(new Rule().ignore).equal(false)
     })
 
-    it("should silently pass through the input if applied", function () {
+    it("should silently pass through the input if applied", function() {
       expect(new Rule().apply('sample/input')).equal('sample/input')
     })
   })
