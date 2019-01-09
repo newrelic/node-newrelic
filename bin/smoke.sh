225e0a9b964f71aa4931defb7e7238ba6ccb7148
Peter Svetlichny <psvetlichny@newrelic.com>
2019-01-09 11:48:33 -0800

    update smoke script

--- bin/smoke.sh
+++ bin/smoke.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 
 npm install --production --loglevel warn --no-package-lock
-npm install tap --no-package-lock
-cd test/smoke && npm install --no-package-lock
+npm --prefix test/smoke install --no-package-lock
 time node test/smoke/*.tap.js
