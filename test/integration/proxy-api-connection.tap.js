1b2774b2487e519a2cb524d8141daf404ab21119
Peter Svetlichny <psvetlichny@newrelic.com>
2019-01-23 16:48:27 -0800

    test: remove another stray shutdown payload assertion

--- test/integration/proxy-api-connection.tap.js
+++ test/integration/proxy-api-connection.tap.js
@@ -183,9 +183,8 @@ tap.test('no proxy set should not use proxy agent', (t) => {
     t.ok(returned.agent_run_id, 'got run ID')
     t.ok(agent.config.run_id, 'run ID set in configuration')
 
-    api.shutdown((error, command) => {
+    api.shutdown((error) => {
       t.notOk(error, 'should have shut down without issue')
-      t.equal(command.payload, null, 'collector explicitly returns null')
       t.notOk(agent.config.run_id, 'run ID should have been cleared by shutdown')
 
       t.end()
