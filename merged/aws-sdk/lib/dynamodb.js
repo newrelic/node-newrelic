689facc7545036c9f10f9a3a971e7fd1da5ef1a9
Michael Goin <mgoin@newrelic.com>
2019-06-13 13:40:47 -0700

    Wraps DocumentClient prototype instead of each constructor returned instance.

--- merged/aws-sdk/lib/dynamodb.js
+++ merged/aws-sdk/lib/dynamodb.js
@@ -23,6 +23,8 @@ const DOC_CLIENT_OPERATIONS = [
 function instrument(shim, AWS) {
   shim.setDatastore(shim.DYNAMODB)
 
+  // DynamoDB's service API methods are dynamically generated
+  // in the constructor so we have to wrap the return.
   shim.wrapReturn(AWS, 'DynamoDB', function wrapDynamo(shim, fn, name, ddb) {
     shim.recordOperation(
       ddb,
@@ -44,32 +46,28 @@ function instrument(shim, AWS) {
     )
   })
 
+  // DocumentClient's API is predefined so we can instrument the prototype.
   // DocumentClient does defer to DynamoDB but it also does enough individual
   // steps for the request we want to hide that instrumenting specifically and
-  // setting to opaque is required.
-  shim.wrapReturn(
-    AWS.DynamoDB,
-    'DocumentClient',
-    function wrapDocumentClient(shim, fn, name, documentClient) {
-      shim.recordOperation(
-        documentClient,
-        DOC_CLIENT_OPERATIONS,
-        function wrapOperation(shim, original, operationName, args) {
-          const params = args[0]
-          const dynamoOperation = this.serviceClientOperationsMap[operationName]
+  // setting to opaque is currently required.
+  const docClientProto = AWS.DynamoDB.DocumentClient.prototype
+  shim.recordOperation(
+    docClientProto,
+    DOC_CLIENT_OPERATIONS,
+    function wrapOperation(shim, original, operationName, args) {
+      const params = args[0]
+      const dynamoOperation = this.serviceClientOperationsMap[operationName]
 
-          return {
-            name: dynamoOperation,
-            parameters: {
-              host: this.service.endpoint.host,
-              port_path_or_id: this.service.endpoint.port,
-              collection: params && params.TableName || 'Unknown'
-            },
-            callback: shim.LAST,
-            opaque: true
-          }
-        }
-      )
+      return {
+        name: dynamoOperation,
+        parameters: {
+          host: this.service.endpoint.host,
+          port_path_or_id: this.service.endpoint.port,
+          collection: params && params.TableName || 'Unknown'
+        },
+        callback: shim.LAST,
+        opaque: true
+      }
     }
   )
 }
