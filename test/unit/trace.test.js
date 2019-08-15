ee9cf6d49dfe2713098c0b82042893d348b5125f
Alan Storm <astorm@newrelic.com>
2019-08-15 15:00:44 -0700

    Revert "Adding transaction id to trace data in order to bring us in line with other agents"
    This reverts commit 9d382c15ec27daab66f2b6042bf1515802ed671b.

--- test/unit/trace.test.js
+++ test/unit/trace.test.js
@@ -735,7 +735,7 @@ function makeTrace(agent, callback) {
         'WebTransaction/NormalizedUri/*', // scope
         '/test',  // URI path
         encoded,  // compressed segment / segment data
-        transaction.id, // guid
+        '',       // FIXME: depends on RUM token in session
         null,     // reserved, always NULL
         false,    // FIXME: RUM2 session persistence, not worrying about it for now
         null,     // FIXME: xraysessionid
