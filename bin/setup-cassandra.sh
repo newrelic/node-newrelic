f5c0b98bb4956d678437996264f253674583bd7f
Bryan Clement <bclement@newrelic.com>
2019-09-03 14:52:38 -0700

    full circle

--- bin/setup-cassandra.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CASSANDRA_BASE=apache-cassandra-$CASSANDRA_VERSION
-
-sudo rm -rf /var/lib/cassandra/*
-curl https://archive.apache.org/dist/cassandra/$CASSANDRA_VERSION/$CASSANDRA_BASE-bin.tar.gz | tar xz
-sudo sh $CASSANDRA_BASE/bin/cassandra -R
