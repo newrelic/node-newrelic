a2efb2bb7d78d79404dc9f4d068b7d4e3b030a45
Bryan Clement <bclement@newrelic.com>
2019-09-03 13:06:52 -0700

    don't make dns test exclusive

--- test/integration/core/dns.tap.js
+++ test/integration/core/dns.tap.js
@@ -130,7 +130,9 @@ test('reverse', function(t) {
         }
       }
 
-      t.deepEqual(names, expected, 'should have expected results')
+      expected.forEach((name) => {
+        t.contains(names, name, 'should have expected name')
+      })
       verifySegments(t, agent, 'dns.reverse')
     })
   })
