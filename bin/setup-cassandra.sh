990e093a2faa4cc12e9d0f114e7a2364be55b2c9
Bryan Clement <bclement@newrelic.com>
2019-09-03 14:16:58 -0700

    pass -R to cassandra

--- bin/setup-cassandra.sh
+++ bin/setup-cassandra.sh
@@ -4,4 +4,4 @@ CASSANDRA_BASE=apache-cassandra-$CASSANDRA_VERSION
 
 sudo rm -rf /var/lib/cassandra/*
 curl https://archive.apache.org/dist/cassandra/$CASSANDRA_VERSION/$CASSANDRA_BASE-bin.tar.gz | tar xz
-sudo sh $CASSANDRA_BASE/bin/cassandra
+sudo sh $CASSANDRA_BASE/bin/cassandra -R
