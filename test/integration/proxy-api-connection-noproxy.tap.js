da03697d08f8164b8c41452ccd74314e07242b28
Alan Storm <astorm@newrelic.com>
2019-09-10 13:09:19 -0700

    Breaking out proxy server tests into individual tests

---
+++ test/integration/proxy-api-connection-noproxy.tap.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const net = require('net')
+const tap = require('tap')
+const join = require('path').join
+const https = require('https')
+const proxySetup = require('@newrelic/proxy')
+const read = require('fs').readFileSync
+const configurator = require('../../lib/config')
+const Agent = require('../../lib/agent')
+const CollectorAPI = require('../../lib/collector/api')
+
+let port = 0
+const SSL_CONFIG = {
+  key: read(join(__dirname, '../lib/test-key.key')),
+  cert: read(join(__dirname, '../lib/self-signed-test-certificate.crt')),
+}
+
+tap.test('no proxy set should not use proxy agent', (t) => {
+  const config = configurator.initialize({
+    app_name: 'node.js Tests',
+    license_key: 'd67afc830dab717fd163bfcb0b8b88423e9a1a3b',
+    host: 'staging-collector.newrelic.com',
+    port: 443,
+    ssl: true,
+    utilization: {
+      detect_aws: false,
+      detect_pcf: false,
+      detect_azure: false,
+      detect_gcp: false,
+      detect_docker: false
+    },
+    logging: {
+      level: 'trace'
+    }
+  })
+  const agent = new Agent(config)
+  const api = new CollectorAPI(agent)
+
+
+  api.connect((error, response) => {
+    t.notOk(error, 'connected without error')
+
+    const returned = response && response.payload
+    t.ok(returned, 'got boot configuration')
+    t.ok(returned.agent_run_id, 'got run ID')
+    t.ok(agent.config.run_id, 'run ID set in configuration')
+
+    api.shutdown((error) => {
+      t.notOk(error, 'should have shut down without issue')
+      t.notOk(agent.config.run_id, 'run ID should have been cleared by shutdown')
+
+      t.end()
+    })
+  })
+})
