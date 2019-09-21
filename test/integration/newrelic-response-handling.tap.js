33541141a91eeaf6bbdc011d0a90212b0de185a9
Carlo Pearson <cpearson@newrelic.com>
2019-09-20 17:10:38 -0700

    fix response handling test - wait for all aggregators to send

--- test/integration/newrelic-response-handling.tap.js
+++ test/integration/newrelic-response-handling.tap.js
@@ -157,46 +157,15 @@ function createStatusCodeTest(testCase) {
 
           // Move clock forward to trigger auto harvests.
           testClock.tick(60000)
-          
-          if (endpointName === 'error_data') {
-            aggregatorCheckOnEnd(agent.errors.traceAggregator)
-          } else if (endpointName === 'error_event_data') {
-            aggregatorCheckOnEnd(agent.errors.eventAggregator)
-          } else if (endpointName === 'transaction_sample_data') {
-            aggregatorCheckOnEnd(agent.traces)
-          } else if (endpointName === 'span_event_data') {
-            aggregatorCheckOnEnd(agent.spanEventAggregator)
-          } else if (endpointName === 'analytic_event_data') {
-            aggregatorCheckOnEnd(agent.transactionEventAggregator)
-          } else if (endpointName === 'custom_event_data') {
-            aggregatorCheckOnEnd(agent.customEventAggregator)
-          } else if (endpointName === 'sql_trace_data') {
-            aggregatorCheckOnEnd(agent.queries)
-          } else if (endpointName === 'metric_data') {
-            aggregatorCheckOnEnd(agent.metrics)
-          } else {
-            throw new Error('Invalid endpoint')
-          }
 
-          function aggregatorCheckOnEnd(aggregator) {
-            aggregator.on(`finished ${endpointName} data send.`, () => {
-              // Since mocking setImmediate doesn't currently work w/o exploding,
-              // we need to allow non-interval work to clear (async payload generation)
-              // for certain aggregators to ensure they are done before the next test.
-              setImmediate(() => {
-                checkEnd()
-
-                subTest.done()
-              })
-            })
-          }
-
-          function checkEnd() {
+          whenAllAggregatorsSend(agent).then(() => {
             subTest.ok(mockEndpoint.isDone(), `called ${endpointName} endpoint`)
 
             verifyRunBehavior()
             verifyDataRetention()
-          }
+
+            subTest.done()
+          })
         })
 
         function verifyAgentStart(error) {
@@ -245,6 +214,93 @@ function createStatusCodeTest(testCase) {
       }
     }
   }
+}
+
+function whenAllAggregatorsSend(agent) {
+  const metricPromise = new Promise((resolve) => {
+    agent.metrics.once(
+      'finished metric_data data send.',
+      function onMetricsFinished() {
+        resolve()
+      }
+    )
+  })
+  
+  const spanPromise = new Promise((resolve) => {
+    agent.spanEventAggregator.once(
+      'finished span_event_data data send.',
+      function onSpansFinished() {
+        resolve()
+      }
+    )
+  })
+
+  const customEventPromise = new Promise((resolve) => {
+    agent.customEventAggregator.once(
+      'finished custom_event_data data send.',
+      function onCustomEventsFinished() {
+        resolve()
+      }
+    )
+  })
+
+  const transactionEventPromise = new Promise((resolve) => {
+    agent.transactionEventAggregator.once(
+      'finished analytic_event_data data send.',
+      function onTransactionEventsFinished() {
+        resolve()
+      }
+    )
+  })
+
+  const transactionTracePromise = new Promise((resolve) => {
+    agent.traces.once(
+      'finished transaction_sample_data data send.',
+      function onTracesFinished() {
+        resolve()
+      }
+    )
+  })
+
+  const sqlTracePromise = new Promise((resolve) => {
+    agent.queries.once(
+      'finished sql_trace_data data send.',
+      function onSqlTracesFinished() {
+        resolve()
+      }
+    )
+  })
+
+  const errorTracePromise = new Promise((resolve) => {
+    agent.errors.traceAggregator.once(
+      'finished error_data data send.',
+      function onErrorTracesFinished() {
+        resolve()
+      }
+    )
+  })
+
+  const errorEventPromise = new Promise((resolve) => {
+    agent.errors.eventAggregator.once(
+      'finished error_event_data data send.',
+      function onErrorEventsFinished() {
+        resolve()
+      }
+    )
+  })
+
+  const promises = [
+    metricPromise, 
+    spanPromise, 
+    customEventPromise, 
+    transactionEventPromise, 
+    transactionTracePromise, 
+    sqlTracePromise, 
+    errorTracePromise, 
+    errorEventPromise
+  ]
+
+  return Promise.all(promises)
 }
 
 /**
