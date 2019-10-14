18d18822ab4045cbd9a06ee5154eb4bd209ac442
Alan Storm <astorm@newrelic.com>
2019-10-14 16:18:46 -0700

    sudo

--- bin/travis-node.sh
+++ bin/travis-node.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-apt-get update
+sudo apt-get update
 
 # so dumb that we need python-software-properties
 # to get add-apt-repository
-apt-get -y install build-essential libssl-dev curl python-software-properties
+sudo apt-get -y install build-essential libssl-dev curl python-software-properties
 
 # Where is ppa:ubuntu-toolchain-r/test?
-add-apt-repository -y ppa:ubuntu-toolchain-r/test
-apt-get -y update
-apt-get -y install libstdc++6-4.7-dev
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get -y update
+sudo apt-get -y install libstdc++6-4.7-dev
 
 # can we get this from an actual repository?
 curl -LO 'http://launchpadlibrarian.net/130794928/libc6_2.17-0ubuntu4_amd64.deb'
-dpkg -i libc6_2.17-0ubuntu4_amd64.deb
+sudo dpkg -i libc6_2.17-0ubuntu4_amd64.deb
 
 # install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
