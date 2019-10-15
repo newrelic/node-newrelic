496e880793bd320902d3c7892e87a9e554ce2b00
Alan Storm <astorm@newrelic.com>
2019-10-15 15:04:09 -0700

    New Matrix

--- bin/travis-node.sh
+++ bin/travis-node.sh
@@ -21,4 +21,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-nvm install 12
+nvm install $NR_NODE_VERSION
