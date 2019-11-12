e252d3140a76e3ee6bcc034e99ac9a8802523938
Alan Storm <astorm@newrelic.com>
2019-11-12 11:13:22 -0800

    Do a feature sniff before instrumenting the Amazon SDK

--- merged/aws-sdk/lib/instrumentation.js
+++ merged/aws-sdk/lib/instrumentation.js
@@ -7,7 +7,27 @@ const INSTRUMENTATIONS = [
   require('./sns')
 ]
 
+/**
+ * Series of tests to determine if the library
+ * has the features needed to provide instrumentation
+ */
+const instrumentationSupported = function instrumentationSupported(AWS) {
+  // instrumentation requires the serviceClientOperationsMap property
+  if (!AWS ||
+      !AWS.DynamoDB ||
+      !AWS.DynamoDB.DocumentClient ||
+      !AWS.DynamoDB.DocumentClient.prototype ||
+      !AWS.DynamoDB.DocumentClient.prototype.serviceClientOperationsMap) {
+    return false
+  }
+
+  return true
+}
+
 module.exports = function initialize(shim, AWS) {
+  if(!instrumentationSupported(AWS)) {
+    return false
+  }
   // Validate every instrumentation before attempting to run any of them.
   for (let instrumentation of INSTRUMENTATIONS) {
     if (!instrumentation.validate(shim, AWS)) {
