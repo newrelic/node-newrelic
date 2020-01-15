9e7df722d05789d407a8b29d28b1464735890401
Michael Goin <mgoin@newrelic.com>
2020-01-15 12:14:07 -0800

    Adds S3 promise tests.

--- merged/aws-sdk/tests/versioned/s3.tap.js
+++ merged/aws-sdk/tests/versioned/s3.tap.js
@@ -28,8 +28,8 @@ tap.test('S3 buckets', (t) => {
     done()
   })
 
-  t.test('commands', (t) => {
-    const Bucket = 'aws-sdk-test-bucket-' + Math.floor(Math.random() * 100000)
+  t.test('commands with callbacks', (t) => {
+    const Bucket = 'delete-aws-sdk-test-bucket-' + Math.floor(Math.random() * 100000)
     t.tearDown(() => {
       // Ensure bucket gets deleted even if test goes awry.
       S3.deleteBucket({Bucket}, () => {})
@@ -48,24 +48,68 @@ tap.test('S3 buckets', (t) => {
               t.error(err)
             }
             tx.end()
-            setImmediate(finish, tx)
+
+            const args = [t, tx]
+            setImmediate(finish, ...args)
           })
         })
       })
     })
+  })
 
-    function finish(tx) {
-      const externals = common.checkAWSAttributes(t, tx.trace.root, common.EXTERN_PATTERN)
-      t.equal(externals.length, 3, 'should have 3 aws externals')
-      const [head, create, del] = externals
-      checkAttrs(t, head, 'headBucket')
-      checkAttrs(t, create, 'createBucket')
-      checkAttrs(t, del, 'deleteBucket')
+  t.test('commands with promises', (t) => {
+    const Bucket = 'delete-aws-sdk-test-bucket-' + Math.floor(Math.random() * 100000)
+    t.tearDown(() => {
+      // Ensure bucket gets deleted even if test goes awry.
+      S3.deleteBucket({Bucket}, () => {})
+    })
 
-      t.end()
-    }
+    helper.runInTransaction(async tx => {
+      const bucketParams = {Bucket}
+      let headBucketError = null
+      try {
+        await S3.headBucket(bucketParams).promise()
+      } catch (err) {
+        headBucketError = err
+      } finally {
+        t.matches(headBucketError, {code: 'NotFound'}, 'should get not found for bucket')
+      }
+
+      try {
+        const createData = await S3.createBucket(bucketParams).promise()
+        t.matches(createData, {Location: `/${Bucket}`}, 'should have matching location')
+      } catch (err) {
+        t.error(err)
+      }
+
+      try {
+        await S3.deleteBucket(bucketParams).promise()
+      } catch (err) {
+        // Sometimes S3 doesn't make the bucket quickly enough. The cleanup
+        // in `t.tearDown` should get it after we do all our checks.
+        if (err && err.code !== 'NoSuchBucket') {
+          t.error(err)
+        }
+      }
+
+      tx.end()
+
+      const args = [t, tx]
+      setImmediate(finish, ...args)
+    })
   })
 })
+
+function finish(t, tx) {
+  const externals = common.checkAWSAttributes(t, tx.trace.root, common.EXTERN_PATTERN)
+  t.equal(externals.length, 3, 'should have 3 aws externals')
+  const [head, create, del] = externals
+  checkAttrs(t, head, 'headBucket')
+  checkAttrs(t, create, 'createBucket')
+  checkAttrs(t, del, 'deleteBucket')
+
+  t.end()
+}
 
 function checkAttrs(t, segment, operation) {
   const attrs = segment.attributes.get(common.SEGMENT_DESTINATION)
