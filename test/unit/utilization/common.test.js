af529116df00cdadb2ae3d8db1c319b75f140525
Michael Goin <mgoin@newrelic.com>
2020-02-20 15:22:16 -0800

    Converts /utilization unit tests to tap via Mocha DSL.

--- test/unit/utilization/common.test.js
+++ test/unit/utilization/common.test.js
@@ -1,5 +1,9 @@
 'use strict'
 
+// TODO: convert to normal tap style.
+// Below allows use of mocha DSL with tap runner.
+require('tap').mochaGlobals()
+
 const common = require('../../../lib/utilization/common')
 const expect = require('chai').expect
 const helper = require('../../lib/agent_helper.js')
