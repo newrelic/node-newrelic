25c3f5ee7437390dde86ee63d847d4a3764299c8
Michael Goin <mgoin@newrelic.com>
2020-02-20 12:42:41 -0800

    Converts /agent unit tests to tap via Mocha DSL.

--- test/unit/agent/intrinsics.test.js
+++ test/unit/agent/intrinsics.test.js
@@ -1,5 +1,9 @@
 'use strict'
 
+// TODO: convert to normal tap style.
+// Below allows use of mocha DSL with tap runner.
+require('tap').mochaGlobals()
+
 var helper = require('../../lib/agent_helper.js')
 var chai = require('chai')
 var assert = chai.assert
