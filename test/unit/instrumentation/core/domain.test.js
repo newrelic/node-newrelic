5f81001464cecd214c83facf82c4b632602d4f03
Michael Goin <mgoin@newrelic.com>
2020-02-21 14:49:44 -0800

    Converts domain unit tests to tap.

--- test/unit/instrumentation/core/domain.test.js
+++ test/unit/instrumentation/core/domain.test.js
@@ -1,17 +1,28 @@
 'use strict'
 
-var chai    = require('chai')
-var helper  = require('../../../lib/agent_helper')
+const tap = require('tap')
+const test = tap.test
 
-var expect = chai.expect
+// TODO: convert to normal tap style.
+// Below allows use of mocha DSL with tap runner.
+tap.mochaGlobals()
 
-describe('Domains', function() {
-  var agent = null
-  var d = null
-  var tasks = []
-  var interval = null
+const helper  = require('../../../lib/agent_helper')
 
-  beforeEach(function() {
+test('Domains', (t) => {
+  t.autoend()
+
+  let agent = null
+  let d = null
+  let tasks = []
+  let interval = null
+
+  t.beforeEach((done, t) => {
+    // Once on node 10+ only, may be able to replace with below.
+    // t.expectUncaughtException(fn, [expectedError], message, extra)
+    // https://node-tap.org/docs/api/asserts/#texpectuncaughtexceptionfn-expectederror-message-extra
+    helper.temporarilyOverrideTapUncaughtBehavior(tap, t)
+
     agent = helper.instrumentMockedAgent()
 
     // Starting on 9.3.0, calling `domain.exit` does not stop assertions in later
@@ -21,39 +32,43 @@ describe('Domains', function() {
       while (tasks.length) {
         tasks.pop()()
       }
+
+      done()
     }, 10)
   })
 
-  afterEach(function() {
+  t.afterEach((done) => {
     d && d.exit()
     clearInterval(interval)
     helper.unloadAgent(agent)
+
+    done()
   })
 
-  it('should not be loaded just from loading the agent', function() {
-    expect(process).to.have.property('domain', null)
+  t.test('should not be loaded just from loading the agent', (t) => {
+    t.notOk(process.domain)
+    t.end()
   })
 
-  it('should retain transaction scope on error events', function(done) {
-    var domain = require('domain')
+  t.test('should retain transaction scope on error events', (t) => {
+    const domain = require('domain')
     d = domain.create()
 
-    var checkedTransaction
+    let checkedTransaction = null
     d.once('error', function(err) {
       // Asserting in a try catch because Domain will
       // handle the errors resulting in an infinite loop
       try {
-        expect(err).to.exist
-        expect(err.message).to.equal('whole new error!')
+        t.ok(err)
+        t.equal(err.message, 'whole new error!')
 
-        var transaction = agent.getTransaction()
-        expect(transaction && transaction.id)
-          .to.equal(checkedTransaction && checkedTransaction.id)
+        const transaction = agent.getTransaction()
+        t.equal(transaction.id, checkedTransaction.id)
       } catch (err) {
-        done(err) // Bailing out with the error
+        t.end(err) // Bailing out with the error
         return
       }
-      tasks.push(done)
+      tasks.push(t.end)
     })
 
     helper.runInTransaction(agent, function(transaction) {
