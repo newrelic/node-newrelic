b03e0c62e23cfd3dfaff9c07c4a26977615448ff
Michael Goin <mgoin@newrelic.com>
2020-03-10 09:57:06 -0700

    Fixes removal of child_process event listeners. Resolves https://github.com/newrelic/node-newrelic/issues/333.

--- test/integration/core/child_process.tap.js
+++ test/integration/core/child_process.tap.js
@@ -47,7 +47,7 @@ test('transaction context is preserved in subscribed events', function(t) {
   })
 })
 
-test('should not break removeListener', (t) => {
+test('should not break removeListener for single event', (t) => {
   const agent = setupAgent(t)
 
   helper.runInTransaction(agent, function() {
@@ -60,6 +60,54 @@ test('should not break removeListener', (t) => {
 
     child.removeListener('message', onMessage)
     t.notOk(child._events.message)
+
+    child.on('exit', function() {
+      t.end()
+    })
+  })
+})
+
+test('should not break removeListener for multiple events down to single', (t) => {
+  const agent = setupAgent(t)
+
+  helper.runInTransaction(agent, function() {
+    const child = cp.fork('./exec-me.js', {cwd: __dirname})
+
+    function onMessage() {}
+    function onMessage2() {}
+
+    child.on('message', onMessage)
+    child.on('message', onMessage2)
+    t.ok(child._events.message)
+
+    child.removeListener('message', onMessage)
+    t.ok(child._events.message)
+    t.equal(child._events.message.__NR_original, onMessage2)
+
+    child.on('exit', function() {
+      t.end()
+    })
+  })
+})
+
+test('should not break removeListener for multiple events down to multiple', (t) => {
+  const agent = setupAgent(t)
+
+  helper.runInTransaction(agent, function() {
+    const child = cp.fork('./exec-me.js', {cwd: __dirname})
+
+    function onMessage() {}
+    function onMessage2() {}
+    function onMessage3() {}
+
+    child.on('message', onMessage)
+    child.on('message', onMessage2)
+    child.on('message', onMessage3)
+    t.ok(child._events.message)
+
+    child.removeListener('message', onMessage)
+    t.ok(child._events.message)
+    t.equal(child._events.message.length, 2)
 
     child.on('exit', function() {
       t.end()
@@ -81,6 +129,114 @@ test('should not break once() removal of listener', (t) => {
 
     child.on('exit', function() {
       t.ok(invokedMessage, 'Must have onMessage called for test to be valid.')
+      t.end()
+    })
+  })
+})
+
+test('should not break multiple once() for multiple events down to single', (t) => {
+  const agent = setupAgent(t)
+
+  helper.runInTransaction(agent, function() {
+    const child = cp.fork('./exec-me.js', {cwd: __dirname})
+
+    let invokedMessage1 = false
+    let invokedMessage2 = false
+    child.once('message', function onMessage() {
+      invokedMessage1 = true
+    })
+
+    child.once('message', function onMessage2() {
+      invokedMessage2 = true
+    })
+
+    function onMessage3() {}
+    child.on('message', onMessage3)
+
+    child.on('exit', function() {
+      t.ok(invokedMessage1, 'Must have onMessage called for test to be valid.')
+      t.ok(invokedMessage2, 'Must have onMessage2 called for test to be valid.')
+
+      t.equal(child._events.message.__NR_original, onMessage3)
+      t.end()
+    })
+  })
+})
+
+test('should not break multiple once() for multiple events down to multiple', (t) => {
+  const agent = setupAgent(t)
+
+  helper.runInTransaction(agent, function() {
+    const child = cp.fork('./exec-me.js', {cwd: __dirname})
+
+    let invokedMessage1 = false
+    let invokedMessage2 = false
+    child.once('message', function onMessage() {
+      invokedMessage1 = true
+    })
+
+    child.once('message', function onMessage2() {
+      invokedMessage2 = true
+    })
+
+    child.on('message', function onMessage3() {})
+    child.on('message', function onMessage4() {})
+
+    child.on('exit', function() {
+      t.ok(invokedMessage1, 'Must have onMessage called for test to be valid.')
+      t.ok(invokedMessage2, 'Must have onMessage2 called for test to be valid.')
+
+      t.ok(child._events.message)
+      t.equal(child._events.message.length, 2)
+
+      t.end()
+    })
+  })
+})
+
+// Don't expect this should be possible but lets protect ourselves anyways.
+test('should not break removal of non-wrapped listener', (t) => {
+  const agent = setupAgent(t)
+
+  helper.runInTransaction(agent, function() {
+    const child = cp.fork('./exec-me.js', {cwd: __dirname})
+
+    // Avoid our instrumentation via private method.
+    // TODO: should we also be instrumenting addListener?
+    function nonWrappedListener() {}
+    child.addListener('message', nonWrappedListener)
+
+    child.removeListener('message', nonWrappedListener)
+    t.notOk(child._events.message)
+
+    child.on('exit', function() {
+      t.end()
+    })
+  })
+})
+
+// Don't expect this should be possible but lets protect ourselves anyways.
+test('should not break when non-wrapped listener exists', (t) => {
+  const agent = setupAgent(t)
+
+  helper.runInTransaction(agent, function() {
+    const child = cp.fork('./exec-me.js', {cwd: __dirname})
+
+    let invokedMessage = false
+    child.once('message', function onMessage() {
+      invokedMessage = true
+    })
+
+    // Avoid our instrumentation via private method.
+    function nonWrappedListener() {}
+    child.addListener('message', nonWrappedListener)
+
+    child.on('exit', function() {
+      t.ok(invokedMessage, 'Must have onMessage called for test to be valid.')
+
+      t.ok(child._events.message)
+      t.equal(child._events.message, nonWrappedListener)
+
       t.end()
     })
   })
