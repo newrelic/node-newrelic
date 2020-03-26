a4d24a91da8752f1028f7003c542016e4c9083b3
Michael Goin <mgoin@newrelic.com>
2020-03-26 12:33:29 -0700

    Extracts aggregator creation logic to factory function. Fixes and moves some validation.

---
+++ test/unit/spans/create-span-event-aggregator.test.js
@@ -0,0 +1,185 @@
+'use strict'
+
+const tap = require('tap')
+
+const Config = require('../../../lib/config')
+const SpanEventAggregator = require('../../../lib/spans/span-event-aggregator')
+const StreamingSpanEventAggregator = require('../../../lib/spans/streaming-span-event-aggregator')
+const createSpanEventAggregator = require('../../../lib/spans/create-span-event-aggregator')
+
+const NAMES = require('../../../lib/metrics/names')
+
+const VALID_URL = 'https://infinite_tracing.test:443'
+const INVALID_URL = '//infinite_tracing.test:443'
+
+tap.test('should return standard when infinite feature flag disabled', (t) => {
+  const config = Config.initialize({
+    feature_flag: {
+      infinite_tracing: false
+    }
+  })
+
+  const aggregator = createSpanEventAggregator(config)
+  assertStandardSpanAggregator(t, aggregator)
+
+  t.end()
+})
+
+tap.test('should return standard when trace observer not configured', (t) => {
+  const config = Config.initialize({
+    feature_flag: {
+      infinite_tracing: true
+    }
+  })
+
+  const aggregator = createSpanEventAggregator(config)
+  assertStandardSpanAggregator(t, aggregator)
+
+  t.end()
+})
+
+tap.test('should return standard when trace observer not valid', (t) => {
+  const stubbedMetrics = createMetricAggregatorStub()
+
+  const config = Config.initialize({
+    feature_flag: {
+      infinite_tracing: true
+    },
+    infinite_tracing: { trace_observer: INVALID_URL }
+  })
+
+  const aggregator = createSpanEventAggregator(config, null, stubbedMetrics)
+  assertStandardSpanAggregator(t, aggregator)
+
+  t.end()
+})
+
+tap.test('should reset/disable trace observer when trace observer not valid', (t) => {
+  const stubbedMetrics = createMetricAggregatorStub()
+
+  const config = Config.initialize({
+    feature_flag: {
+      infinite_tracing: true
+    },
+    infinite_tracing: { trace_observer: INVALID_URL }
+  })
+
+  createSpanEventAggregator(config, null, stubbedMetrics)
+  t.equal(config.infinite_tracing.trace_observer, '')
+
+  t.end()
+})
+
+tap.test('should generate malformed support metric when trace observer not valid', (t) => {
+  const config = Config.initialize({
+    feature_flag: {
+      infinite_tracing: true
+    },
+    infinite_tracing: { trace_observer: INVALID_URL }
+  })
+
+  const stubbedMetrics = createMetricAggregatorStub((incrementedMetricName) => {
+    t.equal(incrementedMetricName, NAMES.INFINITE_TRACING.MALFORMED_TRACE_OBSERVER)
+
+    t.end()
+  })
+
+  createSpanEventAggregator(config, null, stubbedMetrics)
+})
+
+tap.test('should return standard when in serverless mode, trace observer valid', (t) => {
+  const config = Config.initialize({
+    feature_flag: {
+      infinite_tracing: true
+    },
+    serverless_mode: { enabled: true },
+    infinite_tracing: { trace_observer: VALID_URL }
+  })
+
+  const aggregator = createSpanEventAggregator(config)
+  assertStandardSpanAggregator(t, aggregator)
+
+  t.end()
+})
+
+
+tap.test('should return standard aggregator when node version < gprc minimum', (t) => {
+  tempOverrideNodeVersion(t, 'v10.0.0')
+
+  const config = Config.initialize({
+    feature_flag: {
+      infinite_tracing: true
+    },
+    infinite_tracing: { trace_observer: VALID_URL }
+  })
+
+  const aggregator = createSpanEventAggregator(config)
+  assertStandardSpanAggregator(t, aggregator)
+
+  t.end()
+})
+
+tap.test('should reset/disable trace observer when node version < gprc minimum', (t) => {
+  tempOverrideNodeVersion(t, 'v10.0.0')
+
+  const config = Config.initialize({
+    feature_flag: {
+      infinite_tracing: true
+    },
+    infinite_tracing: { trace_observer: VALID_URL }
+  })
+
+  createSpanEventAggregator(config)
+  t.equal(config.infinite_tracing.trace_observer, '')
+
+  t.end()
+})
+
+
+tap.test('should return streaming when trace observer configured', (t) => {
+  const config = Config.initialize({
+    feature_flag: {
+      infinite_tracing: true
+    },
+    infinite_tracing: { trace_observer: VALID_URL }
+  })
+
+  const aggregator = createSpanEventAggregator(config)
+  const isStreamingAggregator = aggregator instanceof StreamingSpanEventAggregator
+
+  t.ok(isStreamingAggregator)
+
+  t.end()
+})
+
+function tempOverrideNodeVersion(t, newVersion) {
+  const originalVersion = process.version
+  Object.defineProperty(process, 'version', {value: newVersion, writable: true})
+  t.teardown(() => {
+    process.version = originalVersion
+  })
+}
+
+function assertStandardSpanAggregator(t, aggregator) {
+  const isSpanEventAggregator = aggregator instanceof SpanEventAggregator
+  const isStreamingAggregator = aggregator instanceof StreamingSpanEventAggregator
+
+  t.ok(isSpanEventAggregator)
+  t.notOk(isStreamingAggregator)
+}
+
+function createMetricAggregatorStub(onMetricIncremented) {
+  const stubbedMetricAggregator = {
+    getOrCreateMetric: (name) => {
+      return {
+        incrementCallCount: () => {
+          if (onMetricIncremented) {
+            onMetricIncremented(name)
+          }
+        }
+      }
+    }
+  }
+
+  return stubbedMetricAggregator
+}
