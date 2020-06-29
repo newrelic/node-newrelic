df0391069f0e967db30642ccfba9d379d9a487df
Michael Goin <mgoin@newrelic.com>
2020-06-29 09:47:59 -0700

    Adds copyright header to JS files.

--- examples/api/distributed-tracing/example1-background.js
+++ examples/api/distributed-tracing/example1-background.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 var newrelic = require('newrelic')
 // Give the agent some time to start up.
 setTimeout(runTest, 2000)
--- test/unit/metric/staging-rules.js
+++ test/unit/metric/staging-rules.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 'use strict'
 
 // rules captured from the staging collector on 2012-08-29
