c87837a020dcf7a6cd10a682fdec3631a295ca0b
Tomas Hanacek <tomas.hanacek1@gmail.com>
2020-08-05 11:25:30 +0200

    Fix large JSON inserts obfluscation

--- test/unit/util/obfuscate-sql.test.js
+++ test/unit/util/obfuscate-sql.test.js
@@ -37,5 +37,13 @@ tap.test('sql obfuscation', (t) => {
     t.end()
   })
 
+  t.test('should handle large JSON inserts', (t) => {
+    const JSONData = '{"data": "' + new Array(8400000).fill('a').join('') + '"}'
+    const result = obfuscate(
+      'INSERT INTO "Documents" ("data") VALUES (\'' + JSONData + '\')', 'postgres')
+    t.equal(result, 'INSERT INTO "Documents" ("data") VALUES (?)')
+    t.end()
+  })
+
   t.end()
 })
