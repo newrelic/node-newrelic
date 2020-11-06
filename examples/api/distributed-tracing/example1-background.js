2420d9f9050de7edf26b87be01afa3f965b34d73
Nick Tzaperas <nick@nijotz.com>
2020-11-06 10:43:45 -0800

    Update example to not use deprecated methods

--- examples/api/distributed-tracing/example1-background.js
+++ examples/api/distributed-tracing/example1-background.js
@@ -3,23 +3,24 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-var newrelic = require('newrelic')
+const newrelic = require('newrelic')
+
 // Give the agent some time to start up.
 setTimeout(runTest, 2000)
 
 function runTest() {
   newrelic.startWebTransaction('Custom web transaction', function() {
     // Call newrelic.getTransaction to retrieve a handle on the current transaction.
-    var transactionHandle = newrelic.getTransaction()
+    let transactionHandle = newrelic.getTransaction()
 
     // Generate the payload right before creating the linked transaction.
-    var payload = transactionHandle.createDistributedTracePayload()
-    var jsonPayload = payload.text()
+    let headers = {}
+    transactionHandle.insertDistributedTraceHeaders(headers)
 
     newrelic.startBackgroundTransaction('Background task', function executeTransaction() {
-      var backgroundHandle = newrelic.getTransaction()
+      let backgroundHandle = newrelic.getTransaction()
       // Link the nested transaction by accepting the payload with the background transaction's handle
-      backgroundHandle.acceptDistributedTracePayload(jsonPayload)
+      backgroundHandle.acceptDistributedTraceHeaders(headers)
       // End the transactions
       backgroundHandle.end(transactionHandle.end)
     })
