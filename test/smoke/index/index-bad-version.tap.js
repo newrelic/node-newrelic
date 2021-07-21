13c4aa6264ab6c91adbfadc40d2f53e06fc9b2e9
Bob Evans <robert.evans25@gmail.com>
2021-07-21 15:35:35 -0400

    refactored bad version smoke tests to verify stub api are loaded

--- test/smoke/index/index-bad-version.tap.js
+++ test/smoke/index/index-bad-version.tap.js
@@ -7,45 +7,39 @@
 
 const tap = require('tap')
 const {getTestSecret} = require('../../helpers/secrets')
+const StubApi = require('../../../stub_api')
 
 
 const license = getTestSecret('TEST_LICENSE')
-tap.test('loading the agent with a bad version', {timeout: 20000}, (t) => {
-  let agent = null
-
+const VERSIONS = ['garbage', '4.0.0']
+tap.test('load agent with bad versions should load stub agent', (t) => {
   process.env.NEW_RELIC_HOME = __dirname + '/..'
   process.env.NEW_RELIC_HOST = 'staging-collector.newrelic.com'
   process.env.NEW_RELIC_LICENSE_KEY = license
 
-  t.doesNotThrow(function() {
-    var _version = process.version
-    Object.defineProperty(process, 'version', {value: 'garbage', writable: true})
-    t.equal(process.version, 'garbage', 'should have set bad version')
+  t.afterEach((done) => {
+    // must delete both of these to force a reload
+    // of the index.js file
+    delete require.cache.__NR_cache
+    delete require.cache[require.resolve('../../../index.js')]
+    done()
+  })
 
-    var api = require('../../../index.js')
-    agent = api.agent
-    t.ok(agent)
+  VERSIONS.forEach((version) => {
+    t.test(`agent version: ${version}`, (t) => {
+      t.doesNotThrow(function() {
+        var _version = process.version
+        Object.defineProperty(process, 'version', {value: version, writable: true})
+        t.equal(process.version, version, 'should have set bad version')
 
-    process.version = _version
-  }, "malformed process.version doesn't blow up the process")
-  if (!t.passing()) {
-    t.comment('Bailing out early.')
-    return t.end()
-  }
+        var api = require('../../../index.js')
+        t.ok(api instanceof StubApi)
 
-  function shutdown() {
-    t.equal(agent._state, 'started', "agent didn't error connecting to staging")
-    t.deepEquals(agent.config.applications(), ['My Application'], "app name is valid")
-    t.equals(agent.config.agent_enabled, true, "the agent is still enabled")
-
-    agent.stop(function cb_stop(err) {
-      t.notOk(err, 'should not error when stopping')
-      t.equal(agent._state, 'stopped', "agent didn't error shutting down")
-
+        process.version = _version
+      }, "malformed process.version doesn't blow up the process")
       t.end()
     })
-  }
+  })
 
-  agent.once('errored', shutdown)
-  agent.once('started', shutdown)
+  t.end()
 })
