2c11e11e8b1a42b875f893852f4d5c795eb6e29c
Diana Thayer <dthayer@newrelic.com>
2021-09-24 16:16:53 -0700

    update examples to fit new jsdoc rules

---
+++ examples/.eslintrc.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+module.exports = {
+  extends: ['@newrelic', 'plugin:jsdoc/recommended'],
+  plugins: ['jsdoc'],
+  rules: {
+    'consistent-return': 'off',
+    'no-console': 'off',
+    'node/no-extraneous-require': 'off'
+  }
+}
--- examples/api/segments/example1-callbacks.js
+++ examples/api/segments/example1-callbacks.js
@@ -5,12 +5,51 @@
 
 'use strict'
 
-var newrelic = require('newrelic')
+const newrelic = require('newrelic')
+
+// we'll stub out an async task that runs as part of monitoring a segment
+/**
+ * @param callback
+ */
+function myAsyncTask(callback) {
+  const sleep = new Promise((resolve) => {
+    setTimeout(resolve, 1)
+  })
+  sleep.then(() => {
+    callback(null, 'hello world')
+  })
+}
+
+// then we stub out the task that handles that task's result,
+// to show how the result is passed throughthe segment handler
+/**
+ * @param greetings
+ * @param callback
+ */
+function myNextTask(greetings, callback) {
+  callback(null, `${greetings}, it's me!`)
+}
+
+// this task will be run as its own segment within our transaction handler
+/**
+ * @param cb
+ */
+function someTask(cb) {
+  myAsyncTask(function firstCb(err1, result) {
+    if (err1) {
+      return cb(err1)
+    }
+
+    myNextTask(result, function secondCb(err2, output) {
+      cb(err2, output)
+    })
+  })
+}
 
 // Segments can only be created inside of transactions. They could be automatically
 // generated HTTP transactions or custom transactions.
 newrelic.startBackgroundTransaction('bg-tx', function transHandler() {
-  var tx = newrelic.getTransaction()
+  const tx = newrelic.getTransaction()
 
   // `startSegment()` takes a segment name, a boolean if a metric should be
   // created for this segment, the handler function, and an optional callback.
@@ -20,19 +59,7 @@ newrelic.startBackgroundTransaction('bg-tx', function transHandler() {
 
   newrelic.startSegment('myCustomSegment', false, someTask, function cb(err, output) {
     // Handle the error and output as appropriate.
-    console.log(output)
+    console.log(output) // "hello world, it's me!"
     tx.end()
   })
 })
-
-function someTask(cb) {
-  myAsyncTask(function firstCb(err, result) {
-    if (err) {
-      return cb(err)
-    }
-
-    myNextTask(result, function secondCb(err, output) {
-      cb(err, output)
-    })
-  })
-}
