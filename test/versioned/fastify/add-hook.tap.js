a35b96e24f764c4d94666bcb3fb68b5b73911212
Bob Evans <robert.evans25@gmail.com>
2021-10-06 17:13:26 -0400

    added the add-hook test to fastify versioned stanzas, fixed the test to properly make requests and register/assert segments

--- test/versioned/fastify/add-hook.tap.js
+++ test/versioned/fastify/add-hook.tap.js
@@ -6,78 +6,97 @@
 'use strict'
 
 const tap = require('tap')
+const util = require('util')
 const requestClient = require('request')
+const getAsync = util.promisify(requestClient.get)
 const helper = require('../../lib/agent_helper')
 const metrics = require('../../lib/metrics_helper')
 
-const REQUEST_HOOKS = [
-  'onRequest',
-  'preParsing',
-  'preValidation',
-  'preHandler',
-  'preSerialization',
-  'onSend',
-  'onResponse'
-]
+// all of these events fire before the route handler
+// See: https://www.fastify.io/docs/latest/Lifecycle/
+// for more info on sequence
+const REQUEST_HOOKS = ['onRequest', 'preParsing', 'preValidation', 'preHandler']
 
-tap.test(function (t) {
-  t.autoend()
+// these events fire after the route
+// handler. they are in separate arrays
+// for segment relationship assertions later
+const AFTER_HANDLER_HOOKS = ['preSerialization', 'onSend']
 
-  let agent = null
-  let fastify = null
+// the onResponse hook fires after a response
+// is received by client which is out of context
+// of the transaction
+const AFTER_TX_HOOKS = ['onResponse']
 
-  t.beforeEach(() => {
-    agent = helper.instrumentMockedAgent({
+const ALL_HOOKS = [...REQUEST_HOOKS, ...AFTER_HANDLER_HOOKS, ...AFTER_TX_HOOKS]
+
+tap.test(
+  'should properly name segments for each lifecycle hook event',
+  async function hookNameTests(t) {
+    const agent = helper.instrumentMockedAgent({
       feature_flag: {
         fastify_instrumentation: true
       }
     })
-    fastify = require('fastify')()
+    const fastify = require('fastify')()
 
-    fastify.get('/', function helloWorld() {
-      return { hello: 'world' }
+    fastify.get('/', function routeHandler(_, reply) {
+      t.comment('route handler called')
+      reply.send({ hello: 'world' })
     })
-  })
 
-  t.afterEach(() => {
-    helper.unloadAgent(agent)
-    fastify.close()
-  })
+    t.teardown(() => {
+      helper.unloadAgent(agent)
+      fastify.close()
+    })
 
-  t.test('should wrap all lifecycle hooks', async function (t) {
+    function getSegmentNames(hooks) {
+      return hooks.map((hookName) => {
+        return `Nodejs/Middleware/Fastify/${hookName}/testHook`
+      })
+    }
+
     // setup hooks
-    const ok = REQUEST_HOOKS.reduce((all, hookName) => {
+    const ok = ALL_HOOKS.reduce((all, hookName) => {
+      t.comment(`registering ${hookName}`)
       all[hookName] = false
       return all
     }, {})
-    for (const hookName of REQUEST_HOOKS) {
-      fastify.addHook(hookName, function testHook(req, res, next) {
+
+    ALL_HOOKS.forEach((hookName) => {
+      fastify.addHook(hookName, function testHook(...args) {
+        t.comment(`${hookName} called`)
+        // lifecycle signatures vary between the events
+        // the last arg is always the next function though
+        const next = args[args.length - 1]
         ok[hookName] = true
         next()
       })
-    }
-    // setup transaction monitor
-    const promise = new Promise((resolve) => {
-      agent.on('transactionFinished', (transaction) => {
-        t.equal('WebFrameworkUri/Fastify/GET//', transaction.getName(), `transaction name matched`)
-        metrics.assertSegments(transaction.trace.root, [
-          'WebTransaction/WebFrameworkUri/Fastify/GET//',
-          REQUEST_HOOKS.map((hookName) => {
-            return `Nodejs/Middleware/Fastify/${hookName}/testHook`
-          })
-        ])
-        resolve()
-      })
     })
-    // start and ping the server
+
+    agent.on('transactionFinished', (transaction) => {
+      t.equal('WebFrameworkUri/Fastify/GET//', transaction.getName(), `transaction name matched`)
+      // all the hooks are siblings of the route handler
+      // except the AFTER_HANDLER_HOOKS which are children of the route handler
+      metrics.assertSegments(transaction.trace.root, [
+        'WebTransaction/WebFrameworkUri/Fastify/GET//',
+        [
+          ...getSegmentNames(REQUEST_HOOKS),
+          'Nodejs/Middleware/Fastify/routeHandler',
+          getSegmentNames(AFTER_HANDLER_HOOKS)
+        ]
+      ])
+    })
+
     await fastify.listen(0)
     const { port } = fastify.server.address()
-    await requestClient.get(`http://127.0.0.1:${port}/`)
-    await promise
-    // validate
+    const result = await getAsync(`http://127.0.0.1:${port}/`)
+    t.equal(result.statusCode, 200)
+    t.equal(result.body, JSON.stringify({ hello: 'world' }))
+
+    // verify every hook was called after response
     for (const [hookName, isOk] of Object.entries(ok)) {
       t.equal(isOk, true, `${hookName} captured`)
     }
     t.end()
-  })
-})
+  }
+)
