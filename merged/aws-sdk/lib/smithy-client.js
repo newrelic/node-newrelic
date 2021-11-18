f18a6ec95502950738cf571a7815d4de6d709d59
Bob Evans <robert.evans25@gmail.com>
2021-11-18 15:09:13 -0500

    abstracted adding aws attributes and properly await the build middleware

--- merged/aws-sdk/lib/smithy-client.js
+++ merged/aws-sdk/lib/smithy-client.js
@@ -65,9 +65,9 @@ function getPlugin(shim, config) {
  *
  */
 function headerMiddleware(shim, next) {
-  return function wrappedHeaderMw(args) {
+  return async function wrappedHeaderMw(args) {
     args.request.headers[shim.DISABLE_DT] = true
-    return next(args)
+    return await next(args)
   }
 }
 
@@ -87,16 +87,35 @@ function attrMiddleware(shim, config, next, context) {
     try {
       region = await config.region()
     } catch (err) {
-      shim.logger.error(err, 'Failed to get the AWS config and region')
+      shim.logger.debug(err, 'Failed to get the AWS region')
     } finally {
       const result = await next(args)
-      const { response } = result
-      const segment = shim.getSegment(response.body.req)
-      segment.addAttribute('aws.operation', context.commandName || UNKNOWN)
-      segment.addAttribute('aws.requestId', response.headers['x-amzn-requestid'] || UNKNOWN)
-      segment.addAttribute('aws.service', (config && config.serviceId) || UNKNOWN)
-      segment.addAttribute('aws.region', region || UNKNOWN)
+      addAwsAttributes({ result, config, region, shim, context })
       return result
     }
+  }
+}
+
+/**
+ * Adds the necessary aws.* attributes to either the External or first
+ * class operation segment
+ *
+ * @param {Object} params
+ * @param {Object} params.result result from middleware
+ * @param {Object} params.config AWS config
+ * @param {string} params.region AWS region
+ * @param {Shim} params.shim
+ * @param {Object} params.context smithy client context
+ */
+function addAwsAttributes({ result, config, region, shim, context }) {
+  try {
+    const { response } = result
+    const segment = shim.getSegment(response.body.req)
+    segment.addAttribute('aws.operation', context.commandName || UNKNOWN)
+    segment.addAttribute('aws.requestId', response.headers['x-amzn-requestid'] || UNKNOWN)
+    segment.addAttribute('aws.service', config.serviceId || UNKNOWN)
+    segment.addAttribute('aws.region', region || UNKNOWN)
+  } catch (err) {
+    shim.logger.debug(err, 'Failed to add AWS attributes to segment')
   }
 }
