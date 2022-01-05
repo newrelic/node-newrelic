8759b398d2b6688e2eca29bad1c8ac7f2c2514c9
Bob Evans <robert.evans25@gmail.com>
2022-01-05 11:19:08 -0500

    added semver check on agent to determine if it is appropos to register v3 instrumentation

--- merged/aws-sdk/nr-hooks.js
+++ merged/aws-sdk/nr-hooks.js
@@ -4,36 +4,56 @@
  */
 
 'use strict'
+const newrelic = require('newrelic')
+const semver = require('semver')
+const agentVersion = newrelic && newrelic.agent && newrelic.agent.version
 
-module.exports = [
+const instrumentations = [
   {
     type: 'conglomerate',
     moduleName: 'aws-sdk',
     onRequire: require('./lib/v2/instrumentation')
-  },
-  {
-    type: 'generic',
-    moduleName: '@aws-sdk/smithy-client',
-    onResolved: require('./lib/v3/smithy-client')
-  },
-  {
-    type: 'message',
-    moduleName: '@aws-sdk/client-sns',
-    onResolved: require('./lib/v3/sns')
-  },
-  {
-    type: 'message',
-    moduleName: '@aws-sdk/client-sqs',
-    onResolved: require('./lib/v3/sqs')
-  },
-  {
-    type: 'datastore',
-    moduleName: '@aws-sdk/client-dynamodb',
-    onResolved: require('./lib/v3/client-dynamodb')
-  },
-  {
-    type: 'datastore',
-    moduleName: '@aws-sdk/lib-dynamodb',
-    onResolved: require('./lib/v3/lib-dynamodb')
   }
 ]
+
+// TODO: Remove this semver check and semver module when we ship Node 18 support
+// A bug existed in 8.6.0 when we introduced the `onResolved` hook.
+// See: https://github.com/newrelic/node-newrelic/pull/986
+// To avoid unnecessary support issues we will require agent version >= 8.7.0 to
+// register AWS SDK v3 instrumentation
+if (semver.satisfies(agentVersion, '>=8.7.0')) {
+  instrumentations.push(
+    {
+      type: 'generic',
+      moduleName: '@aws-sdk/smithy-client',
+      onResolved: require('./lib/v3/smithy-client')
+    },
+    {
+      type: 'message',
+      moduleName: '@aws-sdk/client-sns',
+      onResolved: require('./lib/v3/sns')
+    },
+    {
+      type: 'message',
+      moduleName: '@aws-sdk/client-sqs',
+      onResolved: require('./lib/v3/sqs')
+    },
+    {
+      type: 'datastore',
+      moduleName: '@aws-sdk/client-dynamodb',
+      onResolved: require('./lib/v3/client-dynamodb')
+    },
+    {
+      type: 'datastore',
+      moduleName: '@aws-sdk/lib-dynamodb',
+      onResolved: require('./lib/v3/lib-dynamodb')
+    }
+  )
+} else {
+  newrelic.shim.logger.warn(
+    'The New Relic Node.js agent must be >= 8.7.0 to instrument AWS SDK v3, current version: %s',
+    agentVersion
+  )
+}
+
+module.exports = instrumentations
