7c874017ed870f08c286861fbd3ab7287a7119a0
Michael Goin <michaelgoin@gmail.com>
2022-02-11 10:49:04 -0800

    Adds transaction naming and request parameter capture support.

--- merged/nextjs/index.js
+++ merged/nextjs/index.js
@@ -4,3 +4,12 @@
  */
 
 'use strict'
+
+/**
+ * Allows users to `require('@newrelic/next')` directly in their app. If they
+ * for some reason choose to explicitly use an older version of our instrumentation
+ * then the supportability metrics for custom instrumentation will trigger.
+ */
+const newrelic = require('newrelic')
+
+newrelic.instrumentWebframework('./next-server', require('./lib/next-server'))
--- merged/nextjs/lib/instrumentation.js
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright 2021 New Relic Corporation. All rights reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-'use strict'
