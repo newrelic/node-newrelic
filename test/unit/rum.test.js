ee86c84d72f809457a7983d3dd2a3502b37a2527
Bob Evans <robert.evans25@gmail.com>
2022-06-03 13:53:44 -0400

    added extra assertions on ending of browser timing header to ensure it is complete

--- test/unit/rum.test.js
+++ test/unit/rum.test.js
@@ -128,36 +128,39 @@ describe('the RUM API', function () {
   it('should get browser agent script with wrapping tag', function () {
     helper.runInTransaction(agent, function (t) {
       t.finalizeNameFromUri('hello')
-      api
-        .getBrowserTimingHeader()
+      const timingHeader = api.getBrowserTimingHeader()
+      timingHeader
         .startsWith(
           `<script type=\'text/javascript\'>window.NREUM||(NREUM={});NREUM.info = {"licenseKey":1234,"applicationID":12345,`
         )
         .should.equal(true)
+      timingHeader.endsWith(`}; function() {}</script>`).should.equal(true)
     })
   })
 
   it('should get browser agent script with wrapping tag and add nonce attribute to script if passed in options', function () {
     helper.runInTransaction(agent, function (t) {
       t.finalizeNameFromUri('hello')
-      api
-        .getBrowserTimingHeader({ nonce: '12345' })
+      const timingHeader = api.getBrowserTimingHeader({ nonce: '12345' })
+      timingHeader
         .startsWith(
           `<script type=\'text/javascript\' nonce="12345">window.NREUM||(NREUM={});NREUM.info = {"licenseKey":1234,"applicationID":12345,`
         )
         .should.equal(true)
+      timingHeader.endsWith(`}; function() {}</script>`).should.equal(true)
     })
   })
 
   it('should get browser agent script without wrapping tag if hasToRemoveScriptWrapper passed in options', function () {
     helper.runInTransaction(agent, function (t) {
       t.finalizeNameFromUri('hello')
-      api
-        .getBrowserTimingHeader({ hasToRemoveScriptWrapper: true })
+      const timingHeader = api.getBrowserTimingHeader({ hasToRemoveScriptWrapper: true })
+      timingHeader
         .startsWith(
           'window.NREUM||(NREUM={});NREUM.info = {"licenseKey":1234,"applicationID":12345,'
         )
         .should.equal(true)
+      timingHeader.endsWith(`}; function() {}`).should.equal(true)
     })
   })
 
