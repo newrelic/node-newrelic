9f7bbb9c22008e2bb7ade01ec36f497639c6d51f
Diana Thayer <dthayer@newrelic.com>
2022-06-07 10:53:07 -0700

    scaffold grpc client instrumentation

---
+++ test/unit/grpc/client.test.js
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2022 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+const tap = require('tap')
+// const instrument = require('../../../lib/instrumentation/@grpc/grpc-js')
+
+tap.test('grpc client instrumentation', (t) => {
+  t.autoend()
+
+  // TODO
+})
