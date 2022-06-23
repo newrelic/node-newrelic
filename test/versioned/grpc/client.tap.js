936049772601aef073ae88b2ff5f7f6c488453cc
Bob Evans <robert.evans25@gmail.com>
2022-06-23 16:09:29 -0400

    fixed an issue with setting host in older versions and switched to channel.target.path. added more tests around DT assertions

--- test/versioned/grpc/client.tap.js
+++ test/versioned/grpc/client.tap.js
@@ -56,21 +56,22 @@ tap.test('grpc client instrumentation', (t) => {
     proto = null
   })
 
-  t.test('should instrument client ping in a transaction', (t) => {
+  t.test('should track unary client requests as an external when in a transaction', (t) => {
     helper.runInTransaction(agent, 'web', async (tx) => {
       agent.on('transactionFinished', (transaction) => {
-        const myTx = transaction.trace.root.children[0]
+        const segment = transaction.trace.root.children[0]
         segmentName = `${EXTERNAL.PREFIX}${CLIENT_ADDR}${helloName}`
-        t.equal(myTx.name, segmentName, 'segment name is correct')
-        const { attributes } = myTx.attributes
+        t.equal(segment.name, segmentName, 'segment name is correct')
+        const attributes = segment.getAttributes()
         t.equal(
-          attributes['http.url'].value,
+          attributes['http.url'],
           `grpc://${CLIENT_ADDR}${helloName}`,
           'http.url attribute should be correct'
         )
-        t.equal(attributes['http.method'].value, helloName, 'method name should be correct')
-        t.equal(attributes['grpc.statusCode'].value, 0, 'status code should be zero')
-        t.equal(attributes.component.value, 'gRPC', 'should have the component set to "gRPC"')
+        t.equal(attributes['http.method'], helloName, 'method name should be correct')
+        t.equal(attributes['grpc.statusCode'], 0, 'status code should be zero')
+        t.equal(attributes['grpc.statusText'], 'OK', 'status text should be OK')
+        t.equal(attributes.component, 'gRPC', 'should have the component set to "gRPC"')
 
         for (const metricName of metrics) {
           const metric = agent.metrics.getMetric(metricName)
@@ -91,7 +92,60 @@ tap.test('grpc client instrumentation', (t) => {
     })
   })
 
-  t.test('should not instrument client outside of a transaction', async (t) => {
+  t.test('should include distributed trace headers when enabled', (t) => {
+    helper.runInTransaction(agent, 'dt-test', async (tx) => {
+      await new Promise((resolve) => {
+        client.sayHello({ name: 'dt test' }, (err) => {
+          t.error(err)
+          const dtMeta = server.metadataMap.get('dt test')
+          t.match(
+            dtMeta.get('traceparent')[0],
+            /^[\w\d\-]{55}$/,
+            'should have traceparent in server metadata'
+          )
+          t.equal(dtMeta.get('newrelic')[0], '', 'should have newrelic in server metadata')
+          tx.end()
+          t.end()
+          resolve()
+        })
+      })
+    })
+  })
+
+  t.test('should not include distributed trace headers when not in transaction', async (t) => {
+    await new Promise((resolve) => {
+      client.sayHello({ name: 'dt test' }, (err) => {
+        t.error(err)
+        const dtMeta = server.metadataMap.get('dt test')
+        t.notOk(dtMeta.has('traceparent'), 'should not have traceparent in server metadata')
+        t.notOk(dtMeta.has('newrelic'), 'should not have newrelic in server metadata')
+        t.end()
+        resolve()
+      })
+    })
+  })
+
+  t.test(
+    'should not include distributed trace headers when distributed_tracing.enabled is set to false',
+    (t) => {
+      agent.config.distributed_tracing.enabled = false
+      helper.runInTransaction(agent, 'dt-test', async (tx) => {
+        await new Promise((resolve) => {
+          client.sayHello({ name: 'dt test' }, (err) => {
+            t.error(err)
+            const dtMeta = server.metadataMap.get('dt test')
+            t.notOk(dtMeta.has('traceparent'), 'should not have traceparent in server metadata')
+            t.notOk(dtMeta.has('newrelic'), 'should not have newrelic in server metadata')
+            tx.end()
+            t.end()
+            resolve()
+          })
+        })
+      })
+    }
+  )
+
+  t.test('should not track external unary client requests outside of a transaction', async (t) => {
     await new Promise((resolve) => {
       client.sayHello({ name: 'New Relic' }, (err, response) => {
         t.error(err)
@@ -112,26 +166,26 @@ tap.test('grpc client instrumentation', (t) => {
   t.test('should record errors in a transaction', (t) => {
     helper.runInTransaction(agent, 'web', async (tx) => {
       agent.on('transactionFinished', (transaction) => {
-        const myTx = transaction.trace.root.children[0]
-        const { attributes } = myTx.attributes
+        const segment = transaction.trace.root.children[0]
+        const attributes = segment.getAttributes()
         t.equal(agent.errors.traceAggregator.errors.length, 1, 'should record a single error')
         t.equal(
-          attributes['http.url'].value,
+          attributes['http.url'],
           `grpc://${CLIENT_ADDR}${errorName}`,
           'http.url attribute should be correct'
         )
-        t.equal(attributes['http.method'].value, errorName, 'method name should be correct')
+        t.equal(attributes['http.method'], errorName, 'method name should be correct')
         t.equal(
-          attributes['grpc.statusCode'].value,
+          attributes['grpc.statusCode'],
           grpc.status.FAILED_PRECONDITION,
           'status code should correspond to FAILED_PRECONDITION'
         )
         t.equal(
-          attributes['grpc.statusText'].value,
+          attributes['grpc.statusText'],
           'i think i will cause problems on purpose',
           'status text should be error message'
         )
-        t.equal(attributes.component.value, 'gRPC', 'should have the component set to "gRPC"')
+        t.equal(attributes.component, 'gRPC', 'should have the component set to "gRPC"')
         const error = agent.errors.traceAggregator.errors[0][2]
         t.equal(error, 'i think i will cause problems on purpose', 'should have the error message')
         for (const metricName of metrics) {
@@ -162,8 +216,12 @@ tap.test('grpc client instrumentation', (t) => {
 async function getServer(grpc, proto) {
   const server = new grpc.Server()
   const credentials = grpc.ServerCredentials.createInsecure()
+  // quick and dirty map to store metadata for a given gRPC call
+  server.metadataMap = new Map()
   const implementation = {
-    sayHello: function sayHello({ request: { name } }, cb) {
+    sayHello: function sayHello({ metadata, request: { name } }, cb) {
+      // add the metadata from client that the server receives so we can assert DT functionality
+      server.metadataMap.set(name, metadata.internalRepr)
       const message = 'Hello ' + name
       cb(null, { message })
     },
