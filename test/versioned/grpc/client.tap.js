27d8eac55a6ad2ccc113a4f4c38070ee706001df
Bob Evans <robert.evans25@gmail.com>
2022-06-27 17:17:51 -0400

    refactored common code into utils

--- test/versioned/grpc/client.tap.js
+++ test/versioned/grpc/client.tap.js
@@ -8,12 +8,19 @@
 const tap = require('tap')
 const protoLoader = require('@grpc/proto-loader')
 
-const EXTERNAL = require('../../../lib/metrics/names').EXTERNAL
-
 const helper = require('../../lib/agent_helper')
 
 const PROTO_PATH = `${__dirname}/example.proto`
-const { CLIENT_ADDR } = require('./constants')
+const {
+  assertExternalSegment,
+  assertMetricsNotExisting,
+  makeUnaryRequest,
+  makeClientStreamingRequest,
+  makeServerStreamingRequest,
+  makeBidiStreamingRequest,
+  getServer,
+  getClient
+} = require('./util')
 
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   keepCase: true,
@@ -31,21 +38,10 @@ tap.test('grpc client instrumentation', (t) => {
   let server
   let proto
   let grpc
-  let segmentName
-
-  const helloName = '/helloworld.Greeter/SayHello'
-  const helloClientStreamName = '/helloworld.Greeter/SayHelloClientStream'
-  const helloServerStreamName = '/helloworld.Greeter/SayHelloServerStream'
-  const helloBidiStreamName = '/helloworld.Greeter/SayHelloBidiStream'
-  const errorName = '/helloworld.Greeter/SayError'
-  const rollupHost = `${EXTERNAL.PREFIX}${CLIENT_ADDR}/all`
-  const grpcMetricName = `${EXTERNAL.PREFIX}${CLIENT_ADDR}/gRPC`
-  const metrics = [grpcMetricName, rollupHost, EXTERNAL.WEB, EXTERNAL.ALL]
 
   t.beforeEach(async () => {
     agent = helper.instrumentMockedAgent()
     grpc = require('@grpc/grpc-js')
-    const { getServer, getClient } = require('./util')
     proto = grpc.loadPackageDefinition(packageDefinition).helloworld
     server = await getServer(grpc, proto)
     client = getClient(grpc, proto)
@@ -62,117 +58,58 @@ tap.test('grpc client instrumentation', (t) => {
   t.test('should track unary client requests as an external when in a transaction', (t) => {
     helper.runInTransaction(agent, 'web', async (tx) => {
       agent.on('transactionFinished', (transaction) => {
-        const segment = transaction.trace.root.children[0]
-        segmentName = `${EXTERNAL.PREFIX}${CLIENT_ADDR}${helloName}`
-        t.equal(segment.name, segmentName, 'segment name is correct')
-        const attributes = segment.getAttributes()
-        t.equal(
-          attributes['http.url'],
-          `grpc://${CLIENT_ADDR}${helloName}`,
-          'http.url attribute should be correct'
-        )
-        t.equal(attributes['http.method'], helloName, 'method name should be correct')
-        t.equal(attributes['grpc.statusCode'], 0, 'status code should be zero')
-        t.equal(attributes['grpc.statusText'], 'OK', 'status text should be OK')
-        t.equal(attributes.component, 'gRPC', 'should have the component set to "gRPC"')
-
-        for (const metricName of metrics) {
-          const metric = agent.metrics.getMetric(metricName)
-          t.ok(metric.callCount > 0, `ensure ${metricName} was recorded`)
-        }
-        t.end()
+        assertExternalSegment({ t, tx: transaction, fnName: 'SayHello' })
       })
 
-      await new Promise((resolve) => {
-        client.sayHello({ name: 'New Relic' }, (err, response) => {
-          t.error(err)
-          t.ok(response, 'response exists')
-          t.equal(response.message, 'Hello New Relic', 'response message is correct')
-          tx.end()
-          resolve()
-        })
+      const response = await makeUnaryRequest({
+        client,
+        fnName: 'sayHello',
+        payload: { name: 'New Relic' }
       })
+      t.ok(response, 'response exists')
+      t.equal(response.message, 'Hello New Relic', 'response message is correct')
+      tx.end()
     })
   })
 
   t.test('should track client streaming requests as an external when in a transaction', (t) => {
     helper.runInTransaction(agent, 'web', async (tx) => {
       agent.on('transactionFinished', (transaction) => {
-        const segment = transaction.trace.root.children[0]
-        segmentName = `${EXTERNAL.PREFIX}${CLIENT_ADDR}${helloClientStreamName}`
-        t.equal(segment.name, segmentName, 'segment name is correct')
-        const attributes = segment.getAttributes()
-        t.equal(
-          attributes['http.url'],
-          `grpc://${CLIENT_ADDR}${helloClientStreamName}`,
-          'http.url attribute should be correct'
-        )
-        t.equal(attributes['http.method'], helloClientStreamName, 'method name should be correct')
-        t.equal(attributes['grpc.statusCode'], 0, 'status code should be zero')
-        t.equal(attributes['grpc.statusText'], 'OK', 'status text should be OK')
-        t.equal(attributes.component, 'gRPC', 'should have the component set to "gRPC"')
-
-        for (const metricName of metrics) {
-          const metric = agent.metrics.getMetric(metricName)
-          t.ok(metric.callCount > 0, `ensure ${metricName} was recorded`)
-        }
-        t.end()
+        assertExternalSegment({ t, tx: transaction, fnName: 'SayHelloClientStream' })
       })
 
-      await new Promise((resolve) => {
-        const call = client.sayHelloClientStream((err, response) => {
-          t.error(err)
-          t.ok(response, 'response exists')
-          t.equal(response.message, 'Hello bob, jordi, corey', 'response message is correct')
-          tx.end()
-          resolve()
-        })
-
-        const names = ['bob', 'jordi', 'corey']
-        names.forEach((name) => call.write({ name }))
-        call.end()
+      const names = [{ name: 'Bob' }, { name: 'Jordi' }, { name: 'Corey' }]
+      const response = await makeClientStreamingRequest({
+        client,
+        fnName: 'sayHelloClientStream',
+        payload: names
       })
+      t.ok(response, 'response exists')
+      t.equal(
+        response.message,
+        `Hello ${names.map(({ name }) => name).join(', ')}`,
+        'response message is correct'
+      )
+      tx.end()
     })
   })
 
   t.test('should track server streaming requests as an external when in a transaction', (t) => {
     helper.runInTransaction(agent, 'web', async (tx) => {
       agent.on('transactionFinished', (transaction) => {
-        const segment = transaction.trace.root.children[0]
-        segmentName = `${EXTERNAL.PREFIX}${CLIENT_ADDR}${helloServerStreamName}`
-        t.equal(segment.name, segmentName, 'segment name is correct')
-        const attributes = segment.getAttributes()
-        t.equal(
-          attributes['http.url'],
-          `grpc://${CLIENT_ADDR}${helloServerStreamName}`,
-          'http.url attribute should be correct'
-        )
-        t.equal(attributes['http.method'], helloServerStreamName, 'method name should be correct')
-        t.equal(attributes['grpc.statusCode'], 0, 'status code should be zero')
-        t.equal(attributes['grpc.statusText'], 'OK', 'status text should be OK')
-        t.equal(attributes.component, 'gRPC', 'should have the component set to "gRPC"')
-
-        for (const metricName of metrics) {
-          const metric = agent.metrics.getMetric(metricName)
-          t.ok(metric.callCount > 0, `ensure ${metricName} was recorded`)
-        }
-        t.end()
+        assertExternalSegment({ t, tx: transaction, fnName: 'SayHelloServerStream' })
       })
 
-      await new Promise((resolve) => {
-        const names = ['bob', 'jordi', 'corey']
-        let counter = 0
-        const call = client.sayHelloServerStream({ name: names })
-        call.on('data', function (response) {
-          t.ok(response, 'response exists')
-          t.equal(response.message, `Hello ${names[counter]}`, 'response stream message is correct')
-          counter++
-        })
-        call.on('end', () => {
-          tx.end()
-          resolve()
-        })
+      const names = ['Moe', 'Larry', 'Curly']
+      const responses = await makeServerStreamingRequest({
+        client,
+        fnName: 'sayHelloServerStream',
+        payload: { name: names }
       })
+      names.forEach((name, i) => {
+        t.equal(responses[i], `Hello ${name}`, 'response stream message should be correct')
+      })
+      tx.end()
     })
   })
 
@@ -181,82 +118,46 @@ tap.test('grpc client instrumentation', (t) => {
     (t) => {
       helper.runInTransaction(agent, 'web', async (tx) => {
         agent.on('transactionFinished', (transaction) => {
-          const segment = transaction.trace.root.children[0]
-          segmentName = `${EXTERNAL.PREFIX}${CLIENT_ADDR}${helloBidiStreamName}`
-          t.equal(segment.name, segmentName, 'segment name is correct')
-          const attributes = segment.getAttributes()
-          t.equal(
-            attributes['http.url'],
-            `grpc://${CLIENT_ADDR}${helloBidiStreamName}`,
-            'http.url attribute should be correct'
-          )
-          t.equal(attributes['http.method'], helloBidiStreamName, 'method name should be correct')
-          t.equal(attributes['grpc.statusCode'], 0, 'status code should be zero')
-          t.equal(attributes['grpc.statusText'], 'OK', 'status text should be OK')
-          t.equal(attributes.component, 'gRPC', 'should have the component set to "gRPC"')
-
-          for (const metricName of metrics) {
-            const metric = agent.metrics.getMetric(metricName)
-            t.ok(metric.callCount > 0, `ensure ${metricName} was recorded`)
-          }
-          t.end()
+          assertExternalSegment({ t, tx: transaction, fnName: 'SayHelloBidiStream' })
         })
 
-        await new Promise((resolve) => {
-          const names = ['bob', 'jordi', 'corey']
-          let counter = 0
-          const call = client.sayHelloBidiStream()
-          names.forEach((name) => call.write({ name }))
-          call.on('data', function (response) {
-            t.ok(response, 'response exists')
-            t.equal(
-              response.message,
-              `Hello ${names[counter]}`,
-              'response stream message is correct'
-            )
-            counter++
-          })
-          call.on('end', () => {
-            tx.end()
-            resolve()
-          })
-          call.end()
+        const names = [{ name: 'Huey' }, { name: 'Dewey' }, { name: 'Louie' }]
+        const responses = await makeBidiStreamingRequest({
+          client,
+          fnName: 'sayHelloBidiStream',
+          payload: names
         })
+        names.forEach(({ name }, i) => {
+          t.equal(responses[i], `Hello ${name}`, 'response stream message should be correct')
+        })
+
+        tx.end()
       })
     }
   )
 
   t.test('should include distributed trace headers when enabled', (t) => {
     helper.runInTransaction(agent, 'dt-test', async (tx) => {
-      await new Promise((resolve) => {
-        client.sayHello({ name: 'dt test' }, (err) => {
-          t.error(err)
-          const dtMeta = server.metadataMap.get('dt test')
-          t.match(
-            dtMeta.get('traceparent')[0],
-            /^[\w\d\-]{55}$/,
-            'should have traceparent in server metadata'
-          )
-          t.equal(dtMeta.get('newrelic')[0], '', 'should have newrelic in server metadata')
-          tx.end()
-          t.end()
-          resolve()
-        })
-      })
+      const payload = { name: 'dt test' }
+      await makeUnaryRequest({ client, fnName: 'sayHello', payload })
+      const dtMeta = server.metadataMap.get(payload.name)
+      t.match(
+        dtMeta.get('traceparent')[0],
+        /^[\w\d\-]{55}$/,
+        'should have traceparent in server metadata'
+      )
+      t.equal(dtMeta.get('newrelic')[0], '', 'should have newrelic in server metadata')
+      tx.end()
+      t.end()
     })
   })
 
   t.test('should not include distributed trace headers when not in transaction', async (t) => {
-    await new Promise((resolve) => {
-      client.sayHello({ name: 'dt test' }, (err) => {
-        t.error(err)
-        const dtMeta = server.metadataMap.get('dt test')
-        t.notOk(dtMeta.has('traceparent'), 'should not have traceparent in server metadata')
-        t.notOk(dtMeta.has('newrelic'), 'should not have newrelic in server metadata')
-        t.end()
-        resolve()
-      })
-    })
+    const payload = { name: 'dt not in transaction' }
+    await makeUnaryRequest({ client, fnName: 'sayHello', payload })
+    const dtMeta = server.metadataMap.get(payload.name)
+    t.notOk(dtMeta.has('traceparent'), 'should not have traceparent in server metadata')
+    t.notOk(dtMeta.has('newrelic'), 'should not have newrelic in server metadata')
   })
 
   t.test(
@@ -264,85 +165,51 @@ tap.test('grpc client instrumentation', (t) => {
     (t) => {
       agent.config.distributed_tracing.enabled = false
       helper.runInTransaction(agent, 'dt-test', async (tx) => {
-        await new Promise((resolve) => {
-          client.sayHello({ name: 'dt test' }, (err) => {
-            t.error(err)
-            const dtMeta = server.metadataMap.get('dt test')
-            t.notOk(dtMeta.has('traceparent'), 'should not have traceparent in server metadata')
-            t.notOk(dtMeta.has('newrelic'), 'should not have newrelic in server metadata')
-            tx.end()
-            t.end()
-            resolve()
-          })
-        })
+        const payload = { name: 'dt disabled' }
+        await makeUnaryRequest({ client, payload, fnName: 'sayHello' })
+        const dtMeta = server.metadataMap.get(payload.name)
+        t.notOk(dtMeta.has('traceparent'), 'should not have traceparent in server metadata')
+        t.notOk(dtMeta.has('newrelic'), 'should not have newrelic in server metadata')
+        tx.end()
+        t.end()
       })
     }
   )
 
   t.test('should not track external unary client requests outside of a transaction', async (t) => {
-    await new Promise((resolve) => {
-      client.sayHello({ name: 'New Relic' }, (err, response) => {
-        t.error(err)
-        t.ok(response, 'response exists')
-        t.equal(response.message, 'Hello New Relic', 'response message is correct')
-        resolve()
-      })
-    })
-
-    for (const metricName of metrics) {
-      const metric = agent.metrics.getMetric(metricName)
-      t.notOk(metric, `${metricName} should not be recorded`)
-    }
-
-    t.end()
+    const payload = { name: 'New Relic' }
+    const response = await makeUnaryRequest({ client, fnName: 'sayHello', payload })
+    t.ok(response, 'response exists')
+    t.equal(response.message, 'Hello New Relic', 'response message is correct')
+    assertMetricsNotExisting({ t, agent })
   })
 
   t.test('should record errors in a transaction', (t) => {
+    const expectedStatusText = 'i think i will cause problems on purpose'
+    const expectedStatusCode = grpc.status.FAILED_PRECONDITION
     helper.runInTransaction(agent, 'web', async (tx) => {
       agent.on('transactionFinished', (transaction) => {
-        const segment = transaction.trace.root.children[0]
-        const attributes = segment.getAttributes()
         t.equal(agent.errors.traceAggregator.errors.length, 1, 'should record a single error')
-        t.equal(
-          attributes['http.url'],
-          `grpc://${CLIENT_ADDR}${errorName}`,
-          'http.url attribute should be correct'
-        )
-        t.equal(attributes['http.method'], errorName, 'method name should be correct')
-        t.equal(
-          attributes['grpc.statusCode'],
-          grpc.status.FAILED_PRECONDITION,
-          'status code should correspond to FAILED_PRECONDITION'
-        )
-        t.equal(
-          attributes['grpc.statusText'],
-          'i think i will cause problems on purpose',
-          'status text should be error message'
-        )
-        t.equal(attributes.component, 'gRPC', 'should have the component set to "gRPC"')
         const error = agent.errors.traceAggregator.errors[0][2]
-        t.equal(error, 'i think i will cause problems on purpose', 'should have the error message')
-        for (const metricName of metrics) {
-          const metric = agent.metrics.getMetric(metricName)
-          t.ok(metric.callCount > 0, `ensure ${metricName} was recorded`)
-        }
-      })
-
-      await new Promise((resolve) => {
-        client.sayError({ oh: 'noes' }, (err, response) => {
-          t.ok(err, 'should get an error')
-          t.notOk(response, 'response should not exist')
-          t.equal(err.code, grpc.status.FAILED_PRECONDITION, 'should get the right status code')
-          t.equal(
-            err.details,
-            'i think i will cause problems on purpose',
-            'should get the correct error message'
-          )
-          tx.end()
-          resolve()
+        t.equal(error, expectedStatusText, 'should have the error message')
+        assertExternalSegment({
+          t,
+          tx: transaction,
+          fnName: 'SayError',
+          expectedStatusText,
+          expectedStatusCode
         })
       })
-      t.end()
+
+      try {
+        const payload = { oh: 'noes' }
+        await makeUnaryRequest({ client, fnName: 'sayError', payload })
+      } catch (err) {
+        t.ok(err, 'should get an error')
+        t.equal(err.code, expectedStatusCode, 'should get the right status code')
+        t.equal(err.details, expectedStatusText, 'should get the correct error message')
+        tx.end()
+      }
     })
   })
 })
--- test/versioned/grpc/util.js
+++ test/versioned/grpc/util.js
@@ -5,9 +5,32 @@
 
 'use strict'
 const util = module.exports
-const { CLIENT_ADDR } = require('./constants')
 const SERVER_ADDR = '0.0.0.0:50051'
+const CLIENT_ADDR = 'localhost:50051'
+const metricsHelpers = require('../../lib/metrics_helper')
+const { EXTERNAL } = require('../../../lib/metrics/names')
+const rollupHost = `${EXTERNAL.PREFIX}${CLIENT_ADDR}/all`
+const grpcMetricName = `${EXTERNAL.PREFIX}${CLIENT_ADDR}/gRPC`
+const metrics = [grpcMetricName, rollupHost, EXTERNAL.WEB, EXTERNAL.ALL]
+const expectedMetrics = metrics.map((metric) => ({ name: metric }))
 
+util.assertMetricsNotExisting = function assertMetricsNotExisting({ t, agent }) {
+  metrics.forEach((metricName) => {
+    const metric = agent.metrics.getMetric(metricName)
+    t.notOk(metric, `${metricName} should not be recorded`)
+  })
+
+  t.end()
+}
+
+/**
+ * Creates a gRPC server witht he Greeter service and the server methods
+ * from `./grpc-server`
+ *
+ * @param {Object} grpc grpc module
+ * @param {Object} proto protobuf API example.proto
+ * @returns {Object} server grpc server started
+ */
 util.getServer = async function getServer(grpc, proto) {
   const server = new grpc.Server()
   const credentials = grpc.ServerCredentials.createInsecure()
@@ -29,7 +52,107 @@ util.getServer = async function getServer(grpc, proto) {
   return server
 }
 
+/**
+ * Gets the client for the Greeter service
+ *
+ * @param {Object} grpc grpc module
+ * @param {Object} proto protobuf API example.proto
+ * @returns {Object} client grpc client for Greeter service
+ */
 util.getClient = function getClient(grpc, proto) {
   const credentials = grpc.credentials.createInsecure()
   return new proto.Greeter(CLIENT_ADDR, credentials)
+}
+
+util.getRPCName = function getRPCName(fnName) {
+  return `/helloworld.Greeter/${fnName}`
+}
+
+util.assertExternalSegment = function assertExternalSegment({
+  t,
+  tx,
+  fnName,
+  expectedStatusCode = 0,
+  expectedStatusText = 'OK'
+}) {
+  const methodName = util.getRPCName(fnName)
+  const segmentName = `${EXTERNAL.PREFIX}${CLIENT_ADDR}${methodName}`
+  metricsHelpers.assertSegments(tx.trace.root, [segmentName], { exact: false })
+  const segment = metricsHelpers.findSegment(tx.trace.root, segmentName)
+  const attributes = segment.getAttributes()
+  t.equal(
+    attributes['http.url'],
+    `grpc://${CLIENT_ADDR}${methodName}`,
+    'http.url attribute should be correct'
+  )
+  t.equal(attributes['http.method'], methodName, 'method name should be correct')
+  t.equal(
+    attributes['grpc.statusCode'],
+    expectedStatusCode,
+    `status code should be ${expectedStatusCode}`
+  )
+  t.equal(
+    attributes['grpc.statusText'],
+    expectedStatusText,
+    `status text should be ${expectedStatusText}`
+  )
+  t.equal(attributes.component, 'gRPC', 'should have the component set to "gRPC"')
+  metricsHelpers.assertMetrics(tx.metrics, [expectedMetrics], false, false)
+  t.end()
+}
+
+util.makeUnaryRequest = function makeUnaryRequest({ client, fnName, payload }) {
+  return new Promise((resolve, reject) => {
+    client[fnName](payload, (err, response) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      resolve(response)
+    })
+  })
+}
+
+util.makeClientStreamingRequest = function makeClientStreamingRequest({ client, fnName, payload }) {
+  return new Promise((resolve, reject) => {
+    const call = client[fnName]((err, response) => {
+      if (err) {
+        reject(err)
+        return
+      }
+
+      resolve(response)
+    })
+
+    payload.forEach((data) => call.write(data))
+    call.end()
+  })
+}
+
+util.makeServerStreamingRequest = function makeServerStreamingRequest({ client, fnName, payload }) {
+  return new Promise((resolve) => {
+    const serverData = []
+    const call = client[fnName](payload)
+    call.on('data', (response) => {
+      serverData.push(response.message)
+    })
+    call.on('end', () => {
+      resolve(serverData)
+    })
+  })
+}
+
+util.makeBidiStreamingRequest = function makeBidiStreamingRequest({ client, fnName, payload }) {
+  return new Promise((resolve) => {
+    const serverData = []
+    const call = client[fnName]()
+    payload.forEach((data) => call.write(data))
+    call.on('data', function (response) {
+      serverData.push(response.message)
+    })
+    call.on('end', () => {
+      resolve(serverData)
+    })
+    call.end()
+  })
 }
