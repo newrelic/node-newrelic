8c7b31203226d85f02c8c82239de709a76f0701f
Michael Goin <mgoin@newrelic.com>
2022-07-28 11:48:35 -0700

    Fixes issue where versioned runs would ignore some directories due to jobs parameter handling.

--- bin/run-versioned-tests.sh
+++ bin/run-versioned-tests.sh
@@ -29,9 +29,15 @@ export AGENT_PATH=`pwd`
 echo "JOBS = ${JOBS}"
 echo "NPM7 = ${NPM7}"
 
+# if $JOBS is not empy
+if [ ! -z "$JOBS" ];
+then
+  JOBS_ARGS="--jobs $JOBS"
+fi
+
 if [[ "${NPM7}" = 1 ]];
 then
-  time ./node_modules/.bin/versioned-tests $VERSIONED_MODE -i 2 --all --strict --samples $SAMPLES --jobs $JOBS ${directories[@]}
+  time ./node_modules/.bin/versioned-tests $VERSIONED_MODE -i 2 --all --strict --samples $SAMPLES $JOBS_ARGS ${directories[@]}
 else
-  time ./node_modules/.bin/versioned-tests $VERSIONED_MODE -i 2 --strict --samples $SAMPLES --jobs $JOBS ${directories[@]}
+  time ./node_modules/.bin/versioned-tests $VERSIONED_MODE -i 2 --strict --samples $SAMPLES $JOBS_ARGS ${directories[@]}
 fi
