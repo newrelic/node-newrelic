8caba5ef76c088787685a6260d8b88d9e777f070
Bob Evans <robert.evans25@gmail.com>
2022-08-29 17:03:44 -0400

    fixed link to support section in troubleshooting link

---
+++ merged/nextjs/tests/versioned/newrelic.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+exports.config = {
+  app_name: ['My Application'],
+  license_key: 'license key here'
+}
