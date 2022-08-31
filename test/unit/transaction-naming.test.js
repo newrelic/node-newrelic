46e5a1a1a0ea54810dddc87bc8560f67726c5d0b
Bob Evans <robert.evans25@gmail.com>
2022-08-31 17:07:57 -0400

    Fixed transaction name finalization to properly copy the appropriate transaction name to baseSegment.  Updated some erroneous tests that stated Custom naming rules trumped API calls which is wherein the bug existed.

--- test/unit/transaction-naming.test.js
+++ test/unit/transaction-naming.test.js
@@ -5,150 +5,166 @@
 
 'use strict'
 
-// TODO: convert to normal tap style.
-// Below allows use of mocha DSL with tap runner.
-require('tap').mochaGlobals()
-
-const chai = require('chai')
-const expect = chai.expect
 const helper = require('../lib/agent_helper')
 const API = require('../../api')
+const { test } = require('tap')
 
-describe('Transaction naming:', function () {
+test('Transaction naming:', function (t) {
+  t.autoend()
   let agent
 
-  beforeEach(function () {
+  t.beforeEach(function () {
     agent = helper.loadMockedAgent()
   })
 
-  afterEach(function () {
+  t.afterEach(function () {
     helper.unloadAgent(agent)
   })
 
-  it('Transaction should be named /* without any other naming source', function (done) {
+  t.test('Transaction should be named /* without any other naming source', function (t) {
     helper.runInTransaction(agent, function (transaction) {
       transaction.finalizeNameFromUri('http://test.test.com/', 200)
-      expect(transaction.name).equal('WebTransaction/NormalizedUri/*')
-      done()
+      t.equal(transaction.name, 'WebTransaction/NormalizedUri/*')
+      t.equal(transaction.name, transaction.getFullName(), 'name should be equal to finalized name')
+      t.end()
     })
   })
 
-  it('Transaction should not be normalized when 404', function (done) {
+  t.test('Transaction should not be normalized when 404', function (t) {
     helper.runInTransaction(agent, function (transaction) {
       transaction.nameState.setName('Expressjs', 'GET', '/', null)
       transaction.finalizeNameFromUri('http://test.test.com/', 404)
-      expect(transaction.name).to.equal('WebTransaction/Expressjs/GET/(not found)')
-      done()
+      t.equal(transaction.name, 'WebTransaction/Expressjs/GET/(not found)')
+      t.equal(transaction.name, transaction.getFullName(), 'name should be equal to finalized name')
+      t.end()
     })
   })
 
-  it('Instrumentation should trump default naming', function (done) {
+  t.test('Instrumentation should trump default naming', function (t) {
     helper.runInTransaction(agent, function (transaction) {
       simulateInstrumentation(transaction)
       transaction.finalizeNameFromUri('http://test.test.com/', 200)
-      expect(transaction.name).equal('WebTransaction/Expressjs/GET//setByInstrumentation')
-      done()
+      t.equal(transaction.name, 'WebTransaction/Expressjs/GET//setByInstrumentation')
+      t.equal(transaction.name, transaction.getFullName(), 'name should be equal to finalized name')
+      t.end()
     })
   })
 
-  it('API naming should trump default naming', function (done) {
+  t.test('API naming should trump default naming', function (t) {
     const api = new API(agent)
     helper.runInTransaction(agent, function (transaction) {
       api.setTransactionName('override')
       transaction.finalizeNameFromUri('http://test.test.com/', 200)
-      expect(transaction.name).equal('WebTransaction/Custom/override')
-      done()
+      t.equal(transaction.name, 'WebTransaction/Custom/override')
+      t.equal(transaction.name, transaction.getFullName(), 'name should be equal to finalized name')
+      t.end()
     })
   })
 
-  it('API naming should trump instrumentation naming', function (done) {
+  t.test('API naming should trump instrumentation naming', function (t) {
     const api = new API(agent)
     helper.runInTransaction(agent, function (transaction) {
       simulateInstrumentation(transaction)
       api.setTransactionName('override')
       transaction.finalizeNameFromUri('http://test.test.com/', 200)
-      expect(transaction.name).equal('WebTransaction/Custom/override')
-      done()
+      t.equal(transaction.name, 'WebTransaction/Custom/override')
+      t.equal(transaction.name, transaction.getFullName(), 'name should be equal to finalized name')
+      t.end()
     })
   })
 
-  it('API naming should trump instrumentation naming (order should not matter)', function (done) {
+  t.test('API naming should trump instrumentation naming (order should not matter)', function (t) {
     const api = new API(agent)
     helper.runInTransaction(agent, function (transaction) {
       api.setTransactionName('override')
       simulateInstrumentation(transaction)
       transaction.finalizeNameFromUri('http://test.test.com/', 200)
-      expect(transaction.name).equal('WebTransaction/Custom/override')
-      done()
+      t.equal(transaction.name, 'WebTransaction/Custom/override')
+      t.equal(transaction.name, transaction.getFullName(), 'name should be equal to finalized name')
+      t.end()
     })
   })
 
-  it('API should trump 404', function (done) {
+  t.test('API should trump 404', function (t) {
     const api = new API(agent)
     helper.runInTransaction(agent, function (transaction) {
       api.setTransactionName('override')
       simulateInstrumentation(transaction)
       transaction.finalizeNameFromUri('http://test.test.com/', 404)
-      expect(transaction.name).equal('WebTransaction/Custom/override')
-      done()
+      t.equal(transaction.name, 'WebTransaction/Custom/override')
+      t.equal(transaction.name, transaction.getFullName(), 'name should be equal to finalized name')
+      t.end()
     })
   })
 
-  it('Custom naming rules should trump default naming', function (done) {
+  t.test('Custom naming rules should trump default naming', function (t) {
     agent.userNormalizer.addSimple(/\//, '/test-transaction')
     helper.runInTransaction(agent, function (transaction) {
       transaction.finalizeNameFromUri('http://test.test.com/', 200)
-      expect(transaction.name).equal('WebTransaction/NormalizedUri/test-transaction')
-      done()
+      t.equal(transaction.name, 'WebTransaction/NormalizedUri/test-transaction')
+      t.equal(transaction.name, transaction.getFullName(), 'name should be equal to finalized name')
+      t.end()
     })
   })
 
-  it('Server sent naming rules should be applied when user specified rules are set', function (done) {
-    agent.urlNormalizer.addSimple(/\d+/, '*')
-    agent.userNormalizer.addSimple(/123/, 'abc')
-    helper.runInTransaction(agent, function (transaction) {
-      transaction.finalizeNameFromUri('http://test.test.com/123/456', 200)
-      expect(transaction.name).equal('WebTransaction/NormalizedUri/abc/*')
-      done()
-    })
-  })
+  t.test(
+    'Server sent naming rules should be applied when user specified rules are set',
+    function (t) {
+      agent.urlNormalizer.addSimple(/\d+/, '*')
+      agent.userNormalizer.addSimple(/123/, 'abc')
+      helper.runInTransaction(agent, function (transaction) {
+        transaction.finalizeNameFromUri('http://test.test.com/123/456', 200)
+        t.equal(transaction.name, 'WebTransaction/NormalizedUri/abc/*')
+        t.equal(
+          transaction.name,
+          transaction.getFullName(),
+          'name should be equal to finalized name'
+        )
+        t.end()
+      })
+    }
+  )
 
-  it('Custom naming rules should be cleaned up', function (done) {
+  t.test('Custom naming rules should be cleaned up', function (t) {
     agent.userNormalizer.addSimple(/\//, 'test-transaction')
     helper.runInTransaction(agent, function (transaction) {
       transaction.finalizeNameFromUri('http://test.test.com/', 200)
-      expect(transaction.name).equal('WebTransaction/NormalizedUri/test-transaction')
-      done()
+      t.equal(transaction.name, 'WebTransaction/NormalizedUri/test-transaction')
+      t.equal(transaction.name, transaction.getFullName(), 'name should be equal to finalized name')
+      t.end()
     })
   })
 
-  it('Custom naming rules should trump instrumentation naming', function (done) {
+  t.test('Custom naming rules should trump instrumentation naming', function (t) {
     agent.userNormalizer.addSimple(/\//, '/test-transaction')
     helper.runInTransaction(agent, function (transaction) {
       simulateInstrumentation(transaction)
       transaction.finalizeNameFromUri('http://test.test.com/', 200)
-      expect(transaction.name).equal('WebTransaction/NormalizedUri/test-transaction')
-      done()
+      t.equal(transaction.name, 'WebTransaction/NormalizedUri/test-transaction')
+      t.equal(transaction.name, transaction.getFullName(), 'name should be equal to finalized name')
+      t.end()
     })
   })
 
-  it('Custom naming rules should trump API calls', function (done) {
+  t.test('API calls should trump Custom naming rules', function (t) {
     agent.userNormalizer.addSimple(/\//, '/test-transaction')
     const api = new API(agent)
     helper.runInTransaction(agent, function (transaction) {
       api.setTransactionName('override')
       transaction.finalizeNameFromUri('http://test.test.com/', 200)
-      expect(transaction.name).equal('WebTransaction/NormalizedUri/test-transaction')
-      done()
+      t.equal(transaction.name, 'WebTransaction/Custom/override')
+      t.equal(transaction.name, transaction.getFullName(), 'name should be equal to finalized name')
+      t.end()
     })
   })
 
-  it('Custom naming rules should trump 404', function (done) {
+  t.test('Custom naming rules should trump 404', function (t) {
     agent.userNormalizer.addSimple(/\//, '/test-transaction')
     helper.runInTransaction(agent, function (transaction) {
       transaction.finalizeNameFromUri('http://test.test.com/', 404)
-      expect(transaction.name).equal('WebTransaction/NormalizedUri/test-transaction')
-      done()
+      t.equal(transaction.name, 'WebTransaction/NormalizedUri/test-transaction')
+      t.equal(transaction.name, transaction.getFullName(), 'name should be equal to finalized name')
+      t.end()
     })
   })
 })
