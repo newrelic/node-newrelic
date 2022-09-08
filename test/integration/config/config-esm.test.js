e40759a0e53e0e710bac56c47a3bf7f511aeff8d
Jordi Gutiérrez Hermoso <jgutierrezhermoso@newrelic.com>
2022-09-08 17:13:42 -0400

    config: re-throw the error if it's an ESM import error
    This results in the error bubbling up to the user interface and
    letting the user know, in Node's own words, that something is wrong
    with their filename and they should change it.

---
+++ test/integration/config/config-esm.test.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+const tap = require('tap')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+
+tap.test('should gracefully handle ESM imports', (t) => {
+  t.autoend()
+
+  t.test('when newrelic.js is misnamed', async (t) => {
+    const { stderr } = await exec('node index.js', { cwd: `${__dirname}/esm-bad` })
+    t.match(stderr, 'ERR_REQUIRE_ESM', 'should mention ERR_REQUIRE_ESM in error message')
+    t.end()
+  })
+
+  t.test('when newrelic.cjs is properly named', async (t) => {
+    const { stdout, stderr } = await exec('node index.js', { cwd: `${__dirname}/esm-good` })
+    t.same(stdout, '', 'all should be quiet in stdout')
+    t.same(stderr, '', 'all should be quiet in stderr')
+    t.end()
+  })
+})
---
+++ test/integration/config/esm-bad/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+
+/*
+ * Copyright 2022 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* eslint-disable */
+
+import newrelic from '../../../../index.js'
+
+export default function greeter(name) {
+  return `Hello ${name}`
+}
