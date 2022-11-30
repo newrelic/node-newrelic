1b17dd5c5af2c27ad6eb6c7b81a89ec60ede8fd4
Bob Evans <robert.evans25@gmail.com>
2022-11-30 15:50:39 -0500

    NEWRELIC-5563 changed default string to (anonymous) based on discussions with CLM team. also properly ignore attributes if name or path are > 255

--- test/unit/util/code-level-metrics.test.js
+++ test/unit/util/code-level-metrics.test.js
@@ -12,6 +12,17 @@ const path = require('path')
 const helperPath = path.resolve(`${__dirname}/../../lib/clm-helper.js`)
 const sinon = require('sinon')
 
+/**
+ * Helper to generate a long string
+ *
+ * @param {number} len length of string
+ * @returns {string} string of proper length
+ */
+function longString(len) {
+  // add 1 because arrays start at 0
+  return Array(len + 1).join('a')
+}
+
 tap.test('CLM Meta', (t) => {
   t.autoend()
 
@@ -21,7 +32,7 @@ tap.test('CLM Meta', (t) => {
     t.same(meta, {
       'code.filepath': __filename,
       'code.function': 'testFunction',
-      'code.lineno': 19
+      'code.lineno': 30
     })
     t.end()
   })
@@ -32,7 +43,7 @@ tap.test('CLM Meta', (t) => {
     t.same(meta, {
       'code.filepath': __filename,
       'code.function': 'testFunction',
-      'code.lineno': 30
+      'code.lineno': 41
     })
     t.end()
   })
@@ -50,36 +61,73 @@ tap.test('CLM Meta', (t) => {
     }
   )
 
-  t.test('should return anonymous as code.function from anonymous function reference', (t) => {
+  t.test('should return (anonymous) as code.function from (anonymous) function reference', (t) => {
     const meta = getCLMMeta(anon)
     t.same(meta, {
       'code.filepath': helperPath,
-      'code.function': 'anonymous',
+      'code.function': '(anonymous)',
       'code.lineno': 9
     })
     t.end()
   })
 
-  t.test('should return anonymous as code.function from arrow function reference', (t) => {
+  t.test('should return (anonymous) as code.function from arrow function reference', (t) => {
     const meta = getCLMMeta(arrow)
     t.same(meta, {
       'code.filepath': helperPath,
-      'code.function': 'anonymous',
+      'code.function': '(anonymous)',
       'code.lineno': 10
     })
     t.end()
   })
 
-  t.test('should only return code.function if retrieving function metadata fails', (t) => {
-    const fnInspector = require('@contrast/fn-inspect')
-    sinon.stub(fnInspector, 'funcInfo')
-    const err = new Error('failed to get function meta')
-    fnInspector.funcInfo.throws(err)
-    function test() {}
-    const meta = getCLMMeta(test)
-    t.same(meta, {
-      'code.function': 'test'
-    })
+  t.test('should not return code attributes if function name > 255', (t) => {
+    const fnName = longString(256)
+    const fn = new Function(`return function ${fnName}() {}`)()
+    const meta = getCLMMeta(fn)
+    t.notOk(meta)
     t.end()
+  })
+
+  t.test('failure cases', (t) => {
+    t.autoend()
+    const fnInspector = require('@contrast/fn-inspect')
+
+    t.beforeEach(() => {
+      sinon.stub(fnInspector, 'funcInfo')
+    })
+
+    t.afterEach(() => {
+      fnInspector.funcInfo.restore()
+    })
+
+    t.test('should not return code attributes if filepath > 255', (t) => {
+      const longPath = longString(300)
+      fnInspector.funcInfo.returns({ lineNumber: 1, method: 'unitTest', file: longPath })
+      const meta = getCLMMeta(() => {})
+      t.notOk(meta)
+      t.end()
+    })
+
+    t.test('should only return code.function if retrieving function metadata fails', (t) => {
+      const err = new Error('failed to get function meta')
+      fnInspector.funcInfo.throws(err)
+      function test() {}
+      const meta = getCLMMeta(test)
+      t.same(meta, {
+        'code.function': 'test'
+      })
+      t.end()
+    })
+
+    t.test('should not return code attributes if function name is > 255', (t) => {
+      const fnName = longString(256)
+      const fn = new Function(`return function ${fnName}() {}`)()
+      const err = new Error('oh noes, not again')
+      fnInspector.funcInfo.throws(err)
+      const meta = getCLMMeta(fn)
+      t.notOk(meta)
+      t.end()
+    })
   })
 })
