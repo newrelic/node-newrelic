9f532a24d4b6546e389a8f49ea40e91750f41ef5
Bob Evans <robert.evans25@gmail.com>
2022-12-01 14:16:51 -0500

    NEWRELIC-5564 updated code-level-metrics unit tests to properly test adding code.* attrs to active segment

--- test/unit/util/code-level-metrics.test.js
+++ test/unit/util/code-level-metrics.test.js
@@ -6,11 +6,20 @@
 'use strict'
 
 const tap = require('tap')
-const getCLMMeta = require('../../../lib/util/code-level-metrics')
+const addCLMAttributes = require('../../../lib/util/code-level-metrics')
 const { anon, arrow, named } = require('../../lib/clm-helper')
 const path = require('path')
 const helperPath = path.resolve(`${__dirname}/../../lib/clm-helper.js`)
 const sinon = require('sinon')
+const symbols = require('../../../lib/symbols')
+tap.Test.prototype.addAssert('clmAttrs', 2, function clmAttrs(segmentStub, expectedAttrs) {
+  const attrs = segmentStub.addAttribute.args
+  const attrsObj = attrs.reduce((obj, [key, value]) => {
+    obj[key] = value
+    return obj
+  }, {})
+  this.same(attrsObj, expectedAttrs, 'CLM attrs should match')
+})
 
 /**
  * Helper to generate a long string
@@ -25,25 +34,36 @@ function longString(len) {
 
 tap.test('CLM Meta', (t) => {
   t.autoend()
+  let segmentStub
+
+  t.beforeEach(() => {
+    segmentStub = {
+      addAttribute: sinon.stub()
+    }
+  })
 
   t.test('should return function name as code.function from function reference', (t) => {
     function testFunction() {}
-    const meta = getCLMMeta(testFunction)
-    t.same(meta, {
+    testFunction[symbols.clm] = true
+    addCLMAttributes(testFunction, segmentStub)
+    t.clmAttrs(segmentStub, {
       'code.filepath': __filename,
       'code.function': 'testFunction',
-      'code.lineno': 30
+      'code.lineno': 46,
+      'code.column': 25
     })
     t.end()
   })
 
   t.test('should return variable name as code.function from function reference', (t) => {
     const testFunction = function () {}
-    const meta = getCLMMeta(testFunction)
-    t.same(meta, {
+    testFunction[symbols.clm] = true
+    addCLMAttributes(testFunction, segmentStub)
+    t.clmAttrs(segmentStub, {
       'code.filepath': __filename,
       'code.function': 'testFunction',
-      'code.lineno': 41
+      'code.lineno': 59,
+      'code.column': 34
     })
     t.end()
   })
@@ -51,32 +71,38 @@ tap.test('CLM Meta', (t) => {
   t.test(
     'should return function name not variable name as code.function from function reference',
     (t) => {
-      const meta = getCLMMeta(named)
-      t.same(meta, {
+      named[symbols.clm] = true
+      addCLMAttributes(named, segmentStub)
+      t.clmAttrs(segmentStub, {
         'code.filepath': helperPath,
         'code.function': 'testFunction',
-        'code.lineno': 11
+        'code.lineno': 11,
+        'code.column': 39
       })
       t.end()
     }
   )
 
   t.test('should return (anonymous) as code.function from (anonymous) function reference', (t) => {
-    const meta = getCLMMeta(anon)
-    t.same(meta, {
+    anon[symbols.clm] = true
+    addCLMAttributes(anon, segmentStub)
+    t.clmAttrs(segmentStub, {
       'code.filepath': helperPath,
       'code.function': '(anonymous)',
-      'code.lineno': 9
+      'code.lineno': 9,
+      'code.column': 26
     })
     t.end()
   })
 
   t.test('should return (anonymous) as code.function from arrow function reference', (t) => {
-    const meta = getCLMMeta(arrow)
-    t.same(meta, {
+    arrow[symbols.clm] = true
+    addCLMAttributes(arrow, segmentStub)
+    t.clmAttrs(segmentStub, {
       'code.filepath': helperPath,
       'code.function': '(anonymous)',
-      'code.lineno': 10
+      'code.lineno': 10,
+      'code.column': 18
     })
     t.end()
   })
@@ -84,8 +110,9 @@ tap.test('CLM Meta', (t) => {
   t.test('should not return code attributes if function name > 255', (t) => {
     const fnName = longString(256)
     const fn = new Function(`return function ${fnName}() {}`)()
-    const meta = getCLMMeta(fn)
-    t.notOk(meta)
+    fn[symbols.clm] = true
+    addCLMAttributes(fn, segmentStub)
+    t.notOk(segmentStub.addAttribute.callCount)
     t.end()
   })
 
@@ -101,11 +128,20 @@ tap.test('CLM Meta', (t) => {
       fnInspector.funcInfo.restore()
     })
 
+    t.test('should not try to get function metadata if clm symbol does not exist', (t) => {
+      addCLMAttributes(() => {}, segmentStub)
+      t.notOk(fnInspector.funcInfo.callCount, 'should not call funcInfo')
+      t.notOk(segmentStub.addAttribute.callCount, 'should not call segment.addAttribute')
+      t.end()
+    })
+
     t.test('should not return code attributes if filepath > 255', (t) => {
       const longPath = longString(300)
       fnInspector.funcInfo.returns({ lineNumber: 1, method: 'unitTest', file: longPath })
-      const meta = getCLMMeta(() => {})
-      t.notOk(meta)
+      const fn = () => {}
+      fn[symbols.clm] = true
+      addCLMAttributes(fn, segmentStub)
+      t.notOk(segmentStub.addAttribute.callCount)
       t.end()
     })
 
@@ -113,20 +149,21 @@ tap.test('CLM Meta', (t) => {
       const err = new Error('failed to get function meta')
       fnInspector.funcInfo.throws(err)
       function test() {}
-      const meta = getCLMMeta(test)
-      t.same(meta, {
-        'code.function': 'test'
-      })
+      test[symbols.clm] = true
+      addCLMAttributes(test, segmentStub)
+      t.equal(segmentStub.addAttribute.callCount, 1)
+      t.same(segmentStub.addAttribute.args[0], ['code.function', 'test'])
       t.end()
     })
 
     t.test('should not return code attributes if function name is > 255', (t) => {
       const fnName = longString(256)
       const fn = new Function(`return function ${fnName}() {}`)()
+      fn[symbols.clm] = true
       const err = new Error('oh noes, not again')
       fnInspector.funcInfo.throws(err)
-      const meta = getCLMMeta(fn)
-      t.notOk(meta)
+      addCLMAttributes(fn, segmentStub)
+      t.notOk(segmentStub.addAttribute.callCount)
       t.end()
     })
   })
