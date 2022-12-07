a99e15bf45d8929d52cdc9c9a5d6aa3622a53679
Jessica Lopatta <jlopatta@newrelic.com>
2022-12-07 11:44:24 -0500

    NEWRELIC-5565 consolidate middleware tests into one suite

--- test/versioned/fastify/code-level-metrics-middleware.tap.js
+++ test/versioned/fastify/code-level-metrics-middleware.tap.js
@@ -8,6 +8,8 @@
 const tap = require('tap')
 const helper = require('../../lib/agent_helper')
 const common = require('./common')
+const semver = require('semver')
+const { version: pkgVersion } = require('fastify/package')
 
 tap.Test.prototype.addAssert('clmAttrs', 1, helper.assertCLMAttrs)
 
@@ -17,12 +19,22 @@ async function setupFastifyServer(fastify, calls) {
   common.registerMiddlewares({ fastify, calls })
 }
 
+function setupFastifyServerV2(fastify, calls) {
+  common.setupRoutes(fastify)
+  common.registerMiddlewares({ fastify, calls })
+}
+
 async function setup(test, config) {
   const agent = helper.instrumentMockedAgent(config)
   const fastify = require('fastify')()
   const calls = { test: 0, middleware: 0 }
 
-  await setupFastifyServer(fastify, calls)
+  // TODO: once we drop v2 support, update to use `setupFastifyServer` exclusively
+  const setupServerFn = semver.satisfies(pkgVersion, '>=3')
+    ? setupFastifyServer
+    : setupFastifyServerV2
+
+  await setupServerFn(fastify, calls)
 
   test.context.agent = agent
   test.context.fastify = fastify
@@ -32,6 +44,50 @@ async function setup(test, config) {
     helper.unloadAgent(agent)
     fastify.close()
   })
+}
+
+function assertSegments(test, baseSegment, isCLMEnabled) {
+  // TODO: once we drop v2 support, this function can be removed and assert inline in test below
+  if (semver.satisfies(pkgVersion, '>=3')) {
+    const [middieSegment, handlerSegment] = baseSegment.children
+    test.clmAttrs({
+      segments: [
+        {
+          segment: middieSegment,
+          name: 'runMiddie',
+          filepath: 'test/versioned/fastify/node_modules/middie/index.js'
+        },
+        {
+          segment: handlerSegment,
+          name: '(anonymous)',
+          filepath: 'test/versioned/fastify/common.js'
+        }
+      ],
+      enabled: isCLMEnabled
+    })
+  } else {
+    const [middieSegment, mwSegment, handlerSegment] = baseSegment.children
+    test.clmAttrs({
+      segments: [
+        {
+          segment: middieSegment,
+          name: 'testMiddleware',
+          filepath: 'test/versioned/fastify/common.js'
+        },
+        {
+          segment: mwSegment,
+          name: 'pathMountedMiddleware',
+          filepath: 'test/versioned/fastify/common.js'
+        },
+        {
+          segment: handlerSegment,
+          name: '(anonymous)',
+          filepath: 'test/versioned/fastify/common.js'
+        }
+      ],
+      enabled: isCLMEnabled
+    })
+  }
 }
 
 tap.test('Fastify CLM Middleware Based', (test) => {
@@ -44,24 +100,7 @@ tap.test('Fastify CLM Middleware Based', (test) => {
 
       agent.on('transactionFinished', (transaction) => {
         calls.test++
-
-        const baseSegment = transaction.trace.root.children[0]
-        const [middieSegment, handlerSegment] = baseSegment.children
-        t.clmAttrs({
-          segments: [
-            {
-              segment: middieSegment,
-              name: 'runMiddie',
-              filepath: 'test/versioned/fastify/node_modules/middie/index.js'
-            },
-            {
-              segment: handlerSegment,
-              name: '(anonymous)',
-              filepath: 'test/versioned/fastify/common.js'
-            }
-          ],
-          enabled: isCLMEnabled
-        })
+        assertSegments(t, transaction.trace.root.children[0], isCLMEnabled)
       })
 
       await fastify.listen(0)
