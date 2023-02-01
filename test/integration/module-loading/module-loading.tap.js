f156270f71a6e62810430087d73ea602a00e7696
Bob Evans <robert.evans25@gmail.com>
2023-02-01 14:31:39 -0500

    NEWRELIC-6384 added ability to track packages when loaded but do not require instrumentation from our agent. This will be used to track packages where we plan on loading otel instrumentation but also logging libraries

--- test/integration/module-loading/module-loading.tap.js
+++ test/integration/module-loading/module-loading.tap.js
@@ -11,12 +11,11 @@ const path = require('path')
 const helper = require('../../lib/agent_helper')
 const shimmer = require('../../../lib/shimmer')
 const symbols = require('../../../lib/symbols')
+const { FEATURES } = require('../../../lib/metrics/names')
 
 const CUSTOM_MODULE_PATH = './node_modules/customTestPackage'
-const EXPECTED_RESOLVED_METRIC_NAME =
-  'Supportability/Features/Instrumentation/OnResolved/./node_modules/customTestPackage'
-const EXPECTED_REQUIRE_METRIC_NAME =
-  'Supportability/Features/Instrumentation/OnRequire/./node_modules/customTestPackage'
+const EXPECTED_RESOLVED_METRIC_NAME = `${FEATURES.INSTRUMENTATION.ON_RESOLVED}/${CUSTOM_MODULE_PATH}`
+const EXPECTED_REQUIRE_METRIC_NAME = `${FEATURES.INSTRUMENTATION.ON_REQUIRE}/${CUSTOM_MODULE_PATH}`
 
 tap.test('Should properly track module paths to enable shim.require()', function (t) {
   t.autoend()
@@ -47,6 +46,32 @@ tap.test('Should properly track module paths to enable shim.require()', function
   const shimLoadedCustom = shim.require('custom')
   t.ok(shimLoadedCustom, 'shim.require() should load module')
   t.equal(shimLoadedCustom.name, 'customFunction', 'Should grab correct module')
+})
+
+tap.test('should only log supportability metric for tracking type instrumentation', function (t) {
+  t.autoend()
+
+  let agent = helper.instrumentMockedAgent()
+
+  t.teardown(() => {
+    helper.unloadAgent(agent)
+    agent = null
+  })
+
+  const PKG = `${FEATURES.INSTRUMENTATION.ON_REQUIRE}/@prisma/client`
+  const PKG_VERSION = `${FEATURES.INSTRUMENTATION.ON_REQUIRE}/@prisma/client/Version/3`
+
+  // eslint-disable-next-line node/no-extraneous-require
+  const prisma = require('@prisma/client')
+  const prismaOnRequiredMetric = agent.metrics._metrics.unscoped[PKG]
+  t.equal(prismaOnRequiredMetric.callCount, 1, `should record ${PKG}`)
+  const prismaVersionMetric = agent.metrics._metrics.unscoped[PKG_VERSION]
+  t.equal(prismaVersionMetric.callCount, 1, `should record ${PKG_VERSION}`)
+  t.notOk(
+    prisma[symbols.instrumented],
+    'should not try to instrument a package that is of type tracking'
+  )
+  t.end()
 })
 
 tap.test('shim.require() should play well with multiple test runs', (t) => {
