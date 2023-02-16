5331d5952a1f01157b1144ec013bf1f7a7f3d517
Bob Evans <robert.evans25@gmail.com>
2023-02-16 16:10:26 -0500

    NEWRELIC-6862 added grpc.ignore_status_codes to the canonicalize function to properly parse status codes as integers or a range of integers

--- test/unit/config/config-env.test.js
+++ test/unit/config/config-env.test.js
@@ -732,4 +732,15 @@ tap.test('when overriding configuration values via environment variables', (t) =
       t.end()
     })
   })
+
+  t.test('should convert NEW_RELIC_GRPC_IGNORE_STATUS_CODES to integers', (t) => {
+    const env = {
+      NEW_RELIC_GRPC_IGNORE_STATUS_CODES: ['9', '5-7', 'blah', 2]
+    }
+
+    idempotentEnv(env, (config) => {
+      t.same(config.grpc.ignore_status_codes, [9, 5, 6, 7, 2])
+      t.end()
+    })
+  })
 })
