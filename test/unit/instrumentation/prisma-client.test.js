6537d935c41587706be9db8a07baf0d8e6f4ec5d
Bob Evans <robert.evans25@gmail.com>
2023-03-02 13:57:02 -0500

    NEWRELIC-7171 updated prisma instrumentation to work with version 4.11.0.  I also added more defensive code to avoid future crashes with instrumentation if prisma internals change

--- test/unit/instrumentation/prisma-client.test.js
+++ test/unit/instrumentation/prisma-client.test.js
@@ -11,6 +11,7 @@ const helper = require('../../lib/agent_helper')
 const DatastoreShim = require('../../../lib/shim/datastore-shim.js')
 const symbols = require('../../../lib/symbols')
 const sinon = require('sinon')
+const semver = require('semver')
 
 let agent = null
 let initialize = null
@@ -27,19 +28,28 @@ test('PrismaClient unit tests', (t) => {
     agent = helper.instrumentMockedAgent()
     initialize = require('../../../lib/instrumentation/@prisma/client')
     shim = new DatastoreShim(agent, 'prisma')
+    sinon.stub(shim, 'require')
+    shim.require.returns({ version: '4.0.0' })
   })
 
   t.afterEach(function () {
     helper.unloadAgent(agent)
   })
 
-  function getMockModule() {
+  function getMockModule(version = '4.0.0') {
     function Engine() {}
 
     Engine.prototype.getConfig = sinon.stub()
-
-    function PrismaClient() {
-      this._engine = new Engine()
+    let PrismaClient
+    if (semver.gte(version, '4.11.0')) {
+      PrismaClient = function PrismaClient() {
+        this._engine = {}
+        this._engine.library = new Engine()
+      }
+    } else {
+      PrismaClient = function PrismaClient() {
+        this._engine = new Engine()
+      }
     }
 
     PrismaClient.prototype._executeRequest = sinon.stub().resolves()
@@ -143,12 +153,16 @@ test('PrismaClient unit tests', (t) => {
         args: { query: 'select test from unit-test;' },
         action: 'executeRaw'
       })
+      await client._executeRequest({
+        args: { query: 'select test from schema.unit-test;' },
+        action: 'executeRaw'
+      })
       const { children } = tx.trace.root
-      t.equal(children.length, 2, 'should have 2 segments')
-      const firstSegment = children[0]
-      const secondSegment = children[1]
+      t.equal(children.length, 3, 'should have 3 segments')
+      const [firstSegment, secondSegment, thirdSegment] = children
       t.equal(firstSegment.name, 'Datastore/statement/Prisma/user/create')
-      t.equal(secondSegment.name, 'Datastore/statement/Prisma/executeRaw(select)/unit-test')
+      t.equal(secondSegment.name, 'Datastore/statement/Prisma/unit-test/executeRaw(select)')
+      t.equal(thirdSegment.name, 'Datastore/statement/Prisma/unit-test/executeRaw(select)')
       t.same(firstSegment.getAttributes(), {
         product: 'Prisma',
         host: 'my-host',
@@ -177,6 +191,98 @@ test('PrismaClient unit tests', (t) => {
         }
       ]
     })
+
+    helper.runInTransaction(agent, async () => {
+      await client._executeRequest({ clientMethod: 'user.create', action: 'create' })
+      t.same(client[symbols.prismaConnection], {})
+      t.end()
+    })
+  })
+
+  t.test('should not set connection params if it fails to retrieve config', (t) => {
+    const MockPrismaClient = getMockModule()
+    const prisma = { PrismaClient: MockPrismaClient }
+
+    initialize(agent, prisma, '@prisma/client', shim)
+    const client = new prisma.PrismaClient()
+    const err = new Error('i failed')
+    client._engine.getConfig.rejects(err)
+
+    helper.runInTransaction(agent, async () => {
+      await client._executeRequest({ clientMethod: 'user.create', action: 'create' })
+      t.same(client[symbols.prismaConnection], {})
+      t.end()
+    })
+  })
+
+  t.test('should not crash if it fails to extract query from call', (t) => {
+    const MockPrismaClient = getMockModule()
+    const prisma = { PrismaClient: MockPrismaClient }
+
+    initialize(agent, prisma, '@prisma/client', shim)
+    const client = new prisma.PrismaClient()
+    client._engine.getConfig.resolves({
+      datasources: [
+        { provider: 'postgres', url: { value: 'postgresql://postgres:prisma@my-host:5436/db' } }
+      ]
+    })
+
+    helper.runInTransaction(agent, async (tx) => {
+      await client._executeRequest({ action: 'executeRaw' })
+      const { children } = tx.trace.root
+      const [firstSegment] = children
+      t.equal(firstSegment.name, 'Datastore/statement/Prisma/other/executeRaw(other)')
+      t.end()
+    })
+  })
+
+  t.test('should work on 4.11.0', (t) => {
+    const version = '4.11.0'
+    const MockPrismaClient = getMockModule(version)
+    const prisma = { PrismaClient: MockPrismaClient }
+
+    shim.require.returns({ version })
+    initialize(agent, prisma, '@prisma/client', shim)
+    const client = new prisma.PrismaClient()
+    client._engine.library.getConfig.returns({
+      datasources: [
+        { provider: 'postgres', url: { value: 'postgresql://postgres:prisma@my-host:5436/db' } }
+      ]
+    })
+
+    helper.runInTransaction(agent, async (tx) => {
+      await client._executeRequest({ clientMethod: 'user.create', action: 'create' })
+      await client._executeRequest({
+        args: [['select test from unit-test;']],
+        action: 'executeRaw'
+      })
+      const { children } = tx.trace.root
+      t.equal(children.length, 2, 'should have 2 segments')
+      const firstSegment = children[0]
+      const secondSegment = children[1]
+      t.equal(firstSegment.name, 'Datastore/statement/Prisma/user/create')
+      t.equal(secondSegment.name, 'Datastore/statement/Prisma/unit-test/executeRaw(select)')
+      t.same(firstSegment.getAttributes(), {
+        product: 'Prisma',
+        host: 'my-host',
+        port_path_or_id: '5436',
+        database_name: 'db'
+      })
+      t.same(firstSegment.getAttributes(), secondSegment.getAttributes())
+      t.end()
+    })
+  })
+
+  t.test('should not set connection params in 4.11.0+ if it fails to retrieve config', (t) => {
+    const version = '4.11.0'
+    const MockPrismaClient = getMockModule(version)
+    const prisma = { PrismaClient: MockPrismaClient }
+
+    shim.require.returns({ version })
+    initialize(agent, prisma, '@prisma/client', shim)
+    const client = new prisma.PrismaClient()
+    const err = new Error('i failed')
+    client._engine.library.getConfig.throws(err)
 
     helper.runInTransaction(agent, async () => {
       await client._executeRequest({ clientMethod: 'user.create', action: 'create' })
