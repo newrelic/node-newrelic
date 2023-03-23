bb32ce720bd33c9bae7b8439f3bc7da6d543b026
Bob Evans <robert.evans25@gmail.com>
2023-03-23 16:33:46 -0400

    fixed linting errors on TestMod

--- test/integration/api/TestMod.js
+++ test/integration/api/TestMod.js
@@ -1,7 +1,11 @@
+/*
+ * Copyright 2023 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 'use strict'
 function TestMod() {}
 TestMod.prototype.foo = function foo(bar) {
   return `value of ${bar}`
 }
 module.exports = TestMod
-
