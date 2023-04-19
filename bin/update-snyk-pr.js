4cfe8a9d20eeba58ec8572b1fc99d55e1a047a12
Jessica Lopatta <jlopatta@newrelic.com>
2023-04-19 11:46:11 -0400

    Update exports for CI function to work for both GHA and unit tests (also backport to release-please workflow for consistency)

--- bin/update-snyk-pr.js
+++ bin/update-snyk-pr.js
@@ -8,7 +8,7 @@
 const Github = require('./github')
 const SECURITY_PREFIX = 'security:'
 
-module.exports = async function updateSnykPR() {
+async function updateSnykPR() {
   const org = process.env.RELEASE_ORG || 'newrelic'
   const repo = process.env.RELEASE_REPO || 'node-newrelic'
   const prId = process.env.SNYK_PR_ID
@@ -28,4 +28,15 @@ module.exports = async function updateSnykPR() {
   const newTitle = [SECURITY_PREFIX, originalTitle].join(' ')
 
   await github.updatePullRequest({ id: prId, title: newTitle })
+}
+
+/*
+ * Exports slightly differ for tests vs. Github Actions
+ * this allows us to require the function without it executing for tests,
+ * and executing via `node` cli in GHA
+ */
+if (require.main === module) {
+  updateSnykPR()
+} else {
+  module.exports = updateSnykPR
 }
