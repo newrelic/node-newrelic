86d83f20028ec8a135241d4ad92032488ff5ad3a
James Sumners <jsumners@newrelic.com>
2023-11-20 12:04:36 -0500

    test: Add streaming responses to mock OpenAI sever (#1881)

--- test/lib/openai-mock-server.js
+++ test/lib/openai-mock-server.js
@@ -8,6 +8,7 @@
 module.exports = openaiMockServer
 
 const http = require('node:http')
+const { Readable } = require('node:stream')
 const RESPONSES = require('./openai-responses')
 
 /**
@@ -69,8 +70,44 @@ function handler(req, res) {
       res.setHeader(key, value)
     }
     res.statusCode = code
-    res.write(JSON.stringify(body))
-    res.end()
+
+    if (payload.stream === true) {
+      // OpenAI streamed responses are double newline delimited lines that
+      // are prefixed with the string `data: `. The end of the stream is
+      // terminated with a `done: [DONE]` string.
+      const parts = body.split(' ')
+      let i = 0
+      const outStream = new Readable({
+        read() {
+          if (i < parts.length) {
+            const content = parts.length - 1 === i ? parts[i] : `${parts[i]} `
+            const chunk = JSON.stringify({
+              id: 'chatcmpl-8MzOfSMbLxEy70lYAolSwdCzfguQZ',
+              object: 'chat.completion.chunk',
+              // 2023-11-20T09:00:00-05:00
+              created: 1700488800,
+              model: 'gpt-4',
+              choices: [
+                {
+                  index: 0,
+                  finish_reason: null,
+                  delta: { role: 'assistant', content }
+                }
+              ]
+            })
+            this.push(`data: ${chunk}\n\n`)
+            i += 1
+          } else {
+            this.push('data: [DONE]\n\n')
+            this.push(null)
+          }
+        }
+      })
+      outStream.pipe(res)
+    } else {
+      res.write(JSON.stringify(body))
+      res.end()
+    }
   })
 }
 
