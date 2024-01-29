9c9d84892b63b64092bdb05f5ce2fa743e74ffb3
James Sumners <jsumners@newrelic.com>
2024-01-29 14:56:36 -0500

    chore: Update eslint config (#1980)

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,15 +30,14 @@ module.exports = {
     ]
   },
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2022
   },
   ignorePatterns: ['test/versioned-external'],
   overrides: [
     {
       files: ['**/*.mjs'],
       parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 2022
+        sourceType: 'module'
       },
       rules: {
         // TODO: remove this when we decide on how to address
