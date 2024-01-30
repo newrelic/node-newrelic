70a59f8ce0d7df7bba64f3a4b89e06706039b60e
James Sumners <jsumners@newrelic.com>
2024-01-30 09:14:04 -0500

    feat: Add message entities for LangChain

---
+++ test/unit/llm-events/langchain/chat-completion-message.test.js
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2024 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+const tap = require('tap')
+const LangChainCompletionMessage = require('../../../../lib/llm-events/langchain/chat-completion-message')
+
+tap.beforeEach((t) => {
+  t.context._tx = {
+    trace: {
+      custom: {
+        get() {
+          return {
+            'llm.conversation_id': 'test-conversation'
+          }
+        }
+      }
+    }
+  }
+
+  t.context.agent = {
+    config: {
+      applications() {
+        return ['test-app']
+      }
+    },
+    tracer: {
+      getTransaction() {
+        return t.context._tx
+      }
+    }
+  }
+
+  t.context.segment = {
+    id: 'segment-1',
+    transaction: {
+      id: 'tx-1',
+      traceId: 'trace-1'
+    }
+  }
+
+  t.context.runId = 'run-1'
+  t.context.metadata = { foo: 'foo' }
+})
+
+tap.test('creates entity', async (t) => {
+  const msg = new LangChainCompletionMessage({
+    ...t.context,
+    role: 'human',
+    sequence: 1,
+    content: 'hello world'
+  })
+  t.match(msg, {
+    id: /[a-z0-9-]{36}/,
+    appName: 'test-app',
+    conversation_id: 'test-conversation',
+    span_id: 'segment-1',
+    request_id: 'run-1',
+    transaction_id: 'tx-1',
+    trace_id: 'trace-1',
+    metadata: { foo: 'foo' },
+    ingest_source: 'Node',
+    vendor: 'langchain',
+    virtual_llm: true,
+    role: 'human',
+    sequence: 1,
+    content: 'hello world',
+    completion_id: /[a-z0-9-]{36}/
+  })
+})
---
+++ test/unit/llm-events/langchain/chat-completion-summary.test.js
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2024 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+const tap = require('tap')
+const LangChainCompletionSummary = require('../../../../lib/llm-events/langchain/chat-completion-summary')
+
+tap.beforeEach((t) => {
+  t.context._tx = {
+    trace: {
+      custom: {
+        get() {
+          return {
+            'llm.conversation_id': 'test-conversation'
+          }
+        }
+      }
+    }
+  }
+
+  t.context.agent = {
+    config: {
+      applications() {
+        return ['test-app']
+      }
+    },
+    tracer: {
+      getTransaction() {
+        return t.context._tx
+      }
+    }
+  }
+
+  t.context.segment = {
+    id: 'segment-1',
+    transaction: {
+      id: 'tx-1',
+      traceId: 'trace-1'
+    },
+    getDurationInMillis() {
+      return 42
+    }
+  }
+
+  t.context.runId = 'run-1'
+  t.context.metadata = { foo: 'foo' }
+})
+
+tap.test('creates entity', async (t) => {
+  const msg = new LangChainCompletionSummary(t.context)
+  t.match(msg, {
+    'id': /[a-z0-9-]{36}/,
+    'appName': 'test-app',
+    'conversation_id': 'test-conversation',
+    'span_id': 'segment-1',
+    'request_id': 'run-1',
+    'transaction_id': 'tx-1',
+    'trace_id': 'trace-1',
+    'metadata': { foo: 'foo' },
+    'ingest_source': 'Node',
+    'vendor': 'langchain',
+    'virtual_llm': true,
+    'tags': '',
+    'duration': 42,
+    'response.number_of_messages': 0
+  })
+})
+
+tap.test('sets tags', async (t) => {
+  const msg = new LangChainCompletionSummary(t.context)
+  t.match(msg, {
+    'id': /[a-z0-9-]{36}/,
+    'appName': 'test-app',
+    'conversation_id': 'test-conversation',
+    'span_id': 'segment-1',
+    'request_id': 'run-1',
+    'transaction_id': 'tx-1',
+    'trace_id': 'trace-1',
+    'metadata': { foo: 'foo' },
+    'ingest_source': 'Node',
+    'vendor': 'langchain',
+    'virtual_llm': true,
+    'tags': '',
+    'duration': 42,
+    'response.number_of_messages': 0
+  })
+
+  msg.tags = 'foo,bar'
+  t.equal(msg.tags, 'foo,bar')
+
+  msg.tags = ['bar', 'foo']
+  t.equal(msg.tags, 'bar,foo')
+})
