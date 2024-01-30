50fa9d553b0d46f52cc0baaea89ec21ad923cc6e
Svetlana Brennan <50715937+svetlanabrennan@users.noreply.github.com>
2024-01-30 17:10:59 -0600

    Add vector search entities and tests

---
+++ test/unit/llm-events/langchain/vector-search-result.test.js
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2024 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+const tap = require('tap')
+const LangChainVectorSearchResult = require('../../../../lib/llm-events/langchain/vector-search-result')
+
+tap.beforeEach((t) => {
+  t.context._tx = {
+    trace: {
+      custom: {
+        get() {
+          return {
+            'llm.conversation_id': 'test-conversation'
+          }
+        }
+      }
+    }
+  }
+
+  t.context.agent = {
+    config: {
+      applications() {
+        return ['test-app']
+      }
+    },
+    tracer: {
+      getTransaction() {
+        return t.context._tx
+      }
+    }
+  }
+
+  t.context.segment = {
+    id: 'segment-1',
+    transaction: {
+      id: 'tx-1',
+      traceId: 'trace-1'
+    }
+  }
+
+  t.context.runId = 'run-1'
+  t.context.metadata = { foo: 'foo' }
+})
+
+tap.test('create entity', async (t) => {
+  const search = new LangChainVectorSearchResult({
+    ...t.context,
+    sequence: 1,
+    pageContent: 'hello world'
+  })
+  t.match(search, {
+    id: /[a-z0-9-]{36}/,
+    appName: 'test-app',
+    conversation_id: 'test-conversation',
+    request_id: 'run-1',
+    span_id: 'segment-1',
+    transaction_id: 'tx-1',
+    trace_id: 'trace-1',
+    metadata: { foo: 'foo' },
+    ingest_source: 'Node',
+    vendor: 'langchain',
+    virtual_llm: true,
+    sequence: 1,
+    page_content: 'hello world',
+    search_id: /[a-z0-9-]{36}/
+  })
+})
