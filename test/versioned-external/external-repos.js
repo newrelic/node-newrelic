93d458f3b08752d08d0886a72af558f08fc10c9d
Bob Evans <robert.evans25@gmail.com>
2024-02-26 12:32:52 -0500

    chore: Updated branch for aws sdk as the trackedIds has been removed

--- test/versioned-external/external-repos.js
+++ test/versioned-external/external-repos.js
@@ -15,7 +15,7 @@ const repos = [
   {
     name: 'aws-sdk',
     repository: 'https://github.com/newrelic/node-newrelic-aws-sdk.git',
-    branch: 'main'
+    branch: 'update-llm-feedback'
   },
   {
     name: 'koa',
