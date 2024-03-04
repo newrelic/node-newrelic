74ed8182ebcab42a0ff32bb7111a2e0cb4b458f7
James Sumners <jsumners@newrelic.com>
2024-03-04 13:56:01 -0500

    chore: Added some delays to docs publishing

--- bin/publish-docs.sh
+++ bin/publish-docs.sh
@@ -10,8 +10,11 @@ git fetch origin gh-pages
 git checkout gh-pages
 git merge -
 npm run public-docs
+sleep 1
 git rm -r docs
+sleep 1
 mv out docs
+sleep 1
 git add docs
 git commit -m "docs: update for ${PACKAGE_VERSION}"
 git push origin gh-pages
