808323f0832952870fd1e94474b3fd3e0ab1b8c4
Bob Evans <revans@newrelic.com>
2024-04-04 14:43:04 -0400

    refactor(mongodb): Removed instrumentation that handles connecting via unix domain socket. (#2129)

--- test/versioned/mongodb/db-common.js
+++ test/versioned/mongodb/db-common.js
@@ -19,8 +19,6 @@ if (semver.satisfies(common.pkgVersion, '2.2.x')) {
 
 function dbTest(name, run) {
   mongoTest(name, function init(t, agent) {
-    const LOCALHOST = agent.config.getHostnameSafe()
-    const domainPath = common.getDomainSocketPath()
     const mongodb = require('mongodb')
     let db = null
     let client = null
@@ -52,43 +50,6 @@ function dbTest(name, run) {
           t.notOk(agent.getTransaction(), 'should not have transaction')
           t.end()
         })
-      })
-
-      t.test('with transaction', function (t) {
-        t.notOk(agent.getTransaction(), 'should not have transaction')
-        helper.runInTransaction(agent, function (transaction) {
-          run(t, db, function (names, opts = {}) {
-            verifyMongoSegments(t, agent, transaction, names, opts)
-            transaction.end()
-            t.end()
-          })
-        })
-      })
-    })
-
-    // The domain socket tests should only be run if there is a domain socket
-    // to connect to, which only happens if there is a Mongo instance running on
-    // the same box as these tests.
-    const shouldTestDomain = domainPath
-
-    t.test('domain socket', { skip: !shouldTestDomain }, function (t) {
-      t.autoend()
-      t.beforeEach(async function () {
-        // mongo >= 3.6.9 fails if you try to create an existing collection
-        // drop before executing tests
-        if (name === 'createCollection') {
-          await collectionCommon.dropTestCollections(mongodb)
-        }
-        MONGO_HOST = LOCALHOST
-        MONGO_PORT = domainPath
-
-        const res = await common.connect(mongodb, domainPath)
-        client = res.client
-        db = res.db
-      })
-
-      t.afterEach(function () {
-        return common.close(client, db)
       })
 
       t.test('with transaction', function (t) {
