b6c04840d1e94e77b8a48ec1e80a2f836a92537a
James Sumners <jsumners@newrelic.com>
2024-04-26 14:57:00 -0400

    address feedback

--- test/unit/is-absolute-path.test.js
+++ test/unit/is-absolute-path.test.js
@@ -6,13 +6,14 @@
 'use strict'
 
 const tap = require('tap')
-const isAbsolutePath = require('../../lib/is-absolute-path')
+const isAbsolutePath = require('../../lib/util/is-absolute-path')
 
 tap.test('verifies paths correctly', async (t) => {
   const tests = [
     ['./foo/bar.js', true],
     ['/foo/bar.cjs', true],
     ['/foo.mjs', true],
+    ['/foo.smj', false],
     ['foo', false],
     ['foo.js', false]
   ]
