161cc0e3ab65c2c119f2e69119947b2bf73f4fa6
James Sumners <jsumners@newrelic.com>
2024-04-26 10:11:46 -0400

    better? impl

--- test/versioned/esm-package/parse-json-instrumentation.mjs
+++ test/versioned/esm-package/parse-json-instrumentation.mjs
@@ -3,8 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-'use-strict'
-
 export default function initialize(shim, parseJson) {
   shim.wrap(parseJson, 'default', function wrappedParseJsonLib(_shim, orig) {
     return function wrappedParseJsonFunc() {
