caa9584114c185b849bfcc9fda4612d6a9cb2611
Bob Evans <robert.evans25@gmail.com>
2024-05-29 14:38:16 -0400

    chore: updated instrumentation to be kafkajs, fixed tests

---
+++ test/versioned/kafkajs/kafka.tap.js
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2024 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+const tap = require('tap')
+const helper = require('../../lib/agent_helper')
+const { removeModules } = require('../../lib/cache-buster')
+
+tap.beforeEach(async (t) => {
+  t.context.agent = helper.instrumentMockedAgent()
+
+  const { Kafka } = require('kafkajs')
+  t.context.Kafka = Kafka
+  const kafka = new Kafka({
+    clientId: 'kafka-test',
+    brokers: ['127.0.0.1:9092']
+  })
+
+  const producer = kafka.producer()
+  await producer.connect()
+  t.context.producer = producer
+  const consumer = kafka.consumer({ groupId: 'kafka' })
+  await consumer.connect()
+  t.context.consumer = consumer
+})
+
+tap.afterEach(async (t) => {
+  helper.unloadAgent(t.context.agent)
+  removeModules(['kafkajs'])
+  await t.context.consumer.disconnect()
+  await t.context.producer.disconnect()
+})
+
+tap.test('stub', async (t) => {
+  const { consumer, producer } = t.context
+  const topic = 'test-topic'
+
+  await consumer.subscribe({ topics: [topic] })
+  const testPromise = new Promise((resolve) => {
+    consumer.run({
+      eachMessage: async ({ message }) => {
+        t.equal(message.value.toString(), 'test message')
+        resolve()
+      }
+    })
+  })
+  await producer.send({
+    topic,
+    messages: [{ key: 'key', value: 'test message' }]
+  })
+  await testPromise
+})
