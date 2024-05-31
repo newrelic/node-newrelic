0568401c0c71ee397759054272f0fdb4803ea963
Sumit Suthar <ssuthar@newrelic.com>
2024-05-31 16:54:23 +0530

    udpated metric count for security agent versioned test run

--- test/versioned/express/ignoring.tap.js
+++ test/versioned/express/ignoring.tap.js
@@ -38,7 +38,7 @@ test('ignoring an Express route', function (t) {
 
     const metrics = agent.metrics._metrics.unscoped
     // loading k2 adds instrumentation metrics for things it loads
-    const expectedMetrics = helper.isSecurityAgentEnabled(agent) ? 9 : 3
+    const expectedMetrics = helper.isSecurityAgentEnabled(agent) ? 11 : 3
     t.equal(
       Object.keys(metrics).length,
       expectedMetrics,
