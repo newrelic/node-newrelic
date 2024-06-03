982276cd4fca57bbf64219e846a0a93cdfb3400b
sumitsuthar <ssuthar@newrelic.com>
2024-06-03 18:06:17 +0530

    chore: Updated versioned tests due to update in @newrelic/security-agent V1.3.0 (#2239)

--- test/versioned/hapi/ignoring.tap.js
+++ test/versioned/hapi/ignoring.tap.js
@@ -29,7 +29,7 @@ test('ignoring a Hapi route', function (t) {
 
     const metrics = agent.metrics._metrics.unscoped
     // loading k2 adds instrumentation metrics for packages it instruments
-    const expectedMetrics = helper.isSecurityAgentEnabled(agent) ? 10 : 3
+    const expectedMetrics = helper.isSecurityAgentEnabled(agent) ? 11 : 3
     t.equal(
       Object.keys(metrics).length,
       expectedMetrics,
