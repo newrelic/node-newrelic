7e3cab9739a9924b904ce78380fd70b1e30cd89f
James Sumners <jsumners@newrelic.com>
2024-06-14 13:22:53 -0400

    feat: Added support for Anthropic Claude 3 messages API (#2278)

--- test/lib/aws-server-stubs/ai-server/responses/index.js
+++ test/lib/aws-server-stubs/ai-server/responses/index.js
@@ -8,6 +8,7 @@
 const ai21 = require('./ai21')
 const amazon = require('./amazon')
 const claude = require('./claude')
+const claude3 = require('./claude3')
 const cohere = require('./cohere')
 const llama2 = require('./llama2')
 
@@ -15,6 +16,7 @@ module.exports = {
   ai21,
   amazon,
   claude,
+  claude3,
   cohere,
   llama2
 }
