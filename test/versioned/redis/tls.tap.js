25de7e2e4f8019fab7d3bb0c353c01f2554527d0
James Sumners <jsumners@newrelic.com>
2024-08-12 07:59:07 -0400

    improve tls option testing

--- test/versioned/redis/tls.tap.js
+++ test/versioned/redis/tls.tap.js
@@ -7,14 +7,20 @@
 
 const tap = require('tap')
 const helper = require('../../lib/agent_helper')
+const promiseResolvers = require('../../lib/promise-resolvers')
 const { redis_tls_host: HOST, redis_tls_port: PORT } = require('../../lib/params')
 const { removeModules } = require('../../lib/cache-buster')
 
 tap.test('redis over tls connection', (t) => {
-  t.beforeEach(async (t) => {
-    t.agent = helper.instrumentMockedAgent()
-    t.redis = require('redis')
-    t.client = await t.redis
+  t.afterEach(() => {
+    removeModules(['redis'])
+  })
+
+  t.test('should work with self-signed tls cert on server', async (t) => {
+    const { promise, resolve } = promiseResolvers()
+    const agent = helper.instrumentMockedAgent()
+    const redis = require('redis')
+    const client = await redis
       .createClient({
         url: `rediss://${HOST}:${PORT}`,
         socket: {
@@ -26,26 +32,59 @@ tap.test('redis over tls connection', (t) => {
         throw error
       })
       .connect()
-    await t.client.flushAll()
-  })
+    await client.flushAll()
 
-  t.afterEach(async (t) => {
-    await t.client.flushAll()
-    await t.client.disconnect()
-    helper.unloadAgent(t.agent)
-    removeModules(['redis'])
-  })
+    t.teardown(async () => {
+      await client.flushAll()
+      await client.disconnect()
+      helper.unloadAgent(agent)
+    })
 
-  t.test('should work with self-signed tls cert on server', (t) => {
-    const { agent, client } = t
     helper.runInTransaction(agent, async function transactionInScope() {
       const tx = agent.getTransaction()
       await client.set('tls-test', 'foo')
       const found = await client.get('tls-test')
       t.equal(found, 'foo')
       tx.end()
-      t.end()
+      resolve()
     })
+
+    await promise
+  })
+
+  t.test('url parsing should add tls true', async (t) => {
+    const { promise, resolve } = promiseResolvers()
+    const agent = helper.instrumentMockedAgent()
+    const redis = require('redis')
+    const client = await redis
+      .createClient({
+        url: `rediss://${HOST}:${PORT}`,
+        socket: {
+          rejectUnauthorized: false
+        }
+      })
+      .on('error', (error) => {
+        throw error
+      })
+      .connect()
+    await client.flushAll()
+
+    t.teardown(async () => {
+      await client.flushAll()
+      await client.disconnect()
+      helper.unloadAgent(agent)
+    })
+
+    helper.runInTransaction(agent, async function transactionInScope() {
+      const tx = agent.getTransaction()
+      await client.set('tls-test', 'foo')
+      const found = await client.get('tls-test')
+      t.equal(found, 'foo')
+      tx.end()
+      resolve()
+    })
+
+    await promise
   })
 
   t.end()
