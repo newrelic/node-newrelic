36b1d9b537198a4863b3a5e269e61dd0ea408d47
Svetlana Brennan <50715937+svetlanabrennan@users.noreply.github.com>
2024-08-19 14:39:20 -0500

    remove files that exist in another pr and add application host files

--- cloud-tooling/azure-site-extension/Content/install.cmd
+++
@@ -1,8 +0,0 @@
-:: Copyright 2022 New Relic Corporation. All rights reserved.
-:: SPDX-License-Identifier: Apache-2.0
-
-@echo off
-
-powershell.exe -ExecutionPolicy RemoteSigned -File install.ps1
-
-echo %ERRORLEVEL%
--- cloud-tooling/azure-site-extension/Content/uninstall.cmd
+++
@@ -1,7 +0,0 @@
-:: Copyright 2022 New Relic Corporation. All rights reserved.
-:: SPDX-License-Identifier: Apache-2.0
-
-SET NEW_RELIC_FOLDER="%HOME%\node_modules/newrelic"
-IF EXIST %NEW_RELIC_FOLDER% (
-  npm uninstall newrelic
-)
