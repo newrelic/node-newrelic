097bb8573a685d9d608d5f562da61224385abdc3
James Sumners <jsumners@newrelic.com>
2024-08-28 09:59:59 -0400

    update key-parser tests

--- test/unit/collector/key-parser.test.js
+++ test/unit/collector/key-parser.test.js
@@ -1,27 +1,24 @@
 /*
- * Copyright 2020 New Relic Corporation. All rights reserved.
+ * Copyright 2024 New Relic Corporation. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
 'use strict'
 
-const tap = require('tap')
+const test = require('node:test')
+const assert = require('node:assert')
 const parse = require('../../../lib/collector/key-parser').parseKey
 
-tap.test('collector license key parser', (t) => {
-  t.test('should return the region prefix when a region is detected', (t) => {
+test('collector license key parser', async (t) => {
+  await t.test('should return the region prefix when a region is detected', () => {
     const testKey = 'eu01xx66c637a29c3982469a3fe8d1982d002c4a'
     const region = parse(testKey)
-    t.equal(region, 'eu01')
-    t.end()
+    assert.equal(region, 'eu01')
   })
 
-  t.test('should return null when a region is not detected', (t) => {
+  await t.test('should return null when a region is not defined', () => {
     const testKey = '08a2ad66c637a29c3982469a3fe8d1982d002c4a'
     const region = parse(testKey)
-    t.equal(region, null)
-    t.end()
+    assert.equal(region, null)
   })
-
-  t.end()
 })
