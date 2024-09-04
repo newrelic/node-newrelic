adfeebc043161e0e0c35de2cf93989dbde9cb8fa
James Sumners <jsumners@newrelic.com>
2024-09-04 13:01:12 -0400

    fix: Fixed detection of REST API type payloads in AWS Lambda (#2543)

--- test/unit/serverless/utils.test.js
+++ test/unit/serverless/utils.test.js
@@ -9,16 +9,23 @@ const test = require('node:test')
 const assert = require('node:assert')
 
 const { isGatewayV1Event, isGatewayV2Event } = require('../../../lib/serverless/api-gateway')
-const { gatewayV1Event, gatewayV2Event, lambaV1InvocationEvent } = require('./fixtures')
+const {
+  restApiGatewayV1Event,
+  httpApiGatewayV1Event,
+  httpApiGatewayV2Event,
+  lambaV1InvocationEvent
+} = require('./fixtures')
 
 test('isGatewayV1Event', () => {
-  assert.equal(isGatewayV1Event(gatewayV1Event), true)
-  assert.equal(isGatewayV1Event(gatewayV2Event), false)
+  assert.equal(isGatewayV1Event(restApiGatewayV1Event), true)
+  assert.equal(isGatewayV1Event(httpApiGatewayV1Event), true)
+  assert.equal(isGatewayV1Event(httpApiGatewayV2Event), false)
   assert.equal(isGatewayV1Event(lambaV1InvocationEvent), false)
 })
 
 test('isGatewayV2Event', () => {
-  assert.equal(isGatewayV2Event(gatewayV1Event), false)
-  assert.equal(isGatewayV2Event(gatewayV2Event), true)
+  assert.equal(isGatewayV2Event(restApiGatewayV1Event), false)
+  assert.equal(isGatewayV2Event(httpApiGatewayV1Event), false)
+  assert.equal(isGatewayV2Event(httpApiGatewayV2Event), true)
   assert.equal(isGatewayV2Event(lambaV1InvocationEvent), false)
 })
