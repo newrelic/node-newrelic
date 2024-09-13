cc217512d4d624ef2bb436b0539a280fd3aa728d
James Sumners <jsumners@newrelic.com>
2024-09-13 12:39:10 -0400

    chore: Migrate block of unit tests to `node:test`

--- test/unit/analytics_events.test.js
+++ test/unit/analytics_events.test.js
@@ -1,293 +1,279 @@
 /*
- * Copyright 2020 New Relic Corporation. All rights reserved.
+ * Copyright 2024 New Relic Corporation. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
 'use strict'
 
-const tap = require('tap')
+const test = require('node:test')
+const assert = require('node:assert')
 
+const { match } = require('../lib/custom-assertions')
 const Transaction = require('../../lib/transaction')
 const helper = require('../lib/agent_helper')
 
 const DESTS = require('../../lib/config/attribute-filter').DESTINATIONS
-
 const LIMIT = 10
 
-tap.test('Analytics events', function (t) {
-  t.autoend()
+test.beforeEach((ctx) => {
+  ctx.nr = {}
+  ctx.nr.agent = helper.loadMockedAgent({
+    transaction_events: { max_samples_stored: LIMIT }
+  })
+  ctx.nr.agent.config.attributes.enabled = true
+})
 
-  let agent = null
-  let trans = null
+test.afterEach((ctx) => {
+  helper.unloadAgent(ctx.nr.agent)
+})
 
-  t.beforeEach(function () {
-    if (agent) {
-      return
-    } // already instantiated
-    agent = helper.loadMockedAgent({
-      transaction_events: {
-        max_samples_stored: LIMIT
-      }
-    })
-    agent.config.attributes.enabled = true
+test('when there are attributes on transaction', async (t) => {
+  helper.unloadAgent(t.nr.agent)
+  t.beforeEach((ctx) => {
+    ctx.nr.trans = new Transaction(ctx.nr.agent)
   })
 
-  t.afterEach(function () {
-    if (!agent) {
-      return
-    } // already destroyed
-    helper.unloadAgent(agent)
-    agent = null
+  await t.test('event should contain those attributes', (t) => {
+    const { agent, trans } = t.nr
+    trans.trace.attributes.addAttribute(DESTS.TRANS_EVENT, 'test', 'TEST')
+    agent._addEventFromTransaction(trans)
+
+    const first = 0
+    const agentAttrs = 2
+
+    const events = getTransactionEvents(agent)
+    const firstEvent = events[first]
+    assert.equal(firstEvent[agentAttrs].test, 'TEST')
+  })
+})
+
+test('when host name is specified by user', async (t) => {
+  helper.unloadAgent(t.nr.agent)
+  t.beforeEach((ctx) => {
+    ctx.nr.agent.config.process_host.display_name = 'test-value'
+    ctx.nr.trans = new Transaction(ctx.nr.agent)
   })
 
-  t.test('when there are attributes on transaction', function (t) {
-    t.autoend()
+  await t.test('name should be sent with event', (t) => {
+    const { agent, trans } = t.nr
+    agent._addEventFromTransaction(trans)
 
-    t.beforeEach(function () {
-      trans = new Transaction(agent)
-    })
+    const first = 0
+    const agentAttrs = 2
 
-    t.test('event should contain those attributes', function (t) {
-      trans.trace.attributes.addAttribute(DESTS.TRANS_EVENT, 'test', 'TEST')
-      agent._addEventFromTransaction(trans)
-
-      const first = 0
-      const agentAttrs = 2
-
-      const events = getTransactionEvents(agent)
-      const firstEvent = events[first]
-      t.equal(firstEvent[agentAttrs].test, 'TEST')
-      t.end()
-    })
-  })
-
-  t.test('when host name is specified by user', function (t) {
-    t.autoend()
-
-    t.beforeEach(function () {
-      agent.config.process_host.display_name = 'test-value'
-      trans = new Transaction(agent)
-    })
-
-    t.test('name should be sent with event', function (t) {
-      agent._addEventFromTransaction(trans)
-
-      const first = 0
-      const agentAttrs = 2
-
-      const events = getTransactionEvents(agent)
-      const firstEvent = events[first]
-      t.same(firstEvent[agentAttrs], {
+    const events = getTransactionEvents(agent)
+    const firstEvent = events[first]
+    assert.equal(
+      match(firstEvent[agentAttrs], {
         'host.displayName': 'test-value'
-      })
-      t.end()
+      }),
+      true
+    )
+  })
+})
+
+test('when analytics events are disabled', async (t) => {
+  helper.unloadAgent(t.nr.agent)
+
+  await t.test('collector cannot enable remotely', (t) => {
+    const { agent } = t.nr
+    agent.config.transaction_events.enabled = false
+    assert.doesNotThrow(function () {
+      agent.config.onConnect({ collect_analytics_events: true })
     })
+    assert.equal(agent.config.transaction_events.enabled, false)
+  })
+})
+
+test('when analytics events are enabled', async (t) => {
+  helper.unloadAgent(t.nr.agent)
+
+  await t.test('collector can disable remotely', (t) => {
+    const { agent } = t.nr
+    agent.config.transaction_events.enabled = true
+    assert.doesNotThrow(function () {
+      agent.config.onConnect({ collect_analytics_events: false })
+    })
+    assert.equal(agent.config.transaction_events.enabled, false)
+  })
+})
+
+test('on transaction finished', async (t) => {
+  helper.unloadAgent(t.nr.agent)
+  t.beforeEach((ctx) => {
+    ctx.nr.trans = new Transaction(ctx.nr.agent)
   })
 
-  t.test('when analytics events are disabled', function (t) {
-    t.autoend()
-
-    t.test('collector cannot enable remotely', function (t) {
-      agent.config.transaction_events.enabled = false
-      t.doesNotThrow(function () {
-        agent.config.onConnect({ collect_analytics_events: true })
-      })
-      t.equal(agent.config.transaction_events.enabled, false)
-      t.end()
-    })
+  await t.test('should queue an event', async (t) => {
+    const { agent, trans } = t.nr
+    agent._addEventFromTransaction = (transaction) => {
+      assert.equal(transaction, trans)
+      trans.end()
+    }
   })
 
-  t.test('when analytics events are enabled', function (t) {
-    t.autoend()
+  await t.test('should generate an event from transaction', (t) => {
+    const { agent, trans } = t.nr
+    trans.end()
 
-    t.test('collector can disable remotely', function (t) {
-      agent.config.transaction_events.enabled = true
-      t.doesNotThrow(function () {
-        agent.config.onConnect({ collect_analytics_events: false })
-      })
-      t.equal(agent.config.transaction_events.enabled, false)
-      t.end()
-    })
+    const events = getTransactionEvents(agent)
+
+    assert.equal(events.length, 1)
+
+    const event = events[0]
+    assert.ok(Array.isArray(event))
+    const eventValues = event[0]
+    assert.equal(typeof eventValues, 'object')
+    assert.equal(typeof eventValues.webDuration, 'number')
+    assert.equal(Number.isNaN(eventValues.webDuration), false)
+    assert.equal(eventValues.webDuration, trans.timer.getDurationInMillis() / 1000)
+    assert.equal(typeof eventValues.timestamp, 'number')
+    assert.equal(Number.isNaN(eventValues.timestamp), false)
+    assert.equal(eventValues.timestamp, trans.timer.start)
+    assert.equal(eventValues.name, trans.name)
+    assert.equal(eventValues.duration, trans.timer.getDurationInMillis() / 1000)
+    assert.equal(eventValues.type, 'Transaction')
+    assert.equal(eventValues.error, false)
   })
 
-  t.test('on transaction finished', function (t) {
-    t.autoend()
+  await t.test('should flag errored transactions', (t) => {
+    const { agent, trans } = t.nr
+    trans.addException(new Error('wuh oh'))
+    trans.end()
 
-    t.beforeEach(function () {
-      trans = new Transaction(agent)
-    })
+    const events = getTransactionEvents(agent)
+    assert.equal(events.length, 1)
 
-    t.test('should queue an event', async function (t) {
-      agent._addEventFromTransaction = (transaction) => {
-        t.equal(transaction, trans)
-        trans.end()
-        t.end()
+    const event = events[0]
+    assert.ok(Array.isArray(event))
+    const eventValues = event[0]
+    assert.equal(typeof eventValues, 'object')
+    assert.equal(typeof eventValues.webDuration, 'number')
+    assert.equal(Number.isNaN(eventValues.webDuration), false)
+    assert.equal(eventValues.webDuration, trans.timer.getDurationInMillis() / 1000)
+    assert.equal(typeof eventValues.timestamp, 'number')
+    assert.equal(Number.isNaN(eventValues.timestamp), false)
+    assert.equal(eventValues.timestamp, trans.timer.start)
+    assert.equal(eventValues.name, trans.name)
+    assert.equal(eventValues.duration, trans.timer.getDurationInMillis() / 1000)
+    assert.equal(eventValues.type, 'Transaction')
+    assert.equal(eventValues.error, true)
+  })
+
+  await t.test('should add DT parent attributes with an accepted payload', (t) => {
+    const { agent } = t.nr
+    agent.config.distributed_tracing.enabled = true
+    agent.config.primary_application_id = 'test'
+    agent.config.account_id = 1
+    const trans = new Transaction(agent)
+    const payload = trans._createDistributedTracePayload().text()
+    trans.isDistributedTrace = null
+    trans._acceptDistributedTracePayload(payload)
+    trans.end()
+
+    const events = getTransactionEvents(agent)
+
+    assert.equal(events.length, 1)
+
+    const attributes = events[0][0]
+    assert.equal(attributes.traceId, trans.traceId)
+    assert.equal(attributes.guid, trans.id)
+    assert.equal(attributes.priority, trans.priority)
+    assert.equal(attributes.sampled, trans.sampled)
+    assert.equal(attributes.parentId, trans.id)
+    assert.equal(attributes['parent.type'], 'App')
+    assert.equal(attributes['parent.app'], agent.config.primary_application_id)
+    assert.equal(attributes['parent.account'], agent.config.account_id)
+    assert.equal(attributes.error, false)
+    assert.equal(trans.sampled, true)
+    assert.ok(trans.priority > 1)
+  })
+
+  await t.test('should add DT attributes', (t) => {
+    const { agent } = t.nr
+    agent.config.distributed_tracing.enabled = true
+    const trans = new Transaction(agent)
+    trans.end()
+
+    const events = getTransactionEvents(agent)
+
+    assert.equal(events.length, 1)
+
+    const attributes = events[0][0]
+    assert.equal(attributes.traceId, trans.traceId)
+    assert.equal(attributes.guid, trans.id)
+    assert.equal(attributes.priority, trans.priority)
+    assert.equal(attributes.sampled, trans.sampled)
+    assert.equal(trans.sampled, true)
+    assert.ok(trans.priority > 1)
+  })
+
+  await t.test('should contain user and agent attributes', (t) => {
+    const { agent, trans } = t.nr
+    trans.end()
+
+    const events = getTransactionEvents(agent)
+
+    assert.equal(events.length, 1)
+
+    const event = events[0]
+    assert.equal(typeof event[0], 'object')
+    assert.equal(typeof event[1], 'object')
+    assert.equal(typeof event[2], 'object')
+  })
+
+  await t.test('should contain custom attributes', (t) => {
+    const { agent, trans } = t.nr
+    trans.trace.addCustomAttribute('a', 'b')
+    trans.end()
+
+    const events = getTransactionEvents(agent)
+    const event = events[0]
+    assert.equal(event[1].a, 'b')
+  })
+
+  await t.test('includes internal synthetics attributes', (t) => {
+    const { agent, trans } = t.nr
+    trans.syntheticsData = {
+      version: 1,
+      accountId: 123,
+      resourceId: 'resId',
+      jobId: 'jobId',
+      monitorId: 'monId'
+    }
+
+    trans.syntheticsInfoData = {
+      version: 1,
+      type: 'unitTest',
+      initiator: 'cli',
+      attributes: {
+        'Attr-Test': 'value',
+        'attr2Test': 'value1',
+        'xTest-Header': 'value2'
       }
-    })
+    }
 
-    t.test('should generate an event from transaction', function (t) {
-      trans.end()
+    trans.end()
 
-      const events = getTransactionEvents(agent)
+    const events = getTransactionEvents(agent)
+    const event = events[0]
+    const attributes = event[0]
+    assert.equal(attributes['nr.syntheticsResourceId'], 'resId')
+    assert.equal(attributes['nr.syntheticsJobId'], 'jobId')
+    assert.equal(attributes['nr.syntheticsMonitorId'], 'monId')
+    assert.equal(attributes['nr.syntheticsType'], 'unitTest')
+    assert.equal(attributes['nr.syntheticsInitiator'], 'cli')
+    assert.equal(attributes['nr.syntheticsAttrTest'], 'value')
+    assert.equal(attributes['nr.syntheticsAttr2Test'], 'value1')
+    assert.equal(attributes['nr.syntheticsXTestHeader'], 'value2')
+  })
 
-      t.equal(events.length, 1)
-
-      const event = events[0]
-      t.ok(Array.isArray(event))
-      const eventValues = event[0]
-      t.equal(typeof eventValues, 'object')
-      t.equal(typeof eventValues.webDuration, 'number')
-      t.not(Number.isNaN(eventValues.webDuration))
-      t.equal(eventValues.webDuration, trans.timer.getDurationInMillis() / 1000)
-      t.equal(typeof eventValues.timestamp, 'number')
-      t.not(Number.isNaN(eventValues.timestamp))
-      t.equal(eventValues.timestamp, trans.timer.start)
-      t.equal(eventValues.name, trans.name)
-      t.equal(eventValues.duration, trans.timer.getDurationInMillis() / 1000)
-      t.equal(eventValues.type, 'Transaction')
-      t.equal(eventValues.error, false)
-      t.end()
-    })
-
-    t.test('should flag errored transactions', function (t) {
-      trans.addException(new Error('wuh oh'))
-      trans.end()
-
-      const events = getTransactionEvents(agent)
-      t.equal(events.length, 1)
-
-      const event = events[0]
-      t.ok(Array.isArray(event))
-      const eventValues = event[0]
-      t.equal(typeof eventValues, 'object')
-      t.equal(typeof eventValues.webDuration, 'number')
-      t.not(Number.isNaN(eventValues.webDuration))
-      t.equal(eventValues.webDuration, trans.timer.getDurationInMillis() / 1000)
-      t.equal(typeof eventValues.timestamp, 'number')
-      t.not(Number.isNaN(eventValues.timestamp))
-      t.equal(eventValues.timestamp, trans.timer.start)
-      t.equal(eventValues.name, trans.name)
-      t.equal(eventValues.duration, trans.timer.getDurationInMillis() / 1000)
-      t.equal(eventValues.type, 'Transaction')
-      t.equal(eventValues.error, true)
-      t.end()
-    })
-
-    t.test('should add DT parent attributes with an accepted payload', function (t) {
-      agent.config.distributed_tracing.enabled = true
-      agent.config.primary_application_id = 'test'
-      agent.config.account_id = 1
-      trans = new Transaction(agent)
-      const payload = trans._createDistributedTracePayload().text()
-      trans.isDistributedTrace = null
-      trans._acceptDistributedTracePayload(payload)
-      trans.end()
-
-      const events = getTransactionEvents(agent)
-
-      t.equal(events.length, 1)
-
-      const attributes = events[0][0]
-      t.equal(attributes.traceId, trans.traceId)
-      t.equal(attributes.guid, trans.id)
-      t.equal(attributes.priority, trans.priority)
-      t.equal(attributes.sampled, trans.sampled)
-      t.equal(attributes.parentId, trans.id)
-      t.equal(attributes['parent.type'], 'App')
-      t.equal(attributes['parent.app'], agent.config.primary_application_id)
-      t.equal(attributes['parent.account'], agent.config.account_id)
-      t.equal(attributes.error, false)
-      t.equal(trans.sampled, true)
-      t.ok(trans.priority > 1)
-      t.end()
-    })
-
-    t.test('should add DT attributes', function (t) {
-      agent.config.distributed_tracing.enabled = true
-      trans = new Transaction(agent)
-      trans.end()
-
-      const events = getTransactionEvents(agent)
-
-      t.equal(events.length, 1)
-
-      const attributes = events[0][0]
-      t.equal(attributes.traceId, trans.traceId)
-      t.equal(attributes.guid, trans.id)
-      t.equal(attributes.priority, trans.priority)
-      t.equal(attributes.sampled, trans.sampled)
-      t.equal(trans.sampled, true)
-      t.ok(trans.priority > 1)
-      t.end()
-    })
-
-    t.test('should contain user and agent attributes', function (t) {
-      trans.end()
-
-      const events = getTransactionEvents(agent)
-
-      t.equal(events.length, 1)
-
-      const event = events[0]
-      t.equal(typeof event[0], 'object')
-      t.equal(typeof event[1], 'object')
-      t.equal(typeof event[2], 'object')
-      t.end()
-    })
-
-    t.test('should contain custom attributes', function (t) {
-      trans.trace.addCustomAttribute('a', 'b')
-      trans.end()
-
-      const events = getTransactionEvents(agent)
-      const event = events[0]
-      t.equal(event[1].a, 'b')
-      t.end()
-    })
-
-    t.test('includes internal synthetics attributes', function (t) {
-      trans.syntheticsData = {
-        version: 1,
-        accountId: 123,
-        resourceId: 'resId',
-        jobId: 'jobId',
-        monitorId: 'monId'
-      }
-
-      trans.syntheticsInfoData = {
-        version: 1,
-        type: 'unitTest',
-        initiator: 'cli',
-        attributes: {
-          'Attr-Test': 'value',
-          'attr2Test': 'value1',
-          'xTest-Header': 'value2'
-        }
-      }
-
-      trans.end()
-
-      const events = getTransactionEvents(agent)
-      const event = events[0]
-      const attributes = event[0]
-      t.equal(attributes['nr.syntheticsResourceId'], 'resId')
-      t.equal(attributes['nr.syntheticsJobId'], 'jobId')
-      t.equal(attributes['nr.syntheticsMonitorId'], 'monId')
-      t.equal(attributes['nr.syntheticsType'], 'unitTest')
-      t.equal(attributes['nr.syntheticsInitiator'], 'cli')
-      t.equal(attributes['nr.syntheticsAttrTest'], 'value')
-      t.equal(attributes['nr.syntheticsAttr2Test'], 'value1')
-      t.equal(attributes['nr.syntheticsXTestHeader'], 'value2')
-      t.end()
-    })
-
-    t.test('not spill over reservoir size', function (t) {
-      for (let i = 0; i < 20; i++) {
-        agent._addEventFromTransaction(trans)
-      }
-      t.equal(getTransactionEvents(agent).length, LIMIT)
-      t.end()
-    })
+  await t.test('not spill over reservoir size', (t) => {
+    const { agent, trans } = t.nr
+    for (let i = 0; i < 20; i++) {
+      agent._addEventFromTransaction(trans)
+    }
+    assert.equal(getTransactionEvents(agent).length, LIMIT)
   })
 })
 
--- test/unit/attributes.test.js
+++ test/unit/attributes.test.js
@@ -1,12 +1,14 @@
 /*
- * Copyright 2020 New Relic Corporation. All rights reserved.
+ * Copyright 2024 New Relic Corporation. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
 'use strict'
 
-const tap = require('tap')
+const test = require('node:test')
+const assert = require('node:assert')
 
+const { match } = require('../lib/custom-assertions')
 const helper = require('../lib/agent_helper')
 const { Attributes } = require('../../lib/attributes')
 const AttributeFilter = require('../../lib/config/attribute-filter')
@@ -14,30 +16,16 @@ const AttributeFilter = require('../../lib/config/attribute-filter')
 const DESTINATIONS = AttributeFilter.DESTINATIONS
 const TRANSACTION_SCOPE = 'transaction'
 
-tap.test('#addAttribute', (t) => {
-  t.autoend()
-
-  let agent = null
-
-  t.beforeEach(() => {
-    agent = helper.loadMockedAgent()
-  })
-
-  t.afterEach(() => {
-    helper.unloadAgent(agent)
-  })
-
-  t.test('adds an attribute to instance', (t) => {
+test('#addAttribute', async (t) => {
+  await t.test('adds an attribute to instance', () => {
     const inst = new Attributes(TRANSACTION_SCOPE)
     inst.addAttribute(DESTINATIONS.TRANS_SCOPE, 'test', 'success')
     const attributes = inst.get(DESTINATIONS.TRANS_SCOPE)
 
-    t.equal(attributes.test, 'success')
-
-    t.end()
+    assert.equal(attributes.test, 'success')
   })
 
-  t.test('does not add attribute if key length limit is exceeded', (t) => {
+  await t.test('does not add attribute if key length limit is exceeded', () => {
     const tooLong = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       'Cras id lacinia erat. Suspendisse mi nisl, sodales vel est eu,',
@@ -49,37 +37,21 @@ tap.test('#addAttribute', (t) => {
     inst.addAttribute(DESTINATIONS.TRANS_SCOPE, tooLong, 'will fail')
     const attributes = Object.keys(inst.attributes)
 
-    t.equal(attributes.length, 0)
-
-    t.end()
+    assert.equal(attributes.length, 0)
   })
 })
 
-tap.test('#addAttributes', (t) => {
-  t.autoend()
-
-  let agent = null
-
-  t.beforeEach(() => {
-    agent = helper.loadMockedAgent()
-  })
-
-  t.afterEach(() => {
-    helper.unloadAgent(agent)
-  })
-
-  t.test('adds multiple attributes to instance', (t) => {
+test('#addAttributes', async (t) => {
+  await t.test('adds multiple attributes to instance', () => {
     const inst = new Attributes(TRANSACTION_SCOPE)
     inst.addAttributes(DESTINATIONS.TRANS_SCOPE, { one: '1', two: '2' })
     const attributes = inst.get(DESTINATIONS.TRANS_SCOPE)
 
-    t.equal(attributes.one, '1')
-    t.equal(attributes.two, '2')
-
-    t.end()
+    assert.equal(attributes.one, '1')
+    assert.equal(attributes.two, '2')
   })
 
-  t.test('only allows non-null-type primitive attribute values', (t) => {
+  await t.test('only allows non-null-type primitive attribute values', () => {
     const inst = new Attributes(TRANSACTION_SCOPE, 10)
     const attributes = {
       first: 'first',
@@ -96,20 +68,18 @@ tap.test('#addAttributes', (t) => {
     inst.addAttributes(DESTINATIONS.TRANS_SCOPE, attributes)
 
     const res = inst.get(DESTINATIONS.TRANS_SCOPE)
-    t.equal(Object.keys(res).length, 3)
+    assert.equal(Object.keys(res).length, 3)
 
     const hasAttribute = Object.hasOwnProperty.bind(res)
-    t.notOk(hasAttribute('second'))
-    t.notOk(hasAttribute('third'))
-    t.notOk(hasAttribute('sixth'))
-    t.notOk(hasAttribute('seventh'))
-    t.notOk(hasAttribute('eighth'))
-    t.notOk(hasAttribute('ninth'))
-
-    t.end()
+    assert.equal(hasAttribute('second'), false)
+    assert.equal(hasAttribute('third'), false)
+    assert.equal(hasAttribute('sixth'), false)
+    assert.equal(hasAttribute('seventh'), false)
+    assert.equal(hasAttribute('eighth'), false)
+    assert.equal(hasAttribute('ninth'), false)
   })
 
-  t.test('disallows adding more than maximum allowed attributes', (t) => {
+  await t.test('disallows adding more than maximum allowed attributes', () => {
     const inst = new Attributes(TRANSACTION_SCOPE, 3)
     const attributes = {
       first: 1,
@@ -121,39 +91,23 @@ tap.test('#addAttributes', (t) => {
     inst.addAttributes(DESTINATIONS.TRANS_SCOPE, attributes)
     const res = inst.get(DESTINATIONS.TRANS_SCOPE)
 
-    t.equal(Object.keys(res).length, 3)
-
-    t.end()
+    assert.equal(Object.keys(res).length, 3)
   })
 
-  t.test('Overwrites value of added attribute with same key', (t) => {
+  await t.test('Overwrites value of added attribute with same key', () => {
     const inst = new Attributes(TRANSACTION_SCOPE, 2)
     inst.addAttribute(0x01, 'Roboto', 1)
     inst.addAttribute(0x01, 'Roboto', 99)
 
     const res = inst.get(0x01)
 
-    t.equal(Object.keys(res).length, 1)
-    t.equal(res.Roboto, 99)
-
-    t.end()
+    assert.equal(Object.keys(res).length, 1)
+    assert.equal(res.Roboto, 99)
   })
 })
 
-tap.test('#get', (t) => {
-  t.autoend()
-
-  let agent = null
-
-  t.beforeEach(() => {
-    agent = helper.loadMockedAgent()
-  })
-
-  t.afterEach(() => {
-    helper.unloadAgent(agent)
-  })
-
-  t.test('gets attributes by destination, truncating values if necessary', (t) => {
+test('#get', async (t) => {
+  await t.test('gets attributes by destination, truncating values if necessary', () => {
     const longVal = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       'Cras id lacinia erat. Suspendisse mi nisl, sodales vel est eu,',
@@ -167,17 +121,15 @@ tap.test('#get', (t) => {
     inst.addAttribute(0x01, 'tooLong', longVal)
     inst.addAttribute(0x08, 'wrongDest', 'hello')
 
-    t.ok(Buffer.byteLength(longVal) > 255)
+    assert.ok(Buffer.byteLength(longVal) > 255)
 
     const res = inst.get(0x01)
-    t.equal(res.valid, 50)
+    assert.equal(res.valid, 50)
 
-    t.equal(Buffer.byteLength(res.tooLong), 255)
-
-    t.end()
+    assert.equal(Buffer.byteLength(res.tooLong), 255)
   })
 
-  t.test('only returns attributes up to specified limit', (t) => {
+  await t.test('only returns attributes up to specified limit', () => {
     const inst = new Attributes(TRANSACTION_SCOPE, 2)
     inst.addAttribute(0x01, 'first', 'first')
     inst.addAttribute(0x01, 'second', 'second')
@@ -186,44 +138,38 @@ tap.test('#get', (t) => {
     const res = inst.get(0x01)
     const hasAttribute = Object.hasOwnProperty.bind(res)
 
-    t.equal(Object.keys(res).length, 2)
-    t.notOk(hasAttribute('third'))
-
-    t.end()
+    assert.equal(Object.keys(res).length, 2)
+    assert.equal(hasAttribute('third'), false)
   })
 })
 
-tap.test('#hasValidDestination', (t) => {
-  t.autoend()
-
-  let agent = null
-
-  t.beforeEach(() => {
-    agent = helper.loadMockedAgent()
+test('#hasValidDestination', async (t) => {
+  t.beforeEach((ctx) => {
+    ctx.nr = {}
+    ctx.nr.agent = helper.loadMockedAgent()
   })
 
-  t.afterEach(() => {
-    helper.unloadAgent(agent)
+  t.afterEach((ctx) => {
+    helper.unloadAgent(ctx.nr.agent)
   })
 
-  t.test('should return true if single destination valid', (t) => {
+  await t.test('should return true if single destination valid', () => {
     const attributes = new Attributes(TRANSACTION_SCOPE)
     const hasDestination = attributes.hasValidDestination(DESTINATIONS.TRANS_EVENT, 'testAttr')
 
-    t.equal(hasDestination, true)
-    t.end()
+    assert.equal(hasDestination, true)
   })
 
-  t.test('should return true if all destinations valid', (t) => {
+  await t.test('should return true if all destinations valid', () => {
     const attributes = new Attributes(TRANSACTION_SCOPE)
     const destinations = DESTINATIONS.TRANS_EVENT | DESTINATIONS.TRANS_TRACE
     const hasDestination = attributes.hasValidDestination(destinations, 'testAttr')
 
-    t.equal(hasDestination, true)
-    t.end()
+    assert.equal(hasDestination, true)
   })
 
-  t.test('should return true if only one destination valid', (t) => {
+  await t.test('should return true if only one destination valid', (t) => {
+    const { agent } = t.nr
     const attributeName = 'testAttr'
     agent.config.transaction_events.attributes.exclude = [attributeName]
     agent.config.emit('transaction_events.attributes.exclude')
@@ -232,11 +178,11 @@ tap.test('#hasValidDestination', (t) => {
     const destinations = DESTINATIONS.TRANS_EVENT | DESTINATIONS.TRANS_TRACE
     const hasDestination = attributes.hasValidDestination(destinations, attributeName)
 
-    t.equal(hasDestination, true)
-    t.end()
+    assert.equal(hasDestination, true)
   })
 
-  t.test('should return false if no valid destinations', (t) => {
+  await t.test('should return false if no valid destinations', (t) => {
+    const { agent } = t.nr
     const attributeName = 'testAttr'
     agent.config.attributes.exclude = [attributeName]
     agent.config.emit('attributes.exclude')
@@ -245,25 +191,12 @@ tap.test('#hasValidDestination', (t) => {
     const destinations = DESTINATIONS.TRANS_EVENT | DESTINATIONS.TRANS_TRACE
     const hasDestination = attributes.hasValidDestination(destinations, attributeName)
 
-    t.equal(hasDestination, false)
-    t.end()
+    assert.equal(hasDestination, false)
   })
 })
 
-tap.test('#reset', (t) => {
-  t.autoend()
-
-  let agent = null
-
-  t.beforeEach(() => {
-    agent = helper.loadMockedAgent()
-  })
-
-  t.afterEach(() => {
-    helper.unloadAgent(agent)
-  })
-
-  t.test('resets instance attributes', (t) => {
+test('#reset', async (t) => {
+  await t.test('resets instance attributes', () => {
     const inst = new Attributes(TRANSACTION_SCOPE)
     inst.addAttribute(0x01, 'first', 'first')
     inst.addAttribute(0x01, 'second', 'second')
@@ -271,8 +204,6 @@ tap.test('#reset', (t) => {
 
     inst.reset()
 
-    t.same(inst.attributes, {})
-
-    t.end()
+    assert.equal(match(inst.attributes, {}), true)
   })
 })
