c99f94a0db0368a1827174ca5d1b620d5dbc82d6
James Sumners <jsumners@newrelic.com>
2024-09-23 12:42:14 -0400

    update protocols tests

--- test/unit/protocols.test.js
+++ test/unit/protocols.test.js
@@ -5,42 +5,48 @@
 
 'use strict'
 
-const tap = require('tap')
+const test = require('node:test')
+const assert = require('node:assert')
 
+const { match } = require('../lib/custom-assertions')
 const helper = require('../lib/agent_helper')
+
 const RemoteMethod = require('../../lib/collector/remote-method')
 
-tap.test('errors', (t) => {
-  let agent
-
-  t.beforeEach(() => {
-    agent = helper.loadMockedAgent()
+test('errors', async (t) => {
+  t.beforeEach((ctx) => {
+    ctx.nr = {}
+    const agent = helper.loadMockedAgent()
     agent.config.attributes.enabled = true
     agent.config.run_id = 1
 
     agent.errors.traceAggregator.reconfigure(agent.config)
+
+    ctx.nr.agent = agent
   })
 
-  t.afterEach(() => {
-    helper.unloadAgent(agent)
+  t.afterEach((ctx) => {
+    helper.unloadAgent(ctx.nr.agent)
   })
 
-  t.test('should serialize down to match the protocol', (t) => {
+  await t.test('should serialize down to match the protocol', (t, end) => {
+    const { agent } = t.nr
     const error = new Error('test')
     error.stack = 'test stack'
     agent.errors.add(null, error)
 
     const payload = agent.errors.traceAggregator._toPayloadSync()
     RemoteMethod.prototype.serialize(payload, (err, errors) => {
-      t.equal(err, null)
-      t.same(
-        errors,
-        '[1,[[0,"Unknown","test","Error",{"userAttributes":{},"agentAttributes":{},' +
-          '"intrinsics":{"error.expected":false},"stack_trace":["test stack"]},null]]]'
+      assert.equal(err, null)
+      assert.equal(
+        match(
+          errors,
+          '[1,[[0,"Unknown","test","Error",{"userAttributes":{},"agentAttributes":{},' +
+            '"intrinsics":{"error.expected":false},"stack_trace":["test stack"]},null]]]'
+        ),
+        true
       )
-      t.end()
+      end()
     })
   })
-
-  t.end()
 })
