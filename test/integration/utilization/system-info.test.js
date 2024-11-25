1d01545c1225fd63f31b7d7c0c9271dc9f4ed7e2
Bob Evans <robert.evans25@gmail.com>
2024-11-25 12:09:35 -0500

    test: Migrated `test/integration/utilization` tests to node `node:test`

--- test/integration/utilization/system-info.test.js
+++ test/integration/utilization/system-info.test.js
@@ -6,17 +6,23 @@
 'use strict'
 
 const helper = require('../../lib/agent_helper')
-const test = require('tap').test
+const test = require('node:test')
+const assert = require('node:assert')
 const nock = require('nock')
 const proxyquire = require('proxyquire')
 const fetchSystemInfo = require('../../../lib/system-info')
 
-test('pricing system-info aws', function (t) {
+test.beforeEach(() => {
+  nock.disableNetConnect()
+})
+
+test.afterEach(() => {
+  nock.enableNetConnect()
+})
+
+test('pricing system-info aws', function (t, end) {
   const awsHost = 'http://169.254.169.254'
   process.env.ECS_CONTAINER_METADATA_URI_V4 = awsHost + '/docker'
-  t.teardown(() => {
-    delete process.env.ECS_CONTAINER_METADATA_URI_V4
-  })
 
   const awsResponses = {
     'dynamic/instance-identity/document': {
@@ -43,33 +49,34 @@ test('pricing system-info aws', function (t) {
       detect_docker: false
     }
   })
-  t.teardown(function () {
+  t.after(function () {
     helper.unloadAgent(agent)
+    delete process.env.ECS_CONTAINER_METADATA_URI_V4
   })
 
   fetchSystemInfo(agent, function fetchSystemInfoCb(err, systemInfo) {
-    t.same(systemInfo.vendors.aws, {
+    assert.deepEqual(systemInfo.vendors.aws, {
       instanceType: 'test.type',
       instanceId: 'test.id',
       availabilityZone: 'us-west-2b'
     })
-    t.same(systemInfo.vendors.ecs, { ecsDockerId: 'ecs-container-1' })
+    assert.deepEqual(systemInfo.vendors.ecs, { ecsDockerId: 'ecs-container-1' })
 
     // This will throw an error if the sys info isn't being cached properly
-    t.ok(awsRedirect.isDone(), 'should exhaust nock endpoints')
-    t.ok(ecsScope.isDone())
+    assert.ok(awsRedirect.isDone(), 'should exhaust nock endpoints')
+    assert.ok(ecsScope.isDone())
     fetchSystemInfo(agent, function checkCache(err, cachedInfo) {
-      t.same(cachedInfo.vendors.aws, {
+      assert.deepEqual(cachedInfo.vendors.aws, {
         instanceType: 'test.type',
         instanceId: 'test.id',
         availabilityZone: 'us-west-2b'
       })
-      t.end()
+      end()
     })
   })
 })
 
-test('pricing system-info azure', function (t) {
+test('pricing system-info azure', function (t, end) {
   const azureHost = 'http://169.254.169.254'
   const azureResponse = {
     location: 'test.location',
@@ -90,12 +97,12 @@ test('pricing system-info azure', function (t) {
       detect_docker: false
     }
   })
-  t.teardown(function () {
+  t.after(function () {
     helper.unloadAgent(agent)
   })
 
   fetchSystemInfo(agent, function fetchSystemInfoCb(err, systemInfo) {
-    t.same(systemInfo.vendors.azure, {
+    assert.deepEqual(systemInfo.vendors.azure, {
       location: 'test.location',
       name: 'test.name',
       vmId: 'test.vmId',
@@ -103,26 +110,20 @@ test('pricing system-info azure', function (t) {
     })
 
     // This will throw an error if the sys info isn't being cached properly
-    t.ok(azureRedirect.isDone(), 'should exhaust nock endpoints')
+    assert.ok(azureRedirect.isDone(), 'should exhaust nock endpoints')
     fetchSystemInfo(agent, function checkCache(err, cachedInfo) {
-      t.same(cachedInfo.vendors.azure, {
+      assert.deepEqual(cachedInfo.vendors.azure, {
         location: 'test.location',
         name: 'test.name',
         vmId: 'test.vmId',
         vmSize: 'test.vmSize'
       })
-      t.end()
+      end()
     })
   })
 })
 
-test('pricing system-info gcp', function (t) {
-  nock.disableNetConnect()
-
-  t.teardown(function () {
-    nock.enableNetConnect()
-  })
-
+test('pricing system-info gcp', function (t, end) {
   const gcpRedirect = nock('http://metadata.google.internal', {
     reqheaders: { 'Metadata-Flavor': 'Google' }
   })
@@ -144,7 +145,7 @@ test('pricing system-info gcp', function (t) {
       detect_docker: false
     }
   })
-  t.teardown(function () {
+  t.after(function () {
     helper.unloadAgent(agent)
   })
 
@@ -155,18 +156,18 @@ test('pricing system-info gcp', function (t) {
       name: 'aef-default-20170501t160547-7gh8',
       zone: 'us-central1-c'
     }
-    t.same(systemInfo.vendors.gcp, expectedData)
+    assert.deepEqual(systemInfo.vendors.gcp, expectedData)
 
     // This will throw an error if the sys info isn't being cached properly
-    t.ok(gcpRedirect.isDone(), 'should exhaust nock endpoints')
+    assert.ok(gcpRedirect.isDone(), 'should exhaust nock endpoints')
     fetchSystemInfo(agent, function checkCache(err, cachedInfo) {
-      t.same(cachedInfo.vendors.gcp, expectedData)
-      t.end()
+      assert.deepEqual(cachedInfo.vendors.gcp, expectedData)
+      end()
     })
   })
 })
 
-test('pricing system-info pcf', function (t) {
+test('pricing system-info pcf', function (t, end) {
   const agent = helper.loadMockedAgent({
     utilization: {
       detect_aws: false,
@@ -176,7 +177,7 @@ test('pricing system-info pcf', function (t) {
       detect_docker: false
     }
   })
-  t.teardown(function () {
+  t.after(function () {
     helper.unloadAgent(agent)
   })
 
@@ -190,12 +191,12 @@ test('pricing system-info pcf', function (t) {
       cf_instance_ip: '10.10.147.130',
       memory_limit: '1024m'
     }
-    t.same(systemInfo.vendors.pcf, expectedData)
-    t.end()
+    assert.deepEqual(systemInfo.vendors.pcf, expectedData)
+    end()
   })
 })
 
-test('pricing system-info docker', function (t) {
+test('pricing system-info docker', function (t, end) {
   const mockUtilization = proxyquire('../../../lib/utilization', {
     './docker-info': {
       getVendorInfo: function (agent, callback) {
@@ -217,7 +218,7 @@ test('pricing system-info docker', function (t) {
       detect_docker: true
     }
   })
-  t.teardown(function () {
+  t.after(function () {
     helper.unloadAgent(agent)
   })
 
@@ -225,7 +226,7 @@ test('pricing system-info docker', function (t) {
     const expectedData = {
       id: '47cbd16b77c50cbf71401c069cd2189f0e659af17d5a2daca3bddf59d8a870b2'
     }
-    t.same(systemInfo.vendors.docker, expectedData)
-    t.end()
+    assert.deepEqual(systemInfo.vendors.docker, expectedData)
+    end()
   })
 })
