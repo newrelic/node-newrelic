83ad1f3e72f06211993b5dcbc2a5af4a27b259fe
James Sumners <jsumners@newrelic.com>
2024-11-26 11:11:53 -0500

    chore: Updated distributed-tracing integration tests to node:test (#2798)

--- test/integration/distributed-tracing/background.test.js
+++ test/integration/distributed-tracing/background.test.js
@@ -5,14 +5,17 @@
 
 'use strict'
 
-const tap = require('tap')
+const test = require('node:test')
+const tspl = require('@matteo.collina/tspl')
+
 const helper = require('../../lib/agent_helper')
 const API = require('../../../api')
 
 let compareSampled = null
 
-tap.test('background transactions should not blow up with DT', (t) => {
-  t.plan(24)
+test('background transactions should not blow up with DT', async (t) => {
+  const plan = tspl(t, { plan: 24 })
+
   const config = {
     distributed_tracing: {
       enabled: true
@@ -34,7 +37,7 @@ tap.test('background transactions should not blow up with DT', (t) => {
   const api = new API(agent)
 
   const server = http.createServer(function (req, res) {
-    t.ok(req.headers.newrelic, 'got incoming newrelic header')
+    plan.ok(req.headers.newrelic, 'got incoming newrelic header')
 
     req.resume()
     res.end()
@@ -59,52 +62,60 @@ tap.test('background transactions should not blow up with DT', (t) => {
 
   const finishedHandlers = [
     function web(trans, event) {
-      t.equal(trans.name, 'WebTransaction/NormalizedUri/*', 'got web trans first')
+      plan.equal(trans.name, 'WebTransaction/NormalizedUri/*', 'got web trans first')
       const intrinsic = event[0]
 
-      t.equal(intrinsic.name, 'WebTransaction/NormalizedUri/*', 'web event has name')
-      t.ok(intrinsic.guid, 'web should have a guid on event')
-      t.ok(intrinsic.traceId, 'web should have a traceId on event')
-      t.ok(intrinsic.priority, 'web should have a priority on event')
-      t.ok(intrinsic.sampled != null, 'web should have a sampled boolean on event')
-      t.ok(intrinsic.parentId, 'web should have parentId on event')
-      t.ok(intrinsic['parent.type'], 'web should have parent type on event')
-      t.ok(intrinsic['parent.app'], 'web should have parent app on event')
-      t.ok(intrinsic['parent.account'], 'web should have parent account on event')
-      t.ok(intrinsic['parent.transportType'], 'web should have parent transport type on event')
-      t.ok(
+      plan.equal(intrinsic.name, 'WebTransaction/NormalizedUri/*', 'web event has name')
+      plan.ok(intrinsic.guid, 'web should have a guid on event')
+      plan.ok(intrinsic.traceId, 'web should have a traceId on event')
+      plan.ok(intrinsic.priority, 'web should have a priority on event')
+      plan.ok(intrinsic.sampled != null, 'web should have a sampled boolean on event')
+      plan.ok(intrinsic.parentId, 'web should have parentId on event')
+      plan.ok(intrinsic['parent.type'], 'web should have parent type on event')
+      plan.ok(intrinsic['parent.app'], 'web should have parent app on event')
+      plan.ok(intrinsic['parent.account'], 'web should have parent account on event')
+      plan.ok(intrinsic['parent.transportType'], 'web should have parent transport type on event')
+      plan.ok(
         intrinsic['parent.transportDuration'],
         'web should have parent transport duration on event'
       )
-      t.notOk(
+      plan.equal(
         intrinsic['nr.alternatePathHashes'],
+        undefined,
         'web should not have an nr.alternatePathHashes on event'
       )
 
-      compareSampled = currySampled(t, {
+      compareSampled = currySampled(plan, {
         sampled: intrinsic.sampled,
         priority: intrinsic.priority
       })
     },
     function background(trans, event) {
-      t.equal(trans.name, 'OtherTransaction/Nodejs/myTx', 'got background trans second')
+      plan.equal(trans.name, 'OtherTransaction/Nodejs/myTx', 'got background trans second')
       const intrinsic = event[0]
 
-      t.ok(intrinsic.traceId, 'bg should have a traceId on event')
-      t.ok(intrinsic.priority, 'bg should have a priority on event')
-      t.ok(intrinsic.guid, 'bg should have a guid on event')
-      t.ok(intrinsic.sampled != null, 'bg should have a sampled boolean on event')
-      t.notOk(
+      plan.ok(intrinsic.traceId, 'bg should have a traceId on event')
+      plan.ok(intrinsic.priority, 'bg should have a priority on event')
+      plan.ok(intrinsic.guid, 'bg should have a guid on event')
+      plan.ok(intrinsic.sampled != null, 'bg should have a sampled boolean on event')
+      plan.equal(
         intrinsic['nr.referringPathHash'],
+        undefined,
         'bg should not have an nr.referringPathHash on event'
       )
-      t.notOk(
+      plan.equal(
         intrinsic['nr.referringTransactionGuid'],
+        undefined,
         'bg should not have an nr.referringTransactionGuid on event'
       )
-      t.notOk(intrinsic['nr.apdexPerfZone'], 'bg should have an nr.apdexPerfZone on event')
-      t.notOk(
+      plan.equal(
+        intrinsic['nr.apdexPerfZone'],
+        undefined,
+        'bg should have an nr.apdexPerfZone on event'
+      )
+      plan.equal(
         intrinsic['nr.alternatePathHashes'],
+        undefined,
         'bg should have an nr.alternatePathHashes on event'
       )
 
@@ -122,12 +133,14 @@ tap.test('background transactions should not blow up with DT', (t) => {
     finishedHandlers[count](trans, event)
     count += 1
   })
+
+  await plan.completed
 })
 
-function currySampled(t, a) {
+function currySampled(plan, a) {
   return (b) => {
     b = b || a
-    t.ok(
+    plan.ok(
       a.sampled === b.sampled && a.priority === b.priority,
       'sampled values and priority persist across transactions'
     )
--- test/integration/distributed-tracing/span-error-attributes.tap.js
+++ test/integration/distributed-tracing/span-error-attributes.tap.js
@@ -5,309 +5,304 @@
 
 'use strict'
 
-const tap = require('tap')
+const test = require('node:test')
+const assert = require('node:assert')
+const tspl = require('@matteo.collina/tspl')
+
+const match = require('../../lib/custom-assertions/match')
 const helper = require('../../lib/agent_helper')
 const API = require('../../../api')
 
-tap.test('span error attributes', (t) => {
-  t.test('core', (t) => {
-    const config = {
-      distributed_tracing: { enabled: true }
+test('core', async (t) => {
+  const plan = tspl(t, { plan: 6 })
+
+  const agent = helper.instrumentMockedAgent({
+    distributed_tracing: { enabled: true }
+  })
+  const api = new API(agent)
+
+  const spanIds = []
+  const numErrors = 3
+
+  agent.on('transactionFinished', () => {
+    const errorEvents = agent.errors.eventAggregator.getEvents()
+    const errorEventSpanIds = errorEvents.map((e) => e[2].spanId)
+
+    plan.deepStrictEqual(
+      new Set(spanIds),
+      new Set(errorEventSpanIds),
+      'Every TransactionError event should have a unique span ID'
+    )
+
+    plan.equal(errorEvents.length, numErrors, 'Every error was reported')
+
+    const spanEvents = agent.spanEventAggregator.getEvents()
+
+    spanEvents.forEach((s) => {
+      const attrs = s.attributes
+      match(attrs['error.message'], /test[0-9]/, { assert: plan })
+
+      match(attrs['error.class'], 'Error', { assert: plan })
+    })
+
+    plan.equal(spanEvents.length, numErrors, 'Every span was reported')
+  })
+
+  helper.runInTransaction(agent, (tx) => {
+    for (let i = 0; i < numErrors; i++) {
+      api.startSegment(`segment${i}`, true, () => {
+        const segment = api.shim.getSegment()
+        spanIds.push(segment.id)
+        api.noticeError(new Error(`test${i}`))
+      })
     }
 
-    const agent = helper.instrumentMockedAgent(config)
-    const api = new API(agent)
+    tx.end()
+  })
 
-    const spanIds = []
-    const numErrors = 3
+  t.after(() => helper.unloadAgent(agent))
 
-    agent.on('transactionFinished', () => {
-      const errorEvents = agent.errors.eventAggregator.getEvents()
-      const errorEventSpanIds = errorEvents.map((e) => e[2].spanId)
+  await plan.completed
+})
 
-      t.same(
-        new Set(spanIds),
-        new Set(errorEventSpanIds),
-        'Every TransactionError event should have a unique span ID'
-      )
+test('should not add error attributes to spans when errors disabled', (t, end) => {
+  const agent = helper.instrumentMockedAgent({
+    distributed_tracing: {
+      enabled: true
+    },
+    error_collector: {
+      enabled: false
+    }
+  })
 
-      t.equal(errorEvents.length, numErrors, 'Every error was reported')
-
-      const spanEvents = agent.spanEventAggregator.getEvents()
-
-      spanEvents.forEach((s) => {
-        const attrs = s.attributes
-        t.match(attrs['error.message'], /test[0-9]/, 'Error attributes are on the spans')
-
-        t.match(attrs['error.class'], 'Error', 'Error attributes are on the spans')
-      })
-
-      t.equal(spanEvents.length, numErrors, 'Every span was reported')
-    })
-
-    helper.runInTransaction(agent, (tx) => {
-      for (let i = 0; i < numErrors; i++) {
-        api.startSegment(`segment${i}`, true, () => {
-          const segment = api.shim.getSegment()
-          spanIds.push(segment.id)
-          api.noticeError(new Error(`test${i}`))
-        })
-      }
-
-      tx.end()
-    })
-
+  t.after(() => {
     helper.unloadAgent(agent)
-
-    t.end()
   })
 
-  t.test('should not add error attributes to spans when errors disabled', (t) => {
-    const agent = helper.instrumentMockedAgent({
-      distributed_tracing: {
-        enabled: true
-      },
-      error_collector: {
-        enabled: false
+  const api = new API(agent)
+
+  let spanId = null
+
+  agent.on('transactionFinished', () => {
+    const spanEvent = agent.spanEventAggregator.getEvents()[0]
+
+    assert.equal(spanEvent.intrinsics.guid, spanId)
+
+    const hasAttribute = Object.hasOwnProperty.bind(spanEvent.attributes)
+    assert.equal(hasAttribute('error.message'), false)
+
+    end()
+  })
+
+  helper.runInTransaction(agent, (tx) => {
+    api.startSegment('segment', true, () => {
+      const segment = api.shim.getSegment()
+      spanId = segment.id
+      api.noticeError(new Error('test'))
+    })
+
+    tx.end()
+  })
+})
+
+test("Span error attributes aren't added with LASP/HSM", (t, end) => {
+  const agent = helper.instrumentMockedAgent({
+    distributed_tracing: { enabled: true },
+    high_security: true
+  })
+  const api = new API(agent)
+
+  let spanId
+
+  agent.on('transactionFinished', () => {
+    const spanEvent = agent.spanEventAggregator.getEvents()[0]
+    const attrs = spanEvent.attributes
+    assert.ok(
+      spanEvent.intrinsics.guid === spanId && !attrs['error.message'],
+      'There should be no error message on the span'
+    )
+    end()
+  })
+
+  helper.runInTransaction(agent, (tx) => {
+    api.startSegment('segment', true, () => {
+      const segment = api.shim.getSegment()
+      spanId = segment.id
+      api.noticeError(new Error('test'))
+    })
+
+    tx.end()
+  })
+
+  t.after(() => helper.unloadAgent(agent))
+})
+
+test("Span error attributes aren't added with ignored classes errors", (t, end) => {
+  const agent = helper.instrumentMockedAgent({
+    distributed_tracing: { enabled: true },
+    error_collector: { ignore_classes: ['CustomError'] }
+  })
+  const api = new API(agent)
+
+  let ignoredSpanId
+  let spanId
+
+  agent.on('transactionFinished', () => {
+    const errorEvents = agent.errors.eventAggregator.getEvents()
+    const spanEvents = agent.spanEventAggregator.getEvents()
+    const ignoredSpanEvent = spanEvents.filter((s) => s.intrinsics.guid === ignoredSpanId)[0]
+    const spanEvent = spanEvents.filter((s) => s.intrinsics.guid === spanId)[0]
+
+    assert.ok(
+      errorEvents.length === 1 && errorEvents[0][2].spanId === spanId,
+      'There should only be the non-ignored error reported'
+    )
+
+    assert.ok(
+      spanEvent.attributes['error.message'] === 'not ignored',
+      'The non-ignored error should be reported'
+    )
+
+    assert.equal(
+      ignoredSpanEvent.attributes['error.message'],
+      undefined,
+      'The ignored error should not be reported'
+    )
+
+    end()
+  })
+
+  helper.runInTransaction(agent, (tx) => {
+    class CustomError extends Error {
+      constructor() {
+        super(...arguments)
+        this.name = 'CustomError'
       }
-    })
-
-    t.teardown(() => {
-      helper.unloadAgent(agent)
-    })
-
-    const api = new API(agent)
-
-    let spanId = null
-
-    agent.on('transactionFinished', () => {
-      const spanEvent = agent.spanEventAggregator.getEvents()[0]
-
-      t.equal(spanEvent.intrinsics.guid, spanId)
-
-      const hasAttribute = Object.hasOwnProperty.bind(spanEvent.attributes)
-      t.notOk(hasAttribute('error.message'))
-
-      t.end()
-    })
-
-    helper.runInTransaction(agent, (tx) => {
-      api.startSegment('segment', true, () => {
-        const segment = api.shim.getSegment()
-        spanId = segment.id
-        api.noticeError(new Error('test'))
-      })
-
-      tx.end()
-    })
-  })
-
-  t.test("Span error attributes aren't added with LASP/HSM", (t) => {
-    const config = {
-      distributed_tracing: { enabled: true },
-      high_security: true
     }
 
-    const agent = helper.instrumentMockedAgent(config)
-    const api = new API(agent)
-
-    let spanId
-
-    agent.on('transactionFinished', () => {
-      const spanEvent = agent.spanEventAggregator.getEvents()[0]
-      const attrs = spanEvent.attributes
-      t.ok(
-        spanEvent.intrinsics.guid === spanId && !attrs['error.message'],
-        'There should be no error message on the span'
-      )
+    api.startSegment('segment1', true, () => {
+      const segment = api.shim.getSegment()
+      ignoredSpanId = segment.id
+      agent.errors.add(tx, new CustomError('ignored'))
     })
 
-    helper.runInTransaction(agent, (tx) => {
-      api.startSegment('segment', true, () => {
-        const segment = api.shim.getSegment()
-        spanId = segment.id
-        api.noticeError(new Error('test'))
-      })
-
-      tx.end()
+    api.startSegment('segment2', true, () => {
+      const segment = api.shim.getSegment()
+      spanId = segment.id
+      agent.errors.add(tx, new Error('not ignored'))
     })
 
-    helper.unloadAgent(agent)
-
-    t.end()
+    tx.end()
   })
 
-  t.test("Span error attributes aren't added with ignored classes errors", (t) => {
-    const config = {
-      distributed_tracing: { enabled: true },
-      error_collector: { ignore_classes: ['CustomError'] }
+  t.after(() => helper.unloadAgent(agent))
+})
+
+test("Span error attributes aren't added with ignored status errors", (t, end) => {
+  const agent = helper.instrumentMockedAgent({
+    distributed_tracing: { enabled: true },
+    error_collector: { ignore_status_codes: [404, 422] }
+  })
+  const api = new API(agent)
+
+  let ignoredSpanId = null
+
+  agent.on('transactionFinished', () => {
+    const errorEvents = agent.errors.eventAggregator.getEvents()
+    const spanEvents = agent.spanEventAggregator.getEvents()
+    const ignoredSpanEvent = spanEvents.filter((s) => s.intrinsics.guid === ignoredSpanId)[0]
+
+    assert.equal(
+      errorEvents.length,
+      0,
+      'There should not be any errors reported because of status code'
+    )
+
+    assert.equal(
+      ignoredSpanEvent.attributes['error.message'],
+      undefined,
+      'The ignored error should not be added to span.'
+    )
+
+    end()
+  })
+
+  helper.runInTransaction(agent, (tx) => {
+    class CustomError extends Error {
+      constructor() {
+        super(...arguments)
+        this.name = 'CustomError'
+      }
     }
 
-    const agent = helper.instrumentMockedAgent(config)
-    const api = new API(agent)
-
-    let ignoredSpanId
-    let spanId
-
-    agent.on('transactionFinished', () => {
-      const errorEvents = agent.errors.eventAggregator.getEvents()
-      const spanEvents = agent.spanEventAggregator.getEvents()
-      const ignoredSpanEvent = spanEvents.filter((s) => s.intrinsics.guid === ignoredSpanId)[0]
-      const spanEvent = spanEvents.filter((s) => s.intrinsics.guid === spanId)[0]
-
-      t.ok(
-        errorEvents.length === 1 && errorEvents[0][2].spanId === spanId,
-        'There should only be the non-ignored error reported'
-      )
-
-      t.ok(
-        spanEvent.attributes['error.message'] === 'not ignored',
-        'The non-ignored error should be reported'
-      )
-
-      t.notOk(
-        ignoredSpanEvent.attributes['error.message'],
-        'The ignored error should not be reported'
-      )
+    api.startSegment('segment1', true, () => {
+      const segment = api.shim.getSegment()
+      ignoredSpanId = segment.id
+      agent.errors.add(tx, new CustomError('ignored'))
     })
 
-    helper.runInTransaction(agent, (tx) => {
-      class CustomError extends Error {
-        constructor() {
-          super(...arguments)
-          this.name = 'CustomError'
-        }
-      }
-
-      api.startSegment('segment1', true, () => {
-        const segment = api.shim.getSegment()
-        ignoredSpanId = segment.id
-        agent.errors.add(tx, new CustomError('ignored'))
-      })
-
-      api.startSegment('segment2', true, () => {
-        const segment = api.shim.getSegment()
-        spanId = segment.id
-        agent.errors.add(tx, new Error('not ignored'))
-      })
-
-      tx.end()
-    })
-
-    helper.unloadAgent(agent)
-
-    t.end()
+    tx.statusCode = 422
+    tx.end()
   })
 
-  t.test("Span error attributes aren't added with ignored status errors", (t) => {
-    const config = {
-      distributed_tracing: { enabled: true },
-      error_collector: { ignore_status_codes: [404, 422] }
+  t.after(() => helper.unloadAgent(agent))
+})
+
+test('should propagate expected error attribute to span', (t, end) => {
+  const agent = helper.instrumentMockedAgent({
+    distributed_tracing: {
+      enabled: true
+    },
+    error_collector: {
+      expected_classes: ['CustomError']
+    }
+  })
+
+  t.after(() => {
+    helper.unloadAgent(agent)
+  })
+
+  const api = new API(agent)
+
+  let expectedSpanId
+  let notExpectedSpanId
+
+  agent.on('transactionFinished', () => {
+    const errorEvents = agent.errors.eventAggregator.getEvents()
+    const spanEvents = agent.spanEventAggregator.getEvents()
+    const expectedSpanEvent = spanEvents.filter((s) => s.intrinsics.guid === expectedSpanId)[0]
+    const spanEvent = spanEvents.filter((s) => s.intrinsics.guid === notExpectedSpanId)[0]
+
+    assert.equal(errorEvents.length, 2)
+
+    assert.equal(expectedSpanEvent.attributes['error.expected'], true)
+
+    const hasAttribute = Object.hasOwnProperty.bind(spanEvent.attributes)
+    assert.equal(hasAttribute('error.expected'), false)
+
+    end()
+  })
+
+  helper.runInTransaction(agent, (tx) => {
+    class CustomError extends Error {
+      constructor() {
+        super(...arguments)
+        this.name = 'CustomError'
+      }
     }
 
-    const agent = helper.instrumentMockedAgent(config)
-    const api = new API(agent)
-
-    let ignoredSpanId = null
-
-    agent.on('transactionFinished', () => {
-      const errorEvents = agent.errors.eventAggregator.getEvents()
-      const spanEvents = agent.spanEventAggregator.getEvents()
-      const ignoredSpanEvent = spanEvents.filter((s) => s.intrinsics.guid === ignoredSpanId)[0]
-
-      t.equal(
-        errorEvents.length,
-        0,
-        'There should not be any errors reported because of status code'
-      )
-
-      t.notOk(
-        ignoredSpanEvent.attributes['error.message'],
-        'The ignored error should not be added to span.'
-      )
+    api.startSegment('segment1', true, () => {
+      const segment = api.shim.getSegment()
+      expectedSpanId = segment.id
+      agent.errors.add(tx, new CustomError('expected'))
     })
 
-    helper.runInTransaction(agent, (tx) => {
-      class CustomError extends Error {
-        constructor() {
-          super(...arguments)
-          this.name = 'CustomError'
-        }
-      }
-
-      api.startSegment('segment1', true, () => {
-        const segment = api.shim.getSegment()
-        ignoredSpanId = segment.id
-        agent.errors.add(tx, new CustomError('ignored'))
-      })
-
-      tx.statusCode = 422
-      tx.end()
+    api.startSegment('segment2', true, () => {
+      const segment = api.shim.getSegment()
+      notExpectedSpanId = segment.id
+      agent.errors.add(tx, new Error('not expected'))
     })
 
-    helper.unloadAgent(agent)
-
-    t.end()
+    tx.end()
   })
-
-  t.test('should propogate expected error attribute to span', (t) => {
-    const agent = helper.instrumentMockedAgent({
-      distributed_tracing: {
-        enabled: true
-      },
-      error_collector: {
-        expected_classes: ['CustomError']
-      }
-    })
-
-    t.teardown(() => {
-      helper.unloadAgent(agent)
-    })
-
-    const api = new API(agent)
-
-    let expectedSpanId
-    let notExpectedSpanId
-
-    agent.on('transactionFinished', () => {
-      const errorEvents = agent.errors.eventAggregator.getEvents()
-      const spanEvents = agent.spanEventAggregator.getEvents()
-      const expectedSpanEvent = spanEvents.filter((s) => s.intrinsics.guid === expectedSpanId)[0]
-      const spanEvent = spanEvents.filter((s) => s.intrinsics.guid === notExpectedSpanId)[0]
-
-      t.equal(errorEvents.length, 2)
-
-      t.equal(expectedSpanEvent.attributes['error.expected'], true)
-
-      const hasAttribute = Object.hasOwnProperty.bind(spanEvent.attributes)
-      t.equal(hasAttribute('error.expected'), false)
-
-      t.end()
-    })
-
-    helper.runInTransaction(agent, (tx) => {
-      class CustomError extends Error {
-        constructor() {
-          super(...arguments)
-          this.name = 'CustomError'
-        }
-      }
-
-      api.startSegment('segment1', true, () => {
-        const segment = api.shim.getSegment()
-        expectedSpanId = segment.id
-        agent.errors.add(tx, new CustomError('expected'))
-      })
-
-      api.startSegment('segment2', true, () => {
-        const segment = api.shim.getSegment()
-        notExpectedSpanId = segment.id
-        agent.errors.add(tx, new Error('not expected'))
-      })
-
-      tx.end()
-    })
-  })
-
-  t.end()
 })
