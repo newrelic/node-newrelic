8bb55be17762aa73414e5877fae965da4e4148ff
Bob Evans <robert.evans25@gmail.com>
2024-12-13 13:14:29 -0500

    test: Skipped integration test that tried to require an ESM package

--- test/integration/config/config-esm.test.js
+++ test/integration/config/config-esm.test.js
@@ -7,16 +7,24 @@
 
 const test = require('node:test')
 const assert = require('node:assert')
+const semver = require('semver')
 
 const match = require('../../lib/custom-assertions/match')
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 
 test('should gracefully handle ESM imports', async (t) => {
-  await t.test('when newrelic.js is misnamed', async () => {
-    const { stderr } = await exec('node index.mjs', { cwd: `${__dirname}/esm-bad` })
-    match(stderr, 'ERR_REQUIRE_ESM', 'should mention ERR_REQUIRE_ESM in error message')
-  })
+  // allowing require of esm made this test invalid
+  // see: https://github.com/nodejs/node/pull/55085/
+  // Until we can figure out if this is still valid we are skipping it
+  await t.test(
+    'when newrelic.js is misnamed',
+    { skip: semver.gte(process.version, '22.12.0') },
+    async () => {
+      const { stderr } = await exec('node index.mjs', { cwd: `${__dirname}/esm-bad` })
+      match(stderr, 'ERR_REQUIRE_ESM', 'should mention ERR_REQUIRE_ESM in error message')
+    }
+  )
 
   await t.test('when newrelic.cjs is properly named', async () => {
     const { stdout, stderr } = await exec('node index.mjs', { cwd: `${__dirname}/esm-good` })
