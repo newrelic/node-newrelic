ed7eea1fd4f0cd72834f6c7f24193fce96e569bd
Amy Chisholm <achisholm@newrelic.com>
2025-03-11 13:45:38 -0700

    wip: pubsub template

---
+++ test/versioned/gcp/pubsub.test.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2025 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+const assert = require('node:assert')
+const test = require('node:test')
+const helper = require('../../lib/agent_helper')
+
+test('GCP PubSub API', async (t) => {
+    t.beforeEach(async (ctx) => {
+        ctx.nr = {}
+        ctx.nr.agent = helper.instrumentMockedAgent()
+        const { PubSub } = require('@google-cloud/pubsub')
+        ctx.nr.pubsub = new PubSub({
+            projectId: 'fake-project-id',
+            credentials: {
+                client_email: 'fake-email@fake-project-id.iam.gserviceaccount.com',
+                private_key: 'test-private-key'
+            },
+            // https://cloud.google.com/iam/docs/full-resource-names
+            apiEndpoint: 'https://custom-endpoint.pubsub.googleapis.com/fake-project-id'
+        })
+    })
+
+    t.afterEach((ctx) => {
+        helper.unloadAgent(ctx.nr.agent)
+    })
+
+    await t.test('publish message', async (ctx) => {
+        const { pubsub } = ctx.nr
+        helper.runInTransaction(ctx, async (tx) => {
+            const topic = pubsub.topic('my-topic')
+            const message = Buffer.from('Hello, world!')
+            const messageId = await topic.publish(message)
+            tx.end()
+            assert.ok(messageId)
+        })
+    })
+
+    await t.test('pull messages', async (ctx) => {
+        const { pubsub } = ctx.nr
+        helper.runInTransaction(ctx, async (tx) => {
+            const subscription = pubsub.subscription('my-subscription')
+            const [messages] = await subscription.pull()
+            assert.ok(messages)
+            messages.forEach(message => {
+                message.ack()
+            })
+            tx.end()
+        })
+    })
+})
