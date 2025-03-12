1be72d54366f3c59a2e7734d1611b818d41f8f6e
Amy Chisholm <achisholm@newrelic.com>
2025-03-12 13:12:43 -0700

    wip: pubsub with sqs as a template

--- test/versioned/gcp/pubsub.test.js
+++ test/versioned/gcp/pubsub.test.js
@@ -7,48 +7,100 @@
 const assert = require('node:assert')
 const test = require('node:test')
 const helper = require('../../lib/agent_helper')
+const sinon = require('sinon')
+const {
+    DESTINATIONS: { TRANS_SEGMENT }
+} = require('../../../lib/config/attribute-filter')
+const { match } = require('../../lib/custom-assertions')
 
 test('GCP PubSub API', async (t) => {
     t.beforeEach(async (ctx) => {
         ctx.nr = {}
         ctx.nr.agent = helper.instrumentMockedAgent()
-        const { PubSub } = require('@google-cloud/pubsub')
-        ctx.nr.pubsub = new PubSub({
-            projectId: 'fake-project-id',
-            credentials: {
-                client_email: 'fake-email@fake-project-id.iam.gserviceaccount.com',
-                private_key: 'test-private-key'
-            },
-            // https://cloud.google.com/iam/docs/full-resource-names
-            apiEndpoint: 'https://custom-endpoint.pubsub.googleapis.com/fake-project-id'
-        })
+        const Shim = require('../../../lib/shim/message-shim')
+        ctx.nr.setLibrarySpy = sinon.spy(Shim.prototype, 'setLibrary')
+        const lib = require('@google-cloud/pubsub')
+        const PubSub = lib.PubSub
+        ctx.nr.lib = lib
+
+        // Assumes Google Cloud credentials are set up
+        // via https://cloud.google.com/pubsub/docs/publish-receive-messages-client-library#node.js
+        ctx.nr.publisher = new PubSub()
+        ctx.nr.subscriber = new PubSub()
     })
 
     t.afterEach((ctx) => {
         helper.unloadAgent(ctx.nr.agent)
+        ctx.nr.setLibrarySpy.restore()
     })
 
     await t.test('publish message', async (ctx) => {
-        const { agent, pubsub } = ctx.nr
-        helper.runInTransaction(agent, async (tx) => {
-            const topic = pubsub.topic('my-topic')
-            const message = Buffer.from('Hello, world!')
-            const messageId = await topic.publish(message)
-            tx.end()
+        const { agent, publisher, setLibrarySpy } = ctx.nr
+        const topicName = 'my-topic'
+        const data = 'Hello, world!'
+        await helper.runInTransaction(agent, async (tx) => {
+            const topic = publisher.topic(topicName)
+            assert.ok(topic)
+            const dataBuffer = Buffer.from(data)
+            const messageId = await topic.publishMessage({ data: dataBuffer })
             assert.ok(messageId)
+            tx.end()
+            finish({ transaction: tx, setLibrarySpy: setLibrarySpy })
         })
     })
 
     await t.test('pull messages', async (ctx) => {
-        const { agent, pubsub } = ctx.nr
-        helper.runInTransaction(agent, async (tx) => {
-            const subscription = pubsub.subscription('my-subscription')
-            const [messages] = await subscription.pull()
-            assert.ok(messages)
-            messages.forEach(message => {
+        const { agent, subscriber, setLibrarySpy } = ctx.nr
+        const subscriptionName = 'my-sub'
+        await helper.runInTransaction(agent, async (tx) => {
+            const subscription = subscriber.subscription(subscriptionName)
+            assert.ok(subscription)
+            const messaageHandler = message => {
                 message.ack()
+            }
+            subscription.on('message', messaageHandler)
+            setTimeout(() => {
+                subscription.removeListener('message', messaageHandler)
+                tx.end()
+                finish({ transaction: tx, setLibrarySpy: setLibrarySpy })
             })
-            tx.end()
         })
     })
 })
+
+function finish({ transaction, setLibrarySpy }) {
+    const expectedSegmentCount = 2
+    const root = transaction.trace.root
+    const segments = checkGCPAttributes({
+        trace: transaction.trace,
+        segment: root,
+        pattern: /.*/ // TODO: replace with proper pattern
+    })
+
+    assert.equal(
+        segments.length,
+        expectedSegmentCount,
+        `should have ${expectedSegmentCount} GCP PubSub segments`
+    )
+}
+
+function checkGCPAttributes({ trace, segment, pattern, markedSegments = [] }) {
+    const expectedAttrs = {
+        'messaging.system': String,
+        'cloud.region': String,
+        'cloud.account.id': String,
+        'messaging.destination.name': String
+    }
+
+    if (pattern.test(segment.name)) {
+        markedSegments.push(segment)
+        const attrs = segment.attributes.get(TRANS_SEGMENT)
+        match(attrs, expectedAttrs)
+    }
+    const children = trace.getChildren(segment.id)
+    children.forEach((child) => {
+        checkGCPAttributes({ trace, segment: child, pattern, markedSegments })
+    })
+
+    return markedSegments
+}
