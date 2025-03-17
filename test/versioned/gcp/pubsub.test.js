51349ca0914dbcbda05500fc45f645b193de6f04
Amy Chisholm <achisholm@newrelic.com>
2025-03-17 13:14:44 -0700

    wip: otel bridge for pubsub? use amqplib as template if not

--- test/versioned/gcp/pubsub.test.js
+++ test/versioned/gcp/pubsub.test.js
@@ -7,68 +7,87 @@
 const assert = require('node:assert')
 const test = require('node:test')
 const helper = require('../../lib/agent_helper')
-const sinon = require('sinon')
 const {
     DESTINATIONS: { TRANS_SEGMENT }
 } = require('../../../lib/config/attribute-filter')
 const { match } = require('../../lib/custom-assertions')
+const otel = require('@opentelemetry/api')
+const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node')
+const { detectResourcesSync } = require('@opentelemetry/resources');
+const { gcpDetector } = require('@opentelemetry/resource-detector-gcp')
 
-test('GCP PubSub API', async (t) => {
-    t.beforeEach(async (ctx) => {
-        ctx.nr = {}
-        ctx.nr.agent = helper.instrumentMockedAgent()
-        const Shim = require('../../../lib/shim/message-shim')
-        ctx.nr.setLibrarySpy = sinon.spy(Shim.prototype, 'setLibrary')
-        const lib = require('@google-cloud/pubsub')
-        const PubSub = lib.PubSub
-        ctx.nr.lib = lib
-
-        // Assumes Google Cloud credentials are set up
-        // via https://cloud.google.com/pubsub/docs/publish-receive-messages-client-library#node.js
-        ctx.nr.publisher = new PubSub()
-        ctx.nr.subscriber = new PubSub()
+test.beforeEach(async (ctx) => {
+    ctx.nr = {}
+    ctx.nr.agent = helper.instrumentMockedAgent({
+        feature_flag: {
+            opentelemetry_bridge: true
+        }
     })
+    const lib = require('@google-cloud/pubsub')
+    ctx.nr.lib = lib
 
-    t.afterEach((ctx) => {
-        helper.unloadAgent(ctx.nr.agent)
-        ctx.nr.setLibrarySpy.restore()
-    })
+    // Assumes Google Cloud credentials are set up
+    // via https://cloud.google.com/pubsub/docs/publish-receive-messages-client-library#node.js
+    ctx.nr.publisher = new lib.PubSub()
+    ctx.nr.subscriber = new lib.PubSub()
 
-    await t.test('publish message', async (ctx) => {
-        const { agent, publisher, setLibrarySpy } = ctx.nr
-        const topicName = 'my-topic'
-        const data = 'Hello, world!'
-        await helper.runInTransaction(agent, async (tx) => {
-            const topic = publisher.topic(topicName)
-            assert.ok(topic)
-            const dataBuffer = Buffer.from(data)
-            const messageId = await topic.publishMessage({ data: dataBuffer })
-            assert.ok(messageId)
-            tx.end()
-            finish({ transaction: tx, setLibrarySpy: setLibrarySpy })
-        })
+    // https://github.com/open-telemetry/opentelemetry-js-contrib/blob/main/detectors/node/opentelemetry-resource-detector-gcp/README.md
+    // TODO: try to get otel spans to work with gcp
+    const resource = detectResourcesSync({
+        detectors: [gcpDetector],
     })
-
-    await t.test('pull messages', async (ctx) => {
-        const { agent, subscriber, setLibrarySpy } = ctx.nr
-        const subscriptionName = 'my-sub'
-        await helper.runInTransaction(agent, async (tx) => {
-            const subscription = subscriber.subscription(subscriptionName)
-            assert.ok(subscription)
-            const messaageHandler = message => {
-                message.ack()
-            }
-            subscription.on('message', messaageHandler)
-            setTimeout(() => {
-                subscription.removeListener('message', messaageHandler)
-                tx.end()
-                finish({ transaction: tx, setLibrarySpy: setLibrarySpy })
-            })
-        })
-    })
+    const tracerProvider = new NodeTracerProvider({ resource });
+    ctx.nr.tracer = tracerProvider.getTracer('default')
 })
 
-function finish({ transaction, setLibrarySpy }) {
+test.afterEach((ctx) => {
+    helper.unloadAgent(ctx.nr.agent)
+    // disable all global constructs from trace sdk
+    otel.trace.disable()
+    otel.context.disable()
+    otel.propagation.disable()
+    otel.diag.disable()
+})
+
+test('publish message and then pull', (ctx, end) => {
+    const { agent, lib, publisher, subscriber, tracer } = ctx.nr
+    const topicName = 'my-topic'
+    const data = 'Hello, world!'
+    helper.runInTransaction(agent, async (tx) => {
+        tx.name = 'publish-message'
+        // https://github.com/open-telemetry/opentelemetry-js-contrib/blob/8e087550ad990732e51c1a2aeae7ba8abe7ecbe6/detectors/node/opentelemetry-resource-detector-gcp/src/detectors/GcpDetector.ts#L75
+        // const otel = lib.openTelemetry // TODO: do I use this otel?
+        tracer.startActiveSpan('publish message', { kind: otel.SpanKind.PRODUCER }, async (span) => {
+            const topic = publisher.topic(topicName)
+            assert.ok(topic)
+            const messageId = await topic.publishMessage({ data: Buffer.from(data) })
+            assert.ok(messageId)
+            const segment = agent.tracer.getSegment()
+            span.end()
+            tx.end()
+            finish(tx)
+            end()
+        })
+    })
+
+    // const subscriptionName = 'my-sub'
+    // await helper.runInTransaction(agent, async (tx) => {
+    //     const subscription = subscriber.subscription(subscriptionName)
+    //     assert.ok(subscription)
+    //     const messageHandler = message => {
+    //         message.ack()
+    //     }
+    //     subscription.on('message', messageHandler)
+    //     setTimeout(() => {
+    //         subscription.removeListener('message', messageHandler)
+    //         tx.end()
+    //         console.log(tx.trace.root)
+    //         finish({ transaction: tx })
+    //     }, 1000)
+    // })
+})
+
+function finish(transaction) {
     const expectedSegmentCount = 2
     const root = transaction.trace.root
     const segments = checkGCPAttributes({
@@ -76,12 +95,6 @@ function finish({ transaction, setLibrarySpy }) {
         segment: root,
         pattern: /.*/ // TODO: replace with proper pattern
     })
-
-    assert.equal(
-        segments.length,
-        expectedSegmentCount,
-        `should have ${expectedSegmentCount} GCP PubSub segments`
-    )
 }
 
 function checkGCPAttributes({ trace, segment, pattern, markedSegments = [] }) {
