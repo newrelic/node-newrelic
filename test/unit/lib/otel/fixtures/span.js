72d00454ca500af486f94858e9156c0aaf0dd2ea
Bob Evans <robert.evans25@gmail.com>
2025-03-24 16:53:31 -0400

    refactor: Upgraded otel packages to 2.0

--- test/unit/lib/otel/fixtures/span.js
+++ test/unit/lib/otel/fixtures/span.js
@@ -4,14 +4,8 @@
  */
 
 'use strict'
-const { ROOT_CONTEXT, TraceFlags } = require('@opentelemetry/api')
-const { Span } = require('@opentelemetry/sdk-trace-base')
+const { ROOT_CONTEXT } = require('@opentelemetry/api')
 
-module.exports = function createSpan({ parentId, tracer, tx, kind, name, spanContext }) {
-  spanContext = spanContext || {
-    traceId: tx?.traceId,
-    spanId: tx?.trace?.root?.id,
-    traceFlags: TraceFlags.SAMPLED
-  }
-  return new Span(tracer, ROOT_CONTEXT, name, spanContext, kind, parentId)
+module.exports = function createSpan({ tracer, kind, name }) {
+  return tracer.startSpan(name, { kind }, ROOT_CONTEXT)
 }
