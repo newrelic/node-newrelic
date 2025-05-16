547171e605461b946e4029e1c2edb5573522b188
Svetlana Brennan <50715937+svetlanabrennan@users.noreply.github.com>
2025-05-16 10:44:11 -0500

    remove tests not needed for node 18

--- test/unit/util/hashes.test.js
+++ test/unit/util/hashes.test.js
@@ -11,8 +11,6 @@ const test = require('node:test')
 const testData = require('../../lib/obfuscation-data')
 const hashes = require('../../../lib/util/hashes')
 
-const major = process.version.slice(1).split('.').map(Number).shift()
-
 test('#makeId always returns the correct length', () => {
   for (let length = 4; length < 64; length++) {
     for (let attempts = 0; attempts < 500; attempts++) {
@@ -51,21 +49,5 @@ test('deobfuscation', async (t) => {
     for (const data of testData) {
       assert.equal(hashes.deobfuscateNameUsingKey(data.output, data.key), data.input)
     }
-  })
-})
-
-// TODO: remove this test when we drop support for node 18
-test('getHash', { skip: major > 18 }, async (t) => {
-  /**
-   * TODO: crypto.DEFAULT_ENCODING has been deprecated.
-   * When fully disabled, this test can likely be removed.
-   * https://nodejs.org/api/deprecations.html#DEP0091
-   */
-  await t.test('should not crash when changing the DEFAULT_ENCODING key on crypto', () => {
-    const crypto = require('node:crypto')
-    const oldEncoding = crypto.DEFAULT_ENCODING
-    crypto.DEFAULT_ENCODING = 'utf-8'
-    assert.doesNotThrow(hashes.getHash.bind(null, 'TEST_APP', 'TEST_TXN'))
-    crypto.DEFAULT_ENCODING = oldEncoding
   })
 })
