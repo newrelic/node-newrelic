e1c33fcde5cdb954a4314f32a03f20ee20b55cbd
Amy Chisholm <achisholm@newrelic.com>
2025-06-03 10:54:28 -0700

    bad json error test

--- test/unit/llm-events/google-genai/error.test.js
+++ test/unit/llm-events/google-genai/error.test.js
@@ -8,7 +8,7 @@ const assert = require('node:assert')
 const LlmErrorMessage = require('#agentlib/llm-events/error-message.js')
 const { req } = require('./common')
 
-test('LlmErrorMessage', async () => {
+test('LlmErrorMessage - cause message ok', async () => {
   // Responses are empty when there is an error
   // in Google GenAI
   const res = {}
@@ -22,6 +22,37 @@ test('LlmErrorMessage', async () => {
     'http.statusCode': 500,
     'error.message': 'got status: INTERNAL. {"error":{"status":"INTERNAL","code":500,"message":"some error"}}',
     'error.code': 500,
+    'error.param': undefined,
+    completion_id: undefined,
+    embedding_id: undefined,
+    vector_store_id: undefined,
+    tool_id: undefined
+  }
+  assert.ok(errorMsg.toString(), 'LlmErrorMessage')
+  assert.equal(errorMsg['http.statusCode'], expected['http.statusCode'])
+  assert.equal(errorMsg['error.message'], expected['error.message'])
+  assert.equal(errorMsg['error.code'], expected['error.code'])
+  assert.equal(errorMsg['error.param'], expected['error.param'])
+  assert.equal(errorMsg.completion_id, expected.completion_id)
+  assert.equal(errorMsg.embedding_id, expected.embedding_id)
+  assert.equal(errorMsg.vector_store_id, expected.vector_store_id)
+  assert.equal(errorMsg.tool_id, expected.tool_id)
+})
+
+test('LlmErrorMessage - cause message invalid json', async () => {
+  // Responses are empty when there is an error
+  // in Google GenAI
+  const res = {}
+  const cause = {
+    name: 'ServerError',
+    message: '{bad:"json"'
+  }
+  const summary = { vendor: 'gemini' }
+  const errorMsg = new LlmErrorMessage({ request: req, response: res, cause, summary })
+  const expected = {
+    'http.statusCode': undefined,
+    'error.message': '{bad:"json"',
+    'error.code': undefined,
     'error.param': undefined,
     completion_id: undefined,
     embedding_id: undefined,
