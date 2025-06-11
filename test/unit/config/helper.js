f3cfa4a32963c25c7795a03063dca7aa5b6e971d
James Sumners <jsumners@newrelic.com>
2025-06-11 09:54:00 -0400

    chore: Added logging of used New Relic environment variables

--- test/unit/config/helper.js
+++ test/unit/config/helper.js
@@ -7,6 +7,32 @@
 
 const Config = require('../../../lib/config')
 
+class TestConfigLogger {
+  #logs = {
+    error: [],
+    trace: [],
+    warn: []
+  }
+
+  get logs() { return this.#logs }
+
+  child() {
+    return this
+  }
+
+  error(...args) {
+    this.#logs.error.push(args)
+  }
+
+  trace(...args) {
+    this.#logs.trace.push(args)
+  }
+
+  warn(...args) {
+    this.#logs.warn.push(args)
+  }
+}
+
 function idempotentEnv(envConfig, initialConfig, callback) {
   const saved = {}
 
@@ -25,8 +51,9 @@ function idempotentEnv(envConfig, initialConfig, callback) {
     process.env[key] = envConfig[key]
   })
   try {
-    const tc = Config.initialize(initialConfig)
-    callback(tc)
+    const loggerInstance = new TestConfigLogger()
+    const tc = Config.initialize(initialConfig, { loggerInstance })
+    callback(tc, loggerInstance)
   } finally {
     Object.keys(envConfig).forEach((finalKey) => {
       if (saved[finalKey]) {
