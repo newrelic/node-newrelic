c71185f7730fc12e5cb8860517394b53880a91b7
James Sumners <jsumners@newrelic.com>
2025-08-08 13:42:29 -0400

    chore: Remove diacritical grave encoding (#3286)

--- bin/pending-prs.js
+++ bin/pending-prs.js
@@ -153,7 +153,6 @@ async function findMergedPRs(repo, ignoredLabels) {
         .replaceAll('&', '&amp;')
         .replaceAll('<', '&lt;')
         .replaceAll('>', '&gt;')
-        .replaceAll('`', '&DiacriticalGrave;')
         .replaceAll('|', '&mid;')
       return `<${pr.html_url} | (${pr.number}) ${slackifiedTitle}>`
     })
