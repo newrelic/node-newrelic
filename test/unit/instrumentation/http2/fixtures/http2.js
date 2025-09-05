8fb77143949fd2f3358951862a5e98e2063e5818
mrickard <maurice@mauricerickard.com>
2025-09-05 12:36:24 -0400

    test: Added unit test and mock server for http2 WIP; added tracing for request attributes
    Signed-off-by: mrickard <maurice@mauricerickard.com>

---
+++ test/unit/instrumentation/http2/fixtures/http2.js
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2023 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+module.exports = createHttp2ResponseServer
+
+const http = require('http2')
+const crypto = require('crypto')
+const { Readable } = require('stream')
+
+/**
+ * Creates a new HTTP server to serve responses for HTTP2 requests.
+ * The returned server is listening on `localhost` and a random port.
+ *
+ * @returns {Promise<object>} Has `server`, `host`, `port`, `baseUrl`,
+ * and `responses` properties.
+ */
+function createHttp2ResponseServer() {
+  const server = http.createServer(handler)
+  const sockets = new Set()
+
+  server.on('connection', (socket) => {
+    sockets.add(socket)
+    socket.once('close', () => sockets.delete(socket))
+    socket.once('error', (err) => {
+      console.log('servererror', err)
+    })
+  })
+  server.destroy = function destroy() {
+    sockets.forEach((s) => s.destroy())
+    server.close()
+  }
+  server.on('stream', (stream, headers) => {
+    sockets.add(stream)
+    stream.on('close', () => {
+      sockets.delete(stream)
+    })
+  })
+
+  return new Promise((resolve, reject) => {
+    server.listen({ host: '127.0.0.1', port: 0 }, (error) => {
+      if (error) {
+        return reject(error)
+      }
+
+      const addy = server.address()
+      return resolve({
+        server,
+        host: addy.address,
+        port: addy.port,
+        baseUrl: `http://${addy.address}:${addy.port}`
+      })
+    })
+  })
+}
+
+function handler(req, res) {
+  let data = Buffer.alloc(0)
+  let response = {}
+  req.on('data', (chunk) => {
+    data = Buffer.concat([data, chunk])
+  })
+  req.on('error', (err) => {
+    console.error('o no', err)
+    response = {
+      statusCode: 500,
+      body: err
+    }
+  })
+
+  req.on('end', () => {
+    let payload = {}
+    if (data.length && data.length > 0) {
+      payload = JSON.parse(data.toString('utf8'))
+    }
+    response = {
+      statusCode: 200,
+      body: payload
+    }
+    res.statusCode = response.statusCode
+    const headersForResponse = ['content-type', 'traceparent', 'referer', 'user-agent']
+    for (const k of headersForResponse) {
+      if (req.headers[k]) {
+        res.setHeader(k, req.headers[k])
+      }
+    }
+    // for (const [key, value] of Object.entries(req.headers)) {
+    //   if (key[0] !== ':') { // can't set pseudoheaders
+    //     try {
+    //       res.setHeader(key, value)
+    //     } catch (e) {
+    //       console.error('error setting header', e)
+    //     }
+    //   }
+    // }
+
+    if (payload?.data === 'bad stream') {
+      const stream = infiniteStream()
+      let count = 0
+      stream.on('data', () => {
+        if (count >= 100) {
+          stream.destroy()
+          res.destroy()
+        }
+        count += 1
+      })
+      stream.pipe(res)
+      return
+    }
+
+    res.end(JSON.stringify(response.body))
+  })
+}
+
+/**
+ * Creates a stream that will generate new stream messages until the stream
+ * is destroyed.
+ *
+ * @returns {Readable} readable stream
+ */
+function infiniteStream() {
+  return new Readable({
+    read(size = 16) {
+      const data = crypto.randomBytes(size)
+      this.push(JSON.stringify({ chunk: { bytes: data.toString('base64') } }))
+    }
+  }).pause()
+}
