a998c6e99f3161da912243baf94f96949795f62e
Amy Chisholm <achisholm@newrelic.com>
2025-09-24 15:00:38 -0700

    `pg` and `pg-native` subscriber

--- test/versioned/pg/pg.common.js
+++ test/versioned/pg/pg.common.js
@@ -86,12 +86,13 @@ module.exports = function runTests(name, clientFactory) {
     const unscoped = transaction.metrics.unscoped
 
     const expected = {
-      'Datastore/all': 2,
-      'Datastore/allWeb': 2,
-      'Datastore/Postgres/all': 2,
-      'Datastore/Postgres/allWeb': 2,
+      'Datastore/all': 3,
+      'Datastore/allWeb': 3,
+      'Datastore/Postgres/all': 3,
+      'Datastore/Postgres/allWeb': 3,
       'Datastore/operation/Postgres/insert': 1,
-      'Datastore/operation/Postgres/select': 1
+      'Datastore/operation/Postgres/select': 1,
+      'Datastore/operation/Postgres/connect': 1
     }
 
     expected['Datastore/statement/Postgres/' + TABLE + '/insert'] = 1
@@ -99,7 +100,7 @@ module.exports = function runTests(name, clientFactory) {
 
     const metricHostName = getMetricHostName(agent, params.postgres_host)
     const hostId = metricHostName + '/' + params.postgres_port
-    expected['Datastore/instance/Postgres/' + hostId] = 2
+    expected['Datastore/instance/Postgres/' + hostId] = 3
 
     const expectedNames = Object.keys(expected)
     const unscopedNames = Object.keys(unscoped)
@@ -343,6 +344,9 @@ module.exports = function runTests(name, clientFactory) {
 
         client.connect(function (error) {
           assert.ifError(error)
+          // TODO: When orchestrion config specifies Async,
+          // we wrap this Query object in a Promise leading to a TypeError.
+          // How can we explicitly not wrap this?
           const pgQuery = client.query(new pg.Query(selQuery))
 
           pgQuery.on('error', (error) => {
@@ -359,7 +363,7 @@ module.exports = function runTests(name, clientFactory) {
             const metrics = finalTx.metrics.getMetric('Datastore/operation/Postgres/select')
             assert.ok(
               metrics.total > 2.0,
-              'Submittable style Query pg_sleep of 2 seconds should result in > 2 seßc timing'
+              'Submittable style Query pg_sleep of 2 seconds should result in > 2 sec timing'
             )
 
             end()
@@ -445,7 +449,7 @@ module.exports = function runTests(name, clientFactory) {
     })
 
     await t.test('client pooling query', async (t) => {
-      const plan = tspl(t, { plan: 39 })
+      const plan = tspl(t, { plan: 41 })
       const { agent, pg } = t.nr
 
       plan.equal(agent.getTransaction(), undefined, 'no transaction should be in play')
@@ -482,7 +486,7 @@ module.exports = function runTests(name, clientFactory) {
     })
 
     await t.test('using Pool constructor', async (t) => {
-      const plan = tspl(t, { plan: 40 })
+      const plan = tspl(t, { plan: 42 })
       const { agent, pg } = t.nr
 
       plan.equal(agent.getTransaction(), undefined, 'no transaction should be in play')
