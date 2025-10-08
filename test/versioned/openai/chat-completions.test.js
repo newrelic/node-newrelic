ceec8f9ef2ac95285dcc1619ef2ee8e187252beb
Bob Evans <revans@newrelic.com>
2025-10-08 13:27:13 -0400

    test: Updated an openai assertion with setTimeout disabled (#3421)

--- test/versioned/openai/chat-completions.test.js
+++ test/versioned/openai/chat-completions.test.js
@@ -405,7 +405,7 @@ test('chat.completions.create', async (t) => {
         assertSegments(
           tx.trace,
           tx.trace.root,
-          ['timers.setTimeout', `External/${host}:${port}/chat/completions`],
+          [`External/${host}:${port}/chat/completions`],
           { exact: false }
         )
 
