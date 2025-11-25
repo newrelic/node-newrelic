b8eec07a911fd55734f1a2417da36fb8e7c0e181
Amy Chisholm <achisholm@newrelic.com>
2025-11-25 14:21:08 -0800

    Add config-env test and better logging messages

--- test/unit/config/config-env.test.js
+++ test/unit/config/config-env.test.js
@@ -225,6 +225,42 @@ test('when overriding configuration values via environment variables', async (t)
         })
       })
 
+      await t.test(`should not set ${samplerName}_${envVar} sampler.adaptive.sampling_target if adaptive.sampling.target < 1`, (t, end) => {
+        const env = {
+          [`NEW_RELIC_DISTRIBUTED_TRACING_${samplerName}_${envVar}`]: 'adaptive',
+          [`NEW_RELIC_DISTRIBUTED_TRACING_${samplerName}_${envVar}_ADAPTIVE_SAMPLING_TARGET`]: '0'
+        }
+
+        idempotentEnv(env, (tc) => {
+          if (samplerName === 'SAMPLER') {
+            assert.equal(tc.distributed_tracing.sampler[type], 'adaptive')
+            assert.notEqual(tc.distributed_tracing.sampler[type]?.adaptive?.sampling_target, 0)
+          } else {
+            assert.equal(tc.distributed_tracing.sampler[key][type], 'adaptive')
+            assert.notEqual(tc.distributed_tracing.sampler[key][type]?.adaptive?.sampling_target, 0)
+          }
+          end()
+        })
+      })
+
+      await t.test(`should not set ${samplerName}_${envVar} sampler.adaptive.sampling_target if adaptive.sampling.target > 120`, (t, end) => {
+        const env = {
+          [`NEW_RELIC_DISTRIBUTED_TRACING_${samplerName}_${envVar}`]: 'adaptive',
+          [`NEW_RELIC_DISTRIBUTED_TRACING_${samplerName}_${envVar}_ADAPTIVE_SAMPLING_TARGET`]: '121'
+        }
+
+        idempotentEnv(env, (tc) => {
+          if (samplerName === 'SAMPLER') {
+            assert.equal(tc.distributed_tracing.sampler[type], 'adaptive')
+            assert.notEqual(tc.distributed_tracing.sampler[type]?.adaptive?.sampling_target, 121)
+          } else {
+            assert.equal(tc.distributed_tracing.sampler[key][type], 'adaptive')
+            assert.notEqual(tc.distributed_tracing.sampler[key][type]?.adaptive?.sampling_target, 121)
+          }
+          end()
+        })
+      })
+
       await t.test(`should set ${samplerName}_${envVar} sampler to trace id based ratio`, (t, end) => {
         const env = {
           [`NEW_RELIC_DISTRIBUTED_TRACING_${samplerName}_${envVar}`]: 'trace_id_ratio_based',
