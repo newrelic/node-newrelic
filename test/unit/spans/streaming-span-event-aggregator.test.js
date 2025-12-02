bbfe2bd135558d54f4dc83d121c6cc87531b410a
James Sumners <jsumners@newrelic.com>
2025-12-02 15:25:46 -0500

    remove infinite tracing support

--- test/unit/spans/streaming-span-event-aggregator.test.js
+++ test/unit/spans/streaming-span-event-aggregator.test.js
@@ -85,30 +85,3 @@ test('Should attempt to connect on start() after stop() call', () => {
   streamingSpanAggregator.start()
   assert.equal(connectCount, 2)
 })
-
-test('adds links to the stack', () => {
-  const stack = []
-  const opts = {
-    span_streamer: {
-      connect() {},
-      disconnect() {}
-    }
-  }
-  const agg = new StreamingSpanEventAggregator(opts, agent)
-  agg.start()
-
-  agg.add = (item, priority) => {
-    stack.push({ item, priority })
-  }
-
-  const segment = {
-    spanLinks: [{ test: 'link' }]
-  }
-  const transaction = {
-    priority: 0.5
-  }
-  agg.addSegment({ segment, transaction })
-
-  assert.deepStrictEqual(stack, [{ item: { test: 'link' }, priority: 0.5 }])
-  agg.stop()
-})
