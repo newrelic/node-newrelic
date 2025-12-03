0c9ce5f300a80571bf869d8746fc80751c268b42
James Sumners <jsumners@newrelic.com>
2025-12-03 10:37:42 -0500

    refactor getVendors to an async function
    The sole usage of the function within the agent was already promisifying
    the function. So it was already being used as an async function.
    By refactoring it to an actual async function, we can process vendors
    serially. This will make it easier to review trace logs as vendor
    resolution messages will no longer be interleaved.

--- test/unit/utilization/main.test.js
+++ test/unit/utilization/main.test.js
@@ -4,13 +4,15 @@
  */
 
 'use strict'
-const test = require('node:test')
-const assert = require('node:assert')
-const helper = require('../../lib/agent_helper.js')
-const proxyquire = require('proxyquire')
 
-test('getVendors', async function (t) {
-  t.beforeEach(function (ctx) {
+const { describe, test } = require('node:test')
+const assert = require('node:assert')
+
+const helper = require('#testlib/agent_helper.js')
+const { getVendors } = require('#agentlib/utilization/index.js')
+
+describe('getVendors', () => {
+  test.beforeEach(function (ctx) {
     ctx.nr = {}
     ctx.nr.agent = helper.loadMockedAgent()
     ctx.nr.agent.config.utilization = {
@@ -24,11 +26,11 @@ test('getVendors', async function (t) {
     }
   })
 
-  t.afterEach(function (ctx) {
+  test.afterEach(function (ctx) {
     helper.unloadAgent(ctx.nr.agent)
   })
 
-  await t.test('calls all vendors', function (ctx, end) {
+  test('calls all vendors', async (ctx) => {
     const { agent } = ctx.nr
     let awsCalled = false
     let azureCalled = false
@@ -39,75 +41,234 @@ test('getVendors', async function (t) {
     let kubernetesCalled = false
     let pcfCalled = false
 
-    const getVendors = proxyquire('../../../lib/utilization', {
-      './aws-info': function (agentArg, cb) {
+    const vendorDataFuncs = {
+      aws (agentArg, cb) {
         awsCalled = true
         cb()
       },
-      './azure-info': function (agentArg, cb) {
+      azure (agentArg, cb) {
         azureCalled = true
         cb()
       },
-      './azurefunction-info': function (agentArg, cb) {
+      azurefunction (agentArg, cb) {
         azureFunctionCalled = true
         cb()
       },
-      './gcp-info': function (agentArg, cb) {
-        gcpCalled = true
+      docker (agentArg, cb) {
+        dockerCalled = true
         cb()
       },
-      './docker-info': {
-        getVendorInfo: function (agentArg, cb) {
-          dockerCalled = true
-          cb()
-        }
-      },
-      './ecs-info': function (agentArg, cb) {
+      ecs (agentArg, cb) {
         ecsCalled = true
         cb()
       },
-      './kubernetes-info': (agentArg, cb) => {
+      gcp (agentArg, cb) {
+        gcpCalled = true
+        cb()
+      },
+      kubernetes (agentArg, cb) {
         kubernetesCalled = true
         cb()
       },
-      './pcf-info': (agentArg, cb) => {
+      pcf (agentArg, cb) {
         pcfCalled = true
         cb()
       }
-    }).getVendors
+    }
 
-    getVendors(agent, function (err) {
-      assert.ifError(err)
-      assert.ok(awsCalled)
-      assert.ok(azureCalled)
-      assert.ok(azureFunctionCalled)
-      assert.ok(gcpCalled)
-      assert.ok(dockerCalled)
-      assert.ok(ecsCalled)
-      assert.ok(kubernetesCalled)
-      assert.ok(pcfCalled)
-      end()
-    })
+    try {
+      await getVendors(agent, { vendorDataFuncs })
+    } catch (error) {
+      assert.ifError(error)
+    }
+    assert.ok(awsCalled)
+    assert.ok(azureCalled)
+    assert.ok(azureFunctionCalled)
+    assert.ok(gcpCalled)
+    assert.ok(dockerCalled)
+    assert.ok(ecsCalled)
+    assert.ok(kubernetesCalled)
+    assert.ok(pcfCalled)
   })
 
-  await t.test('returns multiple vendors if available', function (ctx, end) {
+  test('returns multiple vendors if available', async (ctx) => {
     const { agent } = ctx.nr
-    const getVendors = proxyquire('../../../lib/utilization', {
-      './aws-info': function (agentArg, cb) {
+    const vendorDataFuncs = {
+      aws (agentArg, cb) {
         cb(null, 'aws info')
       },
-      './docker-info': {
-        getVendorInfo: function (agentArg, cb) {
-          cb(null, 'docker info')
-        }
+      docker (agentArg, cb) {
+        cb(null, 'docker info')
       }
-    }).getVendors
+    }
 
-    getVendors(agent, function (err, vendors) {
-      assert.ifError(err)
-      assert.equal(vendors.aws, 'aws info')
-      assert.equal(vendors.docker, 'docker info')
-      end()
-    })
+    let vendors
+    try {
+      vendors = await getVendors(agent, { vendorDataFuncs })
+    } catch (error) {
+      assert.ifError(error)
+    }
+
+    assert.equal(vendors.aws, 'aws info')
+    assert.equal(vendors.docker, 'docker info')
+  })
+
+  test('logs messages for success case', async (t) => {
+    const { agent } = t.nr
+    const vendorDataFuncs = {
+      aws (agentArg, cb) {
+        cb(null, 'aws info')
+      }
+    }
+    const logs = {
+      error: [],
+      info: [],
+      trace: []
+    }
+    const logger = {
+      error(...args) {
+        logs.error.push(args)
+      },
+      info(...args) {
+        logs.info.push(args)
+      },
+      trace(...args) {
+        logs.trace.push(args)
+      }
+    }
+
+    await getVendors(agent, { vendorDataFuncs, logger })
+    assert.equal(logs.error.length, 0)
+    assert.equal(logs.info.length, 1)
+    assert.equal(logs.trace.length, 2)
+
+    assert.deepStrictEqual(logs.info[0], [
+      { utilization: 'aws', result: 'aws info' },
+      'Information for vendor %s retrieved successfully.',
+      'aws'
+    ])
+    assert.deepStrictEqual(logs.trace[0], [
+      { utilization: 'aws' },
+      'Detecting utilization info for vendor %s.',
+      'aws'
+    ])
+    assert.deepStrictEqual(logs.trace[1], [
+      { utilization: 'aws' },
+      'Vendor %s finished.',
+      'aws'
+    ])
+  })
+
+  test('logs messages for error case', async (t) => {
+    const { agent } = t.nr
+    const vendorDataFuncs = {
+      aws (agentArg, cb) {
+        cb(Error('boom'))
+      }
+    }
+    const logs = {
+      error: [],
+      info: [],
+      trace: []
+    }
+    const logger = {
+      error(...args) {
+        logs.error.push(args)
+      },
+      info(...args) {
+        logs.info.push(args)
+      },
+      trace(...args) {
+        logs.trace.push(args)
+      }
+    }
+
+    await getVendors(agent, { vendorDataFuncs, logger })
+    assert.equal(logs.error.length, 1)
+    assert.equal(logs.info.length, 0)
+    assert.equal(logs.trace.length, 2)
+
+    const [metadata, msg, vendor] = logs.error[0]
+    assert.equal(msg, 'Failed to get information about vendor %s.')
+    assert.equal(vendor, 'aws')
+    assert.equal(metadata.utilization, 'aws')
+    assert.equal(metadata.result, null)
+    assert.match(metadata.error.message, /boom/)
+
+    assert.deepStrictEqual(logs.trace[0], [
+      { utilization: 'aws' },
+      'Detecting utilization info for vendor %s.',
+      'aws'
+    ])
+    assert.deepStrictEqual(logs.trace[1], [
+      { utilization: 'aws' },
+      'Vendor %s finished.',
+      'aws'
+    ])
+  })
+
+  test('logs message for null/undefined information result', async (t) => {
+    const { agent } = t.nr
+    const vendorDataFuncs = {
+      aws (agentArg, cb) {
+        cb(null, null)
+      },
+      foo (agentArg, cb) {
+        cb(null)
+      }
+    }
+    const logs = {
+      error: [],
+      info: [],
+      trace: []
+    }
+    const logger = {
+      error(...args) {
+        logs.error.push(args)
+      },
+      info(...args) {
+        logs.info.push(args)
+      },
+      trace(...args) {
+        logs.trace.push(args)
+      }
+    }
+
+    await getVendors(agent, { vendorDataFuncs, logger })
+    assert.equal(logs.error.length, 0)
+    assert.equal(logs.info.length, 0)
+    assert.equal(logs.trace.length, 6)
+
+    assert.deepStrictEqual(logs.trace[0], [
+      { utilization: 'aws' },
+      'Detecting utilization info for vendor %s.',
+      'aws'
+    ])
+    assert.deepStrictEqual(logs.trace[1], [
+      { utilization: 'aws' },
+      'No information returned for vendor %s.',
+      'aws'
+    ])
+    assert.deepStrictEqual(logs.trace[2], [
+      { utilization: 'aws' },
+      'Vendor %s finished.',
+      'aws'
+    ])
+
+    assert.deepStrictEqual(logs.trace[3], [
+      { utilization: 'foo' },
+      'Detecting utilization info for vendor %s.',
+      'foo'
+    ])
+    assert.deepStrictEqual(logs.trace[4], [
+      { utilization: 'foo' },
+      'No information returned for vendor %s.',
+      'foo'
+    ])
+    assert.deepStrictEqual(logs.trace[5], [
+      { utilization: 'foo' },
+      'Vendor %s finished.',
+      'foo'
+    ])
   })
 })
