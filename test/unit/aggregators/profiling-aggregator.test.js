105da3b788f8e47099a47857e9e490103c6163d4
Bob Evans <revans@newrelic.com>
2026-02-10 16:02:07 -0500

    feat: Added ProfilingManager class, used to orchestrate profilers (#3738)

--- test/unit/aggregators/profiling-aggregator.test.js
+++ test/unit/aggregators/profiling-aggregator.test.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 New Relic Corporation. All rights reserved.
+ * Copyright 2026 New Relic Corporation. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -13,8 +13,11 @@ const helper = require('#testlib/agent_helper.js')
 const RUN_ID = 1337
 
 test.beforeEach((ctx) => {
+  const sandbox = sinon.createSandbox()
   const agent = helper.loadMockedAgent()
   const cpuProfiler = {
+    name: 'CpuProfiler',
+    stop: sandbox.stub(),
     collect() {
       return 'cpu profile data'
     }
@@ -22,27 +25,30 @@ test.beforeEach((ctx) => {
 
   const clock = sinon.useFakeTimers()
   const heapProfiler = {
+    name: 'HeapProfiler',
+    stop: sandbox.stub(),
     collect() {
       return 'heap profile data'
     }
   }
-  const profiler = {
-    profilers: [cpuProfiler, heapProfiler]
-  }
-  sinon.spy(agent.collector, 'send')
-  const profilingAggregator = new ProfilingAggregator({ runId: RUN_ID, periodMs: 100, profiler }, agent)
+  sandbox.spy(agent.collector, 'send')
+  const profilingAggregator = new ProfilingAggregator({ runId: RUN_ID, periodMs: 100 }, agent)
+  const profilingManager = profilingAggregator.profilingManager
+  sandbox.spy(profilingManager, 'register')
+  profilingAggregator.profilingManager.profilers = [cpuProfiler, heapProfiler]
   ctx.nr = {
     agent,
     clock,
     profilingAggregator,
-    profiler
+    profilingManager,
+    sandbox
   }
 })
 
 test.afterEach((ctx) => {
   helper.unloadAgent(ctx.nr.agent)
   ctx.nr.clock.restore()
-  ctx.nr.agent.collector.send.restore()
+  ctx.nr.sandbox.restore()
 })
 
 test('should set the correct default method', (t) => {
@@ -51,15 +57,17 @@ test('should set the correct default method', (t) => {
   assert.equal(method, 'pprof_data')
 })
 
-test('should intialize pprofData and profiler', (t) => {
-  const { profilingAggregator, profiler } = t.nr
-  assert.deepEqual(profilingAggregator.profiler, profiler)
+test('should initialize pprofData and profilingManager', (t) => {
+  const { profilingAggregator, profilingManager } = t.nr
+  assert.deepEqual(profilingAggregator.profilingManager, profilingManager)
   assert.equal(profilingAggregator.pprofData, null)
 })
 
 test('should send 2 messages per interval', (t) => {
-  const { profilingAggregator, clock, agent } = t.nr
+  const { profilingAggregator, profilingManager, clock, agent } = t.nr
+  assert.equal(profilingManager.register.callCount, 0)
   profilingAggregator.start()
+  assert.equal(profilingManager.register.callCount, 1)
   assert.equal(agent.collector.send.callCount, 0)
   clock.tick(100)
   assert.equal(agent.collector.send.callCount, 2)
@@ -69,4 +77,27 @@ test('should send 2 messages per interval', (t) => {
   assert.equal(heapCall[0], 'pprof_data')
   assert.equal(heapCall[1], 'heap profile data')
   assert.equal(profilingAggregator.pprofData, null)
+})
+
+test('should not send any data if there are no profilers registered', (t) => {
+  const { profilingAggregator, clock, agent } = t.nr
+  profilingAggregator.profilingManager.profilers = []
+  profilingAggregator.start()
+  assert.equal(agent.collector.send.callCount, 0)
+  clock.tick(100)
+  assert.equal(agent.collector.send.callCount, 0)
+})
+
+test('should stop ProfilingManager when aggregator is stopped', (t) => {
+  const { profilingAggregator, profilingManager } = t.nr
+  profilingAggregator.start()
+  assert.ok(profilingAggregator.sendTimer)
+  for (const profiler of profilingManager.profilers) {
+    assert.equal(profiler.stop.callCount, 0)
+  }
+  profilingAggregator.stop()
+  assert.equal(profilingAggregator.sendTimer, null)
+  for (const profiler of profilingManager.profilers) {
+    assert.equal(profiler.stop.callCount, 1)
+  }
 })
---
+++ test/unit/lib/profiling/profilers/base.test.js
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2026 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+const test = require('node:test')
+const assert = require('node:assert')
+const BaseProfiler = require('#agentlib/profiling/profilers/base.js')
+
+test.beforeEach((ctx) => {
+  ctx.nr = {
+    profiler: new BaseProfiler()
+  }
+})
+
+test('should set name', (t) => {
+  const { profiler } = t.nr
+  profiler.name = 'TestProfiler'
+  assert.equal(profiler.name, 'TestProfiler')
+})
+
+test('should throw error when start is called', (t) => {
+  const { profiler } = t.nr
+  assert.throws(() => {
+    profiler.start()
+  })
+})
+
+test('should throw error when stop is called', (t) => {
+  const { profiler } = t.nr
+  assert.throws(() => {
+    profiler.stop()
+  })
+})
+
+test('should throw error when collect is called', (t) => {
+  const { profiler } = t.nr
+  assert.throws(() => {
+    profiler.collect()
+  })
+})
