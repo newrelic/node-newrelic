458ceda9a47ff3f988e6769f1ca65a31b4467541
James Sumners <jsumners@newrelic.com>
2026-03-03 09:03:03 -0500

    chore: Refactored nestjs instrumentation to subscriber type

--- test/versioned/nestjs/nest.test.js
+++ test/versioned/nestjs/nest.test.js
@@ -6,16 +6,24 @@
 'use strict'
 const test = require('node:test')
 const assert = require('node:assert')
+const assertPackageMetrics = require('../../lib/custom-assertions/assert-pkg-tracking-metrics.js')
 const helper = require('../../lib/agent_helper')
 const { removeMatchedModules } = require('../../lib/cache-buster')
-const { promisify } = require('node:util')
-const makeRequest = promisify(helper.makeGetRequest)
 const { initNestApp, deleteNestApp } = require('./setup')
-const { assertPackageMetrics } = require('../../lib/custom-assertions')
 
-test('Nest.js', async (t) => {
-  await initNestApp()
-  const agent = helper.instrumentMockedAgent()
+const makeRequest = helper.asyncHttpCall
+
+let appInitialized = false
+test.beforeEach(async (ctx) => {
+  if (appInitialized === false) {
+    await initNestApp()
+    appInitialized = true
+  }
+
+  ctx.nr = {
+    agent: helper.instrumentMockedAgent()
+  }
+
   const { bootstrap } = require('./test-app/dist/main.js')
   const app = await bootstrap(0)
   const address = app.httpServer.address()
@@ -23,36 +31,47 @@ test('Nest.js', async (t) => {
     ? `http://[${address.address}]:${address.port}`
     : `http://${address.address}:${address.port}`
 
-  t.after(async () => {
-    app.close()
-    helper.unloadAgent(agent)
-    removeMatchedModules(/test-app/)
-    await deleteNestApp()
-  })
+  ctx.nr.app = app
+  ctx.nr.address = address
+  ctx.nr.baseUrl = baseUrl
+})
 
-  await t.test('should log tracking metrics', async function() {
-    // eslint-disable-next-line sonarjs/no-internal-api-use
-    const { version } = require('./test-app/node_modules/@nestjs/core/package.json')
-    assertPackageMetrics({ agent, pkg: '@nestjs/core', version })
-  })
+test.afterEach(async (ctx) => {
+  ctx.nr.app.close()
+  helper.unloadAgent(ctx.nr.agent)
+  removeMatchedModules(/test-app/)
+})
 
-  await t.test('should record a transaction in the base case', async () => {
-    const res = await makeRequest(baseUrl)
-    assert.equal(res.body, 'Hello World!', 'should greet the world')
-    assert.equal(res.statusCode, 200, 'should return 200 status')
-    assert.equal(
-      agent.metrics.getMetric('WebTransaction').callCount,
-      1,
-      'should have recorded one web transaction'
-    )
-  })
+test.after(async () => {
+  await deleteNestApp()
+})
 
-  await t.test('should catch stack traces in errors', async () => {
-    const res = await makeRequest(`${baseUrl}?please_error=yes`)
-    assert.equal(res.statusCode, 500, 'should return 500 status')
-    const errors = agent.errors.traceAggregator.errors
-    assert.equal(errors.length, 1, 'there should be one error')
-    assert.equal(errors[0][2], 'erroring out, as requested', 'should get the expected error')
-    assert.ok(errors[0][4].stack_trace, 'should have the stack trace')
-  })
+test.test('should log tracking metrics', async function(t) {
+  const { agent, baseUrl } = t.nr
+  // eslint-disable-next-line sonarjs/no-internal-api-use
+  const { version } = require('./test-app/node_modules/@nestjs/core/package.json')
+  await makeRequest(`${baseUrl}?please_error=yes`)
+  assertPackageMetrics({ agent, pkg: '@nestjs/core', version, subscriberType: true })
+})
+
+test('should record a transaction in the base case', async (t) => {
+  const { agent, baseUrl } = t.nr
+  const { response: res } = await makeRequest(baseUrl)
+  assert.equal(res.body, 'Hello World!', 'should greet the world')
+  assert.equal(res.statusCode, 200, 'should return 200 status')
+  assert.equal(
+    agent.metrics.getMetric('WebTransaction').callCount,
+    1,
+    'should have recorded one web transaction'
+  )
+})
+
+test('should catch stack traces in errors', async (t) => {
+  const { agent, baseUrl } = t.nr
+  const { response: res } = await makeRequest(`${baseUrl}?please_error=yes`)
+  assert.equal(res.statusCode, 500, 'should return 500 status')
+  const errors = agent.errors.traceAggregator.errors
+  assert.equal(errors.length, 1, 'there should be one error')
+  assert.equal(errors[0][2], 'erroring out, as requested', 'should get the expected error')
+  assert.ok(errors[0][4].stack_trace, 'should have the stack trace')
 })
