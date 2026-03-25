d09859f930f3ef7ed3b90363b39845e4ae399b67
Bob Evans <robert.evans25@gmail.com>
2026-03-25 16:36:50 -0400

    chore: Added subscriber packages to `load-externals`

--- load-externals.js
+++ load-externals.js
@@ -6,9 +6,21 @@
 'use strict'
 const instrumentedLibraries = require('./lib/instrumentations')() || {}
 const libNames = Object.keys(instrumentedLibraries)
+const subscriptions = require('./lib/subscriber-configs')
+const subscribers = Object.keys(subscriptions)
+const packages = [...libNames, ...subscribers]
+
+/**
+ * This is to be used with bundlers. It will add all of our instrumented 3rd party modules
+ * into the `externals` array.
+ *
+ * **Note** Only tested with `webpack`, [see](https://webpack.js.org/configuration/externals/)
+ *
+ * @param {object} config bundler config
+ */
 module.exports = function loadExternals(config) {
   if (config.target.includes('node')) {
-    config.externals.push(...libNames)
+    config.externals.push(...packages)
   }
 
   return config
--- test/unit/load-externals.test.js
+++ test/unit/load-externals.test.js
@@ -13,19 +13,20 @@ const loadExternals = require('../../load-externals')
 test('should load libs to webpack externals', async () => {
   const config = {
     target: 'node-20.x',
-    externals: ['next']
+    externals: ['test-pkg']
   }
   loadExternals(config)
+  // This number is arbitrary but accounts for legacy instrumentation + subscriber instrumentation
   assert.ok(
-    config.externals.length > 1,
-    'should add all libraries agent supports to the externals list'
+    config.externals.length > 40,
+    `should add all libraries agent supports to the externals list, got ${config.externals.length}`
   )
 })
 
 test('should not add externals when target is not node', async () => {
   const config = {
     target: 'web',
-    externals: ['next']
+    externals: ['test-pkg']
   }
   loadExternals(config)
   assert.ok(config.externals.length === 1, 'should not agent libraries when target is not node')
