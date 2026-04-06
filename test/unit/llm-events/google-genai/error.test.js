f059d94ad7ca9503fedeb979920430155c77038b
Amy Rowen <achisholm@newrelic.com>
2026-04-06 10:15:03 -0700

    update `error.message` if gemini parsing fails

--- test/unit/llm-events/google-genai/error.test.js
+++ test/unit/llm-events/google-genai/error.test.js
@@ -46,23 +46,13 @@ test('LlmErrorMessage - cause message invalid json', async () => {
     message: '{bad:"json"'
   }
   const errorMsg = new GoogleGenAiLlmErrorMessage({ response: res, cause })
-  const expected = {
-    'http.statusCode': undefined,
-    'error.message': '{bad:"json"',
-    'error.code': undefined,
-    'error.param': undefined,
-    completion_id: undefined,
-    embedding_id: undefined,
-    vector_store_id: undefined,
-    tool_id: undefined
-  }
   assert.ok(errorMsg.toString(), 'LlmErrorMessage')
-  assert.equal(errorMsg['http.statusCode'], expected['http.statusCode'])
-  assert.equal(errorMsg['error.message'], expected['error.message'])
-  assert.equal(errorMsg['error.code'], expected['error.code'])
-  assert.equal(errorMsg['error.param'], expected['error.param'])
-  assert.equal(errorMsg.completion_id, expected.completion_id)
-  assert.equal(errorMsg.embedding_id, expected.embedding_id)
-  assert.equal(errorMsg.vector_store_id, expected.vector_store_id)
-  assert.equal(errorMsg.tool_id, expected.tool_id)
+  assert.equal(errorMsg['http.statusCode'], undefined)
+  assert.match(errorMsg['error.message'], /^failed to parse cause.message: .+/)
+  assert.equal(errorMsg['error.code'], undefined)
+  assert.equal(errorMsg['error.param'], undefined)
+  assert.equal(errorMsg.completion_id, undefined)
+  assert.equal(errorMsg.embedding_id, undefined)
+  assert.equal(errorMsg.vector_store_id, undefined)
+  assert.equal(errorMsg.tool_id, undefined)
 })
