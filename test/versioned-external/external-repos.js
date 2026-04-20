63a0de1c691eb934547c15bfd81a7355568ccc9f
Bob Evans <robert.evans25@gmail.com>
2026-04-20 15:02:37 -0400

    chore: restored apollo server plugin branch to `main`

--- test/versioned-external/external-repos.js
+++ test/versioned-external/external-repos.js
@@ -15,7 +15,7 @@ const repos = [
   {
     name: 'apollo-server',
     repository: 'https://github.com/newrelic/newrelic-node-apollo-server-plugin.git',
-    branch: 'enable-timeout-instrumentation',
+    branch: 'main',
     additionalFiles: [
       'tests/lib',
     ]
