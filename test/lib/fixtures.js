7243499ab03eb8f6d70e4699251bc83a1f30eca4
Bob Evans <revans@newrelic.com>
2026-04-29 11:39:57 -0400

    feat!: Removed support for License, Application, and Security Policies(LASP) (#3927)

--- test/lib/fixtures.js
+++ test/lib/fixtures.js
@@ -8,20 +8,6 @@
 module.exports = {
   // Common http transaction trace attributes
   httpAttributes: ['request.headers.host', 'request.method', 'http.statusCode', 'http.statusText'],
-  // Default security policies
-  securityPolicies: function () {
-    return {
-      record_sql: { enabled: false, required: false },
-      attributes_include: { enabled: false, required: false },
-      allow_raw_exception_messages: { enabled: false, required: false },
-      custom_events: { enabled: false, required: false },
-      custom_parameters: { enabled: false, required: false },
-      custom_instrumentation_editor: { enabled: false, required: false },
-      live_instrumentation: { enabled: false, required: false },
-      message_parameters: { enabled: false, required: false },
-      job_arguments: { enabled: false, required: false }
-    }
-  },
   // Default config for attribute filter
   defaultAttributeConfig: () => {
     return {
