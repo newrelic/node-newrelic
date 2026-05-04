720979bccb65c93a143d1e069a32904bff78f02b
Bob Evans <revans@newrelic.com>
2026-05-04 10:32:22 -0400

    feat!: Dropped support for `fastify` 3.x (#3931)

--- test/unit/lib/subscribers/dc-base.test.js
+++ test/unit/lib/subscribers/dc-base.test.js
@@ -12,7 +12,8 @@ const loggerMock = require('../../mocks/logger')
 const helper = require('#testlib/agent_helper.js')
 const Subscriber = require('#agentlib/subscribers/dc-base.js')
 
-const PROCESS_MAJOR = require('../../../../package.json').version.split('.')[0]
+const { version: PKG_VERSION } = require('../../../../package.json')
+const PROCESS_MAJOR = PKG_VERSION.split('.')[0]
 
 test.beforeEach((ctx) => {
   const agent = helper.loadMockedAgent()
@@ -22,7 +23,8 @@ test.beforeEach((ctx) => {
     logger,
     packageName: 'test-package'
   })
-  ctx.nr = { agent, subscriber }
+  const testChannel = 'test.channel'
+  ctx.nr = { agent, subscriber, testChannel, logger }
 })
 
 test.afterEach((ctx) => {
@@ -33,18 +35,20 @@ test.afterEach((ctx) => {
 })
 
 test('records supportability metric on first usage', (t) => {
-  t.plan(5)
-  const { agent, subscriber } = t.nr
+  t.plan(9)
+  const { agent, logger, subscriber, testChannel } = t.nr
 
   let invocations = 0
   const metricNameBase = 'Supportability/Features/Instrumentation/SubscriberUsed/test-package'
-  const chan = dc.channel('test.channel')
+  const chan = dc.channel(testChannel)
   subscriber.channels = [
-    { channel: 'test.channel', hook: handler }
+    { channel: testChannel, hook: handler }
   ]
   subscriber.subscribe()
-
+  t.assert.equal(dc.hasSubscribers(testChannel), true)
   chan.publish({ foo: 'foo' })
+  t.assert.equal(logger.warn.callCount, 0)
+  t.assert.equal(dc.hasSubscribers(testChannel), true)
 
   function handler () {
     invocations += 1
@@ -58,6 +62,7 @@ test('records supportability metric on first usage', (t) => {
       chan.publish({ bar: 'bar' })
       const cachedChan = subscriber.channels[0]
       const keys = Object.keys(cachedChan).sort()
+      t.assert.equal(dc.hasSubscribers(testChannel), true)
       t.assert.deepStrictEqual(
         keys,
         ['boundHook', 'channel', 'eventHandler', 'hook'],
@@ -65,4 +70,26 @@ test('records supportability metric on first usage', (t) => {
       )
     }
   }
+})
+
+test('should not call handler if provided versionRange is not satifisfied with actual package version', (t) => {
+  const { logger, subscriber, testChannel } = t.nr
+  // since we rely on the agent version, let's just specify an old version that will never be satisfied
+  subscriber.versionRange = '<1.0.0'
+  const ch = dc.channel(testChannel)
+  subscriber.channels = [
+    { channel: testChannel, hook: handler }
+  ]
+  subscriber.subscribe()
+  t.assert.equal(dc.hasSubscribers(testChannel), true)
+
+  ch.publish({ key: 'value' })
+  t.assert.equal(dc.hasSubscribers(testChannel), false)
+
+  function handler() {
+    throw new Error('should not call handler')
+  }
+
+  t.assert.equal(logger.warn.callCount, 1)
+  t.assert.equal(logger.warn.args[0][0], `Not instrumenting ${testChannel} as it is not within the supported version range ${subscriber.versionRange}, got ${PKG_VERSION}`)
 })
