bb5196cfcec30650708805b2980eb4bd6071c8d5
Amy Rowen <achisholm@newrelic.com>
2026-08-10 13:32:07 -0700

    Address PR feedback: Refactor to `ChildProcessInstrumentation`; add double-wrapped test

--- test/unit/instrumentation/core/child_process.test.js
+++ test/unit/instrumentation/core/child_process.test.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 New Relic Corporation. All rights reserved.
+ * Copyright 2026 New Relic Corporation. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -7,9 +7,7 @@
 
 const test = require('node:test')
 const assert = require('node:assert')
-const sinon = require('sinon')
 const helper = require('#testlib/agent_helper.js')
-const logger = require('#agentlib/logger.js')
 const childProcessInstrumentation = require('#agentlib/instrumentation/core/child_process.js')
 
 test('child_process instrumentation', async (t) => {
@@ -18,16 +16,29 @@ test('child_process instrumentation', async (t) => {
     helper.unloadAgent(agent)
   })
 
-  await t.test('should log and return false when child_process is not available', (t) => {
-    const proto = Object.getPrototypeOf(logger.child({}))
-    const debugStub = sinon.stub(proto, 'debug')
-    t.after(() => {
-      debugStub.restore()
-    })
+  await t.test('should log and return false when child_process is not available', () => {
+    const debugCalls = []
+    const stubLogger = { debug: (msg) => debugCalls.push(msg) }
 
-    const result = childProcessInstrumentation(agent, null)
+    const result = childProcessInstrumentation(agent, null, { logger: stubLogger })
 
     assert.equal(result, false)
-    assert.ok(debugStub.calledWith('Could not find child_process, not instrumenting'))
+    assert.deepEqual(debugCalls, ['Could not find child_process, not instrumenting'])
+  })
+
+  await t.test('should not re-wrap exec/execFile on repeated initialize calls', () => {
+    const fakeChildProcess = {
+      exec: function exec() {},
+      execFile: function execFile() {}
+    }
+
+    childProcessInstrumentation(agent, fakeChildProcess)
+    const wrappedExec = fakeChildProcess.exec
+    const wrappedExecFile = fakeChildProcess.execFile
+
+    childProcessInstrumentation(agent, fakeChildProcess)
+
+    assert.equal(fakeChildProcess.exec, wrappedExec, 'exec should not be wrapped a second time')
+    assert.equal(fakeChildProcess.execFile, wrappedExecFile, 'execFile should not be wrapped a second time')
   })
 })
