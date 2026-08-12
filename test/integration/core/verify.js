9d14df56782597b8f97231807377830dc1f3570d
Bob Evans <robert.evans25@gmail.com>
2026-08-12 17:15:50 -0400

    refactor: Refactored `dns` instrumentation to subscribe to events emitted

--- test/integration/core/verify.js
+++ test/integration/core/verify.js
@@ -7,28 +7,35 @@
 
 module.exports = verifySegments
 
-function verifySegments({ agent, name, children = [], end, assert = require('node:assert') }) {
+function verifySegments({ agent, name, children = [], end, assert = require('node:assert'), assertCallbacks = true }) {
   const { trace } = agent.getTransaction()
   const traceChildren = trace.getChildren(trace.root.id)
   assert.equal(traceChildren.length, 1, 'should have a single child')
   const child = traceChildren[0]
-  const childChildren = trace.getChildren(child.id)
   assert.equal(child.name, name, 'child segment should have correct name')
   assert.ok(child.timer.touched, 'child should started and ended')
-  assert.equal(
-    childChildren.length,
-    1 + children.length,
-    'child should have a single callback segment'
-  )
 
+  if (assertCallbacks) {
+    verifyCallbacks({ trace, child, assert, end, children })
+  } else {
+    end?.()
+  }
+}
+
+function verifyCallbacks({ trace, child, assert, end, children }) {
+  const childChildren = trace.getChildren(child.id)
   for (let i = 0; i < children.length; ++i) {
     assert.equal(childChildren[i].name, children[i])
   }
-
   const callback = childChildren[childChildren.length - 1]
   assert.ok(
     callback.name === 'Callback: anonymous' || callback.name === 'Callback: <anonymous>',
     'callback segment should have correct name'
+  )
+  assert.equal(
+    childChildren.length,
+    1 + children.length,
+    'child should have a single callback segment'
   )
 
   assert.ok(callback.timer.start, 'callback should have started')
